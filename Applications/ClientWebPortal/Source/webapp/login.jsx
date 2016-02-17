764e1c930582e8d9e6fe3e94ca216c2f1b368706
Kamal <kamal@eidolonsystems.com>
2016-02-17 15:52:47 -0500

    Changed the login page.

--- Applications/ClientWebPortal/Source/webapp/login.jsx
+++ Applications/ClientWebPortal/Source/webapp/login.jsx
@@ -1,4 +1,43 @@
+var MyInput = React.createClass({
+  getInitialState: function() {
+    return {
+      value: ""
+    };
+  },
+  
+  handleChange: function(e) {
+    this.setState({
+      value: e.target.value
+    });
+  },
+  
+  render: function() {
+    return (
+      <div>
+        {this.props.name}
+        <input value={this.state.value} onChange={this.handleChange} />
+      </div>);
+  }
+});
+
+var LoginButton = React.createClass({
+  handleClick: function(event) {
+    event.preventDefault();
+    window.location.href = "http://www.google.com";
+  },
+
+  render: function() {
+    return (
+      <button onClick={this.handleClick}>Login</button>
+    );
+  }
+});
+
 ReactDOM.render(
-  <h1>Hello, world!</h1>,
-  document.getElementById('example')
+  <div>
+    <MyInput id="username" name="Username" /><br />
+    <MyInput id="password" name="Password" /><br />
+    <button>Login</button>
+  </div>,
+  document.getElementById('container')
 );
