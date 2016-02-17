8aaf387b52375d3c896019e54f2b89a0fef0a5f4
shahrzad <shahrzad@spiretrading.com>
2016-02-17 17:41:52 -0500

    Added last files to react login page

--- Applications/ClientWebPortal/Source/webapp/login.jsx
+++ Applications/ClientWebPortal/Source/webapp/login.jsx
@@ -1,43 +1,13 @@
-var MyInput = React.createClass({
-  getInitialState: function() {
-    return {
-      value: ""
-    };
-  },
-  
-  handleChange: function(e) {
-    this.setState({
-      value: e.target.value
-    });
-  },
-  
-  render: function() {
-    return (
-      <div>
-        {this.props.name}
-        <input value={this.state.value} onChange={this.handleChange} />
-      </div>);
-  }
-});
-
-var LoginButton = React.createClass({
-  handleClick: function(event) {
-    event.preventDefault();
-    window.location.href = "http://www.google.com";
-  },
-
-  render: function() {
-    return (
-      <button onClick={this.handleClick}>Login</button>
-    );
-  }
-});
-
-ReactDOM.render(
-  <div>
-    <MyInput id="username" name="Username" /><br />
-    <MyInput id="password" name="Password" /><br />
-    <button>Login</button>
-  </div>,
-  document.getElementById('container')
-);
+    var HelloWorld = React.createClass({
+            render: function() {
+                return 
+                      <div>
+                        <form action="demo_form.asp">
+                          <input type="text" name="FirstName" placeholder="Username"><br>
+                          <input type="text" name="LastName" placeholder="Password"><br>
+                          <input type="submit" value="Login">
+                        </form>
+                      </div>;
+            }
+        });
+        ReactDOM.render(<HelloWorld />, document.getElementById("container"));
