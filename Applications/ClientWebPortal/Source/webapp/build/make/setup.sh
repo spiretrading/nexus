3b4c17faf471f282d5ced989a924db6c95e3d7c7
shahrzad <shahrzad@spiretrading.com>
2016-02-17 16:18:49 -0500

    Username pass working react page

--- Applications/ClientWebPortal/Source/webapp/build/make/setup.sh
+++ Applications/ClientWebPortal/Source/webapp/build/make/setup.sh
@@ -2,5 +2,9 @@
 directory=$(dirname $(readlink -f $0))
 pushd $directory/
 cd $directory/../../
+mkdir ../../Application/webapp/third-party
+cp /home/developers/react-0.14.7/react-0.14.7.js ../../Application/webapp/third-party/react.js
+cp /home/developers/react-0.14.7/react-dom-0.14.7.js ../../Application/webapp/third-party/react-dom.js
+cp /home/developers/requirejs-2.1.22/* ../../Application/webapp/third-party
 npm install babel-cli babel-preset-es2015 babel-preset-react
 popd
