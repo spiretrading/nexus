bfc95f613ec74ecd60b7b7b0b866927d3f65a17e
shahrzad <shahrzad@spiretrading.com>
2016-02-29 13:55:17 -0500

    Added handle request function for submit button

--- Applications/ClientWebPortal/Source/webapp/login.jsx
+++ Applications/ClientWebPortal/Source/webapp/login.jsx
@@ -1,7 +1,8 @@
     var LoginPage = React.createClass({
             render: function() {
                 return (<div className="login_page">
-                        <img id="logo" src="img/spire_white.png" alt="Spire Trading Logo"/>
+                        <img id="logo" src="img/spire_white.png" 
+                        alt="Spire Trading Logo"/>
                         <form id="login_form" onSubmit={this.handleSubmit}>
                           <input 
                             autoFocus 
@@ -21,6 +22,7 @@
                             name="login_password" 
                             placeholder="Password"
                             ref="login_password" 
+                            onChange={this.handlePasswordChange}
                             onClick={this.disappear} /><br/>
                           <input 
                             id="login_submit" 
@@ -28,25 +30,49 @@
                             value="Login" 
                             ref="login_submit"
                             onMouseEnter={this.decOpacity} 
-                            onMouseLeave={this.incOpacity} />
+                            onMouseLeave={this.incOpacity}
+                            onClick={this.handleSubmit} />
                           <ul className="error_messages"></ul>
                         </form>
                       </div>);
             },
             getInitialState: function() {
-              return {username: ''};
+              return {username: '', password: ''};
             },
             componentDidMount: function(){
               
             },
             handleSubmit: function(e) {
+              console.log("Handle submit is working!");
               e.preventDefault();
-              var author = this.state.username.trim();
-              if (!username) {
+              var submitted_username = this.state.username.trim();
+              var submitted_password = this.state.password.trim();
+              if (!submitted_username) {
+                console.log("No username or password");
                 return;
               }
               // TODO: send request to the server
               this.setState({username: ''});
+              this.setState({password: ''});
+              console.log("Entered Username is: "+ submitted_username);
+              console.log("Entered Password is: "+ submitted_password);
+              $.ajax({
+                url: this.props.url,
+                dataType: 'json',
+                type: 'POST',
+                data: {username: submitted_username, password: submitted_password},
+                success: function(data) {
+                  console.log("Request was successful");
+                  ReactDOM.render(<Dashboard />,
+                  document.getElementById('container'));
+                  this.setState({data: data});
+                }.bind(this),
+                error: function(xhr, status, err) {
+                  console.log("Request failed!");
+                  //this.setState({data: {username, password}});
+                  console.log(status);
+                }.bind(this)
+              });    
             },
             onMouseEnter: function (e) {
 
@@ -57,41 +83,74 @@
             handleUsernameChange: function (e) {
               this.setState({username: e.target.value});
             },
+            handlePasswordChange: function (e) {
+              this.setState({password: e.target.value});
+            },
             disappear: function () {
 
-            },
-            sendLoginRequest: function () {
-              console.log(event); // => nullified object.
-              console.log(event.type); // => "click"
-              var eventType = event.type; // => "click"
-
-              setTimeout(function() {
-                console.log(event.type); // => null
-                console.log(eventType); // => "click"
-              }, 0);
-
-              this.setState({clickEvent: event}); // Won't work. this.state.clickEvent will only contain null values.
-              this.setState({eventType: event.type});
-
-              var xhttp;
-              var data = JSON.stringify(formToJson(document.getElementById("login-form")));
-              if (window.XMLHttpRequest) {
-                // code for modern browsers
-                xhttp = new XMLHttpRequest();
-                } else {
-                // code for IE6, IE5
-                xhttp = new ActiveXObject("Microsoft.XMLHTTP");
-              }
-              xhttp.onreadystatechange = function() {
-                if (xhttp.readyState == 4 && xhttp.status == 200) {
-                  //document.getElementById("demo").innerHTML = xhttp.responseText;
-
-                }
-              };
-              xhttp.open("POST", "/api/service_locator/login", false);
-              xhttp.withCredentials = true;
-              xhttp.send(data);
-              window.location.href = '/dashboard';
-            } 
+            }, 
         });
-        ReactDOM.render(<LoginPage />, document.getElementById("container"));
+    //Dashboard Page
+    var Dashboard = React.createClass({
+      render: function() {
+        return (
+          <div>
+            <h3> Welcome to Spire</h3>
+            <div id="dashboard_container" />
+            ReactDOM.render(
+              <TimerExample start={Date.now()} />,
+              document.getElementById('dashboard_container')
+            );
+          </div>
+        );
+      }
+    });
+    //Timer
+    var Timer = React.createClass({
+
+      getInitialState: function(){
+
+          // This is called before our render function. The object that is 
+          // returned is assigned to this.state, so we can use it later.
+
+          return { elapsed: 0 };
+      },
+
+      componentDidMount: function(){
+
+          // componentDidMount is called by react when the component 
+          // has been rendered on the page. We can set the interval here:
+
+          this.timer = setInterval(this.tick, 50);
+      },
+
+      componentWillUnmount: function(){
+
+          // This method is called immediately before the component is removed
+          // from the page and destroyed. We can clear the interval here:
+
+          clearInterval(this.timer);
+      },
+
+      tick: function(){
+
+          // This function is called every 50 ms. It updates the 
+          // elapsed counter. Calling setState causes the component to be re-rendered
+
+          this.setState({elapsed: new Date() - this.props.start});
+      },
+
+      render: function() {
+          
+          var elapsed = Math.round(this.state.elapsed / 100);
+
+          // This will give a number with one digit after the decimal dot (xx.x):
+          var seconds = (elapsed / 10).toFixed(1);    
+
+          // Although we return an entire <p> element, react will smartly update
+          // only the changed parts, which contain the seconds variable.
+
+          return <p>Timer was started <b>{seconds} seconds</b> ago.</p>;
+      }
+    });
+        ReactDOM.render(<LoginPage url="/api/service_locator/login" />, document.getElementById("container"));
