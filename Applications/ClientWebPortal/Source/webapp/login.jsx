2bcf563d132dbfed5a15f066c9ae78edc101e862
Kamal <kamal@eidolonsystems.com>
2016-03-02 12:29:59 -0600

    Fixed AJAX callbacks.

--- Applications/ClientWebPortal/Source/webapp/login.jsx
+++ Applications/ClientWebPortal/Source/webapp/login.jsx
@@ -1,175 +1,140 @@
-    //var React = require('react');
-    //var Router = require('react-router');
-    //var Link = Router.Link;
-    var LoginPage = React.createClass({
-            render: function() {
-                return (<div className="login_page">
-                        <img id="logo" src="img/spire_white.png" 
-                        alt="Spire Trading Logo"/>
-                        <form id="login_form" onSubmit={this.handleSubmit}>
-                          <input 
-                            autoFocus 
-                            className="login_input" 
-                            id="login_username" 
-                            type="text" 
-                            name="login_username" 
-                            placeholder="Username" 
-                            ref="login_username"
-                            value={this.state.username}
-                            onChange={this.handleUsernameChange}
-                            onClick={this.disappear}/><br/>
-                          <input 
-                            className="login_input" 
-                            id="login_password" 
-                            type="password"
-                            name="login_password" 
-                            placeholder="Password"
-                            ref="login_password" 
-                            onChange={this.handlePasswordChange}
-                            onClick={this.disappear} /><br/>
-                          <input 
-                            id="login_submit" 
-                            type="submit" 
-                            value="Login" 
-                            ref="login_submit"
-                            onMouseEnter={this.decOpacity} 
-                            onMouseLeave={this.incOpacity} />
-                          <ul className="error_messages"></ul>
-                        </form>
-                      </div>);
-            },
-            getInitialState: function() {
-              return {username: '', password: ''};
-            },
-            componentDidMount: function(){
-              
-            },
-            handleSubmit: function(e) {
-              console.log("Handle submit is working!");
-              e.preventDefault();
-              var submitted_username = this.state.username.trim();
-              var submitted_password = this.state.password.trim();
-              if (!submitted_username) {
-                console.log("No username or password");
-                return;
-              }
-              // TODO: send request to the server
-              this.setState({username: ''});
-              this.setState({password: ''});
-              console.log("Entered Username is: "+ submitted_username);
-              console.log("Entered Password is: "+ submitted_password);
-              $.ajax({
-                url: this.props.url,
-                dataType: 'json',
-                method: 'POST',
-                data: JSON.stringify({ username: submitted_username, password: submitted_password }),
-                done: function() {
-                  console.log("done!");
-                }.bind(this),
-                always: function(data, status, xhr) {
-                  console.log("Request was successful");
-                  console.log("Response data: " + JSON.stringify(data) + " Status: " + status + " xhr: "+ xhr);
-                  ReactDOM.render(<Dashboard source="http://192.168.1.118:8080/dashboard" />,
-                  document.getElementById('container'));
-                  //this.setState({data: data});
-                }.bind(this),
-                fail: function(xhr, status, err) {
-                  console.log("Request failed! ERROR Section");
-                  //console.log("Response " + JSON.stringify(data));
-                  //this.setState({data: {username, password}});
-                  console.log("Status: " + status + " err: " + err);
-                }.bind(this),
-              });    
-            },
-            onMouseEnter: function (e) {
-
-            },
-            onMouseLeave: function (e) {
-
-            },
-            handleUsernameChange: function (e) {
-              this.setState({username: e.target.value});
-            },
-            handlePasswordChange: function (e) {
-              this.setState({password: e.target.value});
-            },
-            disappear: function () {
-
-            }, 
-        });
-    //Dashboard Page
-    var Dashboard = React.createClass({
-      render: function() {
-        return (
-          <div>
-            <h3> Welcome to Spire</h3>
-            <div id="dashboard_container" />
-            ReactDOM.render(
-              <TimerExample start={Date.now()} />,
-              document.getElementById('dashboard_container')
-            );
-          </div>
-        );
-      },
-
-      componentDidMount: function() {
-        this.serverRequest = $.get(this.props.source, function (result) {
-          var lastGist = result[0];
-          console.log("result: " + result);
-          console.log("result[0]: " + lastGist);
+var LoginPage = React.createClass({
+  render: function() {
+    return (
+      <div className="login_page">
+        <img id="logo" src="img/spire_white.png"
+        alt="Spire Trading Logo"/>
+        <form id="login_form" onSubmit={this.handleSubmit}>
+          <input 
+            autoFocus 
+            className="login_input" 
+            id="login_username" 
+            type="text" 
+            name="login_username" 
+            placeholder="Username" 
+            ref="login_username"
+            value={this.state.username}
+            onChange={this.handleUsernameChange}
+            onClick={this.disappear}/><br/>
+          <input 
+            className="login_input" 
+            id="login_password" 
+            type="password"
+            name="login_password" 
+            placeholder="Password"
+            ref="login_password" 
+            onChange={this.handlePasswordChange}
+            onClick={this.disappear} /><br/>
+          <input 
+            id="login_submit" 
+            type="submit" 
+            value="Login" 
+            ref="login_submit"
+            onMouseEnter={this.decOpacity} 
+            onMouseLeave={this.incOpacity} />
+          <ul className="error_messages"></ul>
+        </form>
+      </div>);
+  },
+  getInitialState: function() {
+    return {username: '', password: ''};
+  },
+  componentDidMount: function(){
+    
+  },
+  handleSubmit: function(e) {
+    console.log("Handle submit is working!");
+    e.preventDefault();
+    var submitted_username = this.state.username.trim();
+    var submitted_password = this.state.password.trim();
+    if (!submitted_username) {
+      console.log("No username or password");
+      return;
+    }
+    this.setState({username: ''});
+    this.setState({password: ''});
+    console.log("Entered Username is: "+ submitted_username);
+    console.log("Entered Password is: "+ submitted_password);
+    $.ajax(
+      {
+        url: this.props.url,
+        dataType: 'json',
+        method: 'POST',
+        data: JSON.stringify(
+          {
+            username: submitted_username,
+            password: submitted_password
+          })
+      }).done(
+        function() {
+          console.log("done!");
+        }.bind(this)).always(
+        function(data, status, xhr) {
+          console.log("Request was successful");
+          console.log("Response data: " + JSON.stringify(data) +
+            " Status: " + status + " xhr: "+ xhr);
+          ReactDOM.render(<Dashboard />,
+            document.getElementById('container'));
+        }.bind(this)).fail(
+        function(xhr, status, err) {
+          console.log("Request failed! ERROR Section");
+          console.log("Status: " + status + " err: " + err);
         }.bind(this));
-      },
+  },
+  onMouseEnter: function (e) {},
+  onMouseLeave: function (e) {},
+  handleUsernameChange: function (e) {
+    this.setState({username: e.target.value});
+  },
+  handlePasswordChange: function (e) {
+    this.setState({password: e.target.value});
+  },
+  disappear: function () {}, 
+});
 
-      componentWillUnmount: function() {
-        this.serverRequest.abort();
-      }
-    });
-    //Timer
-    var Timer = React.createClass({
+var Dashboard = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <h3> Welcome to Spire</h3>
+        <div id="dashboard_container" />
+        <Timer start={Date.now()} />
+      </div>
+    );
+  },
 
-      getInitialState: function(){
+  componentDidMount: function() {
+    this.serverRequest = $.get(this.props.source, function (result) {
+      var lastGist = result[0];
+      console.log("result: " + result);
+      console.log("result[0]: " + lastGist);
+    }.bind(this));
+  },
 
-          // This is called before our render function. The object that is 
-          // returned is assigned to this.state, so we can use it later.
+  componentWillUnmount: function() {
+    this.serverRequest.abort();
+  }
+});
 
-          return { elapsed: 0 };
-      },
+var Timer = React.createClass({
+  getInitialState: function(){
+    return { elapsed: 0 };
+  },
+  componentDidMount: function(){
+    this.timer = setInterval(this.tick, 50);
+  },
+  componentWillUnmount: function(){
+    clearInterval(this.timer);
+  },
+  tick: function(){
+    this.setState({elapsed: new Date() - this.props.start});
+  },
+  render: function() {
+    var elapsed = Math.round(this.state.elapsed / 100);
+    var seconds = (elapsed / 10).toFixed(1);    
+    return <p>Timer was started <b>{seconds} seconds</b> ago.</p>;
+  }
+});
 
-      componentDidMount: function(){
-
-          // componentDidMount is called by react when the component 
-          // has been rendered on the page. We can set the interval here:
-
-          this.timer = setInterval(this.tick, 50);
-      },
-
-      componentWillUnmount: function(){
-
-          // This method is called immediately before the component is removed
-          // from the page and destroyed. We can clear the interval here:
-
-          clearInterval(this.timer);
-      },
-
-      tick: function(){
-
-          // This function is called every 50 ms. It updates the 
-          // elapsed counter. Calling setState causes the component to be re-rendered
-
-          this.setState({elapsed: new Date() - this.props.start});
-      },
-
-      render: function() {
-          
-          var elapsed = Math.round(this.state.elapsed / 100);
-
-          // This will give a number with one digit after the decimal dot (xx.x):
-          var seconds = (elapsed / 10).toFixed(1);    
-
-          // Although we return an entire <p> element, react will smartly update
-          // only the changed parts, which contain the seconds variable.
-
-          return <p>Timer was started <b>{seconds} seconds</b> ago.</p>;
-      }
-    });
-        ReactDOM.render(<LoginPage url="/api/service_locator/login" />, document.getElementById("container"));
+ReactDOM.render(<LoginPage url="/api/service_locator/login" />,
+  document.getElementById("container"));
