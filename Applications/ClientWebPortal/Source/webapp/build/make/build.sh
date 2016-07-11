49467ddc6a5c3a3d76e39fe14db88c973f5128cd
Jimin Park <jimin@spiretrading.com>
2016-07-11 11:02:17 -0400

    fb543: Build script changes.

--- Applications/ClientWebPortal/Source/webapp/build/make/build.sh
+++ Applications/ClientWebPortal/Source/webapp/build/make/build.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 directory=$(dirname $(readlink -f $0))
 pushd $directory/
-cd $directory/..
-npm install
-# need to have webpack installed by executing
-# npm install webpack -g
+cd $directory/../..
 webpack
 rm -rf ../../Application/webapp
 cp -a dist/. ../../Application/webapp/
