6fa4d24a49f5f62af6c538960151b3e0ac0296d2
Jimin Park <jimin@spiretrading.com>
2016-08-04 10:32:34 -0400

    fb558: minor bug fix when handling server response for sign in.

--- Applications/ClientWebPortal/Source/webapp/src/utils/spire-clients/admin/index.js
+++ Applications/ClientWebPortal/Source/webapp/src/utils/spire-clients/admin/index.js
@@ -25,6 +25,36 @@ class AdminClient {
       return accountRoles.parse(roles);
     }
   }
+
+  loadAccountProfile(directoryEntry) {
+    let apiPath = Config.BACKEND_API_ROOT_URL + 'administration_service/load_account_identity';
+    let payload = {
+      account: directoryEntry
+    };
+
+    return httpConnectionManager.send(apiPath, payload)
+      .then(parseResponse)
+      .catch(this.logErrorAndThrow);
+
+    function parseResponse(response) {
+      return {
+        addressOne: response.address_line_one,
+        addressTwo: response.address_line_two,
+        addressThree: response.address_line_three,
+        city: response.city,
+        country: response.country,
+        email: response.e_mail,
+        firstName: response.first_name,
+        lastSigninDateTime: response.last_login_time,
+        lastName: response.last_name,
+        picture: response.photo_id,
+        province: response.province,
+        registrationDateTime: response.registration_time,
+        userName: directoryEntry.name,
+        userNotes: response.user_notes
+      };
+    }
+  }
 }
 
 export default new AdminClient();
