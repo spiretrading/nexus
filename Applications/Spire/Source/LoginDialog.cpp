870919a68698b3ce72af7ea21353be798872ff0c
wei <wei@spiretrading.com>
2016-11-15 16:05:41 -0500

    resolution

--- Applications/Spire/Source/LoginDialog.cpp
+++ Applications/Spire/Source/LoginDialog.cpp
@@ -5,10 +5,11 @@
 #include <QtGui>
 #include "ui_LoginDialog.h"
 
+
 using namespace Spire;
 
 LoginDialog::LoginDialog(QWidget* parent)
-    : QDialog{parent},
+  : QDialog{ parent },
     m_ui{std::make_unique<Ui_LoginDialog>()} {
   setWindowFlags(Qt::FramelessWindowHint);
   m_ui->setupUi(this);
@@ -30,8 +31,8 @@ LoginDialog::LoginDialog(QWidget* parent)
   m_ui->m_bottomSpacer->changeSize(0, 0.0 * physicalDotsPerInchY,
     QSizePolicy::Fixed);
   m_ui->m_widget->move(0, 0);
-  m_ui->m_widget->setFixedHeight(height());
-  m_ui->m_widget->setFixedWidth(width());
+  m_ui->m_widget->setFixedHeight(LOGIN_DIALOG_HEIGHT_INCHES*physicalDotsPerInchY);
+  m_ui->m_widget->setFixedWidth(LOGIN_DIALOG_WIDTH_INCHES*physicalDotsPerInchX);
   m_ui->m_widget->setStyleSheet("background:transparent;");
   const auto M_USERNAMEINPUT_HEIGHT = 0.28;
   m_ui->m_usernameInput->setFixedHeight(M_USERNAMEINPUT_HEIGHT * 
@@ -44,20 +45,24 @@ LoginDialog::LoginDialog(QWidget* parent)
   const auto M_VERSIONLABEL_HEIGHT = 0.28;
   m_ui->m_versionLabel->setFixedHeight(M_VERSIONLABEL_HEIGHT * 
     physicalDotsPerInchY);
-  m_ui->m_versionLabel->setFixedWidth(physicalDotsPerInchX);
+  m_ui->m_versionLabel->setFixedWidth(1.15 * physicalDotsPerInchX);
   m_ui->m_versionLabel->setStyleSheet("color:rgb(255,255,255)");
   m_ui->m_versionLabel->setText("Version 1.0.0");
   m_ui->m_versionLabel->setFont(f);
-  m_ui->m_versionLoginSpacer->changeSize(0.40 * physicalDotsPerInchX, 0,
-    QSizePolicy::Fixed);
+  const auto M_VERSIONLOGINSPACER_WIDTH = 0.15; 
+  m_ui->m_versionLoginSpacer->changeSize(M_VERSIONLOGINSPACER_WIDTH * 
+    physicalDotsPerInchX, 0, QSizePolicy::Fixed);
   auto movie = new QMovie(":/newPrefix/spire_desktop_loading_icon.gif");
   m_ui->m_loadingLabel->setMovie(movie);
   const auto M_LOADINGLABEL_HEIGHT = 0.28;
   m_ui->m_loadingLabel->setFixedHeight(M_LOADINGLABEL_HEIGHT * 
     physicalDotsPerInchY);
-  m_ui->m_loadingLabel->setFixedWidth(physicalDotsPerInchX);
+  m_ui->m_loadingLabel->setFixedWidth(0.25 * physicalDotsPerInchX);
   m_ui->m_loadingLabel->setAlignment(Qt::AlignHCenter | Qt::AlignVCenter);
   movie->start();
+  const auto M_LOADINGLABELLOGINSPACER_WIDTH = 0.25;
+  m_ui->m_loadingLabelLoginSpacer->changeSize(M_LOADINGLABELLOGINSPACER_WIDTH *
+    physicalDotsPerInchX, 0, QSizePolicy::Fixed);
   const auto M_LOGINBUTTON_HEIGHT = 0.28;
   m_ui->m_loginButton->setFixedHeight(M_LOGINBUTTON_HEIGHT 
     * physicalDotsPerInchY);
@@ -66,7 +71,6 @@ LoginDialog::LoginDialog(QWidget* parent)
   m_ui->m_loginButton->setStyleSheet(
     "background-color:rgb(104, 75, 199);"
     "color: white; border: 2px solid rgb(141,120,236);");
-
   const auto M_SPIRELOGOLABEL_HEIGHT = 0.5;
   m_ui->m_spireLogoLabel->setFixedHeight(M_SPIRELOGOLABEL_HEIGHT * 
     physicalDotsPerInchY);
