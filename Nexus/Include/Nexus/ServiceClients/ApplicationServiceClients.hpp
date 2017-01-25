8d2f741d65b353184bacd95c93e5f3484a5c4aab
Kamal <kamal@eidolonsystems.com>
2017-01-25 17:08:43 -0500

    Implemented VirtualServiceClients and ApplicationServiceClients.

---
+++ Nexus/Include/Nexus/ServiceClients/ApplicationServiceClients.hpp
@@ -0,0 +1,333 @@
+#ifndef NEXUS_APPLICATIONSERVICECLIENTS_HPP
+#define NEXUS_APPLICATIONSERVICECLIENTS_HPP
+#include <Beam/IO/OpenState.hpp>
+#include <Beam/RegistryService/ApplicationDefinitions.hpp>
+#include <Beam/ServiceLocator/ApplicationDefinitions.hpp>
+#include <Beam/Threading/Mutex.hpp>
+#include <Beam/TimeService/NtpTimeClient.hpp>
+#include <boost/noncopyable.hpp>
+#include "Nexus/AdministrationService/ApplicationDefinitions.hpp"
+#include "Nexus/ChartingService/ApplicationDefinitions.hpp"
+#include "Nexus/Compliance/ApplicationDefinitions.hpp"
+#include "Nexus/DefinitionsService/ApplicationDefinitions.hpp"
+#include "Nexus/MarketDataService/ApplicationDefinitions.hpp"
+#include "Nexus/OrderExecutionService/ApplicationDefinitions.hpp"
+#include "Nexus/RiskService/ApplicationDefinitions.hpp"
+
+namespace Nexus {
+
+  /*! \class ApplicationServiceClients
+      \brief Implements the ServiceClients interface using live application
+             clients.
+   */
+  class ApplicationServiceClients : private boost::noncopyable {
+    public:
+      using ServiceLocatorClient =
+        Beam::ServiceLocator::ApplicationServiceLocatorClient::Client;
+
+      using RegistryClient =
+        Beam::RegistryService::ApplicationRegistryClient::Client;
+
+      using AdministrationClient =
+        AdministrationService::ApplicationAdministrationClient::Client;
+
+      using DefinitionsClient =
+        DefinitionsService::ApplicationDefinitionsClient::Client;
+
+      using MarketDataClient =
+        MarketDataService::ApplicationMarketDataClient::Client;
+
+      using ChartingClient =
+        ChartingService::ApplicationChartingClient::Client;
+
+      using ComplianceClient =
+        Compliance::ApplicationComplianceClient::Client;
+
+      using OrderExecutionClient =
+        OrderExecutionService::ApplicationOrderExecutionClient::Client;
+
+      using RiskClient = RiskService::ApplicationRiskClient::Client;
+
+      using TimeClient = Beam::TimeService::LiveNtpTimeClient;
+
+      //! Constructs an ApplicationServiceClients.
+      /*!
+        \param address The IpAddress to connect to.
+        \param username The client's username.
+        \param password The client's password.
+        \param socketThreadPool The SocketThreadPool to use by the clients.
+        \param timerThreadPool The TimerThreadPool to use by the clients.
+      */
+      ApplicationServiceClients(const Beam::Network::IpAddress& address,
+        const std::string& username, const std::string& password,
+        Beam::RefType<Beam::Network::SocketThreadPool> socketThreadPool,
+        Beam::RefType<Beam::Threading::TimerThreadPool> timerThreadPool);
+
+      ~ApplicationServiceClients();
+
+      ServiceLocatorClient& GetServiceLocatorClient();
+
+      RegistryClient& GetRegistryClient();
+
+      AdministrationClient& GetAdministrationClient();
+
+      DefinitionsClient& GetDefinitionsClient();
+
+      MarketDataClient& GetMarketDataClient();
+
+      ChartingClient& GetChartingClient();
+
+      ComplianceClient& GetComplianceClient();
+
+      OrderExecutionClient& GetOrderExecutionClient();
+
+      RiskClient& GetRiskClient();
+
+      TimeClient& GetTimeClient();
+
+      void Open();
+
+      void Close();
+
+    private:
+      Beam::Network::IpAddress m_address;
+      std::string m_username;
+      std::string m_password;
+      Beam::Network::SocketThreadPool* m_socketThreadPool;
+      Beam::Threading::TimerThreadPool* m_timerThreadPool;
+      Beam::ServiceLocator::ApplicationServiceLocatorClient
+        m_serviceLocatorClient;
+      Beam::Remote<Beam::RegistryService::ApplicationRegistryClient,
+        Beam::Threading::Mutex> m_registryClient;
+      Beam::Remote<AdministrationService::ApplicationAdministrationClient,
+        Beam::Threading::Mutex> m_administrationClient;
+      Beam::Remote<DefinitionsService::ApplicationDefinitionsClient,
+        Beam::Threading::Mutex> m_definitionsClient;
+      Beam::Remote<MarketDataService::ApplicationMarketDataClient,
+        Beam::Threading::Mutex> m_marketDataClient;
+      Beam::Remote<ChartingService::ApplicationChartingClient,
+        Beam::Threading::Mutex> m_chartingClient;
+      Beam::Remote<Compliance::ApplicationComplianceClient,
+        Beam::Threading::Mutex> m_complianceClient;
+      Beam::Remote<OrderExecutionService::ApplicationOrderExecutionClient,
+        Beam::Threading::Mutex> m_orderExecutionClient;
+      Beam::Remote<RiskService::ApplicationRiskClient, Beam::Threading::Mutex>
+        m_riskClient;
+      Beam::Remote<std::unique_ptr<TimeClient>, Beam::Threading::Mutex>
+        m_timeClient;
+      Beam::IO::OpenState m_openState;
+
+      void Shutdown();
+  };
+
+  inline ApplicationServiceClients::ApplicationServiceClients(
+      const Beam::Network::IpAddress& address, const std::string& username,
+      const std::string& password,
+      Beam::RefType<Beam::Network::SocketThreadPool> socketThreadPool,
+      Beam::RefType<Beam::Threading::TimerThreadPool> timerThreadPool)
+      : m_address{address},
+        m_username{username},
+        m_password{password},
+        m_socketThreadPool{socketThreadPool.Get()},
+        m_timerThreadPool{timerThreadPool.Get()},
+        m_registryClient{
+          [=] (Beam::DelayPtr<
+              Beam::RegistryService::ApplicationRegistryClient>& client) {
+            client.Initialize();
+            client->BuildSession(Beam::Ref(*m_serviceLocatorClient),
+              Beam::Ref(*m_socketThreadPool), Beam::Ref(*m_timerThreadPool));
+          }
+        },
+        m_administrationClient{
+          [=] (Beam::DelayPtr<
+              AdministrationService::ApplicationAdministrationClient>& client) {
+            client.Initialize();
+            client->BuildSession(Beam::Ref(*m_serviceLocatorClient),
+              Beam::Ref(*m_socketThreadPool), Beam::Ref(*m_timerThreadPool));
+          }
+        },
+        m_definitionsClient{
+          [=] (Beam::DelayPtr<
+              DefinitionsService::ApplicationDefinitionsClient>& client) {
+            client.Initialize();
+            client->BuildSession(Beam::Ref(*m_serviceLocatorClient),
+              Beam::Ref(*m_socketThreadPool), Beam::Ref(*m_timerThreadPool));
+          }
+        },
+        m_marketDataClient{
+          [=] (Beam::DelayPtr<
+              MarketDataService::ApplicationMarketDataClient>& client) {
+            client.Initialize();
+            client->BuildSession(Beam::Ref(*m_serviceLocatorClient),
+              Beam::Ref(*m_socketThreadPool), Beam::Ref(*m_timerThreadPool));
+          }
+        },
+        m_chartingClient{
+          [=] (Beam::DelayPtr<
+              ChartingService::ApplicationChartingClient>& client) {
+            client.Initialize();
+            client->BuildSession(Beam::Ref(*m_serviceLocatorClient),
+              Beam::Ref(*m_socketThreadPool), Beam::Ref(*m_timerThreadPool));
+          }
+        },
+        m_complianceClient{
+          [=] (Beam::DelayPtr<
+              Compliance::ApplicationComplianceClient>& client) {
+            client.Initialize();
+            client->BuildSession(Beam::Ref(*m_serviceLocatorClient),
+              Beam::Ref(*m_socketThreadPool), Beam::Ref(*m_timerThreadPool));
+          }
+        },
+        m_orderExecutionClient{
+          [=] (Beam::DelayPtr<
+              OrderExecutionService::ApplicationOrderExecutionClient>& client) {
+            client.Initialize();
+            client->BuildSession(Beam::Ref(*m_serviceLocatorClient),
+              Beam::Ref(*m_socketThreadPool), Beam::Ref(*m_timerThreadPool));
+          }
+        },
+        m_riskClient{
+          [=] (Beam::DelayPtr<RiskService::ApplicationRiskClient>& client) {
+            client.Initialize();
+            client->BuildSession(Beam::Ref(*m_serviceLocatorClient),
+              Beam::Ref(*m_socketThreadPool), Beam::Ref(*m_timerThreadPool));
+          }
+        },
+        m_timeClient{
+          [=] (Beam::DelayPtr<std::unique_ptr<TimeClient>>& client) {
+            auto timeServices = m_serviceLocatorClient->Locate(
+              Beam::TimeService::SERVICE_NAME);
+            if(timeServices.empty()) {
+              client.Initialize(Beam::TimeService::MakeLiveNtpTimeClient({},
+                Beam::Ref(*m_socketThreadPool), Beam::Ref(*m_timerThreadPool)));
+              BOOST_THROW_EXCEPTION(
+                Beam::IO::ConnectException{"No time services available."});
+            }
+            auto& timeService = timeServices.front();
+            auto ntpPool = Beam::FromString<
+              std::vector<Beam::Network::IpAddress>>(boost::get<std::string>(
+              timeService.GetProperties().At("addresses")));
+            client.Initialize(Beam::TimeService::MakeLiveNtpTimeClient(ntpPool,
+              Beam::Ref(*m_socketThreadPool), Beam::Ref(*m_timerThreadPool)));
+            try {
+              (*client)->Open();
+            } catch(const std::exception&) {
+              BOOST_THROW_EXCEPTION(Beam::IO::ConnectException{
+                "Unable to connect to the time service."});
+            }
+          }
+        } {}
+
+  inline ApplicationServiceClients::~ApplicationServiceClients() {
+    Close();
+  }
+
+  inline ApplicationServiceClients::ServiceLocatorClient&
+      ApplicationServiceClients::GetServiceLocatorClient() {
+    return *m_serviceLocatorClient;
+  }
+
+  inline ApplicationServiceClients::RegistryClient&
+      ApplicationServiceClients::GetRegistryClient() {
+    return **m_registryClient;
+  }
+
+  inline ApplicationServiceClients::AdministrationClient&
+      ApplicationServiceClients::GetAdministrationClient() {
+    return **m_administrationClient;
+  }
+
+  inline ApplicationServiceClients::DefinitionsClient&
+      ApplicationServiceClients::GetDefinitionsClient() {
+    return **m_definitionsClient;
+  }
+
+  inline ApplicationServiceClients::MarketDataClient&
+      ApplicationServiceClients::GetMarketDataClient() {
+    return **m_marketDataClient;
+  }
+
+  inline ApplicationServiceClients::ChartingClient&
+      ApplicationServiceClients::GetChartingClient() {
+    return **m_chartingClient;
+  }
+
+  inline ApplicationServiceClients::ComplianceClient&
+      ApplicationServiceClients::GetComplianceClient() {
+    return **m_complianceClient;
+  }
+
+  inline ApplicationServiceClients::OrderExecutionClient&
+      ApplicationServiceClients::GetOrderExecutionClient() {
+    return **m_orderExecutionClient;
+  }
+
+  inline ApplicationServiceClients::RiskClient&
+      ApplicationServiceClients::GetRiskClient() {
+    return **m_riskClient;
+  }
+
+  inline ApplicationServiceClients::TimeClient&
+      ApplicationServiceClients::GetTimeClient() {
+    return **m_timeClient;
+  }
+
+  inline void ApplicationServiceClients::Open() {
+    if(m_openState.SetOpening()) {
+      return;
+    }
+    try {
+      m_serviceLocatorClient.BuildSession(m_address,
+        Beam::Ref(*m_socketThreadPool), Beam::Ref(*m_timerThreadPool));
+      m_serviceLocatorClient->SetCredentials(m_username, m_password);
+      m_serviceLocatorClient->Open();
+    } catch(const std::exception&) {
+      m_openState.SetOpenFailure();
+      Shutdown();
+    }
+    m_openState.SetOpen();
+  }
+
+  inline void ApplicationServiceClients::Close() {
+    if(m_openState.SetClosing()) {
+      return;
+    }
+    Shutdown();
+  }
+
+  inline void ApplicationServiceClients::Shutdown() {
+    try {
+      (*m_timeClient)->Close();
+    } catch(const std::exception&) {}
+    try {
+      (*m_riskClient)->Close();
+    } catch(const std::exception&) {}
+    try {
+      (*m_orderExecutionClient)->Close();
+    } catch(const std::exception&) {}
+    try {
+      (*m_complianceClient)->Close();
+    } catch(const std::exception&) {}
+    try {
+      (*m_chartingClient)->Close();
+    } catch(const std::exception&) {}
+    try {
+      (*m_marketDataClient)->Close();
+    } catch(const std::exception&) {}
+    try {
+      (*m_definitionsClient)->Close();
+    } catch(const std::exception&) {}
+    try {
+      (*m_administrationClient)->Close();
+    } catch(const std::exception&) {}
+    try {
+      (*m_registryClient)->Close();
+    } catch(const std::exception&) {}
+    try {
+      m_serviceLocatorClient->Close();
+    } catch(const std::exception&) {}
+    m_openState.SetClosed();
+  }
+}
+
+#endif
---
+++ Nexus/Include/Nexus/ServiceClients/VirtualServiceClients.hpp
@@ -0,0 +1,320 @@
+#ifndef NEXUS_VIRTUALSERVICECLIENTS_HPP
+#define NEXUS_VIRTUALSERVICECLIENTS_HPP
+#include <Beam/RegistryService/VirtualRegistryClient.hpp>
+#include <Beam/ServiceLocator/VirtualServiceLocatorClient.hpp>
+#include <Beam/Threading/Mutex.hpp>
+#include <Beam/TimeService/VirtualTimeClient.hpp>
+#include <Beam/Utilities/Remote.hpp>
+#include <boost/noncopyable.hpp>
+#include "Nexus/AdministrationService/VirtualAdministrationClient.hpp"
+#include "Nexus/ChartingService/VirtualChartingClient.hpp"
+#include "Nexus/Compliance/VirtualComplianceClient.hpp"
+#include "Nexus/DefinitionsService/VirtualDefinitionsClient.hpp"
+#include "Nexus/MarketDataService/VirtualMarketDataClient.hpp"
+#include "Nexus/OrderExecutionService/VirtualOrderExecutionClient.hpp"
+#include "Nexus/RiskService/VirtualRiskClient.hpp"
+
+namespace Nexus {
+
+  /*! \class VirtualServiceClients
+      \brief Provides a pure virtual interface to the set of ServiceClients.
+   */
+  class VirtualServiceClients : private boost::noncopyable {
+    public:
+      using ServiceLocatorClient =
+        Beam::ServiceLocator::VirtualServiceLocatorClient;
+
+      using RegistryClient = Beam::RegistryService::VirtualRegistryClient;
+
+      using AdministrationClient =
+        AdministrationService::VirtualAdministrationClient;
+
+      using DefinitionsClient = DefinitionsService::VirtualDefinitionsClient;
+
+      using MarketDataClient = MarketDataService::VirtualMarketDataClient;
+
+      using ChartingClient = ChartingService::VirtualChartingClient;
+
+      using ComplianceClient = Compliance::VirtualComplianceClient;
+
+      using OrderExecutionClient =
+        OrderExecutionService::VirtualOrderExecutionClient;
+
+      using RiskClient = RiskService::VirtualRiskClient;
+
+      using TimeClient = Beam::TimeService::VirtualTimeClient;
+
+      virtual ~VirtualServiceClients() = default;
+
+      virtual ServiceLocatorClient& GetServiceLocatorClient() = 0;
+
+      virtual RegistryClient& GetRegistryClient() = 0;
+
+      virtual AdministrationClient& GetAdministrationClient() = 0;
+
+      virtual DefinitionsClient& GetDefinitionsClient() = 0;
+
+      virtual MarketDataClient& GetMarketDataClient() = 0;
+
+      virtual ChartingClient& GetChartingClient() = 0;
+
+      virtual ComplianceClient& GetComplianceClient() = 0;
+
+      virtual OrderExecutionClient& GetOrderExecutionClient() = 0;
+
+      virtual RiskClient& GetRiskClient() = 0;
+
+      virtual TimeClient& GetTimeClient() = 0;
+
+      virtual void Open() = 0;
+
+      virtual void Close() = 0;
+
+    protected:
+
+      //! Constructs a VirtualServiceClients.
+      VirtualServiceClients() = default;
+  };
+
+  /*! \class WrapperServiceClients
+      \brief Wraps ServiceClients providing it with a virtual interface.
+      \tparam ClientType The type of ServiceClients to wrap.
+   */
+  template<typename ClientType>
+  class WrapperServiceClients : public VirtualServiceClients {
+    public:
+      using ServiceLocatorClient =
+        Beam::ServiceLocator::VirtualServiceLocatorClient;
+
+      using RegistryClient = Beam::RegistryService::VirtualRegistryClient;
+
+      using AdministrationClient =
+        AdministrationService::VirtualAdministrationClient;
+
+      using DefinitionsClient = DefinitionsService::VirtualDefinitionsClient;
+
+      using MarketDataClient = MarketDataService::VirtualMarketDataClient;
+
+      using ChartingClient = ChartingService::VirtualChartingClient;
+
+      using ComplianceClient = Compliance::VirtualComplianceClient;
+
+      using OrderExecutionClient =
+        OrderExecutionService::VirtualOrderExecutionClient;
+
+      using RiskClient = RiskService::VirtualRiskClient;
+
+      using TimeClient = Beam::TimeService::VirtualTimeClient;
+
+      //! The ServiceClients to wrap.
+      using Client = Beam::GetTryDereferenceType<ClientType>;
+
+      //! Constructs a WrapperServiceClients.
+      /*!
+        \param client The ServiceClients to wrap.
+      */
+      template<typename ServiceClientsForward>
+      WrapperServiceClients(ServiceClientsForward&& client);
+
+      virtual ~WrapperServiceClients() override = default;
+
+      virtual ServiceLocatorClient& GetServiceLocatorClient() override;
+
+      virtual RegistryClient& GetRegistryClient() override;
+
+      virtual AdministrationClient& GetAdministrationClient() override;
+
+      virtual DefinitionsClient& GetDefinitionsClient() override;
+
+      virtual MarketDataClient& GetMarketDataClient() override;
+
+      virtual ChartingClient& GetChartingClient() override;
+
+      virtual ComplianceClient& GetComplianceClient() override;
+
+      virtual OrderExecutionClient& GetOrderExecutionClient() override;
+
+      virtual RiskClient& GetRiskClient() override;
+
+      virtual TimeClient& GetTimeClient() override;
+
+      virtual void Open() override;
+
+      virtual void Close() override;
+
+    private:
+      Beam::GetOptionalLocalPtr<ClientType> m_client;
+      Beam::Remote<std::unique_ptr<ServiceLocatorClient>,
+        Beam::Threading::Mutex> m_serviceLocatorClient;
+      Beam::Remote<std::unique_ptr<RegistryClient>, Beam::Threading::Mutex>
+        m_registryClient;
+      Beam::Remote<std::unique_ptr<AdministrationClient>,
+        Beam::Threading::Mutex> m_administrationClient;
+      Beam::Remote<std::unique_ptr<DefinitionsClient>, Beam::Threading::Mutex>
+        m_definitionsClient;
+      Beam::Remote<std::unique_ptr<MarketDataClient>, Beam::Threading::Mutex>
+        m_marketDataClient;
+      Beam::Remote<std::unique_ptr<ChartingClient>, Beam::Threading::Mutex>
+        m_chartingClient;
+      Beam::Remote<std::unique_ptr<ComplianceClient>, Beam::Threading::Mutex>
+        m_complianceClient;
+      Beam::Remote<std::unique_ptr<OrderExecutionClient>,
+        Beam::Threading::Mutex> m_orderExecutionClient;
+      Beam::Remote<std::unique_ptr<RiskClient>, Beam::Threading::Mutex>
+        m_riskClient;
+      Beam::Remote<std::unique_ptr<TimeClient>, Beam::Threading::Mutex>
+        m_timeClient;
+  };
+
+  //! Wraps ServiceClients into a VirtualServiceClients.
+  /*!
+    \param client The client to wrap.
+  */
+  template<typename ServiceClients>
+  std::unique_ptr<VirtualServiceClients> MakeVirtualServiceClients(
+      ServiceClients&& client) {
+    return std::make_unique<WrapperServiceClients<ServiceClients>>(
+      std::forward<ServiceClients>(client));
+  }
+
+  template<typename ClientType>
+  template<typename ServiceClientsForward>
+  WrapperServiceClients<ClientType>::WrapperServiceClients(
+      ServiceClientsForward&& client)
+      : m_client{std::forward<ServiceClientsForward>(client)},
+        m_serviceLocatorClient{
+          [=] (Beam::DelayPtr<std::unique_ptr<ServiceLocatorClient>>& client) {
+            client.Initialize(
+              Beam::ServiceLocator::MakeVirtualServiceLocatorClient(
+              &m_client->GetServiceLocatorClient()));
+          }
+        },
+        m_registryClient{
+          [=] (Beam::DelayPtr<std::unique_ptr<RegistryClient>>& client) {
+            client.Initialize(Beam::RegistryService::MakeVirtualRegistryClient(
+              &m_client->GetRegistryClient()));
+          }
+        },
+        m_administrationClient{
+          [=] (Beam::DelayPtr<std::unique_ptr<AdministrationClient>>& client) {
+            client.Initialize(
+              AdministrationService::MakeVirtualAdministrationClient(
+              &m_client->GetAdministrationClient()));
+          }
+        },
+        m_definitionsClient{
+          [=] (Beam::DelayPtr<std::unique_ptr<DefinitionsClient>>& client) {
+            client.Initialize(DefinitionsService::MakeVirtualDefinitionsClient(
+              &m_client->GetDefinitionsClient()));
+          }
+        },
+        m_marketDataClient{
+          [=] (Beam::DelayPtr<std::unique_ptr<MarketDataClient>>& client) {
+            client.Initialize(MarketDataService::MakeVirtualMarketDataClient(
+              &m_client->GetMarketDataClient()));
+          }
+        },
+        m_chartingClient{
+          [=] (Beam::DelayPtr<std::unique_ptr<ChartingClient>>& client) {
+            client.Initialize(ChartingService::MakeVirtualChartingClient(
+              &m_client->GetChartingClient()));
+          }
+        },
+        m_complianceClient{
+          [=] (Beam::DelayPtr<std::unique_ptr<ComplianceClient>>& client) {
+            client.Initialize(Compliance::MakeVirtualComplianceClient(
+              &m_client->GetComplianceClient()));
+          }
+        },
+        m_orderExecutionClient{
+          [=] (Beam::DelayPtr<std::unique_ptr<OrderExecutionClient>>& client) {
+            client.Initialize(
+              OrderExecutionService::MakeVirtualOrderExecutionClient(
+              &m_client->GetOrderExecutionClient()));
+          }
+        },
+        m_riskClient{
+          [=] (Beam::DelayPtr<std::unique_ptr<RiskClient>>& client) {
+            client.Initialize(RiskService::MakeVirtualRiskClient(
+              &m_client->GetRiskClient()));
+          }
+        },
+        m_timeClient{
+          [=] (Beam::DelayPtr<std::unique_ptr<TimeClient>>& client) {
+            client.Initialize(Beam::TimeService::MakeVirtualTimeClient(
+              &m_client->GetTimeClient()));
+          }
+        } {}
+
+  template<typename ClientType>
+  typename WrapperServiceClients<ClientType>::ServiceLocatorClient&
+      WrapperServiceClients<ClientType>::GetServiceLocatorClient() {
+    return **m_serviceLocatorClient;
+  }
+
+  template<typename ClientType>
+  typename WrapperServiceClients<ClientType>::RegistryClient&
+      WrapperServiceClients<ClientType>::GetRegistryClient() {
+    return **m_registryClient;
+  }
+
+  template<typename ClientType>
+  typename WrapperServiceClients<ClientType>::AdministrationClient&
+      WrapperServiceClients<ClientType>::GetAdministrationClient() {
+    return **m_administrationClient;
+  }
+
+  template<typename ClientType>
+  typename WrapperServiceClients<ClientType>::DefinitionsClient&
+      WrapperServiceClients<ClientType>::GetDefinitionsClient() {
+    return **m_definitionsClient;
+  }
+
+  template<typename ClientType>
+  typename WrapperServiceClients<ClientType>::MarketDataClient&
+      WrapperServiceClients<ClientType>::GetMarketDataClient() {
+    return **m_marketDataClient;
+  }
+
+  template<typename ClientType>
+  typename WrapperServiceClients<ClientType>::ChartingClient&
+      WrapperServiceClients<ClientType>::GetChartingClient() {
+    return **m_chartingClient;
+  }
+
+  template<typename ClientType>
+  typename WrapperServiceClients<ClientType>::ComplianceClient&
+      WrapperServiceClients<ClientType>::GetComplianceClient() {
+    return **m_complianceClient;
+  }
+
+  template<typename ClientType>
+  typename WrapperServiceClients<ClientType>::OrderExecutionClient&
+      WrapperServiceClients<ClientType>::GetOrderExecutionClient() {
+    return **m_orderExecutionClient;
+  }
+
+  template<typename ClientType>
+  typename WrapperServiceClients<ClientType>::RiskClient&
+      WrapperServiceClients<ClientType>::GetRiskClient() {
+    return **m_riskClient;
+  }
+
+  template<typename ClientType>
+  typename WrapperServiceClients<ClientType>::TimeClient&
+      WrapperServiceClients<ClientType>::GetTimeClient() {
+    return **m_timeClient;
+  }
+
+  template<typename ClientType>
+  void WrapperServiceClients<ClientType>::Open() {
+    m_client->Open();
+  }
+
+  template<typename ClientType>
+  void WrapperServiceClients<ClientType>::Close() {
+    m_client->Close();
+  }
+}
+
+#endif
