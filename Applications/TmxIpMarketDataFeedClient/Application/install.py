222ef026b23aac9d36f9b37c245a785a151648df
Kamal <kamal@eidolonsystems.com>
2017-02-07 12:49:46 -0500

    Usability enhancements to TMX IP setup script.

--- Applications/TmxIpMarketDataFeedClient/Application/install.py
+++ Applications/TmxIpMarketDataFeedClient/Application/install.py
@@ -9,7 +9,8 @@ def output(source_path, service_locator_address, local_interface, username,
   source = source.replace('$local_interface', local_interface)
   source = source.replace('$username', '"%s"' % username)
   source = source.replace('$password', '"%s"' % password)
-  print source
+  destination = open(os.path.join(source_path, 'config.yml'), 'w')
+  destination.write(source)
 
 def main():
   parser = argparse.ArgumentParser(
@@ -19,10 +20,12 @@ def main():
   parser.add_argument('-l', '--local', type=str, help='Local Interface',
     required=True)
   parser.add_argument('-u', '--username', type=str, help='Username',
-    required=True)
+    required=False, default='market_data_feed')
   parser.add_argument('-p', '--password', type=str, help='Password',
     required=True)
   args = parser.parse_args()
+  if args.service.find(':') == -1:
+    args.service = '%s:20000' % args.service
   for filename in os.listdir('.'):
     if filename.startswith('tmxip_') and os.path.isdir(filename):
       output(filename, args.service, args.local, args.username, args.password)
