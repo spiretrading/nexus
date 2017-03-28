6959ce53df22aa76c7540714adbd8ceef5293067
Kamal <kamal@eidolonsystems.com>
2017-03-28 17:15:57 -0400

    Removed the need to explicitly query for Order submissions in order to receive ExecutionReport updates on submitted orders.

--- Nexus/Source/OrderExecutionServiceTests/OrderExecutionClientTester.cpp
+++ Nexus/Source/OrderExecutionServiceTests/OrderExecutionClientTester.cpp
@@ -2,7 +2,6 @@
 #include <Beam/ServiceLocator/NullAuthenticator.hpp>
 #include <Beam/SignalHandling/NullSlot.hpp>
 #include <boost/functional/factory.hpp>
-#include <boost/functional/value_factory.hpp>
 #include "Nexus/Definitions/DefaultCountryDatabase.hpp"
 #include "Nexus/Definitions/DefaultMarketDatabase.hpp"
 
@@ -23,44 +22,50 @@ using namespace Nexus::Queries;
 using namespace std;
 
 void OrderExecutionClientTester::setUp() {
-  m_serverConnection.Initialize();
-  m_server.Initialize(&*m_serverConnection,
-    factory<std::shared_ptr<TriggerTimer>>(), NullSlot(), NullSlot());
+  m_serverConnection.emplace();
+  m_server.emplace(&*m_serverConnection,
+    factory<std::shared_ptr<TriggerTimer>>(), NullSlot{}, NullSlot{});
   Nexus::Queries::RegisterQueryTypes(Store(m_server->GetSlots().GetRegistry()));
   RegisterOrderExecutionServices(Store(m_server->GetSlots()));
   RegisterOrderExecutionMessages(Store(m_server->GetSlots()));
-  ServiceProtocolClientBuilder builder(
+  ServiceProtocolClientBuilder builder{
     [&] {
-      return std::make_unique<ServiceProtocolClientBuilder::Channel>(("test"),
+      return std::make_unique<ServiceProtocolClientBuilder::Channel>("test",
         Ref(*m_serverConnection));
     },
     [&] {
       return std::make_unique<ServiceProtocolClientBuilder::Timer>();
-    });
-  m_client.Initialize(builder);
+    }};
+  m_client.emplace(builder);
   m_server->Open();
   m_client->Open();
 }
 
 void OrderExecutionClientTester::tearDown() {
-  m_client.Reset();
-  m_server.Reset();
-  m_serverConnection.Reset();
+  m_client.reset();
+  m_server.reset();
+  m_serverConnection.reset();
 }
 
 void OrderExecutionClientTester::TestSubmitOrder() {
-  Security security("TST", DefaultMarkets::NYSE(), DefaultCountries::US());
-  bool sentSubmitOrderRequest = false;
+  Security security{"TST", DefaultMarkets::NYSE(), DefaultCountries::US()};
+  auto sentSubmitOrderRequest = false;
   OrderFields orderFields;
   ServiceProtocolServer::ServiceProtocolClient* serverClient;
+  QueryOrderSubmissionsService::AddSlot(Store(m_server->GetSlots()),
+    [&] (auto& client, auto& query) {
+      CPPUNIT_ASSERT(query.GetIndex() == DirectoryEntry::GetRootAccount());
+      OrderSubmissionQueryResult result;
+      result.m_queryId = -1;
+      return result;
+    });
   NewOrderSingleService::AddSlot(Store(m_server->GetSlots()),
-    [&] (ServiceProtocolServer::ServiceProtocolClient& client,
-        const OrderFields& requestedOrderFields) {
+    [&] (auto& client, auto& requestedOrderFields) {
       CPPUNIT_ASSERT(requestedOrderFields == orderFields);
       sentSubmitOrderRequest = true;
       serverClient = &client;
       SequencedAccountOrderInfo order;
-      order.GetSequence() = Beam::Queries::Sequence(5);
+      order.GetSequence() = Beam::Queries::Sequence{5};
       order->GetIndex() = requestedOrderFields.m_account;
       (*order)->m_orderId = 1;
       return order;
@@ -72,23 +77,22 @@ void OrderExecutionClientTester::TestSubmitOrder() {
   orderFields.m_side = Side::BID;
   orderFields.m_type = OrderType::LIMIT;
   orderFields.m_destination = "TST";
-  const Order& order = m_client->Submit(orderFields);
+  auto& order = m_client->Submit(orderFields);
   CPPUNIT_ASSERT(sentSubmitOrderRequest);
-  std::shared_ptr<Queue<ExecutionReport>> updates =
-    std::make_shared<Queue<ExecutionReport>>();
+  auto updates = std::make_shared<Queue<ExecutionReport>>();
   order.GetPublisher().Monitor(updates);
-  ExecutionReport pendingNewReportOut = ExecutionReport::BuildInitialReport(
+  auto pendingNewReportOut = ExecutionReport::BuildInitialReport(
     order.GetInfo().m_orderId, microsec_clock::universal_time());
   SendRecordMessage<OrderUpdateMessage>(*serverClient, pendingNewReportOut);
-  ExecutionReport pendingNewReportIn = updates->Top();
+  auto pendingNewReportIn = updates->Top();
   updates->Pop();
   CPPUNIT_ASSERT(pendingNewReportIn.m_status == OrderStatus::PENDING_NEW);
   CPPUNIT_ASSERT(pendingNewReportIn.m_id == 1);
   CPPUNIT_ASSERT(pendingNewReportIn.m_additionalTags.empty());
-  ExecutionReport newReportOut = ExecutionReport::BuildUpdatedReport(
-    pendingNewReportOut, OrderStatus::NEW, microsec_clock::universal_time());
+  auto newReportOut = ExecutionReport::BuildUpdatedReport(pendingNewReportOut,
+    OrderStatus::NEW, microsec_clock::universal_time());
   SendRecordMessage<OrderUpdateMessage>(*serverClient, newReportOut);
-  ExecutionReport newReportIn = updates->Top();
+  auto newReportIn = updates->Top();
   CPPUNIT_ASSERT(newReportIn.m_status == OrderStatus::NEW);
   CPPUNIT_ASSERT(newReportIn.m_id == 1);
   CPPUNIT_ASSERT(newReportIn.m_additionalTags.empty());
