0a775f374b726c1f37eb8a0ddd98678bba5fbcb7
Kamal <kamal@eidolonsystems.com>
2017-06-02 12:14:14 -0400

    .

--- Nexus/Include/Nexus/Backtester/BacktesterTimer.hpp
+++ Nexus/Include/Nexus/Backtester/BacktesterTimer.hpp
@@ -36,11 +36,8 @@ namespace Nexus {
 
     private:
       friend class TimerBacktesterEvent;
-      mutable std::shared_ptr<boost::mutex> m_mutex;
       boost::posix_time::time_duration m_interval;
       BacktesterEventHandler* m_eventHandler;
-      std::shared_ptr<bool> m_isAlive;
-      int m_iteration;
       std::shared_ptr<TimerBacktesterEvent> m_expireEvent;
       std::shared_ptr<TimerBacktesterEvent> m_cancelEvent;
       Beam::MultiQueueWriter<Beam::Threading::Timer::Result> m_publisher;
@@ -62,79 +59,61 @@ namespace Nexus {
         boost::posix_time::ptime timestamp,
         Beam::Threading::Timer::Result result);
 
+      void Cancel();
+
       virtual void Execute() override;
 
     private:
-      mutable std::shared_ptr<boost::mutex> m_mutex;
       BacktesterTimer* m_timer;
-      std::shared_ptr<bool> m_isAlive;
       Beam::Threading::Timer::Result m_result;
-      int m_iteration;
   };
 
   inline BacktesterTimer::BacktesterTimer(
       boost::posix_time::time_duration interval,
       Beam::RefType<BacktesterEventHandler> eventHandler)
-      : m_mutex{std::make_shared<boost::mutex>()},
-        m_interval{interval},
-        m_eventHandler{eventHandler.Get()},
-        m_isAlive{std::make_shared<bool>(true)},
-        m_iteration{0} {}
+      : m_interval{interval},
+        m_eventHandler{eventHandler.Get()} {}
 
   inline BacktesterTimer::~BacktesterTimer() {
     Cancel();
-    boost::lock_guard<boost::mutex> lock{*m_mutex};
-    *m_isAlive = false;
   }
 
   inline void BacktesterTimer::Start() {
-    {
-      boost::lock_guard<boost::mutex> lock{*m_mutex};
-      if(m_expireEvent != nullptr) {
-        return;
-      }
-      m_expireEvent = std::make_shared<TimerBacktesterEvent>(*this,
-        m_eventHandler->GetTestEnvironment().GetTimeEnvironment().GetTime() +
-        m_interval, Beam::Threading::Timer::Result::EXPIRED);
+    if(m_expireEvent != nullptr) {
+      return;
     }
+    m_expireEvent = std::make_shared<TimerBacktesterEvent>(*this,
+      m_eventHandler->GetTestEnvironment().GetTimeEnvironment().GetTime() +
+      m_interval, Beam::Threading::Timer::Result::EXPIRED);
     m_eventHandler->Add(m_expireEvent);
   }
 
   inline void BacktesterTimer::Cancel() {
-    auto addEvent = false;
-    std::shared_ptr<TimerBacktesterEvent> cancelEvent;
-    {
-      boost::lock_guard<boost::mutex> lock{*m_mutex};
-      if(m_expireEvent == nullptr) {
-        return;
-      } else if(m_cancelEvent == nullptr) {
-        m_cancelEvent = std::make_shared<TimerBacktesterEvent>(*this,
-          m_eventHandler->GetTestEnvironment().GetTimeEnvironment().GetTime(),
-          Beam::Threading::Timer::Result::CANCELED);
-        addEvent = true;
-      }
-      cancelEvent = m_cancelEvent;
+    if(m_expireEvent == nullptr) {
+      return;
+    } else if(m_cancelEvent == nullptr) {
+      m_expireEvent->Cancel();
+      m_cancelEvent = std::make_shared<TimerBacktesterEvent>(*this,
+        m_eventHandler->GetTestEnvironment().GetTimeEnvironment().GetTime(),
+        Beam::Threading::Timer::Result::CANCELED);
+      m_eventHandler->Add(m_cancelEvent);
     }
-    if(addEvent) {
-      m_eventHandler->Add(cancelEvent);
-    }
-    cancelEvent->Wait();
+    m_cancelEvent->Wait();
   }
 
   inline void BacktesterTimer::Wait() {
-    std::shared_ptr<TimerBacktesterEvent> event;
-    {
-      boost::lock_guard<boost::mutex> lock{*m_mutex};
-      if(m_expireEvent == nullptr) {
-        return;
-      }
-      if(m_cancelEvent != nullptr &&
-          m_cancelEvent->GetTimestamp() < m_expireEvent->GetTimestamp()) {
-        event = m_cancelEvent;
-      } else {
-        event = m_expireEvent;
-      }
+    if(m_expireEvent == nullptr) {
+      return;
     }
+    auto event =
+      [&] {
+        if(m_cancelEvent != nullptr &&
+            m_cancelEvent->GetTimestamp() < m_expireEvent->GetTimestamp()) {
+          return m_cancelEvent;
+        } else {
+          return m_expireEvent;
+        }
+      }();
     event->Wait();
   }
 
@@ -147,23 +126,21 @@ namespace Nexus {
       boost::posix_time::ptime timestamp,
       Beam::Threading::Timer::Result result)
       : BacktesterEvent{timestamp},
-        m_mutex{timer.m_mutex},
         m_timer{&timer},
-        m_isAlive{m_timer->m_isAlive},
-        m_result{result},
-        m_iteration{timer.m_iteration} {}
+        m_result{result} {}
+
+  inline void TimerBacktesterEvent::Cancel() {
+    m_result = Beam::Threading::Timer::Result::NONE;
+  }
 
   inline void TimerBacktesterEvent::Execute() {
-    {
-      boost::lock_guard<boost::mutex> lock{*m_mutex};
-      if(!*m_isAlive || m_timer->m_iteration != m_iteration) {
-        return;
-      }
-      m_timer->m_publisher.Push(m_result);
-      m_timer->m_expireEvent = nullptr;
-      m_timer->m_cancelEvent = nullptr;
-      ++m_timer->m_iteration;
+    if(m_result == Beam::Threading::Timer::Result::NONE) {
+      Complete();
+      return;
     }
+    m_timer->m_expireEvent.reset();
+    m_timer->m_cancelEvent.reset();
+    m_timer->m_publisher.Push(m_result);
     Complete();
   }
 }
