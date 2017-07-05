7ffdfe8ac9c773115f536199cc2f1a925b725019
Jimin Park <jimin@spiretrading.com>
2017-07-05 15:15:08 -0400

    fb782: save account roles.

--- Applications/ClientWebPortal/Source/api/src/services/commons/account-roles/index.js
+++ Applications/ClientWebPortal/Source/api/src/services/commons/account-roles/index.js
@@ -2,33 +2,37 @@ import AccountRole from './account-role';
 
 /** Account roles util class */
 class AccountRoles {
-  parse(accountRoleNumber) {
-    let roles = {
-      isTrader: ((accountRoleNumber & (1 << AccountRole.TRADER)) != 0),
-      isManager: ((accountRoleNumber & (1 << AccountRole.MANAGER)) != 0),
-      isService: ((accountRoleNumber & (1 << AccountRole.SERVICE)) != 0),
-      isAdmin: ((accountRoleNumber & (1 << AccountRole.ADMINISTRATOR)) != 0)
-    };
-
-    return roles;
-  }
-
-  encode(roles) {
-    let rolesNumber = 0;
-    if (roles.isTrader) {
-      rolesNumber += (1 << AccountRole.TRADER);
-    }
-    if (roles.isManager) {
-      rolesNumber += (1 << AccountRole.MANAGER);
-    }
-    if (roles.isService) {
-      rolesNumber += (1 << AccountRole.SERVICE);
-    }
-    if (roles.isAdmin) {
-      rolesNumber += (1 << AccountRole.ADMINISTRATOR);
-    }
-    return rolesNumber;
+  constructor(isTrader, isManager, isService, isAdmin) {
+    this.isTrader = isTrader;
+    this.isManager = isManager;
+    this.isService = isService;
+    this.isAdmin = isAdmin;
   }
 }
 
-export default new AccountRoles();
+AccountRoles.parse = accountRoleNumber => {
+  let isTrader = ((accountRoleNumber & (1 << AccountRole.TRADER)) != 0);
+  let isManager = ((accountRoleNumber & (1 << AccountRole.MANAGER)) != 0);
+  let isService = ((accountRoleNumber & (1 << AccountRole.SERVICE)) != 0);
+  let isAdmin = ((accountRoleNumber & (1 << AccountRole.ADMINISTRATOR)) != 0);
+  return new AccountRoles(isTrader, isManager, isService, isAdmin);
+};
+
+AccountRoles.encode = roles => {
+  let rolesNumber = 0;
+  if (roles.isTrader) {
+    rolesNumber += (1 << AccountRole.TRADER);
+  }
+  if (roles.isManager) {
+    rolesNumber += (1 << AccountRole.MANAGER);
+  }
+  if (roles.isService) {
+    rolesNumber += (1 << AccountRole.SERVICE);
+  }
+  if (roles.isAdmin) {
+    rolesNumber += (1 << AccountRole.ADMINISTRATOR);
+  }
+  return rolesNumber;
+};
+
+export default AccountRoles;
