e3b2e320dcc4fd9b94b075664c69182eebd0f232
Kamal <kamal@eidolonsystems.com>
2017-07-23 19:41:25 -0400

    Updated the build scripts to factor in the amount of RAM.

--- Build/Make/build.sh
+++ Build/Make/build.sh
@@ -16,10 +16,8 @@ build_function() {
 }
 
 let cores="`grep -c "processor" < /proc/cpuinfo` / 2 + 1"
-if [ $cores -gt 8 ]
-then
-  cores=8
-fi
+let mem="`grep -oP "MemTotal: +\K([[:digit:]]+)(?=.*)" < /proc/meminfo` / 4194304"
+let jobs="$(($cores<$mem?$cores:$mem))"
 
 pushd $directory/../../Nexus/Build/Make
 ./build.sh $config
@@ -48,7 +46,7 @@ applications+="SimulationOrderExecutionServer "
 applications+="TmxIpMarketDataFeedClient "
 applications+="TmxTl1MarketDataFeedClient "
 applications+="UtpMarketDataFeedClient "
-parallel -j$cores --no-notice build_function ::: $applications
+parallel -j$jobs --no-notice build_function ::: $applications
 
 pushd $directory/../../Applications/ClientWebPortal/Source/api/build/make
 ./build.sh $config
