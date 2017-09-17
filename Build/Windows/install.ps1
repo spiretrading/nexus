b9d1c060a90525880e10ec1b2ae06a418ebd7727
Kamal <kamal@eidolonsystems.com>
2017-09-16 23:09:49 -0400

    Switched to using Gow instead of UnxUtils.

--- Build/Windows/install.ps1
+++ Build/Windows/install.ps1
@@ -13,19 +13,20 @@ If($program_files_x86_path -eq "C:\Program Files") {
   $program_files_path = "C:\Progra~2"
 }
 If((Get-Command "grep.exe" -ErrorAction SilentlyContinue) -eq $null) {
-  Invoke-WebRequest -UseBasicParsing -Uri https://downloads.sourceforge.net/project/unxutils/unxutils/current/UnxUtils.zip -UserAgent [Microsoft.PowerShell.Commands.PSUserAgent]::FireFox -OutFile UnxUtils.zip
-  Expand-Archive UnxUtils.zip -DestinationPath "C:\UnxUtils"
-  $old_path = (Get-ItemProperty -Path "Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment" -Name PATH).path
-  $new_path = "$old_path;C:\UnxUtils\usr\local\wbin"
-  Set-ItemProperty -Path "Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment" -Name PATH –Value $new_path
+  Invoke-WebRequest -UseBasicParsing -Uri https://github.com/bmatzelle/gow/releases/download/v0.8.0/Gow-0.8.0.exe -UserAgent [Microsoft.PowerShell.Commands.PSUserAgent]::FireFox -OutFile Gow-0.8.0.exe
+  .\Gow-0.8.0.exe /S
   $env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User")
-  rm .\UnxUtils.zip
+  rm Gow-0.8.0.exe -Force
+}
+If((Get-Command "unzip.exe" -ErrorAction SilentlyContinue) -eq $null) {
+  Invoke-WebRequest -UseBasicParsing -Uri http://www2.cs.uidaho.edu/~jeffery/win32/unzip.exe -UserAgent [Microsoft.PowerShell.Commands.PSUserAgent]::FireFox -OutFile unzip.exe
+  mv ./unzip.exe "$program_files_x86_path\Gow\bin"
 }
 If((Get-Command "git.exe" -ErrorAction SilentlyContinue) -eq $null) {
   New-Item git_config.inf -type file -force -value "[Setup]`r`nPathOption=Cmd"
   Invoke-WebRequest -UseBasicParsing -Uri https://github.com/git-for-windows/git/releases/download/v2.12.2.windows.2/Git-2.12.2.2-64-bit.exe -OutFile Git-2.12.2.2-64-bit.exe
   .\Git-2.12.2.2-64-bit.exe /SILENT /LOADINF="git_config.inf" | Out-Null
-  rm .\Git-2.12.2.2-64-bit.exe -Force -Recurse
+  rm .\Git-2.12.2.2-64-bit.exe -Force
   rm .\git_config.inf -Force
 }
 If((Get-Command "cmake.exe" -ErrorAction SilentlyContinue) -eq $null) {
@@ -35,7 +36,7 @@ If((Get-Command "cmake.exe" -ErrorAction SilentlyContinue) -eq $null) {
   $new_path = "$old_path;C:\Program Files\CMake\bin"
   Set-ItemProperty -Path "Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment" -Name PATH –Value $new_path
   $env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User")
-  rm .\cmake-3.8.0-win64-x64.msi -Force -Recurse
+  rm .\cmake-3.8.0-win64-x64.msi -Force
 }
 If((Get-Command "python.exe" -ErrorAction SilentlyContinue) -eq $null) {
   Invoke-WebRequest -UseBasicParsing -Uri https://www.python.org/ftp/python/2.7.13/python-2.7.13.msi -OutFile python-2.7.13.msi
@@ -44,16 +45,19 @@ If((Get-Command "python.exe" -ErrorAction SilentlyContinue) -eq $null) {
   $new_path = "$old_path;C:\Program Files (x86)\Python27"
   Set-ItemProperty -Path "Registry::HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Session Manager\Environment" -Name PATH –Value $new_path
   $env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User")
-  rm python-2.7.13.msi -Force -Recurse
+  rm python-2.7.13.msi -Force
 }
 If((Get-Command "perl.exe" -ErrorAction SilentlyContinue) -eq $null) {
   Invoke-WebRequest -UseBasicParsing -Uri http://www.strawberryperl.com/download/5.24.1.1/strawberry-perl-5.24.1.1-64bit.msi -OutFile strawberry-perl-5.24.1.1-64bit.msi
   msiexec /quiet /i strawberry-perl-5.24.1.1-64bit.msi TARGETDIR="C:\Strawberry" | Out-Null
-  rm strawberry-perl-5.24.1.1-64bit.msi -Force -Recurse
+  rm strawberry-perl-5.24.1.1-64bit.msi -Force
 }
 If(-Not (Test-Path "C:\Program Files (x86)\Microsoft Visual Studio")) {
   Invoke-WebRequest -UseBasicParsing -Uri "https://aka.ms/vs/15/release/vs_community.exe" -OutFile vs_Community.exe
-  ./vs_Community.exe --add Microsoft.VisualStudio.Workload.NativeDesktop --all --passive --norestart --wait | Out-Null
+  ./vs_Community.exe --passive --norestart --wait `
+    --add Microsoft.VisualStudio.Component.VC.Tools.x86.x64 `
+    --add Microsoft.VisualStudio.Component.Windows10SDK.15063.Desktop `
+    --add Microsoft.VisualStudio.ComponentGroup.NativeDesktop.Win81 | Out-Null
   rm vs_Community.exe
   $env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User")
 }
