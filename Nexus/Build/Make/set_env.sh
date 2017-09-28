51ad7f59e890a8bf2e3a8b5638167e06bc16200a
Kamal <kamal@eidolonsystems.com>
2017-09-27 21:44:33 -0400

    Updated environment paths for QuickFIX.

--- Nexus/Build/Make/set_env.sh
+++ Nexus/Build/Make/set_env.sh
@@ -11,6 +11,6 @@ pushd /home/developers/Beam/Beam/Build/Make
 popd
 export PATH=/usr/local/Qt-5.5.0/bin:$PATH
 export NEXUS_INCLUDE_PATH=$nexus_local_path/../../Include
-export QUICK_FIX_INCLUDE_PATH=/home/developers/quickfix/include
-export QUICK_FIX_LIBRARY_DEBUG_PATH=/home/developers/quickfix/lib/libquickfix.so
-export QUICK_FIX_LIBRARY_OPTIMIZED_PATH=/home/developers/quickfix/lib/libquickfix.so
+export QUICK_FIX_INCLUDE_PATH=/usr/local/include
+export QUICK_FIX_LIBRARY_DEBUG_PATH=/usr/local/lib/libquickfix.so
+export QUICK_FIX_LIBRARY_OPTIMIZED_PATH=/usr/local/lib/libquickfix.so
