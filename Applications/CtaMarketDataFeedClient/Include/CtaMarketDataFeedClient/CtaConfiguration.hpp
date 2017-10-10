9b5290b3e047a312b1ce42af056ab23f9e666d03
Kamal <kamal@eidolonsystems.com>
2017-10-10 14:43:48 -0400

    Implemented the CTA binary protocol specification.

--- Applications/CtaMarketDataFeedClient/Include/CtaMarketDataFeedClient/CtaConfiguration.hpp
+++ Applications/CtaMarketDataFeedClient/Include/CtaMarketDataFeedClient/CtaConfiguration.hpp
@@ -1,9 +1,7 @@
 #ifndef NEXUS_CTACONFIGURATION_HPP
 #define NEXUS_CTACONFIGURATION_HPP
 #include <string>
-#include <Beam/TimeService/ToLocalTime.hpp>
 #include <Beam/Utilities/YamlConfig.hpp>
-#include <boost/date_time/posix_time/posix_time_types.hpp>
 #include "Nexus/Definitions/Country.hpp"
 #include "Nexus/Definitions/Market.hpp"
 
@@ -18,10 +16,6 @@ namespace MarketDataService {
     //! Whether to log messages.
     bool m_isLoggingMessages;
 
-    //! The time used as the origin of market data messages disseminated by
-    //! market data server.
-    boost::posix_time::ptime m_timeOrigin;
-
     //! The Country of origin.
     Nexus::CountryCode m_country;
 
@@ -33,22 +27,16 @@ namespace MarketDataService {
       \param config The YAML Node to parse.
       \param countryDatabase The set of valid countries.
       \param marketDatabase The set of valid markets.
-      \param currentTime The current time used to establish the time origin.
-      \param timeZones The list of time zones.
       \return The CtaConfiguration represented by the <i>config</i>.
     */
     static CtaConfiguration Parse(const YAML::Node& config,
       const CountryDatabase& countryDatabase,
-      const MarketDatabase& marketDatabase,
-      const boost::posix_time::ptime& currentTime,
-      const boost::local_time::tz_database& timeZones);
+      const MarketDatabase& marketDatabase);
   };
 
   inline CtaConfiguration CtaConfiguration::Parse(const YAML::Node& config,
       const CountryDatabase& countryDatabase,
-      const MarketDatabase& marketDatabase,
-      const boost::posix_time::ptime& currentTime,
-      const boost::local_time::tz_database& timeZones) {
+      const MarketDatabase& marketDatabase) {
     CtaConfiguration ctaConfig;
     ctaConfig.m_isLoggingMessages = Beam::Extract<bool>(config,
       "enable_logging", false);
@@ -70,18 +58,6 @@ namespace MarketDataService {
       auto entry = ParseMarketCode(marketIdentifier, marketDatabase);
       ctaConfig.m_marketCodes[code.front()] = entry;
     }
-    auto configTimezone = Beam::Extract<std::string>(config, "time_zone",
-      "Eastern_Time");
-    auto timeZone = timeZones.time_zone_from_region(configTimezone);
-    if(timeZone == nullptr) {
-      BOOST_THROW_EXCEPTION(Beam::MakeYamlParserException(
-        "Time zone not found.", config.FindValue("time_zone")->GetMark()));
-    }
-    boost::posix_time::ptime serverDate{
-      Beam::TimeService::AdjustDateTime(currentTime, "UTC", configTimezone,
-      timeZones).date(), boost::posix_time::seconds(0)};
-    ctaConfig.m_timeOrigin = Beam::TimeService::AdjustDateTime(serverDate,
-      configTimezone, "UTC", timeZones);
     return ctaConfig;
   }
 }
