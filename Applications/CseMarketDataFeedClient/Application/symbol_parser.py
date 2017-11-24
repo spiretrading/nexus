e3363a51cde4c60066f45eab402d2b800791f261
Kamal <kamal@eidolonsystems.com>
2017-11-24 12:24:13 -0500

    Updated the symbol parser.

--- Applications/CseMarketDataFeedClient/Application/symbol_parser.py
+++ Applications/CseMarketDataFeedClient/Application/symbol_parser.py
@@ -1,13 +1,18 @@
+import string
 import xlrd
 
+printable = set(string.printable)
+output = open('symbols.yml', 'w')
 book = xlrd.open_workbook('cse.xls')
 first_sheet = book.sheet_by_index(0)
-print '---'
+output.write('---\n')
 for i in range(5, first_sheet.nrows):
-  symbol = first_sheet.cell(i, 1).value.split('\n')[0]
-  name = first_sheet.cell(i, 0).value.split('\n')[0]
+  symbol = filter(lambda x: x in printable,
+    first_sheet.cell(i, 1).value.split('\n')[0])
+  name = filter(lambda x: x in printable,
+    first_sheet.cell(i, 0).value.split('\n')[0])
   if len(symbol) == 0 or len(name) == 0:
     break
-  print '  - symbol: %s' % symbol
-  print '    name: %s' % name
-print '...'
+  output.write('  - symbol: %s\n' % symbol)
+  output.write('    name: %s\n' % name)
+output.write('...\n')
