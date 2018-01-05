975559ef2d9739542b98d3f2c181e01b8094dc3d
Jimin Park <jimin@spiretrading.com>
2018-01-05 15:40:46 -0500

    fb888: introducing Debug mode builds into build.sh

--- Applications/ClientWebPortal/Source/webapp/build/make/build.sh
+++ Applications/ClientWebPortal/Source/webapp/build/make/build.sh
@@ -4,13 +4,18 @@ directory=$(dirname $(readlink -f $0))
 pushd $directory/
 pushd $directory/../..
 echo $config
-if [ "$config" = "clean" ]
+if [ "$config" = "Clean" ]
 then
   rm -rf dist
 else
   sudo -u $(logname) rm -rf $directory/../../node_modules/spire-client
   sudo -u $(logname) ln -s $directory/../../../api/dist $directory/../../node_modules/spire-client
-  npm run-script build
+  if [ "$config" = "Debug" ]
+  then
+    npm run-script build-dev
+  else
+    npm run-script build
+  fi
   rm -rf ../../Application/webapp
   cp -a dist/. ../../Application/webapp/
 fi
