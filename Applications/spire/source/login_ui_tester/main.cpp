2cfdda69c5c46897e0fe94d5041c9ac92e708a9b
Kamal Mansouri <kamal@eidolonsystems.com>
2018-03-04 22:32:43 -0500

    Added login ui tester. Added scratch project.

--- Applications/spire/source/login_ui_tester/main.cpp
+++ Applications/spire/source/login_ui_tester/main.cpp
@@ -1,18 +1,15 @@
 #include <QApplication>
+#include "spire/login/login_window.hpp"
 #include "spire/spire/resources.hpp"
-#include "spire/spire/spire_controller.hpp"
-#include "spire/version.hpp"
 
 using namespace spire;
 
 int main(int argc, char** argv) {
   auto application = new QApplication(argc, argv);
   application->setOrganizationName(QObject::tr("Eidolon Systems Ltd"));
-  application->setApplicationName(QObject::tr("Spire"));
-  application->setApplicationVersion(SPIRE_VERSION);
+  application->setApplicationName(QObject::tr("Login UI Tester"));
   initialize_resources();
-  spire_controller controller;
-  controller.open();
+  login_window window;
+  window.show();
   application->exec();
-  controller.close();
 }
--- Applications/spire/source/scratch/main.cpp
+++ Applications/spire/source/scratch/main.cpp
@@ -1,18 +1,12 @@
 #include <QApplication>
 #include "spire/spire/resources.hpp"
-#include "spire/spire/spire_controller.hpp"
-#include "spire/version.hpp"
 
 using namespace spire;
 
 int main(int argc, char** argv) {
   auto application = new QApplication(argc, argv);
   application->setOrganizationName(QObject::tr("Eidolon Systems Ltd"));
-  application->setApplicationName(QObject::tr("Spire"));
-  application->setApplicationVersion(SPIRE_VERSION);
+  application->setApplicationName(QObject::tr("Scratch"));
   initialize_resources();
-  spire_controller controller;
-  controller.open();
   application->exec();
-  controller.close();
 }
