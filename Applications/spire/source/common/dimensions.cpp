ab68a78ee22718676d82ccfdc10d421f32bc55b6
Kamal <kamal@eidolonsystems.com>
2018-03-05 15:22:01 -0500

    Added DPI related functions.

---
+++ Applications/spire/source/common/dimensions.cpp
@@ -0,0 +1,23 @@
+#include "spire/spire/dimensions.hpp"
+#include <QApplication>
+#include <QDesktopWidget>
+
+using namespace spire;
+
+int spire::scale_width(int width) noexcept {
+  auto dpiX = QApplication::desktop()->logicalDpiX();
+  return (width * dpiX) / DPI;
+}
+
+int spire::scale_height(int height) noexcept {
+  auto dpiY = QApplication::desktop()->logicalDpiY();
+  return (height * dpiY) / DPI;
+}
+
+QSize spire::scale(int width, int height) noexcept {
+  return QSize(scale_width(width), scale_height(height));
+}
+
+QSize spire::scale(const QSize& size) noexcept {
+  return scale(size.width(), size.height());
+}
