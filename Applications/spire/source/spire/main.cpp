5d5f0b3076e24a928f8117c9bb270b5663cac3b7
Kamal Mansouri <kamal@eidolonsystems.com>
2018-03-04 21:59:29 -0500

    Added Roboto fonts.

--- Applications/spire/source/spire/main.cpp
+++ Applications/spire/source/spire/main.cpp
@@ -2,8 +2,8 @@
 #include <QApplication>
 #include <QFontDatabase>
 #include <QtPlugin>
-#include "spire/version.hpp"
 #include "spire/spire/spire_controller.hpp"
+#include "spire/version.hpp"
 
 #ifdef _MSC_VER
   Q_IMPORT_PLUGIN(QWindowsIntegrationPlugin)
@@ -13,7 +13,31 @@
 
 using namespace spire;
 
-inline void initialize_resources() {}
+void initialize_resources() {
+  Q_INIT_RESOURCE(resources);
+  QStringList fonts;
+  fonts << ":/fonts/Roboto/Roboto-Black.ttf" <<
+    ":/fonts/Roboto/Roboto-BlackItalic.ttf" <<
+    ":/fonts/Roboto/Roboto-Bold.ttf" <<
+    ":/fonts/Roboto/Roboto-BoldItalic.ttf" <<
+    ":/fonts/Roboto/RobotoCondensed-Bold.ttf" <<
+    ":/fonts/Roboto/RobotoCondensed-BoldItalic.ttf" <<
+    ":/fonts/Roboto/RobotoCondensed-Italic.ttf" <<
+    ":/fonts/Roboto/RobotoCondensed-Light.ttf" <<
+    ":/fonts/Roboto/RobotoCondensed-LightItalic.ttf" <<
+    ":/fonts/Roboto/RobotoCondensed-Regular.ttf" <<
+    ":/fonts/Roboto/Roboto-Italic.ttf" <<
+    ":/fonts/Roboto/Roboto-Light.ttf" <<
+    ":/fonts/Roboto/Roboto-LightItalic.ttf" <<
+    ":/fonts/Roboto/Roboto-Medium.ttf" <<
+    ":/fonts/Roboto/Roboto-MediumItalic.ttf" <<
+    ":/fonts/Roboto/Roboto-Regular.ttf" <<
+    ":/fonts/Roboto/Roboto-Thin.ttf" <<
+    ":/fonts/Roboto/Roboto-ThinItalic.ttf";
+  for(auto& font : fonts) {
+    QFontDatabase::addApplicationFont(font);
+  }
+}
 
 int main(int argc, char** argv) {
 #ifdef _DEBUG
