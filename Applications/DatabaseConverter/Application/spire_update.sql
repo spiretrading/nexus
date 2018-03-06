c51d44c2d3ec841ba89ed6159cba1abbd55dc154
Kamal <kamal@eidolonsystems.com>
2018-03-06 15:50:28 -0500

    Added dry run implementation for compliance conversions and order submission conversions.

---
+++ Applications/DatabaseConverter/Application/spire_update.sql
@@ -0,0 +1,11 @@
+ALTER TABLE risk_parameters MODIFY COLUMN buying_power DOUBLE NOT NULL;
+ALTER TABLE risk_parameters MODIFY COLUMN net_loss DOUBLE NOT NULL;
+ALTER TABLE risk_modifications MODIFY COLUMN buying_power DOUBLE NOT NULL;
+ALTER TABLE risk_modifications MODIFY COLUMN net_loss DOUBLE NOT NULL;
+ALTER TABLE submissions MODIFY COLUMN quantity DOUBLE NOT NULL;
+ALTER TABLE submissions MODIFY COLUMN price DOUBLE NOT NULL;
+ALTER TABLE execution_reports MODIFY COLUMN last_quantity DOUBLE NOT NULL;
+ALTER TABLE execution_reports MODIFY COLUMN last_price DOUBLE NOT NULL;
+ALTER TABLE execution_reports MODIFY COLUMN execution_fee DOUBLE NOT NULL;
+ALTER TABLE execution_reports MODIFY COLUMN processing_fee DOUBLE NOT NULL;
+ALTER TABLE execution_reports MODIFY COLUMN commission DOUBLE NOT NULL;
