08d00672945117dfb6eb6b97419ab3ed2c309cfc
Kamal <kamal@eidolonsystems.com>
2018-03-08 15:40:54 -0500

    The SQL conversion scripts use transactions.

--- Applications/DatabaseConverter/Application/spire_update.sql
+++ Applications/DatabaseConverter/Application/spire_update.sql
@@ -1,13 +1,21 @@
-ALTER TABLE risk_parameters MODIFY COLUMN buying_power DOUBLE NOT NULL;
-ALTER TABLE risk_parameters MODIFY COLUMN net_loss DOUBLE NOT NULL;
-ALTER TABLE risk_modifications MODIFY COLUMN buying_power DOUBLE NOT NULL;
-ALTER TABLE risk_modifications MODIFY COLUMN net_loss DOUBLE NOT NULL;
-ALTER TABLE submissions MODIFY COLUMN quantity DOUBLE NOT NULL;
-UPDATE submissions SET quantity = 1000000 * quantity;
-ALTER TABLE submissions MODIFY COLUMN price DOUBLE NOT NULL;
-ALTER TABLE execution_reports MODIFY COLUMN last_quantity DOUBLE NOT NULL;
-UPDATE execution_reports SET last_quantity = 1000000 * last_quantity;
-ALTER TABLE execution_reports MODIFY COLUMN last_price DOUBLE NOT NULL;
-ALTER TABLE execution_reports MODIFY COLUMN execution_fee DOUBLE NOT NULL;
-ALTER TABLE execution_reports MODIFY COLUMN processing_fee DOUBLE NOT NULL;
-ALTER TABLE execution_reports MODIFY COLUMN commission DOUBLE NOT NULL;
+START TRANSACTION
+  ALTER TABLE risk_parameters MODIFY COLUMN buying_power DOUBLE NOT NULL;
+  ALTER TABLE risk_parameters MODIFY COLUMN net_loss DOUBLE NOT NULL;
+COMMIT
+START TRANSACTION
+  ALTER TABLE risk_modifications MODIFY COLUMN buying_power DOUBLE NOT NULL;
+  ALTER TABLE risk_modifications MODIFY COLUMN net_loss DOUBLE NOT NULL;
+COMMIT
+START TRANSACTION
+  ALTER TABLE submissions MODIFY COLUMN quantity DOUBLE NOT NULL;
+  UPDATE submissions SET quantity = 1000000 * quantity;
+  ALTER TABLE submissions MODIFY COLUMN price DOUBLE NOT NULL;
+COMMIT
+START TRANSACTION
+  ALTER TABLE execution_reports MODIFY COLUMN last_quantity DOUBLE NOT NULL;
+  UPDATE execution_reports SET last_quantity = 1000000 * last_quantity;
+  ALTER TABLE execution_reports MODIFY COLUMN last_price DOUBLE NOT NULL;
+  ALTER TABLE execution_reports MODIFY COLUMN execution_fee DOUBLE NOT NULL;
+  ALTER TABLE execution_reports MODIFY COLUMN processing_fee DOUBLE NOT NULL;
+  ALTER TABLE execution_reports MODIFY COLUMN commission DOUBLE NOT NULL;
+COMMIT
