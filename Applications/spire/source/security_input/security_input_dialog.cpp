6cb5447b7fe91fcd14de0dea71a8037c39e412ca
Ken <ken@spiretrading.com>
2018-04-10 12:51:34 -0400

    Refactored security_input_dialog to use drop_shadow

--- Applications/spire/source/security_input/security_input_dialog.cpp
+++ Applications/spire/source/security_input/security_input_dialog.cpp
@@ -1,67 +1,45 @@
 #include "spire/security_input/security_input_dialog.hpp"
 #include <QLabel>
 #include <QMouseEvent>
-#include <QGraphicsDropShadowEffect>
 #include <QVBoxLayout>
 #include "spire/spire/dimensions.hpp"
 #include "spire/security_input/security_info_list_view.hpp"
+#include "spire/ui/drop_shadow.hpp"
 
 using namespace boost;
 using namespace boost::signals2;
 using namespace Nexus;
 using namespace spire;
 
-namespace {
-  auto make_drop_shadow_effect(QWidget* w) {
-    auto drop_shadow = new QGraphicsDropShadowEffect(w);
-    drop_shadow->setBlurRadius(scale_width(12));
-    drop_shadow->setXOffset(0);
-    drop_shadow->setYOffset(0);
-    drop_shadow->setColor(QColor(0, 0, 0, 100));
-    return drop_shadow;
-  }
-}
-
 security_input_dialog::security_input_dialog(security_input_model& model,
     QWidget* parent)
     : QDialog(parent, Qt::Window | Qt::FramelessWindowHint),
       m_is_dragging(false) {
-  setAttribute(Qt::WA_TranslucentBackground);
   setWindowModality(Qt::WindowModal);
-  setGraphicsEffect(make_drop_shadow_effect(this));
+  auto shadow = new drop_shadow(this);
   auto layout = new QVBoxLayout(this);
-  auto margin = static_cast<QGraphicsDropShadowEffect*>(
-    graphicsEffect())->blurRadius();
-  layout->setMargin(margin);
-  m_dialog = new QWidget(this);
-  m_dialog->installEventFilter(this);
-  m_dialog->setFixedSize(scale(196, 68));
-  setFixedSize(m_dialog->size().width() + (margin * 2),
-    m_dialog->size().height() + (margin * 2));
-  m_dialog->setContentsMargins(scale_width(8), scale_height(6), scale_width(8),
+  layout->setContentsMargins(scale_width(8), scale_height(6), scale_width(8),
     scale_height(8));
-  m_dialog->setObjectName("security_input_dialog");
-  m_dialog->setStyleSheet(QString(R"(
+  layout->setSpacing(scale_height(10));
+  setFixedSize(scale(196, 68));
+  setObjectName("security_input_dialog");
+  setStyleSheet(QString(R"(
     #security_input_dialog {
       background-color: #F5F5F5;
       border: %1px solid #A0A0A0;
     }
   )").arg(scale_width(1)));
-  auto dialog_layout = new QVBoxLayout(m_dialog);
-  dialog_layout->setContentsMargins({});
-  dialog_layout->setSpacing(scale_height(10));
   auto text_label = new QLabel(tr("Security"), this);
   text_label->setStyleSheet(QString(R"(
     border: none;
     font-family: Roboto;
     font-size: %1px;
   )").arg(scale_height(12)));
-  dialog_layout->addWidget(text_label);
+  layout->addWidget(text_label);
   m_security_input_box = new security_input_box(model, this);
   m_security_input_box->connect_commit_signal(
     [=] (const Security& s) { set_security(s); });
-  dialog_layout->addWidget(m_security_input_box);
-  layout->addWidget(m_dialog);
+  layout->addWidget(m_security_input_box);
 }
 
 const Security& security_input_dialog::get_security() const noexcept {
@@ -72,35 +50,33 @@ void security_input_dialog::closeEvent(QCloseEvent* event) {
   reject();
 }
 
-bool security_input_dialog::eventFilter(QObject* watched, QEvent* event) {
-  if(watched == m_dialog) {
-    if(event->type() == QEvent::MouseButtonPress) {
-      auto e = static_cast<QMouseEvent*>(event);
-      if(m_is_dragging || e->button() != Qt::LeftButton) {
-        return false;
-      }
-      m_is_dragging = true;
-      m_last_mouse_pos = e->globalPos();
-    } else if(event->type() == QEvent::MouseButtonRelease) {
-      auto e = static_cast<QMouseEvent*>(event);
-      if(e->button() != Qt::LeftButton) {
-        return false;
-      }
-      m_is_dragging = false;
-    } else if(event->type() == QEvent::MouseMove) {
-      auto e = static_cast<QMouseEvent*>(event);
-      if(!m_is_dragging) {
-        return false;
-      }
-      auto delta = e->globalPos();
-      delta -= m_last_mouse_pos;
-      auto window_pos = window()->pos();
-      window_pos += delta;
-      m_last_mouse_pos = e->globalPos();
-      window()->move(window_pos);
-    }
+void security_input_dialog::mouseMoveEvent(QMouseEvent* event) {
+  auto e = static_cast<QMouseEvent*>(event);
+  if(!m_is_dragging) {
+    return;
   }
-  return QDialog::eventFilter(watched, event);
+  auto delta = e->globalPos();
+  delta -= m_last_mouse_pos;
+  auto window_pos = window()->pos();
+  window_pos += delta;
+  m_last_mouse_pos = e->globalPos();
+  window()->move(window_pos);
+}
+
+void security_input_dialog::mousePressEvent(QMouseEvent* event) {
+  if(m_is_dragging || event->button() != Qt::LeftButton) {
+    return;
+  }
+  m_is_dragging = true;
+  m_last_mouse_pos = event->globalPos();
+}
+
+void security_input_dialog::mouseReleaseEvent(QMouseEvent* event) {
+  auto e = static_cast<QMouseEvent*>(event);
+  if(e->button() != Qt::LeftButton) {
+    return;
+  }
+  m_is_dragging = false;
 }
 
 void security_input_dialog::set_security(const Security& security) {
