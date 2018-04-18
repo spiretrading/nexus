24aa47b2843166137d350d63567a3d06f4af1ebc
Ken <ken@spiretrading.com>
2018-04-18 11:32:09 -0400

    Removed redundant code

--- Applications/spire/source/toolbar/toolbar_window.cpp
+++ Applications/spire/source/toolbar/toolbar_window.cpp
@@ -32,21 +32,13 @@ toolbar_window::toolbar_window(recently_closed_model& model,
   auto layout = new QVBoxLayout(m_body);
   layout->setContentsMargins({});
   layout->setSpacing(0);
-  auto title_bar_layout = new QHBoxLayout();
-  title_bar_layout->setContentsMargins({});
-  title_bar_layout->setSpacing(0);
-  layout->addLayout(title_bar_layout);
   window()->setWindowTitle(tr("Spire - Signed in as ") +
     QString::fromStdString(account.m_name));
-  auto input_layout = new QVBoxLayout();
-  input_layout->setContentsMargins({});
-  input_layout->setSpacing(0);
-  layout->addLayout(input_layout);
   auto combo_box_layout = new QHBoxLayout();
   combo_box_layout->setContentsMargins(scale_width(8), scale_height(8),
     scale_width(8), scale_height(5));
   combo_box_layout->setSpacing(0);
-  input_layout->addLayout(combo_box_layout);
+  layout->addLayout(combo_box_layout);
   m_window_manager_button = new toolbar_menu(tr("Window Manager"), m_body);
   m_window_manager_button->setFixedSize(scale(138, 26));
   m_window_manager_button->add(tr("Minimize All"));
@@ -65,7 +57,6 @@ toolbar_window::toolbar_window(recently_closed_model& model,
   button_layout->setContentsMargins(scale_width(8), scale_height(5),
     scale_width(8), scale_height(8));
   button_layout->setSpacing(scale_width(14));
-  input_layout->addLayout(button_layout);
   auto window_button_size = scale(20, 20);
   m_account_button = new icon_button(
     imageFromSvg(":/icons/account-light-purple.svg", window_button_size),
@@ -117,6 +108,7 @@ toolbar_window::toolbar_window(recently_closed_model& model,
     imageFromSvg(":/icons/blotter-purple.svg", window_button_size), m_body);
   m_blotter_button->setToolTip(tr("Blotter"));
   button_layout->addWidget(m_blotter_button);
+  layout->addLayout(button_layout);
 
   // TODO: GCC workaround
   m_model->connect_entry_added_signal(
