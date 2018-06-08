26567a25046c3ce61130c2d7466076b84b81b452
Ken <ken@spiretrading.com>
2018-06-08 13:34:46 -0400

    Reverted ostream constructor

--- Applications/spire/source/time_and_sales/time_and_sales_window.cpp
+++ Applications/spire/source/time_and_sales/time_and_sales_window.cpp
@@ -376,8 +376,10 @@ void time_and_sales_window::export_table() {
     QStandardPaths::writableLocation(QStandardPaths::DocumentsLocation) +
     tr("/time_and_sales"), tr("CSV (*.csv)"));
   if(!filepath.isNull()) {
-    std::ostream stream(std::filebuf().open(filepath.toStdString(),
-      std::ios_base::out | std::ios_base::trunc));
+    std::filebuf buf;
+    buf.open(filepath.toStdString(), std::ios_base::out |
+      std::ios_base::trunc);
+    std::ostream stream(&buf);
     export_model_as_csv(m_model.get(), stream);
   }
   m_overlay_widget->hide();
