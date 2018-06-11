acdf15cd4cbaffa86efd80a041ca2b903e3bd83b
Ken <ken@spiretrading.com>
2018-06-11 15:22:36 -0400

    Added loading_widget class

---
+++ Applications/spire/source/time_and_sales/loading_widget.cpp
@@ -0,0 +1,9 @@
+#include "spire/time_and_sales/loading_widget.hpp"
+#include <QVBoxLayout>
+
+using namespace spire;
+
+loading_widget::loading_widget(QWidget* parent)
+    : QWidget(parent) {
+  
+}
