2ecdda38702f78715dabeca2d75a01e30b86f863
nicola-spire <nicola@spiretrading.com>
2018-06-12 16:42:26 -0400

    began page

--- Applications/web_portal/web_app/library/source/pages/login_page/login_page.tsx
+++ Applications/web_portal/web_app/library/source/pages/login_page/login_page.tsx
@@ -11,15 +11,46 @@ export interface Properties {
   onLogin?: (account: Beam.DirectoryEntry) => void;
 }
 
-export interface State {}
+export interface State {
+  username: string;
+  password: string;
+};
 
 /** Displays the login page. */
 export class LoginPage extends React.Component<Properties, State> {
   constructor(properties: Properties) {
     super(properties);
+    this.state = {
+      username: "",
+      password: ""
+    };
   }
 
   public render(): JSX.Element {
-    return <div>Login Page</div>;
+    return (
+        <div>
+          <input type='text' value={this.state.username}
+                 placeholder='Username'
+                 onChange={(event: any) => {
+                   this.setState({username: event.target.value})
+                 }}/>
+          <input type='text' value={this.state.password}
+                 placeholder='Password'
+                 onChange={(event: any) => {
+                   this.setState({password: event.target.value})
+                 }}/>
+          <button onClick={() => {
+            const login = this.props.model.login.bind(this.props.model)
+            login(this.state.username, this.state.password).
+              then((account: Beam.DirectoryEntry) => {
+                if(this.props.onLogin) {
+                  this.props.onLogin(account)
+                }
+              });
+          }}>
+            Sign In
+          </button>
+        </div>
+      );
   }
 }
--- Applications/web_portal/web_app/tests/login_page_tester/source/index.tsx
+++ Applications/web_portal/web_app/tests/login_page_tester/source/index.tsx
@@ -1,8 +1,11 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import * as WebPortal from 'web_portal';
+import * as Beam from 'beam';
 
 const model = new WebPortal.LocalLoginPageModel();
-
-ReactDOM.render(<WebPortal.LoginPage model={model}/>,
+const onLogin = (account: Beam.DirectoryEntry) => {
+	console.log(account)
+}
+ReactDOM.render(<WebPortal.LoginPage model={model} onLogin={onLogin}/>,
   document.getElementById('main'));
