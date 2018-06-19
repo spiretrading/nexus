2496ba0992875f1af3b71c508f44f7bd3dd168ee
nicola-spire <nicola@spiretrading.com>
2018-06-19 13:06:13 -0400

    indentation

--- Applications/web_portal/web_app/library/source/components/burger_button.tsx
+++ Applications/web_portal/web_app/library/source/components/burger_button.tsx
@@ -77,18 +77,18 @@ export class BurgerButton extends React.Component<Properties> {
   }
   public render(): JSX.Element {
     return (
-        <button onClick={this.onClick}
-                className={css(this.buttonStyle.unHighlighted)}
-                ref={(ref) => this.button = ref}>
-          <svg className={css(this.iconStyle.unHighlighted)}
-               ref={(ref) => this.icon = ref}>
-            <g>
-              <rect y='0' width='20px' height='2'/>
-              <rect y='6' width='20px' height='2'/>
-              <rect y='12' width='20px' height='2'/>
-          </g>
-        </svg>
-      </button>);
+      <button onClick={this.onClick}
+              className={css(this.buttonStyle.unHighlighted)}
+              ref={(ref) => this.button = ref}>
+        <svg className={css(this.iconStyle.unHighlighted)}
+             ref={(ref) => this.icon = ref}>
+          <g>
+            <rect y='0' width='20px' height='2'/>
+            <rect y='6' width='20px' height='2'/>
+            <rect y='12' width='20px' height='2'/>
+        </g>
+      </svg>
+    </button>);
   }
   private onClick() {
     if(this.button.className === css(this.buttonStyle.unHighlighted)) {
