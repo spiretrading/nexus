cb83fd21fba968de38da1a28c9fdc36486c95431
nicola-spire <nicola@spiretrading.com>
2018-06-21 16:29:58 -0400

    made changes

--- Applications/web_portal/web_app/library/source/pages/dashboard_page/dashboard_page.tsx
+++ Applications/web_portal/web_app/library/source/pages/dashboard_page/dashboard_page.tsx
@@ -19,42 +19,43 @@ export class DashboardPage extends React.Component<Properties, State> {
       <VBoxLayout width='100%' height='100%'>
         <div className={css(DashboardPage.STYLE.header)}>
           <HBoxLayout className={css(DashboardPage.STYLE.leftHeader)}
-              width='136px' height='75px'>
+              width='136px' height='60px'>
             <Padding size='18px'/>
-            <VBoxLayout height='75px'>
+            <VBoxLayout height='60px'>
               <Padding/>
-              <VBoxLayout height='30px'>
-                <Padding size='calc(50% - 15px)'/>
-                <BurgerButton width='20px' height='15px'
+              <div className={css(DashboardPage.STYLE.burgerButtonWrapper)}>
+                <BurgerButton width='20px' height='14px'
                   color='#684BC7' highlightColor='#684BC7'/>
-                <Padding size='calc(50% - 15px)'/>
-              </VBoxLayout>
+              </div>
               <Padding/>
             </VBoxLayout>
             <Padding size='20px'/>
-            <VBoxLayout height='75px'>
-              <Padding size='calc(50% - 15px)'/>
-              <img src='resources/dashboard/logo.png'
-                className={css(DashboardPage.STYLE.logo)}/>
-              <Padding size='calc(50% - 15px)'/>
+            <VBoxLayout height='60px'>
+              <Padding/>
+              <div className={css(DashboardPage.STYLE.logo)}>
+                <img src='resources/dashboard/logo.png'
+                  className={css(DashboardPage.STYLE.logo)}/>
+              </div>
+              <Padding/>
             </VBoxLayout>
           </HBoxLayout>
-          <VBoxLayout height='75px' width='45px'
+          <VBoxLayout height='60px' width='45px'
               className={css(DashboardPage.STYLE.rightHeader)}>
-            <Padding size='calc(50% - 15px)'/>
-            <NotificationButton width='15px' height='20px'/>
-            <Padding size='calc(50% - 15px)'/>
+            <Padding/>
+            <div className={css(DashboardPage.STYLE.notificationButtonWrapper)}>
+              <NotificationButton width='15px' height='20px'/>
+            </div>
+            <Padding/>
           </VBoxLayout>
         </div>
+        <div className={css(DashboardPage.STYLE.separator)}/>
       </VBoxLayout>);
   }
   private static STYLE = StyleSheet.create({
     header: {
       width: '100%',
-      height: '75px',
-      backgroundColor: 'rgba(255, 255, 255, 0)',
-      boxShadow: '0 1px 1px -1px rgba(0, 0, 0, 0.16)',
-      '-moz-box-shadow': '0 1px 1px -1px rgba(0, 0, 0, 0.16)'
+      height: '60px',
+      backgroundColor: 'rgba(255, 255, 255, 0)'
     },
     leftHeader: {
       position: 'absolute' as 'absolute',
@@ -66,9 +67,22 @@ export class DashboardPage extends React.Component<Properties, State> {
       top: 0,
       right: 0
     },
+    burgerButtonWrapper: {
+      width: '20px',
+      height: '14px'
+    },
     logo: {
       width: '78px',
       height: '30px'
+    },
+    notificationButtonWrapper: {
+      width: '15px',
+      height: '20px'
+    },
+    separator: {
+      width: '100%',
+      height: '1px',
+      backgroundColor: 'rgba(0, 0, 0, 0.16)'
     }
   });
 }
