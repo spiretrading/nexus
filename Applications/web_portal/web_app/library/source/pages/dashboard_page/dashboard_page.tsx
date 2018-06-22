e9c2613015fbd2ac2997d96c3e2e953b98526d6e
nicola-spire <nicola@spiretrading.com>
2018-06-22 08:43:53 -0400

    styling changes

--- Applications/web_portal/web_app/library/source/pages/dashboard_page/dashboard_page.tsx
+++ Applications/web_portal/web_app/library/source/pages/dashboard_page/dashboard_page.tsx
@@ -17,67 +17,38 @@ export class DashboardPage extends React.Component<Properties, State> {
   public render(): JSX.Element {
     return (
       <VBoxLayout width='100%' height='100%'>
-        <div className={css(DashboardPage.STYLE.header)}>
-          <HBoxLayout className={css(DashboardPage.STYLE.leftHeader)}
-              width='136px' height='60px'>
+        <HBoxLayout width='100%' height='60px'
+            className={css(DashboardPage.STYLE.header)}>
+          <HBoxLayout width='136px' height='60px'>
             <Padding size='18px'/>
             <VBoxLayout height='60px'>
-              <Padding/>
-              <div className={css(DashboardPage.STYLE.burgerButtonWrapper)}>
-                <BurgerButton width='20px' height='14px'
-                  color='#684BC7' highlightColor='#684BC7'/>
-              </div>
-              <Padding/>
+              <Padding size='23px'/>
+              <BurgerButton width='20px' height='14px' color='#684BC7'
+                highlightColor='#684BC7'/>
+              <Padding size='23px'/>
             </VBoxLayout>
             <Padding size='20px'/>
             <VBoxLayout height='60px'>
-              <Padding/>
-              <div className={css(DashboardPage.STYLE.logo)}>
-                <img src='resources/dashboard/logo.png'
-                  className={css(DashboardPage.STYLE.logo)}/>
-              </div>
-              <Padding/>
+              <Padding size='15px'/>
+              <img src='resources/dashboard/logo.png' width='78px'
+                height='30px'/>
+              <Padding size='15px'/>
             </VBoxLayout>
           </HBoxLayout>
-          <VBoxLayout height='60px' width='45px'
-              className={css(DashboardPage.STYLE.rightHeader)}>
-            <Padding/>
-            <div className={css(DashboardPage.STYLE.notificationButtonWrapper)}>
-              <NotificationButton width='15px' height='20px'/>
-            </div>
+          <Padding/>
+          <VBoxLayout height='60px' width='45px'>
+            <Padding size='20px'/>
+            <NotificationButton items={0} isOpen={false}/>
+            <Padding size='20px'/>
             <Padding/>
           </VBoxLayout>
-        </div>
+        </HBoxLayout>
         <div className={css(DashboardPage.STYLE.separator)}/>
       </VBoxLayout>);
   }
   private static STYLE = StyleSheet.create({
     header: {
-      width: '100%',
-      height: '60px',
       backgroundColor: 'rgba(255, 255, 255, 0)'
-    },
-    leftHeader: {
-      position: 'absolute' as 'absolute',
-      top: 0,
-      left: 0
-    },
-    rightHeader: {
-      position: 'absolute' as 'absolute',
-      top: 0,
-      right: 0
-    },
-    burgerButtonWrapper: {
-      width: '20px',
-      height: '14px'
-    },
-    logo: {
-      width: '78px',
-      height: '30px'
-    },
-    notificationButtonWrapper: {
-      width: '15px',
-      height: '20px'
     },
     separator: {
       width: '100%',
--- Applications/web_portal/web_app/library/source/pages/dashboard_page/notification_button.tsx
+++ Applications/web_portal/web_app/library/source/pages/dashboard_page/notification_button.tsx
@@ -4,46 +4,36 @@ import * as React from 'react';
 /** The properties used to render a NotificationButton. */
 export interface Properties {
 
-  /** The width of the button. */
-  width: number | string;
+  /** The number of notification items. */
+  items: number;
 
-  /** The height of the button. */
-  height: number | string;
-
-  /** The onClick event handler. */
-  onClick?: () => void;
+  /** Whether the button is in the open state (being viewed). */
+  isOpen: boolean;
 }
 
 /** Displays a notification button. */
 export class NotificationButton extends React.Component<Properties> {
   public constructor(props: Properties) {
     super(props);
-    this.onClick = this.onClick.bind(this);
   }
   public render(): JSX.Element {
     const STYLE = StyleSheet.create({
       button: {
+        width: '15px',
+        height: '20px',
         border: 'none',
         outline: 0,
         padding: 0,
         backgroundColor: 'rgba(0, 0, 0, 0)',
-        width: this.props.width,
-        height: this.props.height,
         ':hover': {
           cursor: 'pointer'
         }
       }
     });
     return (
-      <button className={css(STYLE.button)} onClick={this.onClick}>
-        <img width={this.props.width} height={this.props.height}
+      <button className={css(STYLE.button)}>
+        <img width='15px' height='20px'
           src='resources/dashboard/notification.svg'/>
       </button>);
-  }
-
-  private onClick() {
-    if(this.props.onClick) {
-      this.props.onClick();
-    }
   }
 }
