18b47afa3a40d395323d138b65b9ec89e0107d81
Ken <ken@spiretrading.com>
2018-06-22 14:48:42 -0400

    Added scroll bar mouse hover hide/show

--- Applications/spire/source/scratch/main.cpp
+++ Applications/spire/source/scratch/main.cpp
@@ -46,6 +46,7 @@ class time_and_sales_table_view : public QScrollArea {
     void set_properties(const time_and_sales_properties& properties);
 
   protected:
+    bool event(QEvent* event) override;
     void resizeEvent(QResizeEvent* event) override;
     void wheelEvent(QWheelEvent* event) override;
 
@@ -58,8 +59,11 @@ class time_and_sales_table_view : public QScrollArea {
 
     void fade_out_horizontal_scroll_bar();
     void fade_out_vertical_scroll_bar();
+    bool within_horizontal_scroll_bar(const QPoint& pos);
+    bool within_vertical_scroll_bar(const QPoint& pos);
+    int true_height();
     void on_header_resize(int index, int old_size, int new_size);
-    void on_header_swap(int logical_index, int old_index, int new_index);
+    void on_header_move(int logical_index, int old_index, int new_index);
     void on_horizontal_slider_value_changed(int new_value);
     void on_rows_about_to_be_inserted();
 };
@@ -106,6 +110,8 @@ time_and_sales_table_view::time_and_sales_table_view(
       width: 0px;
     })").arg(scale_height(12)).arg(scale_height(12))
         .arg(scale_width(30)).arg(scale_height(30)));
+  setMouseTracking(true);
+  setAttribute(Qt::WA_Hover);
   setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
   setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
   connect(horizontalScrollBar(), &QScrollBar::valueChanged, this,
@@ -137,7 +143,7 @@ time_and_sales_table_view::time_and_sales_table_view(
   connect(m_header, &QHeaderView::sectionResized, this,
     &time_and_sales_table_view::on_header_resize);
   connect(m_header, &QHeaderView::sectionMoved, this,
-    &time_and_sales_table_view::on_header_swap);
+    &time_and_sales_table_view::on_header_move);
   auto main_widget = new QWidget(this);
   main_widget->setMinimumWidth(MINIMUM_TABLE_WIDTH);
   auto layout = new QVBoxLayout(main_widget);
@@ -201,17 +207,43 @@ void time_and_sales_table_view::set_properties(
   viewport()->update();
 }
 
+bool time_and_sales_table_view::event(QEvent* event) {
+  if(event->type() == QEvent::HoverMove) {
+    auto e = static_cast<QHoverEvent*>(event);
+    if(within_horizontal_scroll_bar(e->pos())) {
+      setHorizontalScrollBarPolicy(Qt::ScrollBarAsNeeded);
+      setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    } else if(within_vertical_scroll_bar(e->pos())) {
+      setVerticalScrollBarPolicy(Qt::ScrollBarAsNeeded);
+      setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+      m_v_scroll_bar_timer.start();
+    } else {
+      if(!m_v_scroll_bar_timer.isActive() &&
+          verticalScrollBarPolicy() != Qt::ScrollBarAlwaysOff) {
+        fade_out_vertical_scroll_bar();
+      }
+      if(!m_h_scroll_bar_timer.isActive() &&
+          horizontalScrollBarPolicy() != Qt::ScrollBarAlwaysOff) {
+        fade_out_horizontal_scroll_bar();
+      }
+    }
+  }
+  return QScrollArea::event(event);
+}
+
 void time_and_sales_table_view::resizeEvent(QResizeEvent* event) {
   m_header->setFixedWidth(m_table->width());
 }
 
 void time_and_sales_table_view::wheelEvent(QWheelEvent* event) {
   if(event->modifiers() & Qt::ShiftModifier) {
+    setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
     setHorizontalScrollBarPolicy(Qt::ScrollBarAsNeeded);
     horizontalScrollBar()->setValue(horizontalScrollBar()->value() -
       (event->delta() / 2));
     m_h_scroll_bar_timer.start();
   } else if(!event->modifiers().testFlag(Qt::ShiftModifier)) {
+    setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
     setVerticalScrollBarPolicy(Qt::ScrollBarAsNeeded);
     verticalScrollBar()->setValue(verticalScrollBar()->value() -
       (event->delta() / 2));
@@ -229,13 +261,28 @@ void time_and_sales_table_view::fade_out_vertical_scroll_bar() {
   setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
 }
 
+int time_and_sales_table_view::true_height() {
+  return visibleRegion().boundingRect().height() +
+    m_table->visibleRegion().boundingRect().height() +
+    m_header->height();
+}
+
+bool time_and_sales_table_view::within_horizontal_scroll_bar(
+    const QPoint& pos) {
+  return pos.y() > true_height() -  horizontalScrollBar()->height();
+}
+
+bool time_and_sales_table_view::within_vertical_scroll_bar(const QPoint& pos) {
+  return pos.x() > width() - verticalScrollBar()->width();
+}
+
 void time_and_sales_table_view::on_header_resize(int index, int old_size,
     int new_size) {
   m_table->horizontalHeader()->resizeSection(index,
     m_header->sectionSize(index));
 }
 
-void time_and_sales_table_view::on_header_swap(int logical_index,
+void time_and_sales_table_view::on_header_move(int logical_index,
     int old_index, int new_index) {
   m_table->horizontalHeader()->moveSection(old_index, new_index);
 }
