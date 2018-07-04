d5436957b6d1f687c2127d4ed7da966a997a0024
Ken <ken@spiretrading.com>
2018-07-04 14:44:02 -0400

    Implemented basic dialog window

--- Applications/spire/source/book_view/book_view_properties_dialog.cpp
+++ Applications/spire/source/book_view/book_view_properties_dialog.cpp
@@ -1,4 +1,7 @@
 #include "spire/book_view/book_view_properties_dialog.hpp"
+#include <QHBoxLayout>
+#include "spire/spire/dimensions.hpp"
+#include "spire/ui/window.hpp"
 
 using namespace boost;
 using namespace boost::signals2;
@@ -9,7 +12,20 @@ book_view_properties_dialog::book_view_properties_dialog(
     const book_view_properties& properties, const Security& security,
     QWidget* parent, Qt::WindowFlags flags)
     : QDialog(parent, flags | Qt::Window | Qt::FramelessWindowHint |
-        Qt::WindowCloseButtonHint) {}
+        Qt::WindowCloseButtonHint) {
+  auto body = new QWidget(this);
+  body->setFixedSize(scale(492, 394));
+  body->setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);
+  body->setStyleSheet("background-color: #F5F5F5;");
+  auto window_layout = new QHBoxLayout(this);
+  window_layout->setContentsMargins({});
+  auto window = new spire::window(body, this);
+  setWindowTitle(tr("Properties"));
+  window->set_svg_icon(":/icons/bookview-black.svg",
+    ":/icons/bookview-grey.svg");
+  window_layout->addWidget(window);
+  auto layout = new QHBoxLayout(body);
+}
 
 book_view_properties book_view_properties_dialog::get_properties() const {
   return m_properties;
