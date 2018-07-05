56439b068a68c81edd55a45b7b2b014fa48c5cd5
nicola-spire <nicola@spiretrading.com>
2018-07-05 15:54:53 -0400

    linted, need to work on responsive aspect

--- Applications/web_portal/web_app/library/source/pages/account_page/account_page.tsx
+++ Applications/web_portal/web_app/library/source/pages/account_page/account_page.tsx
@@ -54,34 +54,83 @@ export class AccountPage extends React.Component<Properties, State> {
   }
 
   public render(): JSX.Element {
+    const accountHeaderClassName = ((): string => {
+      if(this.state.breakpoint === Breakpoint.SMALL) {
+        return css([AccountPage.ACCOUNT_HEADER_STYLE.base,
+          AccountPage.ACCOUNT_HEADER_STYLE.small]);
+      }
+      return css(AccountPage.ACCOUNT_HEADER_STYLE.base);
+    })();
+    const AccountHeaderPadding = (): JSX.Element => {
+      if(this.state.breakpoint === Breakpoint.SMALL) {
+        return <Padding/>;
+      }
+      return <Padding size='30px'/>;
+    };
+    const accountHeaderWidth = (() => {
+      if(this.state.breakpoint === Breakpoint.SMALL) {
+        return '100%';
+      }
+      return '786px';
+    })();
+    const menuIconContainerClassName = (() => {
+      if(this.state.breakpoint === Breakpoint.SMALL) {
+        return css(AccountPage.ACCOUNT_HEADER_STYLE.smallMenuIconContainer);
+      }
+    })();
     return (
       <VBoxLayout width='100%' height='100%'>
-        <HBoxLayout height='40px' width='100%'
+        <HBoxLayout width='100%' height='40px'
             className={css(AccountPage.STYLE.underlinedBox)}>
-          <Padding size='18px'/>
-          <HBoxLayout>
-            <MenuItem isSelected={false} src='resources/account/account-grey.svg'
-              label='Account' breakpoint={this.state.breakpoint}/>
-            <Padding size='14px'/>
-            <MenuItem isSelected={false}
-              src='resources/account/risk-controls-grey.svg'
-              label='Risk Controls' breakpoint={this.state.breakpoint}/>
-            <Padding size='14px'/>
-            <MenuItem isSelected={false}
-              src='resources/account/entitlements-grey.svg'
-              label='Entitlements' breakpoint={this.state.breakpoint}/>
-            <Padding size='14px'/>
-            <MenuItem isSelected={false}
-              src='resources/account/compliance-grey.svg'
-              label='Compliance' breakpoint={this.state.breakpoint}/>
-            <Padding size='14px'/>
-            <MenuItem isSelected={false}
-              src='resources/account/profit-loss-grey.svg'
-              label='Entitlements' breakpoint={this.state.breakpoint}/>
+          <Padding/>
+          <HBoxLayout width={accountHeaderWidth}
+              className={accountHeaderClassName}>
+            <Padding size='18px'/>
+            <HBoxLayout className={menuIconContainerClassName}>
+              <MenuItem isSelected={false}
+                src='resources/account/account-grey.svg' label='Account'
+                breakpoint={this.state.breakpoint}/>
+              <AccountHeaderPadding/>
+              <MenuItem isSelected={false}
+                src='resources/account/risk-controls-grey.svg'
+                label='Risk Controls' breakpoint={this.state.breakpoint}/>
+              <AccountHeaderPadding/>
+              <MenuItem isSelected={false}
+                src='resources/account/entitlements-grey.svg'
+                label='Entitlements' breakpoint={this.state.breakpoint}/>
+              <AccountHeaderPadding/>
+              <MenuItem isSelected={false}
+                src='resources/account/compliance-grey.svg' label='Compliance'
+                breakpoint={this.state.breakpoint}/>
+              <AccountHeaderPadding/>
+              <MenuItem isSelected={false}
+                src='resources/account/profit-loss-grey.svg' label='Profit/Loss'
+                breakpoint={this.state.breakpoint}/>
+            </HBoxLayout>
+            <Padding/>
+            <VBoxLayout width='68px' height='40px'>
+              <Padding/>
+              <HBoxLayout width='68px' height='14px'>
+                <img src='resources/account/trader-grey.svg' width='14px'
+                  height='14px'/>
+                <Padding size='4px'/>
+                <img src='resources/account/manager-grey.svg' width='14px'
+                  height='14px'/>
+                <Padding size='4px'/>
+                <img src='resources/account/admin-grey.svg' width='14px'
+                  height='14px'/>
+                <Padding size='4px'/>
+                <img src='resources/account/service-grey.svg' width='14px'
+                  height='14px'/>
+              </HBoxLayout>
+              <Padding/>
+            </VBoxLayout>
+            <Padding size='18px'/>
           </HBoxLayout>
+          <Padding/>
         </HBoxLayout>
         <HBoxLayout height='30px' width='100%'
-            className={css(AccountPage.STYLE.underlinedBox)}>
+          className={css(AccountPage.STYLE.underlinedBox)}>
         </HBoxLayout>
       </VBoxLayout>
       );
@@ -106,7 +155,21 @@ export class AccountPage extends React.Component<Properties, State> {
       this.setState({breakpoint: newBreakpoint});
     }
   }
-
+  private static ACCOUNT_HEADER_STYLE = StyleSheet.create({
+    base: {
+      height: '40px'
+    },
+    small: {
+      width: '100%',
+      minWidth: '320px',
+      maxWidth: '460px'
+    },
+    smallMenuIconContainer: {
+      width: 'calc(90% - 104px)',
+      maxWidth: '240px',
+      minWidth: '176px'
+    }
+  });
   private static STYLE = StyleSheet.create({
     underlinedBox: {
       '-webkit-box-shadow': '0px 1px 1px 0px rgba(149,152,154,0.16)',
@@ -117,7 +180,7 @@ export class AccountPage extends React.Component<Properties, State> {
 }
 
 class MenuItem extends React.Component<MenuItemProperties> {
-  render(): JSX.Element {
+  public render(): JSX.Element {
     if(this.props.breakpoint === Breakpoint.SMALL) {
       return (
       <VBoxLayout height='40px'>
@@ -127,23 +190,27 @@ class MenuItem extends React.Component<MenuItemProperties> {
       </VBoxLayout>);
     }
     return (
-      <HBoxLayout>
-        <VBoxLayout height='40px'>
+      <VBoxLayout height='40px'>
+        <Padding/>
+        <HBoxLayout height='16px'>
+          <img src={this.props.src} width='16px' height='16px'/>
           <Padding size='8px'/>
-          <img src={this.props.src} width='24px' height='24px'/>
-          <Padding size='8px'/>
-        </VBoxLayout>
-        <Padding size='8px'/>
-        <VBoxLayout height='40px'>
-          <Padding/>
-          <span className={css(MenuItem.STYLE.label)}>{this.props.label}</span>
-          <Padding/>
-        </VBoxLayout>
-      </HBoxLayout>);
+          <VBoxLayout height='16px'>
+            <Padding/>
+            <span className={css(MenuItem.STYLE.label)}>
+              {this.props.label}
+            </span>
+            <Padding/>
+          </VBoxLayout>
+        </HBoxLayout>
+        <Padding/>
+      </VBoxLayout>);
   }
+
   private static STYLE = StyleSheet.create({
     label: {
-     font: '200 14px Roboto'
+      font: '200 14px Roboto',
+      whiteSpace: 'nowrap'
     }
   });
 }
