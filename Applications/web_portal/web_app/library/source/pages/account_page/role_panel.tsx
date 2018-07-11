43bc2ad5ec28fd58ae19f20744a5fa57b41c1d4e
nicola-spire <nicola@spiretrading.com>
2018-07-11 11:23:27 -0400

    added icon change for roles

--- Applications/web_portal/web_app/library/source/pages/account_page/role_panel.tsx
+++ Applications/web_portal/web_app/library/source/pages/account_page/role_panel.tsx
@@ -10,20 +10,35 @@ interface Properties {
 /** Displays a panel of icons highlighting an account's roles. */
 export class RolePanel extends React.Component<Properties> {
   public render(): JSX.Element {
+    const getIconColour = (role: Nexus.AccountRoles.Role) => {
+      if(this.props.roles.isSet(role)) {
+        return 'purple';
+      }
+      return 'grey';
+    };
     return (
       <VBoxLayout width='68px' height='40px'>
         <Padding/>
         <HBoxLayout width='68px' height='14px'>
-          <img src='resources/account/trader-grey.svg' width='14px'
+          <img src={
+            `resources/account/trader-${
+              getIconColour(Nexus.AccountRoles.Role.TRADER)}.svg`}
+            width='14px'
             height='14px'/>
           <Padding size='4px'/>
-          <img src='resources/account/manager-grey.svg' width='14px'
+          <img src={`resources/account/manager-${
+              getIconColour(Nexus.AccountRoles.Role.MANAGER)}.svg`}
+            width='14px'
             height='14px'/>
           <Padding size='4px'/>
-          <img src='resources/account/admin-grey.svg' width='14px'
+          <img src={`resources/account/admin-${
+              getIconColour(Nexus.AccountRoles.Role.ADMINISTRATOR)}.svg`}
+            width='14px'
             height='14px'/>
           <Padding size='4px'/>
-          <img src='resources/account/service-grey.svg' width='14px'
+          <img src={`resources/account/service-${
+              getIconColour(Nexus.AccountRoles.Role.SERVICE)}.svg`}
+            width='14px'
             height='14px'/>
         </HBoxLayout>
         <Padding/>
