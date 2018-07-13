d3b1008ee9d5042748e32e9ba585649406e63a79
Ken <ken@spiretrading.com>
2018-07-13 10:39:07 -0400

    Cleaned out scratch project

--- Applications/spire/source/scratch/main.cpp
+++ Applications/spire/source/scratch/main.cpp
@@ -1,7 +1,5 @@
 #include <QApplication>
-#include <QTimer>
 #include "spire/spire/resources.hpp"
-#include "spire/ui/flat_button.hpp"
 
 using namespace spire;
 
@@ -10,18 +8,5 @@ int main(int argc, char** argv) {
   application->setOrganizationName(QObject::tr("Eidolon Systems Ltd"));
   application->setApplicationName(QObject::tr("Scratch"));
   initialize_resources();
-  auto button = new flat_button("Test Button");
-  auto s = button->get_style();
-  s.m_background_color = QColor("#FFFFFF");
-  s.m_text_color = Qt::red;
-  s.m_text_size = 40;
-  s.m_font_weight = QFont::Black;
-  button->set_style(s);
-  auto s2 = button->get_disabled_style();
-  s2.m_background_color = QColor("#0000FF");
-  button->set_disabled_style(s2);
-  button->resize(400, 250);
-  button->setDisabled(true);
-  button->show();
   application->exec();
 }
