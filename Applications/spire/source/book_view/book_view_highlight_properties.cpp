74012e777d4de70058ce56add3e533fa02b8ac05
Ken <ken@spiretrading.com>
2018-07-16 16:44:34 -0400

    Undid changes to highlights tab widget

--- Applications/spire/source/book_view/book_view_highlight_properties.cpp
+++ Applications/spire/source/book_view/book_view_highlight_properties.cpp
@@ -1,14 +1,10 @@
 #include "spire/book_view/book_view_highlight_properties_widget.hpp"
-#include <QHBoxLayout>
-#include <QVBoxLayout>
 
 using namespace spire;
 
 book_view_highlight_properties_widget::book_view_highlight_properties_widget(
     const book_view_properties& properties, QWidget* parent)
-    : QWidget(parent) {
-  auto layout = new QHBoxLayout(this);
-}
+    : QWidget(parent) {}
 
 void book_view_highlight_properties_widget::apply(
     book_view_properties& properties) const {
