be422500fb26bebd529c04e0b8c7fa04a80d69d3
nicola-spire <nicola@spiretrading.com>
2018-07-17 16:25:08 -0400

    linted

--- Applications/web_portal/web_app/library/source/pages/account_page/risk_page/risk_parameters_view.tsx
+++ Applications/web_portal/web_app/library/source/pages/account_page/risk_page/risk_parameters_view.tsx
@@ -2,8 +2,8 @@ import {css, StyleSheet} from 'aphrodite';
 import * as Beam from 'beam';
 import * as Nexus from 'nexus';
 import * as React from 'react';
-import {Center, CurrencySelectionBox, HBoxLayout, MoneyInputBox, Padding, VBoxLayout}
-	from '../../..';
+import {Center, CurrencySelectionBox, HBoxLayout, MoneyInputBox, Padding,
+  VBoxLayout} from '../../..';
 
 interface Properties {
 
@@ -27,16 +27,16 @@ interface Properties {
 }
 
 interface FooterProperties {
-	isAdmin: boolean;
+  isAdmin: boolean;
 }
 
 interface MoneyInputBoxProperties {
-	label: string;
-	onChange: (value: number) => void;
+  label: string;
+  onChange: (value: number) => void;
 }
 
 interface State {
-	breakpoint: Breakpoint;
+  breakpoint: Breakpoint;
 }
 
 enum Breakpoint {
@@ -46,16 +46,16 @@ enum Breakpoint {
 }
 
 interface LabelProperties {
-	text: string;
+  text: string;
 }
 
 interface TransistionTimeProperties {
-	onTransitionTimeUpdate: (duration: number, field: string) => void;
+  onTransitionTimeUpdate: (duration: number, field: string) => void;
 }
 /** Implements a React component to display a set of RiskParameters. */
 export class RiskParametersView extends React.Component<Properties, State> {
 
-	public constructor(props: Properties) {
+  public constructor(props: Properties) {
     super(props);
     this.state = {
       breakpoint: RiskParametersView.getBreakpoint()
@@ -72,44 +72,44 @@ export class RiskParametersView extends React.Component<Properties, State> {
   }
 
   public render(): JSX.Element {
-  	const containerClassName = (() => {
-  		switch(this.state.breakpoint) {
-  			case Breakpoint.SMALL:
-  				return css(RiskParametersView.CONTAINERS.small);
-  			case Breakpoint.MEDIUM:
-  				return css(RiskParametersView.CONTAINERS.medium);
-  			case Breakpoint.LARGE:
-  				return css(RiskParametersView.CONTAINERS.large);
-  			default:
-  				Breakpoint.MEDIUM;
-  		}
-  	})();
+    const containerClassName = (() => {
+      switch(this.state.breakpoint) {
+        case Breakpoint.SMALL:
+          return css(RiskParametersView.CONTAINERS.small);
+        case Breakpoint.MEDIUM:
+          return css(RiskParametersView.CONTAINERS.medium);
+        case Breakpoint.LARGE:
+          return css(RiskParametersView.CONTAINERS.large);
+        default:
+          return css(RiskParametersView.CONTAINERS.medium);
+      }
+    })();
     return (
-    		<HBoxLayout width='100%' height='100%'>
-    			<Padding/>
-    			<VBoxLayout className={containerClassName}>
-    				<Padding size='30px'/>
-    				<VBoxLayout width='246px'>
-    				<Label text='Currency'/>
-    				<Padding size='12px'/>
-    				<CurrencySelectionBox currencyDatabase={
-    					this.props.currencyDatabase} onChange={
-    					this.props.onCurrencyChange}/>
-    				<Padding size='30px'/>
-    				<Label text='Buying Power ($)'/>
-    				<Padding size='12px'/>
-    				<MoneyInputBox
-    					onChange={this.props.onBuyingPowerChange}/>
-    				<Padding size='30px'/>
-    				<Label text='Net Loss ($)'/>
-    				<Padding size='12px'/>
-    				<MoneyInputBox
-    					onChange={this.props.onNetLossChange}/>
-    				</VBoxLayout>
-    			</VBoxLayout>
-    			<Padding/>
-    		</HBoxLayout>
-    	);
+        <HBoxLayout width='100%' height='100%'>
+          <Padding/>
+          <VBoxLayout className={containerClassName}>
+            <Padding size='30px'/>
+            <VBoxLayout width='246px'>
+            <Label text='Currency'/>
+            <Padding size='12px'/>
+            <CurrencySelectionBox currencyDatabase={
+              this.props.currencyDatabase} onChange={
+              this.props.onCurrencyChange}/>
+            <Padding size='30px'/>
+            <Label text='Buying Power ($)'/>
+            <Padding size='12px'/>
+            <MoneyInputBox
+              onChange={this.props.onBuyingPowerChange}/>
+            <Padding size='30px'/>
+            <Label text='Net Loss ($)'/>
+            <Padding size='12px'/>
+            <MoneyInputBox
+              onChange={this.props.onNetLossChange}/>
+            </VBoxLayout>
+          </VBoxLayout>
+          <Padding/>
+        </HBoxLayout>
+      );
   }
 
   private static getBreakpoint(): Breakpoint {
@@ -133,48 +133,49 @@ export class RiskParametersView extends React.Component<Properties, State> {
   }
 
   private static defaultProps = {
-  	onCurrencyChange: (currency: Nexus.Currency) => {},
-	  onBuyingPowerChange: (value: Nexus.Money) => {},
-	  onNetLossChange: (value: Nexus.Money) => {},
-	  onTransitionTimeChange: (duration: number, field: string) => {}
+    onCurrencyChange: (currency: Nexus.Currency) => {},
+    onBuyingPowerChange: (value: Nexus.Money) => {},
+    onNetLossChange: (value: Nexus.Money) => {},
+    onTransitionTimeChange: (duration: number, field: string) => {}
   }
   private static CONTAINERS = StyleSheet.create({
-  	small: {
-  		width: '60%',
-  		minWidth: '320px',
-  		maxWidth: '460px'
-  	},
-  	medium: {
-  		width: '768px'
-  	},
-  	large: {
-  		width: '1036px'
-  	}
+    small: {
+      width: '60%',
+      minWidth: '320px',
+      maxWidth: '460px'
+    },
+    medium: {
+      width: '768px'
+    },
+    large: {
+      width: '1036px'
+    }
   });
 }
 class Label extends React.Component<LabelProperties> {
-	public render(): JSX.Element {
-		return (
-			<HBoxLayout width='100%'>
-				<span className={css(Label.STYLE.text)}>
-					{this.props.text}
-				</span>
-				<Padding/>
-			</HBoxLayout>);
-	}
+  public render(): JSX.Element {
+    return (
+      <HBoxLayout width='100%'>
+        <span className={css(Label.STYLE.text)}>
+          {this.props.text}
+        </span>
+        <Padding/>
+      </HBoxLayout>);
+  }
 
-	private static STYLE = StyleSheet.create({
-  	text: {
-  		font: '400 14px Roboto',
-  		color: '#333333',
-  		whiteSpace: 'nowrap'
-  	}
+  private static STYLE = StyleSheet.create({
+    text: {
+      font: '400 14px Roboto',
+      color: '#333333',
+      whiteSpace: 'nowrap'
+    }
   });
 }
 
 class TransistionTime extends React.Component<TransistionTimeProperties> {
 
 }
+
 class Footer extends React.Component<FooterProperties> {
 
 }
