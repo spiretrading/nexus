f34630e7b497fa252ac230c83203a5140c65a25f
nicola-spire <nicola@spiretrading.com>
2018-07-18 10:56:10 -0400

    happy with layout, need to finish control of transaction time/logic

--- Applications/web_portal/web_app/library/source/pages/account_page/risk_page/risk_parameters_view.tsx
+++ Applications/web_portal/web_app/library/source/pages/account_page/risk_page/risk_parameters_view.tsx
@@ -2,8 +2,8 @@ import {css, StyleSheet} from 'aphrodite';
 import * as Beam from 'beam';
 import * as Nexus from 'nexus';
 import * as React from 'react';
-import {Center, CurrencySelectionBox, HBoxLayout, MoneyInputBox, Padding,
-  VBoxLayout} from '../../..';
+import {Center, CurrencySelectionBox, HBoxLayout, IntegerInputBox, 
+  MoneyInputBox, Padding, VBoxLayout} from '../../..';
 
 interface Properties {
 
@@ -28,6 +28,7 @@ interface Properties {
 
 interface FooterProperties {
   isAdmin: boolean;
+  buttonIsDisabled: boolean;
 }
 
 interface MoneyInputBoxProperties {
@@ -50,8 +51,9 @@ interface LabelProperties {
 }
 
 interface TransistionTimeProperties {
-  onTransitionTimeUpdate: (duration: number, field: string) => void;
+  onTransitionTimeUpdate?: (duration: number, field: string) => void;
 }
+
 /** Implements a React component to display a set of RiskParameters. */
 export class RiskParametersView extends React.Component<Properties, State> {
 
@@ -75,40 +77,71 @@ export class RiskParametersView extends React.Component<Properties, State> {
     const containerClassName = (() => {
       switch(this.state.breakpoint) {
         case Breakpoint.SMALL:
-          return css(RiskParametersView.CONTAINERS.small);
-        case Breakpoint.MEDIUM:
-          return css(RiskParametersView.CONTAINERS.medium);
-        case Breakpoint.LARGE:
-          return css(RiskParametersView.CONTAINERS.large);
+          return css([RiskParametersView.CONTAINER_STYLE.small,
+            RiskParametersView.CONTAINER_STYLE.base]);
         default:
-          return css(RiskParametersView.CONTAINERS.medium);
+          return css(RiskParametersView.CONTAINER_STYLE.base);
+      }
+    })();
+    const containerWidth = (() => {
+      switch(this.state.breakpoint) {
+        case Breakpoint.SMALL:
+          return '60%'
+        case Breakpoint.MEDIUM:
+          return '768px';
+        case Breakpoint.LARGE:
+          return '1036px';
+        default:
+          return '768px';
+      }
+    })();
+    const containerPaddingWidth = (() => {
+      switch(this.state.breakpoint) {
+        case Breakpoint.SMALL:
+          return '20%';
       }
     })();
     return (
-        <HBoxLayout width='100%' height='100%'>
-          <Padding/>
-          <VBoxLayout className={containerClassName}>
-            <Padding size='30px'/>
-            <VBoxLayout width='246px'>
-            <Label text='Currency'/>
-            <Padding size='12px'/>
-            <CurrencySelectionBox currencyDatabase={
-              this.props.currencyDatabase} onChange={
-              this.props.onCurrencyChange}/>
-            <Padding size='30px'/>
-            <Label text='Buying Power ($)'/>
-            <Padding size='12px'/>
-            <MoneyInputBox
-              onChange={this.props.onBuyingPowerChange}/>
-            <Padding size='30px'/>
-            <Label text='Net Loss ($)'/>
-            <Padding size='12px'/>
-            <MoneyInputBox
-              onChange={this.props.onNetLossChange}/>
+        <VBoxLayout width='100%' height='100%'>
+          <Padding size='30px'/>
+          <HBoxLayout width='100%'>
+            <Padding/>
+            <VBoxLayout width={containerWidth} className={containerClassName}>
+              <VBoxLayout width='100%' className={css(RiskParametersView.STYLE.controlsContainer)}>
+                <HBoxLayout width='100%'>
+                  <Padding/>
+                  <VBoxLayout width='246px'>
+                    <Label text='Currency'/>
+                    <Padding size='12px'/>
+                    <CurrencySelectionBox currencyDatabase={
+                      this.props.currencyDatabase} onChange={
+                      this.props.onCurrencyChange}/>
+                    <Padding size='30px'/>
+                    <Label text='Buying Power ($)'/>
+                    <Padding size='12px'/>
+                    <MoneyInputBox
+                      onChange={this.props.onBuyingPowerChange}/>
+                    <Padding size='30px'/>
+                    <Label text='Net Loss ($)'/>
+                    <Padding size='12px'/>
+                    <MoneyInputBox
+                      onChange={this.props.onNetLossChange}/>
+                    <Padding size='30px'/>
+                    <Label text='Transition Time'/>
+                    <Padding size='12px'/>
+                    <TransistionTime/>
+                  </VBoxLayout>
+                  <Padding/>
+                </HBoxLayout>
+                <Padding size='30px'/>
+                <div className={css(RiskParametersView.STYLE.divider)}/>
+                <Padding size='30px'/>
+                <Footer isAdmin={false} buttonIsDisabled={false}/>
+              </VBoxLayout>
             </VBoxLayout>
-          </VBoxLayout>
-          <Padding/>
-        </HBoxLayout>
+            <Padding/>
+          </HBoxLayout>
+        </VBoxLayout>
       );
   }
 
@@ -138,17 +171,23 @@ export class RiskParametersView extends React.Component<Properties, State> {
     onNetLossChange: (value: Nexus.Money) => {},
     onTransitionTimeChange: (duration: number, field: string) => {}
   }
-  private static CONTAINERS = StyleSheet.create({
+  private static CONTAINER_STYLE = StyleSheet.create({
     small: {
-      width: '60%',
       minWidth: '320px',
       maxWidth: '460px'
     },
-    medium: {
-      width: '768px'
+    base: {
+      position: 'relative' as 'relative'
+    }
+  });
+  private static STYLE = StyleSheet.create({
+    divider: {
+      width: '100%',
+      height: '1px',
+      backgroundColor: '#E6E6E6'
     },
-    large: {
-      width: '1036px'
+    controlsContainer: {
+      position: 'absolute' as 'absolute'
     }
   });
 }
@@ -173,9 +212,112 @@ class Label extends React.Component<LabelProperties> {
 }
 
 class TransistionTime extends React.Component<TransistionTimeProperties> {
+  public render(): JSX.Element {
+    return (
+      <HBoxLayout width='100%'>
+        <VBoxLayout>
+          <IntegerInputBox min={0}/>
+          <Padding size='10px'/>
+          <span className={css(TransistionTime.STYLE.label)}>
+            HOUR
+          </span>
+        </VBoxLayout>
+        <Padding size='10px'/>
+        <Center height='34px' className={css(TransistionTime.STYLE.colon)}>
+          :
+        </Center>
+        <Padding size='10px'/>
+        <VBoxLayout>
+          <IntegerInputBox min={0} max={59}/>
+          <Padding size='10px'/>
+          <span className={css(TransistionTime.STYLE.label)}>
+            MINUTE
+          </span>
+        </VBoxLayout>
+        <Padding size='10px'/>
+        <Center height='34px' className={css(TransistionTime.STYLE.colon)}>
+          :
+        </Center>
+        <Padding size='10px'/>
+        <VBoxLayout>
+          <IntegerInputBox min={0} max={59}/>
+          <Padding size='10px'/>
+          <span className={css(TransistionTime.STYLE.label)}>
+            SECOND
+          </span>
+        </VBoxLayout>
+      </HBoxLayout>)
+  }
 
+  private static STYLE = StyleSheet.create({
+    label: {
+      font: '400 10px Roboto',
+      color: '#8C8C8C',
+      width: '100%',
+      textAlign: 'center'
+    },
+    colon: {
+      font: '400 16px Roboto'
+    }
+  });
 }
 
 class Footer extends React.Component<FooterProperties> {
+  public render(): JSX.Element {
+    const buttonText = (() => {
+      if(this.props.isAdmin) {
+        return 'Save Changes';
+      }
+      return 'Submit Request';
+    })();
+    const message = (() => {
+      return <span className={css([Footer.MESSAGE_STYLE.base,
+        Footer.MESSAGE_STYLE.validMessage])}>Saved</span>
+    })();
+    return (
+      
+        <HBoxLayout width='100%'>
+          <Padding/>
+            <VBoxLayout>
+            <button disabled={this.props.buttonIsDisabled}
+                className={css(Footer.STYLE.button)}>
+              {buttonText}
+            </button>
+            <Padding size='18px'/>
+            <HBoxLayout>
+              <Padding/>
+                {message}
+              <Padding/>
+            </HBoxLayout>
+            </VBoxLayout>
+          <Padding/>
+      </HBoxLayout>)
+  }
 
+  private static STYLE = StyleSheet.create({
+    button: {
+      width: '246px',
+      height: '34px',
+      backgroundColor: '#684BC7',
+      font: '400 14px Roboto',
+      color: '#FFFFFF',
+      border: 'none',
+      outline: 0,
+      ':active': {
+        backgroundColor: '#4B23A0'
+      },
+      ':disabled': {
+        backgroundColor: '#F8F8F8',
+        color: '#8C8C8C'
+      }
+    }
+  });
+  private static MESSAGE_STYLE = StyleSheet.create({
+    base: {
+      font: '400 14px Roboto'
+    },
+    validMessage: {
+      color: '#36BB55'
+    }
+  });
 }
