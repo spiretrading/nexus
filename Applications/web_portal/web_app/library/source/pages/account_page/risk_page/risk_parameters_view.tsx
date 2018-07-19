c441320cee7d415ef6cc699fe5e2611546fb4052
nicola-spire <nicola@spiretrading.com>
2018-07-19 10:33:34 -0400

    made changes

--- Applications/web_portal/web_app/library/source/pages/account_page/risk_page/risk_parameters_view.tsx
+++ Applications/web_portal/web_app/library/source/pages/account_page/risk_page/risk_parameters_view.tsx
@@ -13,10 +13,6 @@ interface Properties {
   /** Used to lookup currency names and symbols. */
   currencyDatabase: Nexus.CurrencyDatabase;
 
-  /** The message for the footer to display. */
-  footerMessage: FOOTER_MESSAGE;
-
-  isUserAdmin: boolean;
   /** Called on an update of the currency. */
   onCurrencyChange?: (currency: Nexus.Currency) => void;
 
@@ -28,17 +24,10 @@ interface Properties {
 
   /** Called on an update of the transition time. */
   onTransitionTimeChange?: (duration: Beam.Duration) => void;
-
-  /** Called when the submit request button is clicked. */
-  onSubmitRequest?: (message: string) => void;
-
-  /** Called when the save changes button is clicked. */
-  onSaveChanges?: () => void;
 }
 
 interface State {
   breakpoint: Breakpoint;
-  comment: string;
 }
 
 interface FooterProperties {
@@ -77,16 +66,12 @@ export class RiskParametersView extends React.Component<Properties, State> {
   public constructor(props: Properties) {
     super(props);
     this.state = {
-      breakpoint: RiskParametersView.getBreakpoint(),
-      comment: ''
+      breakpoint: RiskParametersView.getBreakpoint()
     };
-    this.originalParameters = this.props.parameters.copy();
     this.onScreenResize = this.onScreenResize.bind(this);
     this.onTransitionSecondChange = this.onTransitionSecondChange.bind(this);
     this.onTransitionMinuteChange = this.onTransitionMinuteChange.bind(this);
     this.onTransitionHourChange = this.onTransitionHourChange.bind(this);
-    this.onCommentChange = this.onCommentChange.bind(this);
-    this.onButtonClick = this.onButtonClick.bind(this);
   }
 
   public componentDidMount() {
@@ -120,106 +105,97 @@ export class RiskParametersView extends React.Component<Properties, State> {
       this.props.parameters.transitionTime.getTotalMinutes() % 60);
     const seconds = Math.floor(
       this.props.parameters.transitionTime.getTotalSeconds() % 60);
-    const isButtonDisabled = this.originalParameters.equals(
-      this.props.parameters);
+    const currencySign = this.props.currencyDatabase.fromCurrency(
+      this.props.parameters.currency).sign;
     return (
-        <VBoxLayout width='100%' height='100%'>
-          <Padding size='30px'/>
-          <HBoxLayout width='100%'>
-            <Padding/>
-            <VBoxLayout className={containerClassName}>
-              <VBoxLayout width='100%' className={
-                  css(RiskParametersView.STYLE.controlsContainer)}>
+      <HBoxLayout width='100%'>
+        <Padding/>
+        <VBoxLayout className={containerClassName}>
+          <VBoxLayout width='100%' className={
+              css(RiskParametersView.STYLE.innerContainer)}>
+            <HBoxLayout width='100%'>
+              <Padding/>
+              <VBoxLayout width='246px'>
+                <Label text='Currency'/>
+                <Padding size='12px'/>
+                <CurrencySelectionBox className={
+                  css(RiskParametersView.STYLE.dropdownButton)}
+                  currencyDatabase={this.props.currencyDatabase}
+                  value={this.props.parameters.currency} onChange={
+                  this.props.onCurrencyChange}/>
+                <Padding size='30px'/>
+                <Label text={`Buying Power (${currencySign})`}/>
+                <Padding size='12px'/>
+                <MoneyInputBox
+                  className={css(RiskParametersView.STYLE.inputBox)}
+                  value={this.props.parameters.buyingPower}
+                  onChange={this.props.onBuyingPowerChange}/>
+                <Padding size='30px'/>
+                <Label text={`Net Loss (${currencySign})`}/>
+                <Padding size='12px'/>
+                <MoneyInputBox
+                  className={css(RiskParametersView.STYLE.inputBox)}
+                  value={this.props.parameters.netLoss}
+                  onChange={this.props.onNetLossChange}/>
+                <Padding size='30px'/>
+                <Label text='Transition Time'/>
+                <Padding size='12px'/>
                 <HBoxLayout width='100%'>
-                  <Padding/>
-                  <VBoxLayout width='246px'>
-                    <Label text='Currency'/>
-                    <Padding size='12px'/>
-                    <CurrencySelectionBox className={
-                      css(RiskParametersView.STYLE.dropdownButton)}
-                      currencyDatabase={this.props.currencyDatabase}
-                      value={this.props.parameters.currency} onChange={
-                      this.props.onCurrencyChange}/>
-                    <Padding size='30px'/>
-                    <Label text='Buying Power ($)'/>
-                    <Padding size='12px'/>
-                    <MoneyInputBox
+                  <VBoxLayout>
+                    <IntegerInputBox min={0} value={hours} padding={2}
                       className={css(RiskParametersView.STYLE.inputBox)}
-                      value={this.props.parameters.buyingPower}
-                      onChange={this.props.onBuyingPowerChange}/>
-                    <Padding size='30px'/>
-                    <Label text='Net Loss ($)'/>
-                    <Padding size='12px'/>
-                    <MoneyInputBox
-                      className={css(RiskParametersView.STYLE.inputBox)}
-                      value={this.props.parameters.netLoss}
-                      onChange={this.props.onNetLossChange}/>
-                    <Padding size='30px'/>
-                    <Label text='Transition Time'/>
-                    <Padding size='12px'/>
-                    <HBoxLayout width='100%'>
-                      <VBoxLayout>
-                        <IntegerInputBox min={0} value={hours} padding={2}
-                          className={css(RiskParametersView.STYLE.inputBox)}
-                          onChange={this.onTransitionHourChange}/>
-                        <Padding size='10px'/>
-                        <span className={
-                            css(RiskParametersView.TRANSITION_STYLE.label)}>
-                          HOUR
-                        </span>
-                      </VBoxLayout>
-                      <Padding size='10px'/>
-                      <Center height='34px' className={
-                          css(RiskParametersView.TRANSITION_STYLE.colon)}>
-                        :
-                      </Center>
-                      <Padding size='10px'/>
-                      <VBoxLayout>
-                        <IntegerInputBox min={0} max={59} value={minutes}
-                          padding={2}
-                          className={css(RiskParametersView.STYLE.inputBox)}
-                          onChange={this.onTransitionMinuteChange}/>
-                        <Padding size='10px'/>
-                        <span className={
-                            css(RiskParametersView.TRANSITION_STYLE.label)}>
-                          MINUTE
-                        </span>
-                      </VBoxLayout>
-                      <Padding size='10px'/>
-                      <Center height='34px' className={
-                          css(RiskParametersView.TRANSITION_STYLE.colon)}>
-                        :
-                      </Center>
-                      <Padding size='10px'/>
-                      <VBoxLayout>
-                        <IntegerInputBox min={0} max={59} value={seconds}
-                          padding={2}
-                          className={css(RiskParametersView.STYLE.inputBox)}
-                          onChange={this.onTransitionSecondChange}/>
-                        <Padding size='10px'/>
-                        <span className={
-                            css(RiskParametersView.TRANSITION_STYLE.label)}>
-                          SECOND
-                        </span>
-                      </VBoxLayout>
-                    </HBoxLayout>
+                      onChange={this.onTransitionHourChange}/>
+                    <Padding size='10px'/>
+                    <span className={
+                        css(RiskParametersView.
+                          TRANSITION_TIME_STYLE.label)}>
+                      HOUR
+                    </span>
                   </VBoxLayout>
-                  <Padding/>
+                  <Padding size='10px'/>
+                  <Center height='34px' className={
+                      css(RiskParametersView.TRANSITION_TIME_STYLE.colon)}>
+                    :
+                  </Center>
+                  <Padding size='10px'/>
+                  <VBoxLayout>
+                    <IntegerInputBox min={0} max={59} value={minutes}
+                      padding={2}
+                      className={css(RiskParametersView.STYLE.inputBox)}
+                      onChange={this.onTransitionMinuteChange}/>
+                    <Padding size='10px'/>
+                    <span className={
+                        css(RiskParametersView.
+                          TRANSITION_TIME_STYLE.label)}>
+                      MINUTE
+                    </span>
+                  </VBoxLayout>
+                  <Padding size='10px'/>
+                  <Center height='34px' className={
+                      css(RiskParametersView.TRANSITION_TIME_STYLE.colon)}>
+                    :
+                  </Center>
+                  <Padding size='10px'/>
+                  <VBoxLayout>
+                    <IntegerInputBox min={0} max={59} value={seconds}
+                      padding={2}
+                      className={css(RiskParametersView.STYLE.inputBox)}
+                      onChange={this.onTransitionSecondChange}/>
+                    <Padding size='10px'/>
+                    <span className={
+                        css(RiskParametersView.
+                          TRANSITION_TIME_STYLE.label)}>
+                      SECOND
+                    </span>
+                  </VBoxLayout>
                 </HBoxLayout>
-                <Padding size='30px'/>
-                <div className={css(RiskParametersView.STYLE.divider)}/>
-                <Padding size='30px'/>
-                <Footer isButtonDisabled={isButtonDisabled}
-                  isUserAdmin={!this.props.isUserAdmin}
-                  onButtonClick={this.onButtonClick}
-                  message={this.props.footerMessage}
-                  comment={this.state.comment}
-                  onCommentChange={this.onCommentChange}/>
               </VBoxLayout>
-            </VBoxLayout>
-            <Padding/>
-          </HBoxLayout>
-        </VBoxLayout>);
+              <Padding/>
+            </HBoxLayout>
+          </VBoxLayout>
+        </VBoxLayout>
+        <Padding/>
+      </HBoxLayout>);
   }
 
   private static getBreakpoint(): Breakpoint {
@@ -275,25 +251,11 @@ export class RiskParametersView extends React.Component<Properties, State> {
     this.props.onTransitionTimeChange(newDuration);
   }
 
-  private onCommentChange(event: React.ChangeEvent<HTMLTextAreaElement>) {
-    this.setState({comment: event.currentTarget.value});
-  }
-
-  private onButtonClick() {
-    if(!this.props.isUserAdmin) {
-      this.props.onSubmitRequest(this.state.comment);
-    } else {
-      this.props.onSaveChanges();
-    }
-  }
-  private originalParameters: Nexus.RiskParameters;
   private static defaultProps = {
     onCurrencyChange: (currency: Nexus.Currency) => {},
     onBuyingPowerChange: (value: Nexus.Money) => {},
     onNetLossChange: (value: Nexus.Money) => {},
-    onTransitionTimeChange: (duration: Beam.Duration) => {},
-    onSubmitRequest: () => {},
-    onSaveChanges: () => {}
+    onTransitionTimeChange: (duration: Beam.Duration) => {}
   };
   private static CONTAINER_STYLE = StyleSheet.create({
     base: {
@@ -311,7 +273,7 @@ export class RiskParametersView extends React.Component<Properties, State> {
       width: '1036px'
     }
   });
-  private static TRANSITION_STYLE = StyleSheet.create({
+  private static TRANSITION_TIME_STYLE = StyleSheet.create({
     label: {
       font: '400 10px Roboto',
       color: '#8C8C8C',
@@ -323,12 +285,7 @@ export class RiskParametersView extends React.Component<Properties, State> {
     }
   });
   private static STYLE = StyleSheet.create({
-    divider: {
-      width: '100%',
-      height: '1px',
-      backgroundColor: '#E6E6E6'
-    },
-    controlsContainer: {
+    innerContainer: {
       position: 'absolute' as 'absolute'
     },
     dropdownButton: {
@@ -366,110 +323,6 @@ class Label extends React.Component<LabelProperties> {
       font: '400 14px Roboto',
       color: '#333333',
       whiteSpace: 'nowrap'
-    }
-  });
-}
-
-class Footer extends React.Component<FooterProperties> {
-  public render(): JSX.Element {
-    const message = (() => {
-      switch(this.props.message) {
-        case FOOTER_MESSAGE.SUBMITTED:
-          return <span className={css([Footer.MESSAGE_STYLE.base,
-            Footer.MESSAGE_STYLE.valid])}>Submitted</span>;
-        case FOOTER_MESSAGE.SERVER_ISSUE:
-          return <span className={css([Footer.MESSAGE_STYLE.base,
-            Footer.MESSAGE_STYLE.invalid])}>Server issue</span>;
-        case FOOTER_MESSAGE.SAVED:
-          return <span className={css([Footer.MESSAGE_STYLE.base,
-            Footer.MESSAGE_STYLE.valid])}>Saved</span>;
-      }
-    })();
-    const commentBox = (() => {
-      if(this.props.isUserAdmin) {
-        return <textarea className={css(Footer.STYLE.commentBox)}
-          value={this.props.comment} placeholder='Leave comment here…'
-          onChange={this.props.onCommentChange}/>;
-      }
-    })();
-    const commentBoxPadding = (() => {
-      if(this.props.isUserAdmin) {
-        return <Padding size='30px'/>;
-      }
-    })();
-    const buttonText = (() => {
-      if(this.props.isUserAdmin) {
-        return 'Submit Request';
-      }
-      return 'Save Changes';
-    })();
-    return (
-      <VBoxLayout width='100%'>
-        {commentBox}
-        {commentBoxPadding}
-        <HBoxLayout width='100%'>
-          <Padding size='calc(50% - 123px)'/>
-          <button disabled={this.props.isButtonDisabled}
-            className={css(Footer.STYLE.button)}
-            onClick={this.props.onButtonClick}>
-            {buttonText}
-          </button>
-          <Padding size='calc(50% - 123px)'/>
-        </HBoxLayout>
-        <Padding size='18px'/>
-        <HBoxLayout width='100%'>
-          <Padding/>
-          {message}
-          <Padding/>
-        </HBoxLayout>
-      </VBoxLayout>);
-  }
-
-  private static defaultProps = {
-    message: ''
-  };
-  private static STYLE = StyleSheet.create({
-    button: {
-      width: '246px',
-      height: '34px',
-      backgroundColor: '#684BC7',
-      font: '400 14px Roboto',
-      color: '#FFFFFF',
-      border: 'none',
-      outline: 0,
-      ':active': {
-        backgroundColor: '#4B23A0'
-      },
-      ':disabled': {
-        backgroundColor: '#F8F8F8',
-        color: '#8C8C8C'
-      }
-    },
-    commentBox: {
-      boxSizing: 'border-box',
-      width: '100%',
-      height: '130px',
-      border: '1px solid #C8C8C8',
-      padding: '10px',
-      margin: 0,
-      resize: 'none',
-      outline: 0,
-      ':focus': {
-        border: '1px solid #684BC7'
-      }
-    }
-  });
-  private static MESSAGE_STYLE = StyleSheet.create({
-    base: {
-      font: '400 14px Roboto',
-      width: '100%',
-      textAlign: 'center'
-    },
-    valid: {
-      color: '#36BB55'
-    },
-    invalid: {
-      color: '#E63F44'
     }
   });
 }
--- Applications/web_portal/web_app/tests/risk_page_tester/source/index.tsx
+++ Applications/web_portal/web_app/tests/risk_page_tester/source/index.tsx
@@ -6,8 +6,6 @@ import * as ReactDOM from 'react-dom';
 import * as WebPortal from 'web_portal';
 
 interface State {
-  isAdmin: boolean;
-  footerMessage: FOOTER_MESSAGE;
   parameters: Nexus.RiskParameters;
 }
 
@@ -15,19 +13,10 @@ interface Properties {
 
 }
 
-enum FOOTER_MESSAGE {
-  SAVED,
-  SUBMITTED,
-  SERVER_ISSUE,
-  NONE
-}
-
 class TestApp extends React.Component<Properties, State> {
   constructor(props: Properties) {
     super(props);
     this.state = {
-      isAdmin: false,
-      footerMessage: FOOTER_MESSAGE.NONE,
       parameters: new Nexus.RiskParameters(
         Nexus.DefaultCurrencies.CAD,
         Nexus.Money.ONE.multiply(100000),
@@ -36,67 +25,23 @@ class TestApp extends React.Component<Properties, State> {
         Beam.Duration.HOUR.multiply(5).add(Beam.Duration.MINUTE.multiply(30)).add(
         Beam.Duration.SECOND.multiply(15)))
     };
-    this.onChangeFooterMessage = this.onChangeFooterMessage.bind(this);
-    this.onToggleIsAdmin = this.onToggleIsAdmin.bind(this);
     this.onCurrencyChange = this.onCurrencyChange.bind(this);
     this.onBuyingPowerChange = this.onBuyingPowerChange.bind(this);
     this.onNetLossChange = this.onNetLossChange.bind(this);
     this.onTransitionTimeChange = this.onTransitionTimeChange.bind(this);
-    this.onSubmitRequest = this.onSubmitRequest.bind(this);
-    this.onSaveChanges = this.onSaveChanges.bind(this);
   }
 
   public render(): JSX.Element {
-    const adminButtonMessage = (() => {
-      if(this.state.isAdmin) {
-        return 'Is Admin';
-      }
-      return 'Not Admin';
-    })()
     return (
       <WebPortal.VBoxLayout width='100%' height='100%'>
-        <WebPortal.HBoxLayout className={css(TestApp.STYLE.controlPanel)}>
-          <button className={css(TestApp.STYLE.controlButton)} onClick={
-              this.onChangeFooterMessage}>
-            Change Footer Message
-          </button>
-          <WebPortal.Padding size='30px'/>
-          <button className={css(TestApp.STYLE.controlButton)} onClick={
-              this.onToggleIsAdmin}>
-            {adminButtonMessage}
-          </button>
-        </WebPortal.HBoxLayout>
+        <WebPortal.Padding size='30px'/>
         <WebPortal.RiskParametersView parameters={this.state.parameters}
           currencyDatabase={Nexus.buildDefaultCurrencyDatabase()}
-          footerMessage={this.state.footerMessage}
-          isUserAdmin={this.state.isAdmin}
           onCurrencyChange={this.onCurrencyChange}
           onBuyingPowerChange={this.onBuyingPowerChange}
           onNetLossChange={this.onNetLossChange}
-          onTransitionTimeChange={this.onTransitionTimeChange}
-          onSubmitRequest={this.onSubmitRequest}
-          onSaveChanges={this.onSaveChanges}/>
+          onTransitionTimeChange={this.onTransitionTimeChange}/>
       </WebPortal.VBoxLayout>);
-  }
-
-  private onToggleIsAdmin() {
-    this.setState({isAdmin: !this.state.isAdmin});
-  }
-
-  private onChangeFooterMessage() {
-    const next = (() => {
-      switch(this.state.footerMessage) {
-      case FOOTER_MESSAGE.SAVED:
-        return FOOTER_MESSAGE.SERVER_ISSUE;
-      case FOOTER_MESSAGE.SERVER_ISSUE:
-        return FOOTER_MESSAGE.SUBMITTED;
-      case FOOTER_MESSAGE.SUBMITTED:
-        return FOOTER_MESSAGE.NONE;
-      case FOOTER_MESSAGE.NONE:
-        return FOOTER_MESSAGE.SAVED;
-      }
-    })();
-    this.setState({footerMessage: next});
   }
 
   private onCurrencyChange(value: Nexus.Currency) {
@@ -122,29 +67,6 @@ class TestApp extends React.Component<Properties, State> {
     newParameters.transitionTime = value;
     this.setState({parameters: newParameters})
   }
-
-  private onSubmitRequest(comment: string) {
-    console.log('submitting with request with comment: ',
-      comment, ' parameters: ', this.state.parameters);
-  }
-
-  private onSaveChanges() {
-    console.log('saving changes with parameters: ',
-      this.state.parameters);
-  }
-
-  private static STYLE = StyleSheet.create({
-    controlPanel: {
-      position: 'absolute' as 'absolute',
-      top: 700,
-      left: 0,
-      width: '100%',
-    },
-    controlButton: {
-      width: '100px',
-      height: '30px'
-    }
-  });
 }
 
 ReactDOM.render(<TestApp/>, document.getElementById('main'));
