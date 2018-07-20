6f922f021ab21e778349960bd3d87fef69d2527e
Ken <ken@spiretrading.com>
2018-07-20 18:47:03 -0400

    Added layout test in scratch

--- Applications/spire/source/scratch/main.cpp
+++ Applications/spire/source/scratch/main.cpp
@@ -1,4 +1,7 @@
 #include <QApplication>
+#include <QHBoxLayout>
+#include <QLabel>
+#include <QWidget>
 #include "spire/spire/resources.hpp"
 
 using namespace spire;
@@ -8,5 +11,27 @@ int main(int argc, char** argv) {
   application->setOrganizationName(QObject::tr("Eidolon Systems Ltd"));
   application->setApplicationName(QObject::tr("Scratch"));
   initialize_resources();
+  auto window = new QWidget();
+  window->resize(300, 500);
+  auto layout = new QHBoxLayout(window);
+  layout->setContentsMargins({});
+  layout->setSpacing(0);
+  auto w1 = new QWidget(window);
+  auto l1 = new QHBoxLayout();
+  l1->setContentsMargins({});
+  l1->setSpacing(0);
+  w1->setFixedSize(150, 20);
+  w1->setStyleSheet("background-color: red;");
+  l1->addWidget(w1);
+  layout->addLayout(l1);
+  auto l2 = new QHBoxLayout();
+  l2->setContentsMargins({});
+  l2->setSpacing(0);
+  auto w2 = new QWidget(window);
+  w2->setFixedSize(150, 20);
+  w2->setStyleSheet("background-color: yellow;");
+  l2->addWidget(w2);
+  layout->addLayout(l2);
+  window->show();
   application->exec();
 }
