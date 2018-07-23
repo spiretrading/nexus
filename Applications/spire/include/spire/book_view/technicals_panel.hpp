fb2dce4e16328062efd2186789c187997da9c16d
Ken <ken@spiretrading.com>
2018-07-23 18:09:00 -0400

    Added technicals_panel stub implementation

---
+++ Applications/spire/include/spire/book_view/technicals_panel.hpp
@@ -0,0 +1,16 @@
+#ifndef SPIRE_TECHNICALS_PANEL_HPP
+#define SPIRE_TECHNICALS_PANEL_HPP
+#include <QWidget>
+
+namespace spire {
+
+  //! Displays 
+  class technicals_panel : public QWidget {
+    public:
+
+      //! Constructs a technicals_panel.
+      technicals_panel(QWidget* parent = nullptr);
+  };
+}
+
+#endif
---
+++ Applications/spire/source/book_view/technicals_panel.cpp
@@ -0,0 +1,8 @@
+#include "spire/book_view/technicals_panel.hpp"
+
+using namespace spire;
+
+technicals_panel::technicals_panel(QWidget* parent)
+    : QWidget(parent) {
+
+}
