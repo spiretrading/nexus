892efecfa2e57567ee93d06e137bf0fe5e565cc8
Ken <ken@spiretrading.com>
2018-07-27 11:27:05 -0400

    Forced resize after call to showNormal()

--- Applications/spire/source/ui/title_bar.cpp
+++ Applications/spire/source/ui/title_bar.cpp
@@ -288,6 +288,7 @@ void title_bar::on_maximize_button_press() {
 
 void title_bar::on_restore_button_press() {
   window()->showNormal();
+  window()->resize(m_restore_geometry.size());
 }
 
 void title_bar::on_close_button_press() {
