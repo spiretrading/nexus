dd1d7efa104e40023ce04f37c3e5f7152530712f
Kamal <kamal@eidolonsystems.com>
2018-10-29 23:06:05 -0400

    Implemented EntitlementKey class and MarketCode.

--- WebApi/source/definitions/market.ts
+++ WebApi/source/definitions/market.ts
@@ -1,2 +1,37 @@
+import * as Beam from 'beam';
+
+/** Stores a market's unique identifer. */
 export class MarketCode {
+
+  /** Parses a MarketCode from JSON. */
+  public static fromJson(value: any): MarketCode {
+    return new MarketCode(value);
+  }
+
+  /** Constructs a MarketCode from its identifier. */
+  constructor(identifier: string) {
+    this.identifier = identifier;
+  }
+
+  /** Tests if two MarketCodes represent the same market. */
+  public equals(other: MarketCode): boolean {
+    return other && this.identifier === other.identifier;
+  }
+
+  /** Returns the market's identifier. */
+  public toString(): string {
+    return this.identifier;
+  }
+
+  /** Converts this object to JSON. */
+  public toJson(): any {
+    return this.identifier;
+  }
+
+  /** Returns a hash of this value. */
+  public hash(): number {
+    return Beam.hashString(this.identifier);
+  }
+
+  private identifier: string;
 }
--- WebApi/source/market_data_service/entitlement_key.ts
+++ WebApi/source/market_data_service/entitlement_key.ts
@@ -1,4 +1,61 @@
+import * as Beam from 'beam';
 import { MarketCode } from '..';
 
+/** Stores an index into a market data entitlement. */
 export class EntitlementKey {
+
+  /** Parses an EntitlementKey from JSON. */
+  public static fromJson(value: any): EntitlementKey {
+    return new EntitlementKey(MarketCode.fromJson(value.market),
+      MarketCode.fromJson(value.source));
+  }
+
+  /** Constructs an EntitlementKey.
+   * @param market - The market the security belongs to.
+   * @param source - The market disseminating the data, the default value is
+   *        the market.
+   */
+  constructor(market: MarketCode, source?: MarketCode) {
+    this._market = market;
+    if(source) {
+      this._source = source;
+    } else {
+      this._source = market;
+    }
+  }
+
+  /** Returns the market the security belongs to. */
+  public get market(): MarketCode {
+    return this._market;
+  }
+
+  /** Returns the market disseminating the data. */
+  public get source(): MarketCode {
+    return this._source;
+  }
+
+  /** Tests if two keys are equal. */
+  public equals(other: EntitlementKey): boolean {
+    return other && this._market.equals(other._market) &&
+      this._source.equals(other._source);
+  }
+
+  /** Converts this object to JSON. */
+  public toJson(): any {
+    return {
+      market: this._market.toJson(),
+      source: this._source.toJson()
+    };
+  }
+
+  /** Returns a hash of this object. */
+  public hash(): number {
+    let seed = 0;
+    seed = Beam.hashCombine(seed, this._market.hash());
+    seed = Beam.hashCombine(seed, this._source.hash());
+    return seed;
+  }
+
+  private _market: MarketCode;
+  private _source: MarketCode;
 }
