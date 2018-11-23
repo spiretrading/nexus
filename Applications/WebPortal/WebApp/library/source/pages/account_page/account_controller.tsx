9f559b648a45b51ffaa5a3548febb5b8ac59ce5c
Kamal <kamal@eidolonsystems.com>
2018-11-23 17:12:23 -0500

    Implemented routing for the AccountController.

--- Applications/WebPortal/WebApp/library/source/pages/account_page/account_controller.tsx
+++ Applications/WebPortal/WebApp/library/source/pages/account_page/account_controller.tsx
@@ -1,10 +1,14 @@
 import * as React from 'react';
+import * as Router from 'react-router-dom';
 import { AccountModel } from './account_model';
 import { AccountPage } from './account_page';
 import { DisplaySize } from '../../display_size';
 import { SubPage } from './sub_page';
 
 interface Properties {
+
+  /** The URL prefix to match. */
+  urlPrefix?: string;
 
   /** Determines the layout to use based on the display device. */
   displaySize: DisplaySize;
@@ -15,36 +19,57 @@ interface Properties {
 
 interface State {
   isLoaded: boolean;
-  subPage: SubPage;
+  redirect: string;
 }
 
 /** Implements a controller for the AccountPage. */
 export class AccountController extends React.Component<Properties, State> {
+  public static readonly defaultProps = {
+    urlPrefix: ''
+  }
+
   constructor(props: Properties) {
     super(props);
     this.state = {
       isLoaded: false,
-      subPage: SubPage.PROFILE
+      redirect: null,
     };
+    this.navigateToProfile = this.navigateToProfile.bind(this);
+    this.navigateToEntitlements = this.navigateToEntitlements.bind(this);
     this.onMenuClick = this.onMenuClick.bind(this);
   }
 
   public render(): JSX.Element {
+    if(this.state.redirect) {
+      return <Router.Redirect push to={this.state.redirect}/>;
+    }
     if(!this.state.isLoaded) {
       return <div/>;
     }
-    const page = ((): JSX.Element => {
-      switch(this.state.subPage) {
-        default:
-          return null;
+    const subPage = (() => {
+      if(window.location.href.endsWith('/profile')) {
+        return SubPage.PROFILE;
+      } else if(window.location.href.endsWith('/entitlements')) {
+        return SubPage.ENTITLEMENTS;
       }
+      return SubPage.NONE;
     })();
+    if(subPage === SubPage.NONE) {
+      return <Router.Redirect to={`${this.props.urlPrefix}/profile`}/>;
+    }
     return (
-      <AccountPage displaySize={this.props.displaySize}
-          subPage={this.state.subPage} account={this.props.model.account}
-          roles={this.props.model.roles} onMenuClick={this.onMenuClick}>
-        {page}
-      </AccountPage>);
+      <Router.BrowserRouter>
+        <Router.Switch>
+          <AccountPage displaySize={this.props.displaySize}
+              subPage={subPage} account={this.props.model.account}
+              roles={this.props.model.roles} onMenuClick={this.onMenuClick}>
+            <Router.Route path={`${this.props.urlPrefix}/profile`}
+              render={this.navigateToProfile}/>
+            <Router.Route path={`${this.props.urlPrefix}/entitlements`}
+              render={this.navigateToEntitlements}/>
+          </AccountPage>
+        </Router.Switch>
+      </Router.BrowserRouter>);
   }
 
   public componentWillMount(): void {
@@ -54,7 +79,29 @@ export class AccountController extends React.Component<Properties, State> {
       });
   }
 
+  public componentDidUpdate(): void {
+    if(this.state.redirect) {
+      this.setState({redirect: null});
+    }
+  }
+
+  private navigateToProfile() {
+    return <div>Profile</div>;
+  }
+
+  private navigateToEntitlements() {
+    return <div>Entitlements</div>;
+  }
+
   private onMenuClick(subPage: SubPage) {
-    this.setState({subPage: subPage});
+    if(subPage === SubPage.PROFILE) {
+      this.setState({
+        redirect: `${this.props.urlPrefix}/profile`,
+      });
+    } else if(subPage === SubPage.ENTITLEMENTS) {
+      this.setState({
+        redirect: `${this.props.urlPrefix}/entitlements`,
+      });
+    }
   }
 }
