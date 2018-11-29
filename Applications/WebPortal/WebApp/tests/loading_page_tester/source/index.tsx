6ae0b31cbff8e1c1401226b066364aaebdf2b6b8
Sofiya Polyanska <sofiya@spiretrading.com>
2018-11-29 17:36:48 -0500

    Adjusted the test.

--- Applications/WebPortal/WebApp/tests/loading_page_tester/source/index.tsx
+++ Applications/WebPortal/WebApp/tests/loading_page_tester/source/index.tsx
@@ -2,5 +2,5 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import * as WebPortal from 'web_portal';
 
-ReactDOM.render(<WebPortal.LoadingPage delay={1000}/>,
+ReactDOM.render(<WebPortal.LoadingPage delay={2000}/>,
   document.getElementById('main'));
