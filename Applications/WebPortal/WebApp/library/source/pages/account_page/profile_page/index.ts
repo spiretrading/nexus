46160c55d1595b968cb45b29d8bba499550ea657
Sofiya Polyanska <sofiya@spiretrading.com>
2018-12-10 12:04:41 -0500

    Moved the slider.

--- Applications/WebPortal/WebApp/library/source/pages/account_page/profile_page/index.ts
+++ Applications/WebPortal/WebApp/library/source/pages/account_page/profile_page/index.ts
@@ -1,7 +1,6 @@
 export * from './form_entry';
 export * from './photo_field';
 export * from './profile_page';
-export * from './slider';
 export * from './roles_field';
 export * from './role_icon';
 export * from './text_field';
