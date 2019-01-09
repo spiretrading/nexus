09e6fff16c9cd4cb653f20b7b64e96c6bd153e45
Ken <ken@spiretrading.com>
2019-01-09 15:38:20 -0500

    Removed duplicate copy of style options

--- Applications/spire/source/book_view/book_view_table_delegate.cpp
+++ Applications/spire/source/book_view/book_view_table_delegate.cpp
@@ -32,9 +32,8 @@ void BookViewTableDelegate::paint(QPainter* painter,
     auto text_pos = option.rect.width() - (text_width + scale_width(7));
     painter->translate(QPoint(text_pos, 0));
   }
-  auto options = opt;
-  options.locale.setNumberOptions(QLocale::DefaultNumberOptions);
-  m_item_delegate->paint(painter, options, index);
+  opt.locale.setNumberOptions(QLocale::DefaultNumberOptions);
+  m_item_delegate->paint(painter, opt, index);
   painter->restore();
 }
 
