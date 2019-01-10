b89f985c02cae1a619a538643c81b7ac6d6bc840
Kamal <kamal@eidolonsystems.com>
2019-01-10 12:18:24 -0500

    Updated all build scripts and dependencies for the web portal test applications.

--- Applications/WebPortal/WebApp/tests/login_page_tester/build.bat
+++ Applications/WebPortal/WebApp/tests/login_page_tester/build.bat
@@ -1,16 +1,37 @@
-ECHO OFF
+@ECHO OFF
 SETLOCAL
 SET UPDATE_NODE=
 SET UPDATE_BUILD=
 SET PROD_ENV=
+IF NOT "%1" == "Debug" (
+  SET PROD_ENV=1
+)
 PUSHD %~dp0
+IF "%1" == "clean" (
+  IF EXIST application (
+    rmdir /s /q application
+  )
+  IF EXIST node_modules\mod_time.txt (
+    del node_modules\mod_time.txt
+  )
+  EXIT /B
+)
+IF "%1" == "reset" (
+  IF EXIST application (
+    rmdir /s /q application
+  )
+  IF EXIST node_modules (
+    rmdir /s /q node_modules
+  )
+  IF EXIST package-lock.json (
+    del package-lock.json
+  )
+  EXIT /B
+)
 SET WEB_PORTAL_PATH=..\..\library
 PUSHD %WEB_PORTAL_PATH%
 CALL build.bat %*
 POPD
-IF NOT "%1" == "Debug" (
-  SET PROD_ENV=1
-)
 IF NOT EXIST node_modules (
   SET UPDATE_NODE=1
 ) ELSE (
@@ -51,7 +72,7 @@ IF NOT EXIST node_modules\mod_time.txt (
   SET UPDATE_BUILD=1
 ) ELSE (
   FOR /F %%i IN (
-    'ls -l --time-style=full-iso %WEB_PORTAL_PATH%\node_modules\mod_time.txt ^| awk "{print $6 $7}"') DO (
+    'ls -l --time-style=full-iso tsconfig.json %WEB_PORTAL_PATH%\node_modules\mod_time.txt ^| awk "{print $6 $7}"') DO (
     FOR /F %%j IN (
       'ls -l --time-style=full-iso node_modules\mod_time.txt ^| awk "{print $6 $7}"') DO (
       IF "%%i" GEQ "%%j" (
@@ -65,9 +86,9 @@ IF "%UPDATE_BUILD%" == "1" (
     rm -rf application
   )
   node .\node_modules\webpack\bin\webpack.js
+  echo "timestamp" > node_modules\mod_time.txt
   cp -r ../../resources application
   cp -r source/index.html application
-  echo "timestamp" > node_modules\mod_time.txt
 )
 POPD
 ENDLOCAL
