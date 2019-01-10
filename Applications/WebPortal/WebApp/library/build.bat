7a6aadbfbf3ec2530411d31aab6aec5545a6949b
Kamal <kamal@eidolonsystems.com>
2019-01-10 11:10:22 -0500

    Added a build script for the web api that only rebuilds when needed.

--- Applications/WebPortal/WebApp/library/build.bat
+++ Applications/WebPortal/WebApp/library/build.bat
@@ -1,16 +1,37 @@
-ECHO OFF
+@ECHO OFF
 SETLOCAL
 SET UPDATE_NODE=
 SET UPDATE_BUILD=
-SET PROD_ENV=
 PUSHD %~dp0
-SET WEB_PORTAL_PATH=..\..\library
-PUSHD %WEB_PORTAL_PATH%
+IF "%1" == "clean" (
+  IF EXIST library (
+    rmdir /s /q library
+  )
+  IF EXIST node_modules\mod_time.txt (
+    del node_modules\mod_time.txt
+  )
+  EXIT /B
+)
+IF "%1" == "reset" (
+  IF EXIST library (
+    rmdir /s /q library
+  )
+  IF EXIST node_modules (
+    rmdir /s /q node_modules
+  )
+  IF EXIST package-lock.json (
+    del package-lock.json
+  )
+  EXIT /B
+)
+SET DALI_PATH=..\..\..\..\..\dali
+SET NEXUS_PATH=..\..\..\..\WebApi
+PUSHD %DALI_PATH%
 CALL build.bat %*
 POPD
-IF NOT "%1" == "Debug" (
-  SET PROD_ENV=1
-)
+PUSHD %NEXUS_PATH%
+CALL build.bat %*
+POPD
 IF NOT EXIST node_modules (
   SET UPDATE_NODE=1
 ) ELSE (
@@ -34,13 +55,13 @@ IF "%UPDATE_NODE%" == "1" (
   SET UPDATE_BUILD=1
   CALL npm install
 )
-IF NOT EXIST application (
+IF NOT EXIST library (
   SET UPDATE_BUILD=1
 ) ELSE (
   FOR /F %%i IN (
     'dir source /s/b/a-d ^| tr "\\" "/" ^| xargs ls -l --time-style=full-iso ^| awk "{print $6 $7}" ^| sort /R ^| head -1') DO (
     FOR /F %%j IN (
-      'dir application /s/b/a-d ^| tr "\\" "/" ^| xargs ls -l --time-style=full-iso ^| awk "{print $6 $7}" ^| sort /R ^| head -1') DO (
+      'dir library /s/b/a-d ^| tr "\\" "/" ^| xargs ls -l --time-style=full-iso ^| awk "{print $6 $7}" ^| sort /R ^| head -1') DO (
       IF "%%i" GEQ "%%j" (
         SET UPDATE_BUILD=1
       )
@@ -51,7 +72,7 @@ IF NOT EXIST node_modules\mod_time.txt (
   SET UPDATE_BUILD=1
 ) ELSE (
   FOR /F %%i IN (
-    'ls -l --time-style=full-iso %WEB_PORTAL_PATH%\node_modules\mod_time.txt ^| awk "{print $6 $7}"') DO (
+    'ls -l --time-style=full-iso tsconfig.json %NEXUS_PATH%\node_modules\mod_time.txt %DALI_PATH%\node_modules\mod_time.txt ^| awk "{print $6 $7}"') DO (
     FOR /F %%j IN (
       'ls -l --time-style=full-iso node_modules\mod_time.txt ^| awk "{print $6 $7}"') DO (
       IF "%%i" GEQ "%%j" (
@@ -61,12 +82,10 @@ IF NOT EXIST node_modules\mod_time.txt (
   )
 )
 IF "%UPDATE_BUILD%" == "1" (
-  IF EXIST application (
-    rm -rf application
+  IF EXIST library (
+    rm -rf library
   )
-  node .\node_modules\webpack\bin\webpack.js
-  cp -r ../../resources application
-  cp -r source/index.html application
+  CALL npm run build
   echo "timestamp" > node_modules\mod_time.txt
 )
 POPD
