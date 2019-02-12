9880547be58742b26ad13f449781bf19d7c689b3
Kamal <kamal@eidolonsystems.com>
2019-02-11 20:05:06 -0500

    Added stub code for CachedChartingModel unit tests.

---
+++ Applications/spire/source/spire_tester/cached_chart_model_tester.cpp
@@ -0,0 +1,22 @@
+#include <catch.hpp>
+
+TEST_CASE("test_right_no_overlap", "[CachedChartModel]") {
+}
+
+TEST_CASE("test_left_no_overlap", "[CachedChartModel]") {
+}
+
+TEST_CASE("test_right_overlap", "[CachedChartModel]") {
+}
+
+TEST_CASE("test_left_overlap", "[CachedChartModel]") {
+}
+
+TEST_CASE("test_subset", "[CachedChartModel]") {
+}
+
+TEST_CASE("test_single_superset", "[CachedChartModel]") {
+}
+
+TEST_CASE("test_multiple_supersets", "[CachedChartModel]") {
+}
