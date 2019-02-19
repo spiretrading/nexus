5ad884b6d361be9c487886e0c0e81d78f98dea1d
Ken <ken@spiretrading.com>
2019-02-19 11:56:14 -0500

    Cleaned out scratch project

--- Applications/spire/source/scratch/main.cpp
+++ Applications/spire/source/scratch/main.cpp
@@ -1,6 +1,4 @@
 #include <QApplication>
-#include <QWidget>
-#include "spire/spire/qt_promise.hpp"
 #include "spire/spire/resources.hpp"
 
 using namespace Spire;
@@ -10,18 +8,5 @@ int main(int argc, char** argv) {
   application->setOrganizationName(QObject::tr("Eidolon Systems Ltd"));
   application->setApplicationName(QObject::tr("Scratch"));
   initialize_resources();
-  auto w = new QWidget();
-  w->resize(500, 500);
-  w->show();
-  auto done = false;
-  auto promise = Spire::make_qt_promise([&] {
-    auto p = Spire::make_qt_promise([&] { return 2; });
-    p.then([&] (auto result) { done = true; });
-    while(!done) {
-      qDebug() << "not done";
-    }
-    return 2;
-  });
-  promise.then([=] (auto result) { qDebug() << "result is: " << result; });
   application->exec();
 }
