7d7cdbc999d62328d6ab3fcc5b04c6da6c24f68e
Ken <ken@spiretrading.com>
2019-02-19 16:40:18 -0500

    CachedChartModel tests pass with no caching used

--- Applications/spire/source/spire_tester/cached_chart_model_tester.cpp
+++ Applications/spire/source/spire_tester/cached_chart_model_tester.cpp
@@ -22,45 +22,143 @@ namespace {
       }());
     return local_model;
   }
+
+  template <typename T>
+  void run_test(T&& test, const QString& name) {
+    auto argc = 0;
+    auto app = QCoreApplication(argc, nullptr);
+    QTimer::singleShot(0,
+      [&] {
+        test();
+        app.exit();
+    });
+    app.exec();
+    qDebug() << "\n****************************************";
+    qDebug() << name << " PASSED";
+    qDebug() << "****************************************\n";
+  }
 }
 
 TEST_CASE("test_right_no_overlap", "[CachedChartModel]") {
-  auto argc = 0;
-  auto app = QCoreApplication(argc, nullptr);
-  QTimer::singleShot(0,
-    [&] {
-      auto model = create_model();
-      auto first = ChartValue(10 * Money::ONE);
-      auto last = ChartValue(20 * Money::ONE);
-      auto model_sticks = wait(model->load(first, last));
-      auto cache = CachedChartModel(*model);
-      auto cached_sticks = wait(cache.load(first, last));
-      REQUIRE(model_sticks == cached_sticks);
-      app.exit();
-  });
-  app.exec();
+  run_test([=] {
+    auto model = create_model();
+    auto model_sticks = wait(model->load(ChartValue(10 * Money::ONE),
+      ChartValue(20 * Money::ONE)));
+    auto cache = CachedChartModel(*model);
+    auto cached_sticks = wait(cache.load(ChartValue(10 * Money::ONE),
+      ChartValue(20 * Money::ONE)));
+    REQUIRE(model_sticks == cached_sticks);
+    model_sticks = wait(model->load(ChartValue(30 * Money::ONE),
+      ChartValue(40 * Money::ONE)));
+    auto test_sticks = wait(cache.load(ChartValue(30 * Money::ONE),
+      ChartValue(40 * Money::ONE)));
+    REQUIRE(test_sticks == model_sticks);
+  }, "test_right_no_overlap");
 }
 
 TEST_CASE("test_left_no_overlap", "[CachedChartModel]") {
-
+  run_test([=] {
+    auto model = create_model();
+    auto model_sticks = wait(model->load(ChartValue(20 * Money::ONE),
+      ChartValue(30 * Money::ONE)));
+    auto cache = CachedChartModel(*model);
+    auto cached_sticks = wait(cache.load(ChartValue(20 * Money::ONE),
+      ChartValue(30 * Money::ONE)));
+    REQUIRE(model_sticks == cached_sticks);
+    model_sticks = wait(model->load(ChartValue(10 * Money::ONE),
+      ChartValue(18 * Money::ONE)));
+    auto test_sticks = wait(cache.load(ChartValue(10 * Money::ONE),
+      ChartValue(18 * Money::ONE)));
+    REQUIRE(test_sticks == model_sticks);
+  }, "test_left_no_overlap");
 }
 
 TEST_CASE("test_right_overlap", "[CachedChartModel]") {
-
+  run_test([=] {
+    auto model = create_model();
+    auto model_sticks = wait(model->load(ChartValue(25 * Money::ONE),
+      ChartValue(50 * Money::ONE)));
+    auto cache = CachedChartModel(*model);
+    auto cached_sticks = wait(cache.load(ChartValue(25 * Money::ONE),
+      ChartValue(50 * Money::ONE)));
+    REQUIRE(model_sticks == cached_sticks);
+    model_sticks = wait(model->load(ChartValue(40 * Money::ONE),
+      ChartValue(60 * Money::ONE)));
+    auto test_sticks = wait(cache.load(ChartValue(40 * Money::ONE),
+      ChartValue(60 * Money::ONE)));
+    REQUIRE(test_sticks == model_sticks);
+  }, "test_right_overlap");
 }
 
 TEST_CASE("test_left_overlap", "[CachedChartModel]") {
-
+  run_test([=] {
+    auto model = create_model();
+    auto model_sticks = wait(model->load(ChartValue(25 * Money::ONE),
+      ChartValue(50 * Money::ONE)));
+    auto cache = CachedChartModel(*model);
+    auto cached_sticks = wait(cache.load(ChartValue(25 * Money::ONE),
+      ChartValue(50 * Money::ONE)));
+    REQUIRE(model_sticks == cached_sticks);
+    model_sticks = wait(model->load(ChartValue(10 * Money::ONE),
+      ChartValue(30 * Money::ONE)));
+    auto test_sticks = wait(cache.load(ChartValue(10 * Money::ONE),
+      ChartValue(30 * Money::ONE)));
+    REQUIRE(test_sticks == model_sticks);
+  }, "test_left_overlap");
 }
 
 TEST_CASE("test_subset", "[CachedChartModel]") {
-
+  run_test([=] {
+    auto model = create_model();
+    auto model_sticks = wait(model->load(ChartValue(25 * Money::ONE),
+      ChartValue(75 * Money::ONE)));
+    auto cache = CachedChartModel(*model);
+    auto cached_sticks = wait(cache.load(ChartValue(25 * Money::ONE),
+      ChartValue(75 * Money::ONE)));
+    REQUIRE(model_sticks == cached_sticks);
+    model_sticks = wait(model->load(ChartValue(40 * Money::ONE),
+      ChartValue(60 * Money::ONE)));
+    auto test_sticks = wait(cache.load(ChartValue(40 * Money::ONE),
+      ChartValue(60 * Money::ONE)));
+    REQUIRE(test_sticks == model_sticks);
+  }, "test_subset");
 }
 
 TEST_CASE("test_single_superset", "[CachedChartModel]") {
-
+  run_test([=] {
+    auto model = create_model();
+    auto model_sticks = wait(model->load(ChartValue(40 * Money::ONE),
+      ChartValue(60 * Money::ONE)));
+    auto cache = CachedChartModel(*model);
+    auto cached_sticks = wait(cache.load(ChartValue(40 * Money::ONE),
+      ChartValue(60 * Money::ONE)));
+    REQUIRE(model_sticks == cached_sticks);
+    model_sticks = wait(model->load(ChartValue(20 * Money::ONE),
+      ChartValue(80 * Money::ONE)));
+    auto test_sticks = wait(cache.load(ChartValue(20 * Money::ONE),
+      ChartValue(80 * Money::ONE)));
+    REQUIRE(test_sticks == model_sticks);
+  }, "test_single_superset");
 }
 
 TEST_CASE("test_multiple_supersets", "[CachedChartModel]") {
-
+  run_test([=] {
+    auto model = create_model();
+    auto model_sticks = wait(model->load(ChartValue(40 * Money::ONE),
+      ChartValue(60 * Money::ONE)));
+    auto cache = CachedChartModel(*model);
+    auto cached_sticks = wait(cache.load(ChartValue(40 * Money::ONE),
+      ChartValue(60 * Money::ONE)));
+    REQUIRE(model_sticks == cached_sticks);
+    model_sticks = wait(model->load(ChartValue(20 * Money::ONE),
+      ChartValue(80 * Money::ONE)));
+    auto test_sticks = wait(cache.load(ChartValue(20 * Money::ONE),
+      ChartValue(80 * Money::ONE)));
+    REQUIRE(test_sticks == model_sticks);
+    model_sticks = wait(model->load(ChartValue(10 * Money::ONE),
+      ChartValue(90 * Money::ONE)));
+    test_sticks = wait(cache.load(ChartValue(10 * Money::ONE),
+      ChartValue(90 * Money::ONE)));
+    REQUIRE(test_sticks == model_sticks);
+  }, "test_multiple_supersets");
 }
