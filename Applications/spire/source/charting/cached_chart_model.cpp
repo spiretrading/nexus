e1aac9dbb0fdf44412eccaeda825b0826a3e5938
Ken <ken@spiretrading.com>
2019-02-25 17:30:14 -0500

    Fixed orderings of comparator lambdas

--- Applications/spire/source/charting/cached_chart_model.cpp
+++ Applications/spire/source/charting/cached_chart_model.cpp
@@ -19,8 +19,7 @@ QtPromise<std::vector<Candlestick>> CachedChartModel::load(ChartValue first,
   auto range = ChartRange{first, last};
   auto range_iterator = std::lower_bound(m_ranges.begin(), m_ranges.end(),
     range, [] (const auto& value, const auto& search_value) {
-      return value.m_start > search_value.m_start &&
-        value.m_end < search_value.m_end;
+      return search_value.m_start < value.m_start;
     });
   if(range_iterator != m_ranges.end() && (*range_iterator).m_start <= first &&
       (*range_iterator).m_end >= last) {
@@ -33,7 +32,7 @@ QtPromise<std::vector<Candlestick>> CachedChartModel::load(ChartValue first,
       auto last_iterator = std::upper_bound(m_loaded_data.begin(),
         m_loaded_data.end(), last,
         [] (const auto& search_value, const auto& value) {
-          return value.GetEnd() > search_value;
+          return search_value < value.GetEnd();
       });
       return std::vector<Candlestick>(first_iterator, last_iterator);
     });
@@ -68,7 +67,7 @@ QtPromise<std::vector<Candlestick>> CachedChartModel::load_data(
         });
       auto range_last = std::lower_bound(m_ranges.begin(), m_ranges.end(),
         data, [] (const auto& lhs, const auto& rhs) {
-          return lhs.m_end < rhs.m_end;
+          return lhs.m_start < rhs.m_start;
         });
       auto range_pos = m_ranges.erase(range_first, range_last);
       m_ranges.insert(range_pos, data);
