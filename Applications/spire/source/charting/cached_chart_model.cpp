610ee803fe4b894e731c88eafdca37c10b6f480e
Ken <ken@spiretrading.com>
2019-02-25 15:14:28 -0500

    Code review changes v2

--- Applications/spire/source/charting/cached_chart_model.cpp
+++ Applications/spire/source/charting/cached_chart_model.cpp
@@ -16,24 +16,24 @@ ChartValue::Type CachedChartModel::get_y_axis_type() const {
 
 QtPromise<std::vector<Candlestick>> CachedChartModel::load(ChartValue first,
     ChartValue last) {
-  auto range = ChartRange({first, last});
+  auto range = ChartRange{first, last};
   auto range_iterator = std::lower_bound(m_ranges.begin(), m_ranges.end(),
-    range, [=] (const auto& value, const auto& search_value) {
-      return value.m_start >= search_value.m_start &&
-        value.m_end <= search_value.m_end;
+    range, [] (const auto& value, const auto& search_value) {
+      return value.m_start > search_value.m_start &&
+        value.m_end < search_value.m_end;
     });
   if(range_iterator != m_ranges.end() && (*range_iterator).m_start <= first &&
       (*range_iterator).m_end >= last) {
     return QtPromise([=] {
       auto first_iterator = std::lower_bound(m_loaded_data.begin(),
         m_loaded_data.end(), first,
-        [=] (const auto& value, const auto& search_value) {
+        [] (const auto& value, const auto& search_value) {
           return value.GetStart() < search_value;
       });
-      auto last_iterator = std::lower_bound(m_loaded_data.begin(),
+      auto last_iterator = std::upper_bound(m_loaded_data.begin(),
         m_loaded_data.end(), last,
-        [=] (const auto& value, const auto& search_value) {
-          return value.GetEnd() <= search_value;
+        [] (const auto& search_value, const auto& value) {
+          return value.GetEnd() > search_value;
       });
       return std::vector<Candlestick>(first_iterator, last_iterator);
     });
@@ -51,23 +51,23 @@ QtPromise<std::vector<Candlestick>> CachedChartModel::load_data(
   return m_chart_model->load(data.m_start, data.m_end).then(
     [=] (auto result) {
       auto first = std::lower_bound(m_loaded_data.begin(),
-        m_loaded_data.end(), data.m_start, [=] (const auto& lhs,
+        m_loaded_data.end(), data.m_start, [] (const auto& lhs,
             const auto& rhs) {
           return lhs.GetStart() < rhs;
         });
       auto last = std::lower_bound(m_loaded_data.begin(),
-        m_loaded_data.end(), data.m_end, [=] (const auto& lhs,
+        m_loaded_data.end(), data.m_end, [] (const auto& lhs,
             const auto& rhs) {
           return lhs.GetStart() < rhs;
         });
       auto pos = m_loaded_data.erase(first, last);
       m_loaded_data.insert(pos, result.Get().begin(), result.Get().end());
       auto range_first = std::lower_bound(m_ranges.begin(), m_ranges.end(),
-        data, [=] (const auto& lhs, const auto& rhs) {
+        data, [] (const auto& lhs, const auto& rhs) {
           return lhs.m_start < rhs.m_start;
         });
       auto range_last = std::lower_bound(m_ranges.begin(), m_ranges.end(),
-        data, [=] (const auto& lhs, const auto& rhs) {
+        data, [] (const auto& lhs, const auto& rhs) {
           return lhs.m_end < rhs.m_end;
         });
       auto range_pos = m_ranges.erase(range_first, range_last);
