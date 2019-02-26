ed2f0cbf2b762e9b05ca02f51032ff623102d206
Sofiya Polyanska <sofiya@spiretrading.com>
2019-02-26 15:02:38 -0500

    Finished styling for the input.

--- Applications/WebPortal/WebApp/library/source/pages/account_directory_page/group_selection_box.tsx
+++ Applications/WebPortal/WebApp/library/source/pages/account_directory_page/group_selection_box.tsx
@@ -1,11 +1,39 @@
 import { css, StyleSheet } from 'aphrodite';
 import * as React from 'react';
+import { DisplaySize } from '../../display_size';
 
-export class GroupSelectionBox extends React.Component<> {
+interface Properties {
+  displaySize: DisplaySize;
+  //selected groups
+  //groups model
+  error?: boolean;
+}
+
+export class GroupSelectionBox extends React.Component<Properties> {
 
   public render(): JSX.Element {
-    return <input type='text'
-    className={css(GroupSelectionBox.DYNAMIC_STYLE.boxSmall)}/>;
+    const boxStyle = (() => {
+      if(this.props.displaySize === DisplaySize.SMALL) {
+        return GroupSelectionBox.DYNAMIC_STYLE.boxSmall;
+      } else if(this.props.displaySize === DisplaySize.MEDIUM) {
+        return GroupSelectionBox.DYNAMIC_STYLE.boxMedium;
+      } else {
+        return GroupSelectionBox.DYNAMIC_STYLE.boxLarge;
+      }
+    })();
+    const errorBoxStyle = (() => {
+      if(this.props.error) {
+        return GroupSelectionBox.DYNAMIC_STYLE.error;
+      } else {
+        return null;
+      }
+    })();
+    return (
+      <div>
+      <input type='text'
+      className={css(boxStyle, errorBoxStyle,
+      GroupSelectionBox.DYNAMIC_STYLE.text)} />
+      </div>);
   }
 
   private static DYNAMIC_STYLE = StyleSheet.create({
@@ -14,7 +42,76 @@ export class GroupSelectionBox extends React.Component<> {
       paddingLeft: '10px',
       height: '34px',
       flexGrow: 1,
-      minWidth: '284px'
+      minWidth: '284px',
+      border: '1px solid #C8C8C8',
+      borderRadius: '1px',
+      ':focus': {
+        borderColor: '#684BC7',
+        ouline: 'none',
+        boxShadow: 'none',
+        webkitBoxShadow: 'none',
+        outlineColor: 'transparent',
+        outlineStyle: 'none'
+      },
+      ':active': {
+        borderColor: '#684BC7'
+      },
+      '::moz-focus-inner': {
+        border: 0
+      }
+    },
+    boxMedium: {
+      boxSizing: 'border-box' as 'border-box',
+      paddingLeft: '10px',
+      height: '34px',
+      flexGrow: 1,
+      minWidth: '284px',
+      border: '1px solid #C8C8C8',
+      borderRadius: '1px',
+      ':focus': {
+        borderColor: '#684BC7',
+        ouline: 'none',
+        boxShadow: 'none',
+        webkitBoxShadow: 'none',
+        outlineColor: 'transparent',
+        outlineStyle: 'none'
+      },
+      ':active': {
+        borderColor: '#684BC7'
+      },
+      '::moz-focus-inner': {
+        border: 0
+      }
+    },
+    boxLarge: {
+      boxSizing: 'border-box' as 'border-box',
+      paddingLeft: '10px',
+      height: '34px',
+      flexGrow: 1,
+      minWidth: '350px',
+      border: '1px solid #C8C8C8',
+      borderRadius: '1px',
+      ':focus': {
+        borderColor: '#684BC7',
+        ouline: 'none',
+        boxShadow: 'none',
+        webkitBoxShadow: 'none',
+        outlineColor: 'transparent',
+        outlineStyle: 'none'
+      },
+      ':active': {
+        borderColor: '#684BC7'
+      },
+      '::moz-focus-inner': {
+        border: 0
+      }
+    },
+    text: {
+      font: '400 14px Roboto',
+      color: '#000000'
+    },
+    error: {
+      border: '1px solid #E63F44'
     }
   });
 }
