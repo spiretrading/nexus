2c4a5158d0c0c9cffaff2dae7fbcd206512c87ac
Ken <ken@spiretrading.com>
2019-03-20 13:51:22 -0400

    Fixed insertion of completed promises' results in all()

--- Applications/spire/source/spire_tester/qt_promise_tester.cpp
+++ Applications/spire/source/spire_tester/qt_promise_tester.cpp
@@ -22,16 +22,16 @@ QtPromise<std::vector<T>> all(std::vector<QtPromise<T>> promises) {
   for(auto& promise : promises) {
     promises_ptr->push_back(std::move(promise));
   }
-  auto completed_promises = std::make_shared<std::vector<T>>(promises.size());
+  auto completed_promises = std::make_shared<std::vector<T>>();
   auto promise = std::move(promises_ptr->front());
   for(auto i = 0; i < promises_ptr->size() - 1; ++i) {
     promise = promise.then([=] (auto result) {
-      completed_promises->at(i) = result.Get();
+      completed_promises->push_back(std::move(result.Get()));
       return std::move(promises_ptr->at(i + 1));
     });
   }
   return promise.then([=] (auto result) {
-    completed_promises->back() = result.Get();
+    completed_promises->push_back(result.Get());
     return *completed_promises;
   });
 }
