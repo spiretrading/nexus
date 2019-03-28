9d84b6e7d7284f3359ae3d3a4c5a38b3bcc9ccfb
Ken <ken@spiretrading.com>
2019-03-28 15:41:53 -0400

    Fixed include for boost signals2, added print_test_name() to spire_tester

--- Applications/spire/include/spire/spire_tester/spire_tester.hpp
+++ Applications/spire/include/spire/spire_tester/spire_tester.hpp
@@ -6,6 +6,17 @@
 namespace Spire {
   class test_chart_model;
 
+  //! Prints a unit test's name using QDebug.
+  /*
+    \param name The unit test's name.
+  */
+  void print_test_name(const QString& name);
+
+  //! Runs a unit test that use QtPromises.
+  /*
+    \param test The unit test to run.
+    \param name The unit test's name.
+  */
   template <typename T>
   void run_test(T&& test, const QString& name) {
     auto argc = 0;
@@ -16,9 +27,7 @@ namespace Spire {
         app.exit();
     });
     app.exec();
-    qDebug() << "\n**************************************************";
-    qDebug() << name << " PASSED";
-    qDebug() << "**************************************************\n";
+    print_test_name(name);
   }
 }
 
