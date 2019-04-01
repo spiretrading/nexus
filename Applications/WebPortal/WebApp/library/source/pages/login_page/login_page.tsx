1b8f830d8721c14fbd2464afba0120a15e49d313
Sofiya Polyanska <sofiya@spiretrading.com>
2019-04-01 13:18:11 -0400

    Fixed the line wiggling issue.

--- Applications/WebPortal/WebApp/library/source/pages/login_page/login_page.tsx
+++ Applications/WebPortal/WebApp/library/source/pages/login_page/login_page.tsx
@@ -123,7 +123,8 @@ export class LoginPage extends React.Component<Properties> {
 
   private static readonly STATIC_STYLES = {
     page: {
-      backgroundColor: '#4B23A0'
+      backgroundColor: '#4B23A0',
+      boxSizing: 'border-box' as 'border-box'
     },
     logoVisible: {
       width: '130px',
@@ -144,7 +145,7 @@ export class LoginPage extends React.Component<Properties> {
 
   private static DYNAMIC_STYLES = StyleSheet.create({
     inputBox: {
-      boxSizing: 'border-box' as 'border-box',
+      boxSizing: 'border-box',
       width: '284px',
       padding: 0,
       height: '34px',
@@ -186,21 +187,21 @@ export class LoginPage extends React.Component<Properties> {
       }
     },
     signInButton: {
-      boxSizing: 'border-box' as 'border-box',
+      boxSizing: 'border-box',
       width: '284px',
       height: '48px',
       color: '#4B23A0',
       backgroundColor: '#E2E0FF',
       font: '400 20px Roboto',
       borderRadius: '1px',
-      border: 'none',
+      border: '1px solid #E2E0FF',
       outline: 0,
       '::-moz-focus-inner': {
         border: 0
       },
       ':disabled': {
         backgroundColor: '#684BC7',
-        border: 'none'
+        border: '1px solid #684BC7'
       },
       ':focus': {
         ':not(:disabled)': {
@@ -210,6 +211,7 @@ export class LoginPage extends React.Component<Properties> {
       },
       ':hover': {
         ':not(:disabled)': {
+          border: '1px solid white',
           backgroundColor: '#FFFFFF'
         }
       }
