4b75f9543a54e973c2b013ce585937d31a4fe103
Kamal Mansouri <kamal@eidolonsystems.com>
2019-04-16 18:30:57 -0400

    Added updated build config for Nexus.

---
+++ Nexus/Config/dependencies.windows.cmake
@@ -0,0 +1,4 @@
+include("${DEPENDENCIES_PATH}/Beam/Beam/Config/dependencies.cmake")
+set(NEXUS_INCLUDE_PATH "${CMAKE_CURRENT_LIST_DIR}/../Include")
+set(NEXUS_SOURCE_PATH "${CMAKE_CURRENT_LIST_DIR}/../Source")
+set(BEAM_INCLUDE_PATH "${DEPENDENCIES_PATH}/Beam/Beam/Include")
