0000f7e62a3ca30dbfc3ed94612762dea7cf9c6c
Kamal Mansouri <kamal@eidolonsystems.com>
2019-04-16 19:53:22 -0400

    Root scripts apply to all Applications.

--- run_cmake.bat
+++ run_cmake.bat
@@ -9,6 +9,24 @@ IF NOT EXIST build.bat (
 )
 CALL:run_cmake Nexus %*
 CALL:run_cmake Applications\AdministrationServer %*
+CALL:run_cmake Applications\AsxItchMarketDataFeedClient %*
+CALL:run_cmake Applications\ChartingServer %*
+CALL:run_cmake Applications\ChiaMarketDataFeedClient %*
+CALL:run_cmake Applications\ComplianceServer %*
+CALL:run_cmake Applications\CseMarketDataFeedClient %*
+CALL:run_cmake Applications\CtaMarketDataFeedClient %*
+CALL:run_cmake Applications\DefinitionsServer %*
+CALL:run_cmake Applications\MarketDataRelayServer %*
+CALL:run_cmake Applications\MarketDataServer %*
+CALL:run_cmake Applications\ReplayMarketDataFeedClient %*
+CALL:run_cmake Applications\RiskServer %*
+CALL:run_cmake Applications\SimulationMarketDataFeedClient %*
+CALL:run_cmake Applications\SimulationOrderExecutionServer %*
+CALL:run_cmake Applications\Spire %*
+CALL:run_cmake Applications\TmxIpMarketDataFeedClient %*
+CALL:run_cmake Applications\TmxTl1MarketDataFeedClient %*
+CALL:run_cmake Applications\UtpMarketDataFeedClient %*
+CALL:run_cmake Applications\WebPortal %*
 ENDLOCAL
 EXIT /B %ERRORLEVEL%
 
