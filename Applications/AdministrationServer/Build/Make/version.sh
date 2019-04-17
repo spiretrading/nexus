1c40f4cf23b50cd7591c78fe7ba4842e859d145c
Kamal <kamal@eidolonsystems.com>
2019-04-17 15:38:45 -0400

    Updated AdministrationServer to use new build config.

--- Applications/AdministrationServer/Build/Make/version.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -o errexit
-set -o pipefail
-mkdir -p ./../../Include/AdministrationServer
-printf "#define ADMINISTRATION_SERVER_VERSION \""> ./../../Include/AdministrationServer/Version.hpp
-git rev-list --count --first-parent HEAD | tr -d "\n" >> ./../../Include/AdministrationServer/Version.hpp
-printf \""\n" >> ./../../Include/AdministrationServer/Version.hpp
--- Nexus/setup.bat
+++ Nexus/setup.bat
@@ -13,8 +13,8 @@ IF NOT "%commit%" == "%beam_commit%" (
   git checkout %beam_commit%
 )
 CALL run_cmake.bat "-DD=%ROOT%"
-CALL Beam\build.bat Debug
-CALL Beam\build.bat Release
+CALL build.bat Debug
+CALL build.bat Release
 POPD
 SET commit=
 ENDLOCAL
