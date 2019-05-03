591f9000d87c8df5394824caf7cdf40e7132d1c9
Kamal <kamal@spiretrading.com>
2019-05-03 14:19:16 -0700

    WebApi builds on POSIX.

--- WebApi/configure.sh
+++ WebApi/configure.sh
@@ -13,17 +13,30 @@ fi
 if [ ! -f "configure.sh" ]; then
   ln -s "$directory/configure.sh" configure.sh
 fi
+for i in "$@"
+do
+case $i in
+  -DD=*)
+  dependencies="${i#*=}"
+  shift
+  ;;
+esac
+done
+if [ "$dependencies" == "" ]; then
+  dependencies="$root/Dependencies"
+fi
+if [ ! -d "$dependencies" ]; then
+  mkdir -p "$dependencies"
+fi
+pushd "$dependencies"
+"$directory"/setup.sh
+popd
+if [ "$dependencies" != "$root/Dependencies" ]; then
+  ln -s "$dependencies" Dependencies
+fi
 if [ "$directory" != "$root" ]; then
-  if [ -L "source" ]; then
+  if [ -d source ]; then
     rm -rf source
   fi
   ln -s "$directory/source" source
-  if [ -L "package.json" ]; then
-    rm package.json
-  fi
-  ln -s "$directory/package.json" package.json
-  if [ -L "tsconfig.json" ]; then
-    rm tsconfig.json
-  fi
-  ln -s "$directory/tsconfig.json" tsconfig.json
 fi
