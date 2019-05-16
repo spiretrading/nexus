26ae126784777a4d05f14a4c60cf2bb54e6c9907
Ken <ken@spiretrading.com>
2019-05-16 17:41:01 -0400

    Added stub methods for TrendLineModel

--- Applications/Spire/include/spire/charting/trend_line_model.hpp
+++ Applications/Spire/include/spire/charting/trend_line_model.hpp
@@ -1,5 +1,6 @@
 #ifndef SPIRE_TREND_LINE_MODEL_HPP
 #define SPIRE_TREND_LINE_MODEL_HPP
+#include "spire/charting/trend_line.hpp"
 
 namespace Spire {
 
@@ -7,10 +8,70 @@ namespace Spire {
   class TrendLineModel {
     public:
 
+      //! Signals that a trend line's property has changed.
+      using UpdateSignal = Signal<void ()>;
+
       //! Constructs an empty TrendLineModel.
       TrendLineModel() = default;
 
+      //! Adds the provided trend line to the model and returns its' ID.
+      /*
+        \param line The trend line to add to the model.
+      */
+      int add(const TrendLine& line);
 
+      //! Returns the trend line with the provided ID. Returns a default
+      //! TrendLine on failure.
+      /*
+        \param id The ID of the trend line to return.
+      */
+      TrendLine get(int id) const;
+
+      //! Returns the trend lines in the model.
+      std::vector<TrendLine> get_lines() const;
+
+      //! Returns a vector of IDs representing the selected trend lines.
+      std::vector<int> get_selected() const;
+
+      //! Returns the ID of the trend line that intersects the provided point.
+      //! Returns -1 if no line intersects the point.
+      /*
+        \param point The point to compre the trend lines to.
+      */
+      int intersects(const ChartPoint& point) const;
+
+      //! Removes the trend line with the provided ID from the model.
+      //! Does nothing if the provided id doesn't exist.
+      /*
+        \param id The id of the trend line to remove.
+      */
+      void remove(int id);
+
+      //! Sets a trend lines status to selected.
+      /*
+        \param id The ID of the trend line to set as selected.
+      */
+      void set_selected(int id);
+
+      //! Sets a trend lines status to unselected.
+      /*
+        \param id the ID of the trend line to set as unselected.
+      */
+      void unset_selected(int id);
+
+      //! Replaces the trend line at the specified id with the provided trend
+      //! line. Does nothing if the specified ID doesn't exist.
+      /*
+        \param line The updated trend line.
+        \param id The id of the trend line to replace.
+      */
+      void update(const TrendLine& line, int id);
+
+      boost::signals2::connection connect_update_signal(
+        const UpdateSignal::slot_type& slot) const;
+
+    private:
+      mutable UpdateSignal m_update_signal;
   };
 }
 
--- Applications/Spire/source/charting/trend_line_model.cpp
+++ Applications/Spire/source/charting/trend_line_model.cpp
@@ -1,3 +1,45 @@
 #include "spire/charting/trend_line_model.hpp"
 
+using namespace boost::signals2;
 using namespace Spire;
+
+int TrendLineModel::add(const TrendLine& line) {
+
+}
+
+TrendLine TrendLineModel::get(int id) const {
+
+}
+
+std::vector<TrendLine> TrendLineModel::get_lines() const {
+
+}
+
+std::vector<int> TrendLineModel::get_selected() const {
+
+}
+
+int TrendLineModel::intersects(const ChartPoint& point) const {
+
+}
+
+void TrendLineModel::remove(int id) {
+
+}
+
+void TrendLineModel::set_selected(int id) {
+
+}
+
+void TrendLineModel::unset_selected(int id) {
+
+}
+
+void TrendLineModel::update(const TrendLine& line, int id) {
+
+}
+
+connection TrendLineModel::connect_update_signal(
+    const UpdateSignal::slot_type& slot) const {
+  return m_update_signal.connect(slot);
+}
