2bc0616422bc757e09b65e0496c6bb2d490d0c8b
Ken <ken@spiretrading.com>
2019-05-24 15:54:16 -0400

    code review changes v2

--- Applications/Spire/source/charting/trend_line_model.cpp
+++ Applications/Spire/source/charting/trend_line_model.cpp
@@ -6,9 +6,8 @@ using namespace Spire;
 
 namespace {
   Quantity slope(const TrendLine& line) {
-    return Quantity(
-      (std::get<1>(line.m_points).m_y - std::get<0>(line.m_points).m_y) /
-      (std::get<1>(line.m_points).m_x - std::get<0>(line.m_points).m_x));
+    return (std::get<1>(line.m_points).m_y - std::get<0>(line.m_points).m_y) /
+      (std::get<1>(line.m_points).m_x - std::get<0>(line.m_points).m_x);
   }
 
   Quantity distance_squared(Quantity x1, Quantity y1, Quantity x2,
@@ -19,7 +18,8 @@ namespace {
   bool is_within_interval(Quantity value, Quantity interval_start,
       Quantity interval_end, Quantity threshold) {
     if(interval_start > interval_end) {
-      is_within_interval(value, interval_end, interval_start, threshold);
+      return is_within_interval(value, interval_end, interval_start,
+        threshold);
     }
     return interval_start - threshold <= value && value <=
       interval_end + threshold;
@@ -27,16 +27,15 @@ namespace {
 
   bool is_within_interval(Quantity value, Quantity interval_start,
       Quantity interval_end) {
-    return is_within_interval(value, interval_start, interval_end,
-      Quantity(0));
+    return is_within_interval(value, interval_start, interval_end, 0);
   }
 
   Quantity y_intercept(Quantity x, Quantity y, Quantity slope) {
-    return y - (x * slope);
+    return y - x * slope;
   }
 
   Quantity calculate_y(Quantity m, Quantity x, Quantity b) {
-    return (m * x) + b;
+    return m * x + b;
   }
 
   Quantity closest_point_distance_squared(Quantity x, Quantity y,
@@ -55,15 +54,14 @@ TrendLineModel::TrendLineModel()
   : m_last_id(0) {}
 
 int TrendLineModel::add(const TrendLine& line) {
-  m_trend_lines.emplace_back(TrendLineEntry{line,
-    ++m_last_id, State::UNSELECTED});
+  m_trend_lines.push_back({line, ++m_last_id, State::UNSELECTED});
   return m_last_id;
 }
 
 TrendLine TrendLineModel::get(int id) {
   auto iter = find_id(id);
   if(iter == m_trend_lines.end()) {
-    throw std::exception("TrendLine not found.");
+    throw std::invalid_argument("TrendLine not found.");
   }
   return iter->m_trend_line;
 }
@@ -98,38 +96,30 @@ int TrendLineModel::intersects(const ChartPoint& point,
     auto dist_squared = std::numeric_limits<Quantity>::infinity();
     auto point_distance_squared = std::numeric_limits<Quantity>::infinity();
     auto line_slope = slope(line.m_trend_line);
-    auto line_b = y_intercept(
-      static_cast<Quantity>(std::get<0>(line.m_trend_line.m_points).m_x),
-      static_cast<Quantity>(std::get<0>(line.m_trend_line.m_points).m_y),
-      line_slope);
+    auto line_x1 = static_cast<Quantity>(
+      std::get<0>(line.m_trend_line.m_points).m_x);
+    auto line_y1 = static_cast<Quantity>(
+      std::get<0>(line.m_trend_line.m_points).m_y);
+    auto line_x2 = static_cast<Quantity>(
+      std::get<1>(line.m_trend_line.m_points).m_x);
+    auto line_y2 = static_cast<Quantity>(
+      std::get<1>(line.m_trend_line.m_points).m_y);
+    auto line_b = y_intercept(line_x1, line_y1, line_slope);
     point_distance_squared = closest_point_distance_squared(point_x,
       point_y, line.m_trend_line);
     if(std::isinf(static_cast<double>(line_slope))) {
-      if(is_within_interval(point_y,
-          static_cast<Quantity>(std::get<0>(
-          line.m_trend_line.m_points).m_y),
-          static_cast<Quantity>(std::get<1>(
-          line.m_trend_line.m_points).m_y))) {
-        dist_squared = distance_squared(point_x, point_y,
-          static_cast<Quantity>(std::get<0>(line.m_trend_line.m_points).m_x),
-          point_y);
+      if(is_within_interval(point_y, line_y1, line_y2)) {
+        dist_squared = distance_squared(point_x, point_y, line_x1, point_y);
       }
     } else if(line_slope == Quantity(0)) {
-      if(is_within_interval(point_x,
-          static_cast<Quantity>(std::get<0>(
-          line.m_trend_line.m_points).m_x),
-          static_cast<Quantity>(std::get<1>(
-          line.m_trend_line.m_points).m_x))) {
-        dist_squared = distance_squared(point_x, point_y, point_x,
-          static_cast<Quantity>(std::get<0>(line.m_trend_line.m_points).m_y));
+      if(is_within_interval(point_x, line_x1, line_x2)) {
+        dist_squared = distance_squared(point_x, point_y, point_x, line_y1);
       }
     } else {
       auto line_point_x =
-        (point_x + (line_slope * point_y) - (line_slope * line_b)) /
-        ((line_slope * line_slope) + 1);
-      if(is_within_interval(line_point_x,
-          static_cast<Quantity>(std::get<0>(line.m_trend_line.m_points).m_x),
-          static_cast<Quantity>(std::get<1>(line.m_trend_line.m_points).m_x))) {
+        (point_x + line_slope * point_y - line_slope * line_b) /
+        (line_slope * line_slope + 1);
+      if(is_within_interval(line_point_x, line_x1, line_x2)) {
         dist_squared = distance_squared(point_x, point_y,
           line_point_x, calculate_y(line_slope, line_point_x, line_b));
       }
@@ -149,6 +139,7 @@ void TrendLineModel::remove(int id) {
   if(iter != m_trend_lines.end()) {
     m_trend_lines.erase(iter);
   }
+  m_update_signal();
 }
 
 void TrendLineModel::set_selected(int id) {
@@ -156,6 +147,7 @@ void TrendLineModel::set_selected(int id) {
   if(iter != m_trend_lines.end()) {
     iter->m_state = State::SELECTED;
   }
+  m_update_signal();
 }
 
 void TrendLineModel::unset_selected(int id) {
@@ -163,6 +155,7 @@ void TrendLineModel::unset_selected(int id) {
   if(iter != m_trend_lines.end()) {
     iter->m_state = State::UNSELECTED;
   }
+  m_update_signal();
 }
 
 void TrendLineModel::update(const TrendLine& line, int id) {
@@ -170,6 +163,7 @@ void TrendLineModel::update(const TrendLine& line, int id) {
   if(iter != m_trend_lines.end()) {
     iter->m_trend_line = line;
   }
+  m_update_signal();
 }
 
 connection TrendLineModel::connect_update_signal(
