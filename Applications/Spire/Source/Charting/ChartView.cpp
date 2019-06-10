dcb9eff3fe8c759cf884beba6e5d49e5ff233720
Ken <ken@spiretrading.com>
2019-06-10 17:10:58 -0400

    Fixed crash when deleting hovered trend line

--- Applications/Spire/Source/Charting/ChartView.cpp
+++ Applications/Spire/Source/Charting/ChartView.cpp
@@ -231,12 +231,12 @@ void ChartView::set_trend_line_style(TrendLineStyle style) {
 void ChartView::remove_selected_trend_lines() {
   if(m_draw_state == DrawState::NEW) {
     m_trend_line_model.remove(m_current_trend_line_id);
-    m_draw_state = DrawState::IDLE;
   } else {
     for(auto id : m_trend_line_model.get_selected()) {
       m_trend_line_model.remove(id);
     }
   }
+  m_draw_state = DrawState::IDLE;
   update();
 }
 
