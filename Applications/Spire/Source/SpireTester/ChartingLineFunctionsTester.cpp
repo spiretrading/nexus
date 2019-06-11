c69746fd396ace48cee189d6811b47f8b29e7453
Ken <ken@spiretrading.com>
2019-06-11 11:33:29 -0400

    Added stub tests for Charting math functions

---
+++ Applications/Spire/Source/SpireTester/ChartingLineFunctionsTester.cpp
@@ -0,0 +1,32 @@
+#include <catch.hpp>
+#include "Spire/Charting/Charting.hpp"
+#include "Spire/Charting/ChartPoint.hpp"
+#include "Spire/Charting/ChartValue.hpp"
+#include "Spire/Charting/TrendLine.hpp"
+#include "Spire/SpireTester/SpireTester.hpp"
+
+using namespace Spire;
+
+TEST_CASE("test_slope", "[Charting]") {
+  print_test_name("test_slope");
+}
+
+TEST_CASE("test_distance_squared", "[Charting]") {
+  print_test_name("test_distance_squared");
+}
+
+TEST_CASE("test_is_within_interval", "[Charting]") {
+  print_test_name("test_is_within_interval");
+}
+
+TEST_CASE("test_y_intercept", "[Charting]") {
+  print_test_name("test_y_intercept");
+}
+
+TEST_CASE("test_calculate_y", "[Charting]") {
+  print_test_name("test_calculate_y");
+}
+
+TEST_CASE("test_closest_point_distance_squared", "[Charting]") {
+  print_test_name("test_closest_point_distance_squared");
+}
