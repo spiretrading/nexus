a48cd7627721ddd5625c532a70ed315dde7adc2b
Ken <ken@spiretrading.com>
2019-06-11 16:55:03 -0400

    Code cleanup

--- Applications/Spire/Source/Charting/ChartView.cpp
+++ Applications/Spire/Source/Charting/ChartView.cpp
@@ -226,9 +226,7 @@ void ChartView::remove_selected_trend_lines() {
   if(m_draw_state == DrawState::NEW) {
     m_trend_line_model.remove(m_current_trend_line_id);
   } else {
-    for(auto id : m_trend_line_model.get_selected()) {
-      m_trend_line_model.remove(id);
-    }
+    m_trend_line_model.clear_selected();
   }
   m_draw_state = DrawState::IDLE;
   update();
