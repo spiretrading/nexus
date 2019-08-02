fcdb0f6531187e8ccfb3fb541048dcce6487c495
Ken <ken@spiretrading.com>
2019-08-02 11:18:47 -0400

    Added tests for LocalChartModel storing and loading

--- Applications/Spire/Source/SpireTester/LocalChartModelTester.cpp
+++ Applications/Spire/Source/SpireTester/LocalChartModelTester.cpp
@@ -8,10 +8,19 @@ using namespace Nexus;
 using namespace Spire;
 
 namespace {
-  std::vector<Candlestick> load(LocalChartModel model, int first, int last,
+  std::vector<Candlestick> load(LocalChartModel* model, int first, int last,
       const SnapshotLimit& limit) {
-    return std::move(wait(model.load(ChartValue(first * Money::ONE),
+    return std::move(wait(model->load(ChartValue(first * Money::ONE),
       ChartValue(last * Money::ONE), limit)));
+  }
+
+  std::vector<Candlestick> generate_range(int first, int last) {
+    auto candlesticks = std::vector<Candlestick>();
+    for(auto i = first; i < last; ++i) {
+      candlesticks.push_back({ChartValue(i), ChartValue(i + 1), ChartValue(2),
+        ChartValue(6), ChartValue(8), ChartValue(0)});
+    }
+    return candlesticks;
   }
 }
 
@@ -19,6 +28,21 @@ TEST_CASE("test_storing_and_loading", "") {
   run_test([=] {
     auto model = LocalChartModel(ChartValue::Type::MONEY,
       ChartValue::Type::MONEY, {});
-
+    auto load1 = load(&model, 0, 100, SnapshotLimit::Unlimited());
+    REQUIRE(load1.size() == 0);
+    model.store(generate_range(10, 20));
+    auto load2 = load(&model, 0, 25, SnapshotLimit::Unlimited());
+    REQUIRE(load2.size() == 10);
+    REQUIRE(load2.front().GetStart() == ChartValue(10));
+    REQUIRE(load2.back().GetEnd() == ChartValue(20));
+    model.store(generate_range(30, 40));
+    auto load3 = load(&model, 25, 50, SnapshotLimit::Unlimited());
+    REQUIRE(load3.size() == 10);
+    REQUIRE(load3.front().GetStart() == ChartValue(30));
+    REQUIRE(load3.back().GetEnd() == ChartValue(40));
+    auto load4 = load(&model, 0, 100, SnapshotLimit::Unlimited());
+    REQUIRE(load4.size() == 20);
+    REQUIRE(load4.front().GetStart() == ChartValue(10));
+    REQUIRE(load4.back().GetEnd() == ChartValue(40));
   }, "test_storing_and_loading");
 }
