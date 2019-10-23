ef3265da3be8e0fd76efcd245b5c99b49cde820b
Sofiya Polyanska <sofiya@spiretrading.com>
2019-10-23 12:14:51 -0400

    Added a blank compliance page and a index file.

---
+++ Applications/WebPortal/WebApp/library/source/pages/account_page/compliance_page/compliance_page.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import * as Nexus from 'nexus';
+
+export class CompliancePage extends React.Component<> {
+  public render(): JSX.Element {
+    return <div/>;
+  }
+}
