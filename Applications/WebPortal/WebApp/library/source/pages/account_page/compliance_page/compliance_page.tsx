c85d7d56cf50434886c3ccf31a10b98abba9612a
Sofiya Polyanska <sofiya@spiretrading.com>
2019-10-24 16:18:03 -0400

    Added styling for mobile viewport.

--- Applications/WebPortal/WebApp/library/source/pages/account_page/compliance_page/compliance_page.tsx
+++ Applications/WebPortal/WebApp/library/source/pages/account_page/compliance_page/compliance_page.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { DisplaySize } from '../../../display_size';
 import { RuleExecutionDropDown, RuleMode } from './rule_execution_drop_down';
+import { RuleRow } from './rule_row';
 
 
 interface Properties {
@@ -9,29 +10,20 @@ interface Properties {
   displaySize: DisplaySize;
 }
 
-interface State {
-  ruleMode: RuleMode;
-}
-
 /* Displays the compliance page.*/
-export class CompliancePage extends React.Component<Properties, State> {
+export class CompliancePage extends React.Component<Properties> {
   constructor(props: Properties) {
     super(props);
     this.state = {
       ruleMode: RuleMode.PASSIVE
     };
-    this.onChange = this.onChange.bind(this);
+    
   }
 
   public render(): JSX.Element {
     return( 
-      <RuleExecutionDropDown 
-        displaySize={this.props.displaySize}
-        value={this.state.ruleMode}
-        onChange={this.onChange}/>);
+      <RuleRow displaySize={this.props.displaySize}/>);
   }
 
-  private onChange(newMode: RuleMode) {
-    this.setState({ruleMode: newMode});
-  }
+
 }
