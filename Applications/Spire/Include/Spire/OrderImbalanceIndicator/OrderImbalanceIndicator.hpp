505c5cff0b2099dcf910e040e77764db186b8238
Ken <ken@spiretrading.com>
2019-11-06 14:44:08 -0500

    Added order imbalance model files

---
+++ Applications/Spire/Include/Spire/OrderImbalanceIndicator/OrderImbalanceIndicator.hpp
@@ -0,0 +1,12 @@
+#ifndef SPIRE_ORDER_IMBALANCE_INDICATOR_HPP
+#define SPIRE_ORDER_IMBALANCE_INDICATOR_HPP
+#include "Spire/Spire/Spire.hpp"
+
+namespace Spire {
+  class CachedOrderImbalanceIndicatorModel;
+  class FilterOrderImbalanceIndicatorModel;
+  class LocalOrderImbalanceIndicatorModel;
+  class OrderImbalanceIndicatorModel;
+}
+
+#endif
