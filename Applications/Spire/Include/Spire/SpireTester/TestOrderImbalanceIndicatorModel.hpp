4b7486e50e83b58dea099cd689a04b03f2d810a2
Ken <ken@spiretrading.com>
2019-11-12 14:02:28 -0500

    Added TestOrderImbalanceIndicatorModel

---
+++ Applications/Spire/Include/Spire/SpireTester/TestOrderImbalanceIndicatorModel.hpp
@@ -0,0 +1,67 @@
+#ifndef SPIRE_TEST_ORDER_IMBALANCE_INDICATOR_MODEL_HPP
+#define SPIRE_TEST_ORDER_IMBALANCE_INDICATOR_MODEL_HPP
+#include <Beam/Threading/ConditionVariable.hpp>
+#include <Beam/Threading/Mutex.hpp>
+#include "Spire/OrderImbalanceIndicator/OrderImbalanceIndicatorModel.hpp"
+
+namespace Spire {
+
+  class TestOrderImbalanceIndicatorModel :
+      public OrderImbalanceIndicatorModel {
+    public:
+
+      class SubscribeEntry {
+        public:
+
+          //! Constructs a SubscribeEntry.
+          /*
+            \param start The start time to begin loading from.
+            \param end The end time to load until.
+          */
+          SubscribeEntry(const boost::posix_time::ptime& start,
+            const boost::posix_time::ptime& end);
+
+          //! Returns the start time of the range to load.
+          const boost::posix_time::ptime& get_start() const;
+
+          //! Returns the end time of the range to load.
+          const boost::posix_time::ptime& get_end() const;
+
+          //! Sets the result of the subscribe operation.
+          /*
+            \param result The list of imbalances that the promise loading the
+                   specified range should evalute to.
+          */
+          void set_result(std::vector<Nexus::OrderImbalance> result);
+
+          //! Returns the order imbalance to load.
+          std::vector<Nexus::OrderImbalance>& get_result();
+
+        private:
+          friend class TestOrderImbalanceIndicatorModel;
+          mutable Beam::Threading::Mutex m_mutex;
+          boost::posix_time::ptime m_start;
+          boost::posix_time::ptime m_end;
+          bool m_is_loaded;
+          std::vector<Nexus::OrderImbalance> m_result;
+          Beam::Threading::ConditionVariable m_subscribe_condition;
+      };
+
+      std::tuple<boost::signals2::connection,
+        QtPromise<std::vector<Nexus::OrderImbalance>>>
+        subscribe(const boost::posix_time::ptime& start,
+          const boost::posix_time::ptime& end,
+          const OrderImbalanceSignal::slot_type& slot) override;
+
+      //! Pops the oldest subscribe request from this model's subcribe
+      //! operation stack.
+      QtPromise<std::shared_ptr<SubscribeEntry>> pop_subscribe();
+
+    private:
+      Beam::Threading::Mutex m_mutex;
+      Beam::Threading::ConditionVariable m_subscribe_condition;
+      std::deque<std::shared_ptr<SubscribeEntry>> m_subscribe_entries;
+  };
+}
+
+#endif
