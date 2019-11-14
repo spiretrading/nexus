6b31f612d92241209039227a3658132844d99368
Ken <ken@spiretrading.com>
2019-11-14 16:17:45 -0500

    Added OrderImbalanceIndicatorModel

---
+++ Applications/Spire/Include/Spire/OrderImbalanceIndicator/OrderImbalanceIndicator.hpp
@@ -0,0 +1,9 @@
+#ifndef SPIRE_ORDER_IMBALANCE_INDICATOR_HPP
+#define SPIRE_ORDER_IMBALANCE_INDICATOR_HPP
+#include "Spire/Spire/Spire.hpp"
+
+namespace Spire {
+  class OrderImbalanceIndicatorModel;
+}
+
+#endif
---
+++ Applications/Spire/Include/spire/OrderImbalanceIndicator/OrderImbalanceIndicatorModel.hpp
@@ -0,0 +1,34 @@
+#ifndef SPIRE_ORDER_IMBALANCE_INDICATOR_MODEL_HPP
+#define SPIRE_ORDER_IMBALANCE_INDICATOR_MODEL_HPP
+#include "Spire/OrderImbalanceIndicator/OrderImbalanceIndicator.hpp"
+#include "Nexus/Definitions/OrderImbalance.hpp"
+
+namespace Spire {
+
+  //! Provides a source for published order imbalances.
+  class OrderImbalanceIndicatorModel {
+    public:
+
+      //! Signals that an order imbalance has been published.
+      /*!
+        \param imbalance The published imbalance.
+      */
+      using OrderImbalanceSignal =
+        Signal<void (const Nexus::OrderImbalance& imbalance)>;
+
+      //! Returns all order imbalances between the given start and end times,
+      //! and calls the given slot when a new imbalance is published.
+      /*
+        \param start Start timestamp of the loaded range.
+        \param end End timestamp of the loaded range.
+        \param slot Slot called when a new imbalance is published.
+      */
+      virtual std::tuple<boost::signals2::connection,
+        QtPromise<std::vector<Nexus::OrderImbalance>>>
+        subscribe(const boost::posix_time::ptime& start,
+          const boost::posix_time::ptime& end,
+          const OrderImbalanceSignal::slot_type& slot) = 0;
+  };
+}
+
+#endif
