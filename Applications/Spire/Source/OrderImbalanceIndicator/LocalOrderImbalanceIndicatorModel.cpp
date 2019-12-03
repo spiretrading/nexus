9444e92884b2f98d54304913422d2a2320198c51
Ken <ken@spiretrading.com>
2019-12-03 15:50:09 -0500

    Code review changes

--- Applications/Spire/Source/OrderImbalanceIndicator/LocalOrderImbalanceIndicatorModel.cpp
+++ Applications/Spire/Source/OrderImbalanceIndicator/LocalOrderImbalanceIndicatorModel.cpp
@@ -1,6 +1,8 @@
 #include "Spire/OrderImbalanceIndicator/LocalOrderImbalanceIndicatorModel.hpp"
 #include "Spire/Spire/Utility.hpp"
 
+using namespace boost;
+using namespace boost::icl;
 using namespace Nexus;
 using namespace Spire;
 
@@ -17,42 +19,42 @@ void LocalOrderImbalanceIndicatorModel::insert(
 
 QtPromise<std::vector<Nexus::OrderImbalance>>
     LocalOrderImbalanceIndicatorModel::load(const TimeInterval& interval) {
-  auto first = std::vector<OrderImbalance>::iterator();
-  auto last = std::vector<OrderImbalance>::iterator();
-  if(boost::icl::is_left_closed(interval.bounds())) {
-    first = std::lower_bound(m_imbalances.begin(), m_imbalances.end(),
-      interval.lower(), [] (const auto& imbalance, const auto& timestamp) {
-        return imbalance.m_timestamp < timestamp;
-      });
-  } else {
-    first = std::upper_bound(m_imbalances.begin(), m_imbalances.end(),
+  auto first = [&] {
+    if(is_left_closed(interval.bounds())) {
+      return std::lower_bound(m_imbalances.begin(), m_imbalances.end(),
+        interval.lower(), [] (const auto& imbalance, const auto& timestamp) {
+          return imbalance.m_timestamp < timestamp;
+        });
+    }
+    return std::upper_bound(m_imbalances.begin(), m_imbalances.end(),
       interval.lower(), [] (const auto& timestamp, const auto& imbalance) {
         return imbalance.m_timestamp > timestamp;
       });
-  }
-  if(boost::icl::is_right_closed(interval.bounds())) {
-    last = std::upper_bound(m_imbalances.begin(), m_imbalances.end(),
-      interval.upper(), [] (const auto& timestamp, const auto& imbalance) {
-        return imbalance.m_timestamp > timestamp;
-      });
-  } else {
-    last = std::lower_bound(m_imbalances.begin(), m_imbalances.end(),
+  }();
+  auto last = [&] {
+    if(is_right_closed(interval.bounds())) {
+     return std::upper_bound(first, m_imbalances.end(),
+        interval.upper(), [] (const auto& timestamp, const auto& imbalance) {
+          return imbalance.m_timestamp > timestamp;
+        });
+    }
+    return std::lower_bound(first, m_imbalances.end(),
       interval.upper(), [] (const auto& imbalance, const auto& timestamp) {
         return imbalance.m_timestamp < timestamp;
       });
-  }
+  }();
   return QtPromise(
     [imbalances = std::vector<OrderImbalance>(first, last)] () mutable {
       return std::move(imbalances);
   });
 }
 
-SubscriptionResult<boost::optional<Nexus::OrderImbalance>>
+SubscriptionResult<optional<Nexus::OrderImbalance>>
     LocalOrderImbalanceIndicatorModel::subscribe(
     const OrderImbalanceSignal::slot_type& slot) {
-  auto last_imbalance = [&] () -> boost::optional<Nexus::OrderImbalance> {
+  auto last_imbalance = [&] () -> optional<Nexus::OrderImbalance> {
     if(m_imbalances.empty()) {
-      return boost::none;
+      return none;
     }
     return m_imbalances.back();
   }();
@@ -69,6 +71,11 @@ std::shared_ptr<OrderImbalanceChartModel>
 
 void LocalOrderImbalanceIndicatorModel::insert_sorted(
     const OrderImbalance& imbalance) {
+  if(m_imbalances.empty() ||
+      m_imbalances.back().m_timestamp < imbalance.m_timestamp) {
+    m_imbalances.push_back(imbalance);
+    return;
+  }
   auto index = std::lower_bound(m_imbalances.begin(), m_imbalances.end(),
     imbalance.m_timestamp,
     [] (const auto& stored_imbalance, const auto& timestamp) {
