efd21d10b4a316b518ea2497c2f4ed8b2dfc9782
Sofiya Polyanska <sofiya@spiretrading.com>
2019-12-06 14:13:26 -0500

    Restructured the component.

--- Applications/WebPortal/WebApp/library/source/pages/page_wrapper.tsx
+++ Applications/WebPortal/WebApp/library/source/pages/page_wrapper.tsx
@@ -1,33 +1,33 @@
 import * as React from 'react';
 
-/** Returns a page wrapped in basic
- * @param BaseComponent - A component that needs proper scrolling behavior.
- */
-export function pageWrapper(BaseComponent: React.ComponentType<any>) {
-  return class extends React.Component {
-    public render() {
-      return (
-        <div style={this.STYLE.page}> 
-          <BaseComponent {...this.props}/>
-        </div>);
-    }
-  
-    readonly STYLE = {
-      page: {
-        boxSizing: 'border-box' as 'border-box',
-        width: '100%',
-        height: '100%',
-        display: 'flex' as 'flex',
-        flex: 'column' as 'column',
-        flexWrap: 'nowrap' as 'nowrap',
-        overflowX: 'auto' as 'auto',
-        overflowY: 'auto' as 'auto',
-        justifyContent: 'center' as 'center',
-        alignItems: 'flex-start' as 'flex-start',
-        margin: 0,
-        padding: 0,
-        minWidth: '320px'
-      }
-    };
-  };
+interface Properties {
+  children: Element | React.Component;
 }
+
+export class PageWrapper extends React.Component {
+  public render(): JSX.Element {
+    return (
+      <div style={this.STYLE.page}>
+        {this.props.children}
+      </div>);
+  }
+
+  readonly STYLE = {
+    page: {
+      boxSizing: 'border-box' as 'border-box',
+      width: '100%',
+      height: '100%',
+      display: 'flex' as 'flex',
+      flex: 'column' as 'column',
+      flexWrap: 'nowrap' as 'nowrap',
+      overflowX: 'auto' as 'auto',
+      overflowY: 'auto' as 'auto',
+      justifyContent: 'center' as 'center',
+      alignItems: 'flex-start' as 'flex-start',
+      margin: 0,
+      padding: 0,
+      minWidth: '320px'
+    }
+  };
+};
+
