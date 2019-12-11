70cb7b9757b8c60d63d6367957cd0af46168ef3b
Sofiya Polyanska <sofiya@spiretrading.com>
2019-12-11 15:03:03 -0500

    Styled the button and the status box.

--- Applications/WebPortal/WebApp/library/source/pages/account_page/compliance_page/compliance_page.tsx
+++ Applications/WebPortal/WebApp/library/source/pages/account_page/compliance_page/compliance_page.tsx
@@ -1,3 +1,4 @@
+import { css, StyleSheet } from 'aphrodite/no-important';
 import * as Nexus from 'nexus';
 import * as React from 'react';
 import { DisplaySize } from '../../../display_size';
@@ -49,6 +50,14 @@ export class CompliancePage extends React.Component<Properties, State> {
         return CompliancePage.STYLE.largeContent;
       }
     })();
+    const status = (() => {
+      if(true) {
+        return(
+          <div style={CompliancePage.EXTRA_STYLE.button}>
+            Saved
+          </div>);
+      }
+    })();
     return (
       <div style={contentStyle}>
         <RulesList
@@ -63,11 +72,13 @@ export class CompliancePage extends React.Component<Properties, State> {
           onAddNewRule={this.props.onRuleAdd}
           schemas={this.props.schemas}/>
         <div style={CompliancePage.STYLE.paddingLarge}/>
-        <HLine/>
+        <HLine color='#E6E6E6'/>
         <div style={CompliancePage.STYLE.paddingLarge}/>
-        <button/>
-        <div style={CompliancePage.STYLE.paddingMedium}/>
-        <div>{'STATUS!'}</div>
+        <button className={css(CompliancePage.EXTRA_STYLE.button)}>
+          {'Save Changes'}
+        </button>
+        <div style={CompliancePage.STYLE.paddingSmall}/>
+        {status}
       </div>);
   }
 
@@ -76,6 +87,10 @@ export class CompliancePage extends React.Component<Properties, State> {
   }
 
   private static readonly STYLE = {
+    paddingSmall: {
+      width: '100%',
+      height: '18px'
+    },
     paddingMedium: {
       width: '100%',
       height: '20px'
@@ -94,7 +109,8 @@ export class CompliancePage extends React.Component<Properties, State> {
       flexBasis: '284px',
       flexGrow: 1,
       minWidth: '284px',
-      maxWidth: '424px'
+      maxWidth: '424px',
+      //alignItems: 'center' as 'center'
     },
     mediumContent: {
       paddingTop: '18px',
@@ -103,7 +119,8 @@ export class CompliancePage extends React.Component<Properties, State> {
       paddingBottom: '60px',
       display: 'flex' as 'flex',
       flexDirection: 'column' as 'column',
-      width: '732px'
+      width: '732px',
+      //alignItems: 'center' as 'center'
     },
     largeContent: {
       paddingTop: '18px',
@@ -112,7 +129,55 @@ export class CompliancePage extends React.Component<Properties, State> {
       paddingBottom: '60px',
       display: 'flex' as 'flex',
       flexDirection: 'column' as 'column',
-      width: '1000px'
+      width: '1000px',
+      //alignItems: 'center' as 'center'
+    },
+    statusBox: {
+      height: '19px',
+      width: '100%',
+      alignItems: 'center' as 'center',
+      justifyContent: 'center' as 'center',
+      font: '400 14px Roboto',
+      color: '#36BB55'
     }
   };
+  private static readonly EXTRA_STYLE = StyleSheet.create({
+    button: {
+      boxSizing: 'border-box' as 'border-box',
+      height: '34px',
+      width: '246px',
+      backgroundColor: '#684BC7',
+      color: '#FFFFFF',
+      border: '0px solid #684BC7',
+      borderRadius: '1px',
+      font: '400 14px Roboto',
+      outline: 'none',
+      MozAppearance: 'none' as 'none',
+      alignSelf: 'center' as 'center',
+      ':active' : {
+        backgroundColor: '#4B23A0'
+      },
+      ':focus': {
+        border: 0,
+        outline: 'none',
+        borderColor: '#4B23A0',
+        backgroundColor: '#4B23A0',
+        boxShadow: 'none',
+        webkitBoxShadow: 'none',
+        outlineColor: 'transparent',
+        outlineStyle: 'none',
+        MozAppearance: 'none' as 'none'
+      },
+      ':hover':{
+        backgroundColor: '#4B23A0'
+      },
+      '::-moz-focus-inner': {
+        border: 0,
+        outline: 0
+      },
+      ':-moz-focusring': {
+        outline: 0
+      }
+    }
+  });
 }
