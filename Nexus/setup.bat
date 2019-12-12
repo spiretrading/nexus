c79510a00bfeb3ed82e8ecddbbde5ca844dccb5e
Kamal <kamal@spiretrading.com>
2019-12-12 14:54:52 -0500

    Updated Beam dependency.

--- Nexus/setup.bat
+++ Nexus/setup.bat
@@ -12,7 +12,7 @@ IF NOT EXIST Beam (
   git clone https://www.github.com/eidolonsystems/beam Beam
   SET BUILD_BEAM=1
 )
-SET beam_commit="cdf9bf448e6e835c4730646f69b83a610d802298"
+SET beam_commit="b459f77f83180c1b8be0b0af21fc1d620bd5899d"
 PUSHD Beam
 git merge-base --is-ancestor "%beam_commit%" HEAD
 IF NOT "%ERRORLEVEL%" == "0" (
