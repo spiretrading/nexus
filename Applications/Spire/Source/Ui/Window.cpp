085de9a5f26246ff599a806c4ed450a3b983d458
Ken <ken@spiretrading.com>
2020-01-20 15:05:14 -0500

    Added 'phantom' WM_NCCACLSIZE message to fix window margins issue

--- Applications/Spire/Source/Ui/Window.cpp
+++ Applications/Spire/Source/Ui/Window.cpp
@@ -3,6 +3,7 @@
 #include <QGuiApplication>
 #include <QScreen>
 #include <QVBoxLayout>
+#include <QWindow>
 #include <dwmapi.h>
 #include <qt_windows.h>
 #include <windowsx.h>
@@ -75,6 +76,8 @@ void Window::closeEvent(QCloseEvent* event) {
 bool Window::event(QEvent* event) {
   if(event->type() == QEvent::WinIdChange) {
     set_window_attributes(m_is_resizeable);
+    connect(windowHandle(), &QWindow::screenChanged, this,
+      &Window::on_screen_changed);
   }
   return QWidget::event(event);
 }
@@ -171,6 +174,12 @@ bool Window::nativeEvent(const QByteArray& eventType, void* message,
 
 void Window::resize_body(const QSize& size) {
   resize({size.width(), size.height() + m_title_bar->height()});
+}
+
+void Window::on_screen_changed(QScreen* screen) {
+  auto hwnd = reinterpret_cast<HWND>(effectiveWinId());
+  auto rect = RECT{ 0, 0, 1, 1 };
+  SendMessage(hwnd, WM_NCCALCSIZE, TRUE, reinterpret_cast<LPARAM>(&rect));
 }
 
 void Window::set_fixed_body_size(const QSize& size) {
