b1ec142b51f1e69e556e089731666e3916a66dde
Ken <ken@spiretrading.com>
2020-01-31 15:47:45 -0500

    Documented InputWidget class

--- Applications/Spire/Include/Spire/Ui/InputWidget.hpp
+++ Applications/Spire/Include/Spire/Ui/InputWidget.hpp
@@ -4,9 +4,14 @@
 
 namespace Spire {
 
+  //! Displays a line edit with the Spire style.
   class InputWidget : public QLineEdit {
     public:
 
+      //! Constructs an InputWidget.
+      /*
+        \param parent The parent widget.
+      */
       explicit InputWidget(QWidget* parent = nullptr);
   };
 }
