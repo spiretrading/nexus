ce4b20770be8e4b3c1974ed0bd6bab7330e26f09
Ken <ken@spiretrading.com>
2020-01-31 15:39:08 -0500

    Added QuantityInputWidget implementation

---
+++ Applications/Spire/Include/Spire/Ui/QuantityInputWidget.hpp
@@ -0,0 +1,49 @@
+#ifndef SPIRE_NUMBER_INPUT_WIDGET_HPP
+#define SPIRE_NUMBER_INPUT_WIDGET_HPP
+#include <QWidget>
+#include "Nexus/Definitions/Quantity.hpp"
+#include "Spire/Ui/CustomQtVariants.hpp"
+#include "Spire/Ui/InputWidget.hpp"
+
+namespace Spire {
+
+  //! Displays an input box that accepts integer values.
+  class QuantityInputWidget : public QWidget {
+    public:
+
+      //! Signals that the user has modified the input box or has submitted
+      //! a value.
+      using InputSignal = Signal<void (Nexus::Quantity)>;
+
+      //! Constructs a QuantityInputWidget.
+      /*
+        \param parent The parent widget.
+      */
+      QuantityInputWidget(QWidget* parent = nullptr);
+
+      //! Sets the value to display in the input box.
+      /*
+        \param value The value to display.
+      */
+      void set_value(Nexus::Quantity value);
+
+      //! Connects a signal to the value committed signal.
+      boost::signals2::connection connect_committed_signal(
+        const InputSignal::slot_type& slot) const;
+
+      //! Connects a signal to the value modified signal.
+      boost::signals2::connection connect_modified_signal(
+        const InputSignal::slot_type& slot) const;
+
+    private:
+      InputWidget* m_line_edit;
+      CustomVariantItemDelegate m_item_delegate;
+      mutable InputSignal m_committed_signal;
+      mutable InputSignal m_modified_signal;
+
+      void on_line_edit_committed();
+      void on_line_edit_modified(const QString& text);
+  };
+}
+
+#endif
---
+++ Applications/Spire/Source/Ui/QuantityInputWidget.cpp
@@ -0,0 +1,53 @@
+#include "Spire/Ui/QuantityInputWidget.hpp"
+#include <QHBoxLayout>
+#include <QRegularExpressionValidator>
+
+using namespace boost::signals2;
+using namespace Nexus;
+using namespace Spire;
+
+QuantityInputWidget::QuantityInputWidget(QWidget* parent)
+    : QWidget(parent),
+      m_item_delegate(this) {
+  auto layout = new QHBoxLayout(this);
+  layout->setSpacing(0);
+  layout->setContentsMargins({});
+  m_line_edit = new InputWidget(this);
+  m_line_edit->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+  m_line_edit->setValidator(new QRegularExpressionValidator(
+    QRegularExpression("^[0-9]*$"), this));
+  layout->addWidget(m_line_edit);
+  connect(m_line_edit, &QLineEdit::editingFinished, this,
+    &QuantityInputWidget::on_line_edit_committed);
+  connect(m_line_edit, &QLineEdit::textEdited, this,
+    &QuantityInputWidget::on_line_edit_modified);
+}
+
+void QuantityInputWidget::set_value(Quantity value) {
+  m_line_edit->setText(m_item_delegate.displayText(
+    QVariant::fromValue(Truncate(value, 0)), QLocale()));
+}
+
+connection QuantityInputWidget::connect_committed_signal(
+    const InputSignal::slot_type& slot) const {
+  return m_committed_signal.connect(slot);
+}
+
+connection QuantityInputWidget::connect_modified_signal(
+    const InputSignal::slot_type& slot) const {
+  return m_modified_signal.connect(slot);
+}
+
+void QuantityInputWidget::on_line_edit_committed() {
+  auto quantity = Quantity::FromValue(m_line_edit->text().toStdString());
+  if(quantity) {
+    m_committed_signal(*quantity);
+  }
+}
+
+void QuantityInputWidget::on_line_edit_modified(const QString& text) {
+  auto quantity = Quantity::FromValue(text.toStdString());
+  if(quantity) {
+    m_modified_signal(*quantity);
+  }
+}
