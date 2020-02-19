9924d31f687fdfc2729b40da05006d5e97962630
Ken <ken@spiretrading.com>
2020-02-19 17:42:44 -0500

    Documented ColonWidget class

--- Applications/Spire/Include/Spire/Ui/ColonWidget.hpp
+++ Applications/Spire/Include/Spire/Ui/ColonWidget.hpp
@@ -4,13 +4,21 @@
 
 namespace Spire {
 
+  //! Displays a text colon with styled top and bottom border for use with
+  //! connecting custom line edits.
   class ColonWidget : public QWidget {
     public:
 
+      //! Constructs a ColonWidget.
+      /*
+        \param parent The parent widget.
+      */
       explicit ColonWidget(QWidget* parent = nullptr);
 
+      //! Sets the default, non-active border style.
       void set_default_style();
 
+      //! Sets the active (hovered and/or focused) border style.
       void set_active_style();
 
     protected:
