1dd9493fcd4c53c2819771435fe069189887f57e
Kamal <kamal@eidolonsystems.com>
2020-03-10 14:05:17 -0400

    Updated Beam.

--- Nexus/setup.bat
+++ Nexus/setup.bat
@@ -12,7 +12,7 @@ IF NOT EXIST Beam (
   git clone https://www.github.com/eidolonsystems/beam Beam
   SET BUILD_BEAM=1
 )
-SET beam_commit="76ce1b7bf2415d637f5167f0abdae42204c6e58b"
+SET beam_commit="c90e630c7a3a7c8c0d77a350436f67392ea01b7b"
 PUSHD Beam
 git merge-base --is-ancestor "%beam_commit%" HEAD
 IF NOT "%ERRORLEVEL%" == "0" (
--- Nexus/setup.sh
+++ Nexus/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 let cores="`grep -c "processor" < /proc/cpuinfo`"
 root="$(pwd)"
-beam_commit="76ce1b7bf2415d637f5167f0abdae42204c6e58b"
+beam_commit="c90e630c7a3a7c8c0d77a350436f67392ea01b7b"
 build_beam=0
 if [ ! -d "Beam" ]; then
   git clone https://www.github.com/eidolonsystems/beam.git Beam
