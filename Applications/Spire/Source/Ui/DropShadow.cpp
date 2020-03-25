5c187abcc6df25febb7a27b1a23d55baad6fd55d
Ken <ken@spiretrading.com>
2020-03-25 15:04:05 -0400

    Used qOverload for signal with parameter

--- Applications/Spire/Source/Ui/DropShadow.cpp
+++ Applications/Spire/Source/Ui/DropShadow.cpp
@@ -43,8 +43,7 @@ DropShadow::DropShadow(bool is_menu_shadow, bool has_top, QWidget* parent)
   setAttribute(Qt::WA_TranslucentBackground);
   setAttribute(Qt::WA_ShowWithoutActivating);
   m_parent->window()->installEventFilter(this);
-  connect(m_parent,
-    static_cast<void (QObject::*)(QObject*)>(&QObject::destroyed),
+  connect(m_parent, qOverload<QObject*>(&QObject::destroyed),
     [=] (auto object) { deleteLater(); });
   qApp->installNativeEventFilter(this);
 }
