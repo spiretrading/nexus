af18890435074bb50cfa59cd620ce44892da9718
Ken <ken@spiretrading.com>
2020-03-25 17:28:46 -0400

    Used ScrollArea as base class for TimeAndSalesTableView

--- Applications/Spire/Include/Spire/TimeAndSales/TimeAndSalesTableView.hpp
+++ Applications/Spire/Include/Spire/TimeAndSales/TimeAndSalesTableView.hpp
@@ -2,16 +2,16 @@
 #define SPIRE_TIME_AND_SALES_TABLE_VIEW_HPP
 #include <QAbstractItemModel>
 #include <QHeaderView>
-#include <QScrollArea>
 #include <QTimer>
 #include <QVBoxLayout>
 #include "Spire/TimeAndSales/TimeAndSalesProperties.hpp"
+#include "Spire/Ui/ScrollArea.hpp"
 #include "Spire/Ui/Ui.hpp"
 
 namespace Spire {
 
   //! Displays a table with horizontal header and loading widget.
-  class TimeAndSalesTableView : public QScrollArea {
+  class TimeAndSalesTableView : public ScrollArea {
     public:
 
       //! Constructs a TimeAndSalesTableView.
@@ -33,10 +33,8 @@ namespace Spire {
       void set_properties(const TimeAndSalesProperties& properties);
 
     protected:
-      bool event(QEvent* event) override;
       bool eventFilter(QObject* watched, QEvent* event) override;
       void resizeEvent(QResizeEvent* event) override;
-      void wheelEvent(QWheelEvent* event) override;
 
     private:
       QVBoxLayout* m_layout;
@@ -46,18 +44,11 @@ namespace Spire {
       TimeAndSalesWindowModel* m_model;
       boost::signals2::scoped_connection m_model_begin_loading_connection;
       boost::signals2::scoped_connection m_model_end_loading_connection;
-      QTimer m_h_scroll_bar_timer;
-      QTimer m_v_scroll_bar_timer;
       std::unique_ptr<SnapshotLoadingWidget> m_loading_widget;
       std::unique_ptr<TransitionWidget> m_transition_widget;
 
-      void fade_out_horizontal_scroll_bar();
-      void fade_out_vertical_scroll_bar();
-      void set_scroll_bar_style(int size);
       void show_loading_widget();
       void update_table_height(int num_rows);
-      bool is_within_horizontal_scroll_bar(const QPoint& pos);
-      bool is_within_vertical_scroll_bar(const QPoint& pos);
       void on_end_loading_signal();
       void on_header_resize(int index, int old_size, int new_size);
       void on_header_move(int logical_index, int old_index, int new_index);
--- Applications/Spire/Source/TimeAndSales/TimeAndSalesTableView.cpp
+++ Applications/Spire/Source/TimeAndSales/TimeAndSalesTableView.cpp
@@ -21,14 +21,8 @@ namespace {
 }
 
 TimeAndSalesTableView::TimeAndSalesTableView(QWidget* parent)
-    : QScrollArea(parent),
+    : ScrollArea(parent),
       m_model(nullptr) {
-  setMouseTracking(true);
-  setAttribute(Qt::WA_Hover);
-  horizontalScrollBar()->setContextMenuPolicy(Qt::NoContextMenu);
-  verticalScrollBar()->setContextMenuPolicy(Qt::NoContextMenu);
-  setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
-  setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
   connect(horizontalScrollBar(), &QScrollBar::valueChanged, this,
     &TimeAndSalesTableView::on_horizontal_slider_value_changed);
   connect(verticalScrollBar(), &QScrollBar::valueChanged, this,
@@ -87,12 +81,6 @@ TimeAndSalesTableView::TimeAndSalesTableView(QWidget* parent)
   m_table->setItemDelegate(new ItemPaddingDelegate(scale_width(5),
     new CustomVariantItemDelegate(), this));
   m_layout->addWidget(m_table);
-  m_h_scroll_bar_timer.setInterval(SCROLL_BAR_FADE_TIME_MS);
-  connect(&m_h_scroll_bar_timer, &QTimer::timeout, this,
-    &TimeAndSalesTableView::fade_out_horizontal_scroll_bar);
-  m_v_scroll_bar_timer.setInterval(SCROLL_BAR_FADE_TIME_MS);
-  connect(&m_v_scroll_bar_timer, &QTimer::timeout, this,
-    &TimeAndSalesTableView::fade_out_vertical_scroll_bar);
   setWidget(main_widget);
 }
 
@@ -135,43 +123,6 @@ void TimeAndSalesTableView::set_properties(
   }
 }
 
-bool TimeAndSalesTableView::event(QEvent* event) {
-  if(event->type() == QEvent::HoverMove) {
-    auto e = static_cast<QHoverEvent*>(event);
-    if(is_within_horizontal_scroll_bar(e->pos()) &&
-        !verticalScrollBar()->isVisible()) {
-      set_scroll_bar_style(SCROLL_BAR_MAX_SIZE);
-      setHorizontalScrollBarPolicy(Qt::ScrollBarAsNeeded);
-      setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
-    } else if(is_within_vertical_scroll_bar(e->pos()) &&
-        !horizontalScrollBar()->isSliderDown()) {
-      set_scroll_bar_style(SCROLL_BAR_MAX_SIZE);
-      setVerticalScrollBarPolicy(Qt::ScrollBarAsNeeded);
-      setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
-    } else {
-      set_scroll_bar_style(SCROLL_BAR_MIN_SIZE);
-      if(!m_v_scroll_bar_timer.isActive() &&
-          verticalScrollBarPolicy() != Qt::ScrollBarAlwaysOff &&
-          !verticalScrollBar()->isSliderDown()) {
-        fade_out_vertical_scroll_bar();
-      }
-      if(!m_h_scroll_bar_timer.isActive() &&
-          horizontalScrollBarPolicy() != Qt::ScrollBarAlwaysOff &&
-          !horizontalScrollBar()->isSliderDown()) {
-        fade_out_horizontal_scroll_bar();
-      }
-    }
-  } else if(event->type() == QEvent::HoverLeave) {
-    if(!m_h_scroll_bar_timer.isActive()) {
-      fade_out_horizontal_scroll_bar();
-    }
-    if(!m_v_scroll_bar_timer.isActive()) {
-      fade_out_vertical_scroll_bar();
-    }
-  }
-  return QScrollArea::event(event);
-}
-
 bool TimeAndSalesTableView::eventFilter(QObject* watched, QEvent* event) {
   if(watched == m_table) {
     if(event->type() == QEvent::Paint) {
@@ -179,75 +130,12 @@ bool TimeAndSalesTableView::eventFilter(QObject* watched, QEvent* event) {
       m_header->update();
     }
   }
-  return QScrollArea::eventFilter(watched, event);
+  return ScrollArea::eventFilter(watched, event);
 }
 
 void TimeAndSalesTableView::resizeEvent(QResizeEvent* event) {
   widget()->resize(width(), widget()->height());
   m_header->setFixedWidth(widget()->width());
-}
-
-void TimeAndSalesTableView::wheelEvent(QWheelEvent* event) {
-  if(event->modifiers() & Qt::ShiftModifier) {
-    setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
-    setHorizontalScrollBarPolicy(Qt::ScrollBarAsNeeded);
-    horizontalScrollBar()->setValue(horizontalScrollBar()->value() -
-      (event->delta() / 2));
-    m_h_scroll_bar_timer.start();
-  } else if(!event->modifiers().testFlag(Qt::ShiftModifier)) {
-    setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
-    setVerticalScrollBarPolicy(Qt::ScrollBarAsNeeded);
-    verticalScrollBar()->setValue(verticalScrollBar()->value() -
-      (event->delta() / 2));
-    m_v_scroll_bar_timer.start();
-  }
-}
-
-void TimeAndSalesTableView::fade_out_horizontal_scroll_bar() {
-  m_h_scroll_bar_timer.stop();
-  setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
-}
-
-void TimeAndSalesTableView::fade_out_vertical_scroll_bar() {
-  m_v_scroll_bar_timer.stop();
-  setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
-}
-
-void TimeAndSalesTableView::set_scroll_bar_style(int size) {
-  setStyleSheet(QString(R"(
-    QWidget {
-      background-color: #FFFFFF;
-      border: none;
-    }
-
-    QScrollBar::horizontal {
-      height: %1px;
-    }
-
-    QScrollBar::vertical {
-      width: %2px;
-    }
-
-    QScrollBar::handle {
-      background-color: #C8C8C8;
-    }
-
-    QScrollBar::handle:horizontal {
-      min-width: %3px;
-    }
-
-    QScrollBar::handle:vertical {
-      min-height: %4px;
-    }
-
-    QScrollBar::add-line, QScrollBar::sub-line,
-    QScrollBar::add-page, QScrollBar::sub-page {
-      background: none;
-      border: none;
-      height: 0px;
-      width: 0px;
-    })").arg(scale_height(size)).arg(scale_width(size))
-        .arg(scale_width(60)).arg(scale_height(60)));
 }
 
 void TimeAndSalesTableView::show_loading_widget() {
@@ -263,16 +151,6 @@ void TimeAndSalesTableView::update_table_height(int num_rows) {
     height += m_loading_widget->height();
   }
   widget()->setFixedHeight(height);
-}
-
-bool TimeAndSalesTableView::is_within_horizontal_scroll_bar(
-    const QPoint& pos) {
-  return pos.y() > height() - scale_height(SCROLL_BAR_MAX_SIZE);
-}
-
-bool TimeAndSalesTableView::is_within_vertical_scroll_bar(
-    const QPoint& pos) {
-  return pos.x() > width() - scale_width(SCROLL_BAR_MAX_SIZE);
 }
 
 void TimeAndSalesTableView::on_end_loading_signal() {
