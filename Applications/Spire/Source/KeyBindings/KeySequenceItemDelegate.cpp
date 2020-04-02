f55d8761bda8fc3c87c4c35abe67b913adacdeaa
Ken <ken@spiretrading.com>
2020-04-02 10:14:33 -0400

    Fixed output text for key modifiers, fixed position of key binding graphic

--- Applications/Spire/Source/KeyBindings/KeySequenceItemDelegate.cpp
+++ Applications/Spire/Source/KeyBindings/KeySequenceItemDelegate.cpp
@@ -10,6 +10,19 @@ namespace {
     static auto padding = scale_width(5);
     return padding;
   }
+
+  auto get_key_text(Qt::Key key) {
+    if(key == Qt::Key_Shift) {
+      return QObject::tr("Shift");
+    }
+    if(key == Qt::Key_Alt) {
+      return QObject::tr("Alt");
+    }
+    if(key == Qt::Key_Control) {
+      return QObject::tr("Ctrl");
+    }
+    return QKeySequence(key).toString();
+  }
 }
 
 KeySequenceItemDelegate::KeySequenceItemDelegate(QWidget* parent)
@@ -24,15 +37,6 @@ QWidget* KeySequenceItemDelegate::createEditor(QWidget* parent,
     this, &KeySequenceItemDelegate::on_editing_finished);
   return editor;
 }
-
-//bool KeySequenceItemDelegate::editorEvent(QEvent* event,
-//    QAbstractItemModel* model, const QStyleOptionViewItem& option,
-//    const QModelIndex& index) {
-//  if(event->type() == QEvent::KeyRelease) {
-//    
-//  }
-//  QStyledItemDelegate::editorEvent(event, model, option, index);
-//}
 
 void KeySequenceItemDelegate::paint(QPainter* painter,
     const QStyleOptionViewItem& option, const QModelIndex& index) const {
@@ -63,8 +67,6 @@ QSize KeySequenceItemDelegate::sizeHint(const QStyleOptionViewItem& option,
 
 void KeySequenceItemDelegate::draw_key_sequence(const QKeySequence& sequence,
     const QRect& rect, QPainter* painter) const {
-  auto keys = sequence.toString(QKeySequence::NativeText).split(",",
-    QString::SkipEmptyParts);
   auto pos = rect.bottomLeft();
   pos.setX(pos.x() + scale_width(8));
   auto font = QFont("Roboto");
@@ -73,9 +75,10 @@ void KeySequenceItemDelegate::draw_key_sequence(const QKeySequence& sequence,
   painter->setFont(font);
   painter->setRenderHint(QPainter::Antialiasing);
   auto metrics = QFontMetrics(font);
-  for(auto& key : keys) {
-    auto text_size = QSize(metrics.horizontalAdvance(key), metrics.height());
-    draw_key(key, text_size, pos, painter);
+  for(auto i = 0; i < sequence.count(); ++i) {
+    auto text = get_key_text(static_cast<Qt::Key>(sequence[i]));
+    auto text_size = QSize(metrics.horizontalAdvance(text), metrics.height());
+    draw_key(text, text_size, pos, painter);
     pos.setX(pos.x() + TEXT_PADDING() * 2 + text_size.width() +
       scale_width(8));
   }
@@ -85,14 +88,14 @@ void KeySequenceItemDelegate::draw_key(const QString& text,
     const QSize& text_size, const QPoint& pos, QPainter* painter) const {
   auto path = QPainterPath();
   path.addRoundedRect(QRectF(pos.x(), pos.y() - scale_height(18) -
-    scale_height(4), text_size.width() + TEXT_PADDING() * 2,
+    scale_height(2), text_size.width() + TEXT_PADDING() * 2,
     scale_height(18)), scale_width(2), scale_height(2));
   painter->setPen({QColor("#E2C899"), static_cast<qreal>(scale_width(1))});
   painter->fillPath(path, QColor("#FFEDCD"));
   painter->drawPath(path);
   painter->setPen(Qt::black);
   painter->drawText(pos.x() + TEXT_PADDING(),
-    pos.y() - (text_size.height() / 2) - scale_height(2), text);
+    pos.y() - (text_size.height() / 2), text);
 }
 
 void KeySequenceItemDelegate::on_editing_finished() {
