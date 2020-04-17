5d5260595eb1f6e449d765a8489dec31579ac6e7
Kamal <kamal@eidolonsystems.com>
2020-04-17 16:52:12 -0400

    Moved BacktesterTests to doctest.

--- Nexus/Source/BacktesterTests/BacktesterTimerTester.cpp
+++ Nexus/Source/BacktesterTests/BacktesterTimerTester.cpp
@@ -1,4 +1,4 @@
-#include "Nexus/BacktesterTests/BacktesterTimerTester.hpp"
+#include <doctest/doctest.h>
 #include <Beam/Queues/RoutineTaskQueue.hpp>
 #include <Beam/Threading/ConditionVariable.hpp>
 #include <Beam/Threading/Mutex.hpp>
@@ -11,74 +11,74 @@ using namespace boost;
 using namespace boost::gregorian;
 using namespace boost::posix_time;
 using namespace Nexus;
-using namespace Nexus::Tests;
 
-void BacktesterTimerTester::TestExpiry() {
-  ptime startTime{date{2016, 5, 6}, seconds(0)};
-  BacktesterEventHandler eventHandler{startTime};
-  eventHandler.Open();
-  BacktesterTimer timer{seconds(1), Ref(eventHandler)};
-  RoutineTaskQueue routines;
-  auto expectedTimestamp = startTime + seconds(1);
-  Mutex queryCompleteMutex;
-  ConditionVariable testCompleteCondition;
-  optional<bool> testSucceeded;
-  timer.GetPublisher().Monitor(routines.GetSlot<Timer::Result>(
-    [&] (Timer::Result result) {
-      boost::lock_guard<Mutex> lock{queryCompleteMutex};
-      auto timestamp = eventHandler.GetTime();
-      if(timestamp == expectedTimestamp &&
-          result == Timer::Result::EXPIRED) {
+TEST_SUITE("BacktesterTimer") {
+  TEST_CASE("expiry") {
+    auto startTime = ptime(date{2016, 5, 6}, seconds(0));
+    auto eventHandler = BacktesterEventHandler(startTime);
+    eventHandler.Open();
+    auto timer = BacktesterTimer(seconds(1), Ref(eventHandler));
+    auto routines = RoutineTaskQueue();
+    auto expectedTimestamp = startTime + seconds(1);
+    auto queryCompleteMutex = Mutex();
+    auto testCompleteCondition = ConditionVariable();
+    auto testSucceeded = optional<bool>();
+    timer.GetPublisher().Monitor(routines.GetSlot<Timer::Result>(
+      [&] (Timer::Result result) {
+        auto lock = lock_guard(queryCompleteMutex);
+        auto timestamp = eventHandler.GetTime();
+        if(timestamp == expectedTimestamp && result == Timer::Result::EXPIRED) {
+          testSucceeded = true;
+          testCompleteCondition.notify_one();
+        } else {
+          testSucceeded = false;
+          testCompleteCondition.notify_one();
+        }
+      }));
+    timer.Start();
+    auto lock = unique_lock<Mutex>(queryCompleteMutex);
+    while(!testSucceeded.is_initialized()) {
+      testCompleteCondition.wait(lock);
+    }
+    REQUIRE(*testSucceeded);
+  }
+
+  TEST_CASE("cancel") {
+    auto startTime = ptime(date(2016, 5, 6), seconds(0));
+    auto eventHandler = BacktesterEventHandler(startTime);
+    eventHandler.Open();
+    auto timerA = BacktesterTimer(seconds(1), Ref(eventHandler));
+    auto timerB = BacktesterTimer(seconds(2), Ref(eventHandler));
+    auto routines = RoutineTaskQueue();
+    auto expectedTimestamp = startTime + seconds(1);
+    auto queryCompleteMutex = Mutex();
+    auto testCompleteCondition = ConditionVariable();
+    auto testSucceeded = optional<bool>();
+    timerA.GetPublisher().Monitor(routines.GetSlot<Timer::Result>(
+      [&] (Timer::Result result) {
+        auto timestamp = eventHandler.GetTime();
+        REQUIRE(timestamp == expectedTimestamp);
+        REQUIRE(result == Timer::Result::EXPIRED);
+        timerB.Cancel();
+      }));
+    timerB.GetPublisher().Monitor(routines.GetSlot<Timer::Result>(
+      [&] (Timer::Result result) {
+        auto timestamp = eventHandler.GetTime();
+        REQUIRE(timestamp == expectedTimestamp);
+        REQUIRE(result == Timer::Result::CANCELED);
+        auto lock = lock_guard<Mutex>(queryCompleteMutex);
         testSucceeded = true;
         testCompleteCondition.notify_one();
-      } else {
-        testSucceeded = false;
-        testCompleteCondition.notify_one();
-      }
-    }));
-  timer.Start();
-  boost::unique_lock<Mutex> lock{queryCompleteMutex};
-  while(!testSucceeded.is_initialized()) {
-    testCompleteCondition.wait(lock);
+      }));
+    routines.Push(
+      [&] {
+        timerB.Start();
+        timerA.Start();
+      });
+    auto lock = unique_lock<Mutex>(queryCompleteMutex);
+    while(!testSucceeded.is_initialized()) {
+      testCompleteCondition.wait(lock);
+    }
+    REQUIRE(*testSucceeded);
   }
-  CPPUNIT_ASSERT(*testSucceeded);
-}
-
-void BacktesterTimerTester::TestCancel() {
-  ptime startTime{date{2016, 5, 6}, seconds(0)};
-  BacktesterEventHandler eventHandler{startTime};
-  eventHandler.Open();
-  BacktesterTimer timerA{seconds(1), Ref(eventHandler)};
-  BacktesterTimer timerB{seconds(2), Ref(eventHandler)};
-  RoutineTaskQueue routines;
-  auto expectedTimestamp = startTime + seconds(1);
-  Mutex queryCompleteMutex;
-  ConditionVariable testCompleteCondition;
-  optional<bool> testSucceeded;
-  timerA.GetPublisher().Monitor(routines.GetSlot<Timer::Result>(
-    [&] (Timer::Result result) {
-      auto timestamp = eventHandler.GetTime();
-      CPPUNIT_ASSERT(timestamp == expectedTimestamp);
-      CPPUNIT_ASSERT(result == Timer::Result::EXPIRED);
-      timerB.Cancel();
-    }));
-  timerB.GetPublisher().Monitor(routines.GetSlot<Timer::Result>(
-    [&] (Timer::Result result) {
-      auto timestamp = eventHandler.GetTime();
-      CPPUNIT_ASSERT(timestamp == expectedTimestamp);
-      CPPUNIT_ASSERT(result == Timer::Result::CANCELED);
-      boost::lock_guard<Mutex> lock{queryCompleteMutex};
-      testSucceeded = true;
-      testCompleteCondition.notify_one();
-    }));
-  routines.Push(
-    [&] {
-      timerB.Start();
-      timerA.Start();
-    });
-  boost::unique_lock<Mutex> lock{queryCompleteMutex};
-  while(!testSucceeded.is_initialized()) {
-    testCompleteCondition.wait(lock);
-  }
-  CPPUNIT_ASSERT(*testSucceeded);
 }
