688cb067389e67e8b3735900db115d877afbfe6b
Kamal <kamal@eidolonsystems.com>
2020-04-17 17:07:42 -0400

    Moved ChartingServiceTests to doctest.

--- Nexus/Source/ChartingServiceTests/ChartingClientTester.cpp
+++ Nexus/Source/ChartingServiceTests/ChartingClientTester.cpp
@@ -1,6 +1,8 @@
-#include "Nexus/ChartingServiceTests/ChartingClientTester.hpp"
+#include <doctest/doctest.h>
+#include <Beam/ServicesTests/ServicesTests.hpp>
 #include <Beam/SignalHandling/NullSlot.hpp>
 #include <boost/functional/factory.hpp>
+#include "Nexus/ChartingService/ChartingClient.hpp"
 #include "Nexus/Definitions/DefaultMarketDatabase.hpp"
 
 using namespace Beam;
@@ -12,57 +14,63 @@ using namespace Beam::Threading;
 using namespace boost;
 using namespace boost::gregorian;
 using namespace boost::posix_time;
+using namespace Nexus;
 using namespace Nexus::ChartingService;
-using namespace Nexus::ChartingService::Tests;
 using namespace Nexus::TechnicalAnalysis;
-using namespace std;
 
-void ChartingClientTester::setUp() {
-  auto serverConnection = std::make_shared<TestServerConnection>();
-  m_server.emplace(serverConnection, factory<std::unique_ptr<TriggerTimer>>(),
-    NullSlot(), NullSlot());
-  RegisterChartingServices(Store(m_server->GetSlots()));
-  m_server->Open();
-  TestServiceProtocolClientBuilder builder{
-    [=] {
-      return std::make_unique<TestServiceProtocolClientBuilder::Channel>("test",
-        Ref(*serverConnection));
-    }, factory<unique_ptr<TestServiceProtocolClientBuilder::Timer>>()};
-  m_client.emplace(builder);
-  m_client->Open();
+namespace {
+  struct Fixture {
+    using TestChartingClient = ChartingClient<TestServiceProtocolClientBuilder>;
+
+    boost::optional<TestServiceProtocolServer> m_server;
+    boost::optional<TestChartingClient> m_client;
+
+    Fixture() {
+      auto serverConnection = std::make_shared<TestServerConnection>();
+      m_server.emplace(serverConnection,
+        factory<std::unique_ptr<TriggerTimer>>(), NullSlot(), NullSlot());
+      RegisterChartingServices(Store(m_server->GetSlots()));
+      m_server->Open();
+      auto builder = TestServiceProtocolClientBuilder(
+        [=] {
+          return std::make_unique<TestServiceProtocolClientBuilder::Channel>(
+            "test", Ref(*serverConnection));
+        }, factory<std::unique_ptr<TestServiceProtocolClientBuilder::Timer>>());
+      m_client.emplace(builder);
+      m_client->Open();
+    }
+  };
 }
 
-void ChartingClientTester::tearDown() {
-  m_client.reset();
-  m_server.reset();
-}
-
-void ChartingClientTester::TestLoadSecurityPriceTimeSeries() {
-  Security security{"TST", DefaultMarkets::NYSE(), DefaultCountries::US()};
-  ptime startTime{date{2010, May, 6}};
-  ptime endTime{date{2011, May, 6}};
-  time_duration interval = hours(24);
-  auto receivedRequest = false;
-  TimePriceSeries expectedSeries;
-  expectedSeries.emplace_back(startTime, startTime + days(1), Money::ONE,
-    Money::ONE, Money::ONE, Money::ONE);
-  expectedSeries.emplace_back(startTime + days(1), startTime + days(2),
-    2 * Money::ONE, 2 * Money::ONE, 2 * Money::ONE, 2 * Money::ONE);
-  LoadSecurityTimePriceSeriesService::AddSlot(Store(m_server->GetSlots()),
-    [&] (auto& client, const Security& serviceSecurity,
-        const ptime& serviceStartTime, const ptime& serviceEndTime,
-        const time_duration& serviceInterval) {
-      receivedRequest = true;
-      CPPUNIT_ASSERT(serviceSecurity == security);
-      CPPUNIT_ASSERT(serviceStartTime == startTime);
-      CPPUNIT_ASSERT(serviceEndTime == endTime);
-      CPPUNIT_ASSERT(serviceInterval == interval);
-      TimePriceQueryResult result;
-      result.series = expectedSeries;
-      return result;
-    });
-  auto series = m_client->LoadTimePriceSeries(security, startTime, endTime,
-    interval);
-  CPPUNIT_ASSERT(receivedRequest);
-  CPPUNIT_ASSERT(series.series == expectedSeries);
+TEST_SUITE("ChartingClient") {
+  TEST_CASE_FIXTURE(Fixture, "load_security_price_time_series") {
+    auto security = Security("TST", DefaultMarkets::NYSE(),
+      DefaultCountries::US());
+    auto startTime = ptime(date(2010, May, 6));
+    auto endTime = ptime(date(2011, May, 6));
+    auto interval = hours(24);
+    auto receivedRequest = false;
+    auto expectedSeries = TimePriceSeries();
+    expectedSeries.emplace_back(startTime, startTime + days(1), Money::ONE,
+      Money::ONE, Money::ONE, Money::ONE);
+    expectedSeries.emplace_back(startTime + days(1), startTime + days(2),
+      2 * Money::ONE, 2 * Money::ONE, 2 * Money::ONE, 2 * Money::ONE);
+    LoadSecurityTimePriceSeriesService::AddSlot(Store(m_server->GetSlots()),
+      [&] (auto& client, const Security& serviceSecurity,
+          ptime serviceStartTime, ptime serviceEndTime,
+          time_duration serviceInterval) {
+        receivedRequest = true;
+        REQUIRE(serviceSecurity == security);
+        REQUIRE(serviceStartTime == startTime);
+        REQUIRE(serviceEndTime == endTime);
+        REQUIRE(serviceInterval == interval);
+        auto result = TimePriceQueryResult();
+        result.series = expectedSeries;
+        return result;
+      });
+    auto series = m_client->LoadTimePriceSeries(security, startTime, endTime,
+      interval);
+    REQUIRE(receivedRequest);
+    REQUIRE(series.series == expectedSeries);
+  }
 }
--- Nexus/Source/ChartingServiceTests/main.cpp
+++ Nexus/Source/ChartingServiceTests/main.cpp
@@ -1,21 +1,2 @@
-#include <cppunit/BriefTestProgressListener.h>
-#include <cppunit/CompilerOutputter.h>
-#include <cppunit/TestResult.h>
-#include <cppunit/extensions/TestFactoryRegistry.h>
-#include <cppunit/ui/text/TestRunner.h>
-#include "Nexus/ChartingServiceTests/ChartingClientTester.hpp"
-#include "Nexus/ChartingServiceTests/ChartingServletTester.hpp"
-
-using namespace Nexus::ChartingService::Tests;
-
-int main() {
-  CPPUNIT_NS::TextUi::TestRunner runner;
-  CppUnit::BriefTestProgressListener listener;
-  runner.addTest(ChartingClientTester::suite());
-  runner.addTest(ChartingServletTester::suite());
-  runner.eventManager().addListener(&listener);
-  runner.setOutputter(new CPPUNIT_NS::CompilerOutputter(&runner.result(),
-    CPPUNIT_NS::stdCOut()));
-  bool wasSucessful = runner.run();
-  return wasSucessful ? 0 : -1;
-}
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <doctest/doctest.h>
