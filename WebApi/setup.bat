42815532877f2fc99ebf43442fd36385d059ebc5
Kamal <kamal@eidolonsystems.com>
2020-04-23 22:49:59 -0400

    Updated Beam for the web API.

--- WebApi/setup.bat
+++ WebApi/setup.bat
@@ -1,15 +1,25 @@
 @ECHO OFF
 SETLOCAL
+SET BEAM_COMMIT="cfb05f2182b9cfb7314e9f164b2ff58611aee9cd"
 IF NOT EXIST Beam (
   git clone https://www.github.com/spiretrading/beam Beam
+  IF !ERRORLEVEL! EQU 0 (
+    PUSHD Beam
+    git checkout "!BEAM_COMMIT!"
+    POPD
+  ) ELSE (
+    RD /S /Q Beam
+    SET EXIT_STATUS=1
+  )
 )
-SET beam_commit="cfb05f2182b9cfb7314e9f164b2ff58611aee9cd"
-PUSHD Beam
-git merge-base --is-ancestor "%beam_commit%" HEAD
-IF NOT "%ERRORLEVEL%" == "0" (
-  git checkout master
-  git pull
-  git checkout "%beam_commit%"
+IF EXIST Beam (
+  PUSHD Beam
+  git merge-base --is-ancestor "!BEAM_COMMIT!" HEAD
+  IF !ERRORLEVEL! NEQ 0 (
+    git checkout master
+    git pull
+    git checkout "!BEAM_COMMIT!"
+  )
+  POPD
 )
-POPD
 ENDLOCAL
--- WebApi/setup.sh
+++ WebApi/setup.sh
@@ -2,10 +2,21 @@
 beam_commit="cfb05f2182b9cfb7314e9f164b2ff58611aee9cd"
 if [ ! -d "Beam" ]; then
   git clone https://www.github.com/spiretrading/beam.git Beam
+  if [ "$?" == "0" ]; then
+    pushd Beam
+    git checkout "$beam_commit"
+    popd
+  else
+    rm -rf Beam
+    exit_status=1
+  fi
 fi
-pushd Beam
-if ! git merge-base --is-ancestor "$beam_commit" HEAD; then
-  git checkout master
-  git pull
-  git checkout "$beam_commit"
+if [ -d "Beam" ]; then
+  pushd Beam
+  if ! git merge-base --is-ancestor "$beam_commit" HEAD; then
+    git checkout master
+    git pull
+    git checkout "$beam_commit"
+  fi
+  popd
 fi
