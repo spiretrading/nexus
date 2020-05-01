1a530e3e3b64d798be922ab0b704c32e8c38183f
Kamal Mansouri <kamal@eidolonsystems.com>
2020-04-30 22:33:10 -0400

    Updated Beam.

--- Nexus/setup.bat
+++ Nexus/setup.bat
@@ -9,7 +9,7 @@ FOR /f "usebackq delims=" %%i IN (`!VSWHERE! -prerelease -latest -property insta
   )
 )
 SET BUILD_BEAM=
-SET BEAM_COMMIT="2d1bd11ec89f48e2ea04dbb2e2e920e6b9c78344"
+SET BEAM_COMMIT="2cebc47a9054e8f44f9953b943edcad5c3388350"
 IF NOT EXIST Beam (
   git clone https://www.github.com/spiretrading/beam Beam
   IF !ERRORLEVEL! EQU 0 (
