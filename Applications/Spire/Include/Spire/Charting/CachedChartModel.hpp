297dcb52490415f1a9ecd744e09f11eddd4a9082
Kamal Mansouri <kamal@eidolonsystems.com>
2020-05-12 23:17:59 -0400

    Removed implementation of the CachedChartModel.

--- Applications/Spire/Include/Spire/Charting/CachedChartModel.hpp
+++ Applications/Spire/Include/Spire/Charting/CachedChartModel.hpp
@@ -1,9 +1,6 @@
 #ifndef SPIRE_CACHED_CHART_MODEL_HPP
 #define SPIRE_CACHED_CHART_MODEL_HPP
-#include <boost/icl/continuous_interval.hpp>
-#include <boost/icl/interval_set.hpp>
-#include "Beam/Queries/SnapshotLimit.hpp"
-#include "Spire/Charting/LocalChartModel.hpp"
+#include "Spire/Charting/ChartModel.hpp"
 
 namespace Spire {
 
@@ -29,28 +26,6 @@ namespace Spire {
 
       boost::signals2::connection connect_candlestick_slot(
         const CandlestickSignal::slot_type& slot) const override;
-
-    private:
-      struct LoadInfo {
-        Scalar m_first;
-        Scalar m_last;
-        Scalar m_requested_first;
-        Scalar m_requested_last;
-        Beam::Queries::SnapshotLimit m_limit;
-      };
-      mutable CandlestickSignal m_candlestick_signal;
-      ChartModel* m_chart_model;
-      LocalChartModel m_cache;
-      boost::icl::interval_set<Scalar> m_ranges;
-      
-      QtPromise<std::vector<Candlestick>> load_from_cache(
-        const LoadInfo& info);
-      QtPromise<std::vector<Candlestick>> load_from_model(
-        const LoadInfo& info);
-      void on_data_loaded(const std::vector<Candlestick>& data,
-        const boost::icl::continuous_interval<Scalar>& loaded_range);
-      void on_data_loaded(const std::vector<Candlestick>& data,
-        const LoadInfo& info);
   };
 }
 
