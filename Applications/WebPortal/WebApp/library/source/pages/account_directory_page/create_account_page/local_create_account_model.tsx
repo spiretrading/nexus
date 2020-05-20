993f275cd6f25c7ae1ea4f112d63b1fed61a652b
Sofiya Polyanska <sofiya@spiretrading.com>
2020-05-20 16:56:37 -0400

    Fixed up whitespace.

--- Applications/WebPortal/WebApp/library/source/pages/account_directory_page/create_account_page/local_create_account_model.tsx
+++ Applications/WebPortal/WebApp/library/source/pages/account_directory_page/create_account_page/local_create_account_model.tsx
@@ -4,7 +4,6 @@ import { CreateAccountModel } from './create_account_model';
 
 /** Implements an LocalCreateAccountModel in memory. */
 export class LocalCreateAccountModel extends CreateAccountModel {
-
   public async createAccount(username: string, groups: Beam.DirectoryEntry, 
     identity: Nexus.AccountIdentity, 
     roles: Nexus.AccountRoles): Promise<void> {}
