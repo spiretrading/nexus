d4bca74b86c1a4f71512d57e9297e62fe65e697f
Sofiya Polyanska <sofiya@spiretrading.com>
2020-05-20 16:34:32 -0400

    Fixed createAccount.

--- Applications/WebPortal/WebApp/library/source/pages/account_directory_page/create_account_page/local_create_account_model.tsx
+++ Applications/WebPortal/WebApp/library/source/pages/account_directory_page/create_account_page/local_create_account_model.tsx
@@ -5,9 +5,7 @@ import { CreateAccountModel } from './create_account_model';
 /** Implements an LocalCreateAccountModel in memory. */
 export class LocalCreateAccountModel extends CreateAccountModel {
 
-  public createAccount(username: string, groups: Beam.DirectoryEntry, 
-      identity: Nexus.AccountIdentity, 
-      roles: Nexus.AccountRoles): Promise<void> {
-    return new Promise(() => {return;});
-  }
+  public async createAccount(username: string, groups: Beam.DirectoryEntry, 
+    identity: Nexus.AccountIdentity, 
+    roles: Nexus.AccountRoles): Promise<void> {}
 }
