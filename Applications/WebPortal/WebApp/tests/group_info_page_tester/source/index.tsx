6502bca7e0f061a1e7b039e3f6b41fac2e4e3aba
Sofiya Polyanska <sofiya@spiretrading.com>
2020-05-22 16:05:08 -0400

    Added group entries to the test.

--- Applications/WebPortal/WebApp/tests/group_info_page_tester/source/index.tsx
+++ Applications/WebPortal/WebApp/tests/group_info_page_tester/source/index.tsx
@@ -3,12 +3,14 @@ import * as Nexus from 'nexus';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import * as WebPortal from 'web_portal';
+import { DirectoryEntry } from 'beam';
 
 interface Properties {
   displaySize: WebPortal.DisplaySize;
 }
 
 interface State {
+  members: WebPortal.AccountEntry[];
 }
 
 /** Displays a sample GroupInfoPage for testing. */
@@ -16,14 +18,37 @@ class TestApp extends React.Component<Properties, State> {
   constructor(props: Properties) {
     super(props);
     this.state = {
+      members: []
     };
   }
 
   public render(): JSX.Element {
+    console.log(this.state.members);
     return(
+    <div>
       <WebPortal.GroupInfoPage
-        members={[]}
-        displaySize={this.props.displaySize}/>);
+        group={this.state.members}
+        displaySize={this.props.displaySize}/>
+    </div>);
+  }
+
+  public componentDidMount() {
+    this.fillGroup();
+  }
+
+  private fillGroup = () => {
+    console.log('fill???');
+    let someGroup = [];
+    someGroup.push(
+      new WebPortal.AccountEntry(Beam.DirectoryEntry.makeAccount(12, 'Bob'), 
+      new Nexus.AccountRoles(2)));
+    someGroup.push(
+      new WebPortal.AccountEntry(Beam.DirectoryEntry.makeAccount(18, 'Sue'), 
+      new Nexus.AccountRoles(2)));
+    someGroup.push(
+      new WebPortal.AccountEntry(Beam.DirectoryEntry.makeAccount(13, 'Carl'), 
+      new Nexus.AccountRoles(2)));
+    this.setState({members: someGroup});
   }
 }
 
