105ebc4a18cb4a2a205da2f8ca528046a92df40f
Kamal <kamal@eidolonsystems.com>
2020-06-24 16:51:45 -0400

    Implemented updated fees for ASX pegged orders.

--- Nexus/Source/FeeHandlingTests/AsxtFeeHandlingTester.cpp
+++ Nexus/Source/FeeHandlingTests/AsxtFeeHandlingTester.cpp
@@ -22,33 +22,118 @@ namespace {
       price);
   }
 
+  auto BuildPeggedOrderFields(Money price) {
+    auto fields = BuildOrderFields(price);
+    fields.m_type = OrderType::PEGGED;
+    return fields;
+  }
+
   auto BuildFeeTable() {
     auto feeTable = AsxtFeeTable();
     feeTable.m_gstRate = 1;
     feeTable.m_tradeRate = 1;
-    feeTable.m_clearingRateTable[0] = rational<int>{1, 1000};
-    feeTable.m_clearingRateTable[1] = rational<int>{1, 100};
-    feeTable.m_clearingRateTable[2] = rational<int>{1, 10};
+    feeTable.m_clearingRateTable[0][0] = rational<int>{1, 1000};
+    feeTable.m_clearingRateTable[0][1] = rational<int>{1, 100};
+    feeTable.m_clearingRateTable[0][2] = rational<int>{1, 10};
+    feeTable.m_clearingRateTable[1][0] = rational<int>{5, 1000};
+    feeTable.m_clearingRateTable[1][1] = rational<int>{5, 100};
+    feeTable.m_clearingRateTable[1][2] = rational<int>{5, 10};
     return feeTable;
-  }
-
-  auto AsxtCalculateProcessingFee(const AsxtFeeTable& feeTable,
-      const ExecutionReport& executionReport) {
-    return CalculateFee(feeTable, executionReport).m_processingFee;
   }
 }
 
 TEST_SUITE("AsxtFeeHandling") {
   TEST_CASE("zero_quantity") {
     auto feeTable = BuildFeeTable();
-    TestFeeCalculation(feeTable, Money::ONE, 0, LiquidityFlag::ACTIVE,
-      AsxtCalculateProcessingFee, Money::ZERO);
+    auto fields = BuildOrderFields(Money::ONE);
+    fields.m_quantity = 0;
+    TestNotionalValueFeeCalculation(feeTable, fields,
+      std::bind(CalculateClearingFee, std::placeholders::_1, fields,
+      std::placeholders::_2), 0);
+    TestNotionalValueFeeCalculation(feeTable, fields, CalculateExecutionFee, 0);
   }
 
-  TEST_CASE("no_gst") {
+  TEST_CASE("regular_tier_one") {
     auto feeTable = BuildFeeTable();
-    auto expectedFee = 100 * feeTable.m_clearingRateTable[2] * Money::ONE;
-    TestFeeCalculation(feeTable, Money::ONE, 100, LiquidityFlag::ACTIVE,
-      AsxtCalculateProcessingFee, expectedFee);
+    auto fields = BuildOrderFields(5 * Money::CENT);
+    fields.m_quantity = 100;
+    TestNotionalValueFeeCalculation(feeTable, fields,
+      std::bind(CalculateClearingFee, std::placeholders::_1, fields,
+      std::placeholders::_2), LookupClearingFee(feeTable,
+      AsxtFeeTable::PriceClass::TIER_ONE,
+      AsxtFeeTable::OrderTypeClass::REGULAR));
+    TestFeeCalculation(feeTable, fields, "A", CalculateExecutionFee,
+      (1 + feeTable.m_gstRate) * feeTable.m_tradeRate * fields.m_quantity *
+      fields.m_price);
+  }
+
+  TEST_CASE("regular_tier_two") {
+    auto feeTable = BuildFeeTable();
+    auto fields = BuildOrderFields(50 * Money::CENT);
+    fields.m_quantity = 500;
+    TestNotionalValueFeeCalculation(feeTable, fields,
+      std::bind(CalculateClearingFee, std::placeholders::_1, fields,
+      std::placeholders::_2), LookupClearingFee(feeTable,
+      AsxtFeeTable::PriceClass::TIER_TWO,
+      AsxtFeeTable::OrderTypeClass::REGULAR));
+    TestFeeCalculation(feeTable, fields, "R", CalculateExecutionFee,
+      (1 + feeTable.m_gstRate) * feeTable.m_tradeRate * fields.m_quantity *
+      fields.m_price);
+  }
+
+  TEST_CASE("regular_tier_three") {
+    auto feeTable = BuildFeeTable();
+    auto fields = BuildOrderFields(Money::ONE);
+    fields.m_quantity = 12000;
+    TestNotionalValueFeeCalculation(feeTable, fields,
+      std::bind(CalculateClearingFee, std::placeholders::_1, fields,
+      std::placeholders::_2), LookupClearingFee(feeTable,
+      AsxtFeeTable::PriceClass::TIER_THREE,
+      AsxtFeeTable::OrderTypeClass::REGULAR));
+    TestFeeCalculation(feeTable, fields, "R", CalculateExecutionFee,
+      (1 + feeTable.m_gstRate) * feeTable.m_tradeRate * fields.m_quantity *
+      fields.m_price);
+  }
+
+  TEST_CASE("pegged_tier_one") {
+    auto feeTable = BuildFeeTable();
+    auto fields = BuildPeggedOrderFields(5 * Money::CENT);
+    fields.m_quantity = 6000;
+    TestNotionalValueFeeCalculation(feeTable, fields,
+      std::bind(CalculateClearingFee, std::placeholders::_1, fields,
+      std::placeholders::_2), LookupClearingFee(feeTable,
+      AsxtFeeTable::PriceClass::TIER_ONE,
+      AsxtFeeTable::OrderTypeClass::PEGGED));
+    TestFeeCalculation(feeTable, fields, "R", CalculateExecutionFee,
+      (1 + feeTable.m_gstRate) * feeTable.m_tradeRate * fields.m_quantity *
+      fields.m_price);
+  }
+
+  TEST_CASE("pegged_tier_two") {
+    auto feeTable = BuildFeeTable();
+    auto fields = BuildPeggedOrderFields(50 * Money::CENT);
+    fields.m_quantity = 300;
+    TestNotionalValueFeeCalculation(feeTable, fields,
+      std::bind(CalculateClearingFee, std::placeholders::_1, fields,
+      std::placeholders::_2), LookupClearingFee(feeTable,
+      AsxtFeeTable::PriceClass::TIER_TWO,
+      AsxtFeeTable::OrderTypeClass::PEGGED));
+    TestFeeCalculation(feeTable, fields, "A", CalculateExecutionFee,
+      (1 + feeTable.m_gstRate) * feeTable.m_tradeRate * fields.m_quantity *
+      fields.m_price);
+  }
+
+  TEST_CASE("pegged_tier_three") {
+    auto feeTable = BuildFeeTable();
+    auto fields = BuildPeggedOrderFields(Money::ONE);
+    fields.m_quantity = 4400;
+    TestNotionalValueFeeCalculation(feeTable, fields,
+      std::bind(CalculateClearingFee, std::placeholders::_1, fields,
+      std::placeholders::_2), LookupClearingFee(feeTable,
+      AsxtFeeTable::PriceClass::TIER_THREE,
+      AsxtFeeTable::OrderTypeClass::PEGGED));
+    TestFeeCalculation(feeTable, fields, "R", CalculateExecutionFee,
+      (1 + feeTable.m_gstRate) * feeTable.m_tradeRate * fields.m_quantity *
+      fields.m_price);
   }
 }
