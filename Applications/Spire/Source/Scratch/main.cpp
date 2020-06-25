2612e50075abc5a554210604c127db63acd9cf83
Ken <ken@spiretrading.com>
2020-06-25 18:36:43 -0400

    Reverted scratch project

--- Applications/Spire/Source/Scratch/main.cpp
+++ Applications/Spire/Source/Scratch/main.cpp
@@ -1,11 +1,6 @@
 #include <QApplication>
 #include "Spire/Spire/Resources.hpp"
 
-#include <QVBoxLayout>
-#include <QLabel>
-
-#include "Spire/Ui/FontSelectorWidget.hpp"
-
 using namespace Spire;
 
 int main(int argc, char** argv) {
@@ -13,16 +8,5 @@ int main(int argc, char** argv) {
   application->setOrganizationName(QObject::tr("Eidolon Systems Ltd"));
   application->setApplicationName(QObject::tr("Scratch"));
   initialize_resources();
-  auto a = new QWidget();
-  a->resize(800, 600);
-  auto w = new FontSelectorWidget(QFont("Roboto"), a);
-  auto l = new QVBoxLayout(a);
-  l->addWidget(w);
-  auto d = new QLabel("Text", w);
-  l->addWidget(d);
-  w->connect_font_signal([=] (const auto& font) {
-    d->setFont(font);
-  });
-  a->show();
   application->exec();
 }
