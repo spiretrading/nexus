a2a02273e259b6d2ac6a56b6a2278b4a4987af9c
andrei-sa <andrei@spiretrading.com>
2020-07-09 13:26:16 -0400

    Capture by reference in TagModel::get_tag_name

--- Applications/Spire/Source/KeyBindings/TagModel.cpp
+++ Applications/Spire/Source/KeyBindings/TagModel.cpp
@@ -7,7 +7,7 @@ TagModel::TagModel(std::vector<Schema> schemas)
   : m_schemas(std::move(schemas)) {}
 
 const std::string& TagModel::get_tag_name(int key) const {
-  return std::find_if(m_schemas.begin(), m_schemas.end(), [=] (auto& schema) {
+  return std::find_if(m_schemas.begin(), m_schemas.end(), [&] (auto& schema) {
     return schema.m_tag.get_key() == key;
   })->m_name;
 }
