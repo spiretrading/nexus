c8679a4e273ce8e9c3b773ed73efaae893977641
Devang Patel <devang@spiretrading.com>
2020-07-20 17:32:54 -0400

    Create GroupPage component

---
+++ Applications/WebPortal/WebApp/library/source/pages/group_page/group_page.tsx
@@ -0,0 +1,212 @@
+import * as Beam from 'beam';
+import { HBoxLayout, Padding, VBoxLayout } from 'dali';
+import * as React from 'react';
+import { DisplaySize, HLine } from '../..';
+import { GroupSubPage } from './group_sub_page';
+import { MenuBar } from './menu_bar';
+
+interface Properties {
+
+  /** The account being displayed. */
+  account: Beam.DirectoryEntry;
+
+  /** The sub page currently selected. */
+  subPage: GroupSubPage;
+
+  /** Determines the layout used to render the page. */
+  displaySize: DisplaySize;
+
+  /** Indicates a sub-page menu item was clicked.
+   * @param subPage - The GroupSubPage that was clicked.
+   */
+  onMenuClick?: (subPage: GroupSubPage) => void;
+}
+
+/** Implements the container used to display account information. */
+export class GroupPage extends React.Component<Properties> {
+  public static readonly defaultProps = {
+    onMenuClick: () => {}
+  }
+
+  public render(): JSX.Element {
+    const Header = (() => {
+      switch(this.props.displaySize) {
+        case DisplaySize.LARGE:
+          return LargeHeader;
+        case DisplaySize.MEDIUM:
+          return MediumHeader;
+        case DisplaySize.SMALL:
+          return SmallHeader;
+      }
+    })();
+    return (
+      <VBoxLayout height='100%' width='100%'
+          style={{overflow: 'hidden' as 'hidden'}}>
+        <Header name={this.props.account.name}>
+          <MenuBar displaySize={this.props.displaySize}
+            selected={this.props.subPage} onClick={this.props.onMenuClick}/>
+        </Header>
+        {this.props.children}
+      </VBoxLayout>);
+  }
+}
+
+class HeaderUnderline extends React.PureComponent {
+  public render(): JSX.Element {
+    return <HLine color='#E6E6E6' height='1px'/>;
+  }
+}
+
+interface HeaderProps {
+  name: string;
+}
+
+class LargeHeader extends React.Component<HeaderProps> {
+  public render(): JSX.Element {
+    return (
+      <VBoxLayout width='100%'>
+        <HBoxLayout width='100%' height='40px'>
+          <Padding/>
+          <HBoxLayout width='1036px' height='40px'>
+            <Padding size='18px'/>
+            {this.props.children}
+            <Padding/>
+            <div style={LargeHeader.STYLES.usernameContainer}>
+              <div style={LargeHeader.STYLES.usernameWrapper}>
+                <UsernameLabel name={this.props.name} height='40px'/>
+                <Padding size='10px'/>
+              </div>
+            </div>
+            <Padding size='18px'/>
+          </HBoxLayout>
+          <Padding/>
+        </HBoxLayout>
+        <HeaderUnderline/>
+      </VBoxLayout>);
+  }
+
+  private static readonly STYLES = {
+    usernameWrapper: {
+      height: '40px',
+      width: 'auto',
+      display: 'flex'
+    } as React.CSSProperties,
+    usernameContainer: {
+      height: '40px',
+      display: 'flex',
+      flexWrap: 'wrap'
+    } as React.CSSProperties
+  }
+}
+
+class MediumHeader extends React.Component<HeaderProps> {
+  public render(): JSX.Element {
+    return (
+      <VBoxLayout width='100%'>
+        <HBoxLayout width='100%' height='40px'>
+          <div style={MediumHeader.STYLES.headerPadding}/>
+          <Padding size='18px'/>
+          <HBoxLayout height='40px' width='750px'>
+            {this.props.children}
+            <div style={MediumHeader.STYLES.innerPadding}/>
+          </HBoxLayout>
+          <Padding size='18px'/>
+          <div style={MediumHeader.STYLES.headerPadding}/>
+        </HBoxLayout>
+        <HeaderUnderline/>
+        <HBoxLayout width='100%' height='30px'>
+          <div style={MediumHeader.STYLES.headerPadding}/>
+          <Padding size='18px'/>
+          <HBoxLayout height='30px' width='750px'>
+            <Padding/>
+            <UsernameLabel name={this.props.name} height='30px'/>
+          </HBoxLayout>
+          <Padding size='18px'/>
+          <div style={MediumHeader.STYLES.headerPadding}/>
+        </HBoxLayout>
+        <HeaderUnderline/>
+      </VBoxLayout>);
+  }
+
+  private static readonly STYLES = {
+    headerPadding: {
+      width: 'calc(50% - 393px)',
+      height: '100%'
+    } as React.CSSProperties,
+    innerPadding: {
+      width: 'calc(100% - 658px)',
+      height: '100%'
+    } as React.CSSProperties
+  }
+}
+
+class SmallHeader extends React.Component<HeaderProps> {
+  public render(): JSX.Element {
+    return (
+      <VBoxLayout width='100%'>
+        <HBoxLayout width='100%' height='40px'>
+          <div style={SmallHeader.STYLES.headerPadding}/>
+          <Padding size='18px'/>
+          <HBoxLayout height='40px' style={SmallHeader.STYLES.accountHeader}>
+            {this.props.children}
+            <div style={SmallHeader.STYLES.innerPadding}/>
+          </HBoxLayout>
+          <Padding size='18px'/>
+          <div style={SmallHeader.STYLES.headerPadding}/>
+        </HBoxLayout>
+        <HeaderUnderline/>
+        <HBoxLayout width='100%' height='30px'>
+          <div style={SmallHeader.STYLES.headerPadding}/>
+          <Padding size='18px'/>
+          <HBoxLayout height='30px' style={SmallHeader.STYLES.accountHeader}>
+            <Padding/>
+            <UsernameLabel name={this.props.name} height='30px'/>
+          </HBoxLayout>
+          <Padding size='18px'/>
+          <div style={SmallHeader.STYLES.headerPadding}/>
+        </HBoxLayout>
+        <HeaderUnderline/>
+      </VBoxLayout>);
+  }
+
+  private static readonly STYLES = {
+    accountHeader: {
+      width: '60%',
+      minWidth: '284px',
+      maxWidth: '424px'
+    } as React.CSSProperties,
+    headerPadding: {
+      width: 'calc(20% - 18px)'
+    } as React.CSSProperties,
+    innerPadding: {
+      width: 'calc(45% - 68px)',
+      height: '100%'
+    } as React.CSSProperties
+  }
+}
+
+interface UsernameProps {
+  name: string;
+  height: string;
+}
+
+class UsernameLabel extends React.Component<UsernameProps>  {
+  public render(): JSX.Element {
+    return (
+      <VBoxLayout height={this.props.height}>
+        <Padding/>
+        <span style={UsernameLabel.STYLES.username}>
+          {this.props.name}
+        </span>
+        <Padding/>
+      </VBoxLayout>);
+  }
+
+  private static readonly STYLES = {
+    username: {
+      font: '500 14px Roboto',
+      color: '#4B23A0',
+      whiteSpace: 'nowrap'
+    } as React.CSSProperties
+  };
+}
---
+++ Applications/WebPortal/WebApp/library/source/pages/group_page/menu_bar.tsx
@@ -0,0 +1,244 @@
+import { css, StyleSheet } from 'aphrodite';
+import { Center, HBoxLayout, Padding, VBoxLayout } from 'dali';
+import * as React from 'react';
+import { DisplaySize } from '../..';
+import { GroupSubPage } from './group_sub_page';
+
+interface Properties {
+
+  /** The sub-page currently selected. */
+  selected: GroupSubPage;
+
+  /** The type of display to render on. */
+  displaySize: DisplaySize;
+
+  /** Indicates a menu item was clicked.
+   * @param subPage - The sub-page item that was clicked.
+   */
+  onClick?: (subPage: GroupSubPage) => void;
+}
+
+interface State {
+  hovered: GroupSubPage;
+}
+
+/** Displays the horizontal menu used to navigate an account's properties. */
+export class MenuBar extends React.Component<Properties, State> {
+  public static defaultProps = {
+    onClick: () => {},
+  };
+
+  constructor(props: Properties) {
+    super(props);
+    this.state = {
+      hovered: GroupSubPage.NONE
+    };
+  }
+
+  public render(): JSX.Element {
+    const menuBarPadding = ((): JSX.Element => {
+      if(this.props.displaySize === DisplaySize.SMALL) {
+        return <div style={MenuBar.STYLE.smallPadding}/>;
+      }
+      return <Padding size='30px'/>;
+    })();
+    const menuIconContainerClassName = (() => {
+      if(this.props.displaySize === DisplaySize.SMALL) {
+        return {...MenuBar.STYLE.base, ...MenuBar.STYLE.small};
+      }
+      return MenuBar.STYLE.base;
+    })();
+    const menuBarWidth = (() => {
+      if(this.props.displaySize !== DisplaySize.SMALL) {
+        return '590px';
+      }
+    })();
+    return (
+      <HBoxLayout id='menu-bar' width={menuBarWidth}
+          style={menuIconContainerClassName}>
+        <VBoxLayout id='item-vbox'  height='40px'>
+          <Item iconSrc={'resources/group_page/group' +
+              `-${this.getIconColor(GroupSubPage.GROUP)}.svg`} name='Group'
+            isSelected={this.props.selected === GroupSubPage.GROUP}
+            displaySize={this.props.displaySize}
+            onMouseEnter={() => this.onIconMouseEnter(GroupSubPage.GROUP)}
+            onMouseLeave={this.onIconMouseLeave}
+            onClick={() => this.onIconClick(GroupSubPage.GROUP)}/>
+          <div style={this.getIconUnderlineStyle(GroupSubPage.GROUP)}/>
+        </VBoxLayout>
+        {menuBarPadding}
+        <VBoxLayout height='40px'>
+          <Item iconSrc={'resources/group_page/compliance' +
+              `-${this.getIconColor(GroupSubPage.COMPLIANCE)}.svg`}
+            name='Compliance'
+            isSelected={this.props.selected === GroupSubPage.COMPLIANCE}
+            displaySize={this.props.displaySize}
+            onMouseEnter={() => this.onIconMouseEnter(GroupSubPage.COMPLIANCE)}
+            onMouseLeave={this.onIconMouseLeave}
+            onClick={() => this.onIconClick(GroupSubPage.COMPLIANCE)}/>
+          <div style={this.getIconUnderlineStyle(GroupSubPage.COMPLIANCE)}/>
+        </VBoxLayout>
+        {menuBarPadding}
+        <VBoxLayout height='40px'>
+          <Item iconSrc={'resources/group_page/profit-loss' +
+              `-${this.getIconColor(GroupSubPage.PROFIT_LOSS)}.svg`}
+            name='Profit/Loss' displaySize={this.props.displaySize}
+            isSelected={this.props.selected === GroupSubPage.PROFIT_LOSS}
+            onMouseEnter={() => this.onIconMouseEnter(GroupSubPage.PROFIT_LOSS)}
+            onMouseLeave={this.onIconMouseLeave}
+            onClick={() => this.onIconClick(GroupSubPage.PROFIT_LOSS)}/>
+          <div style={this.getIconUnderlineStyle(GroupSubPage.PROFIT_LOSS)}/>
+        </VBoxLayout>
+      </HBoxLayout>);
+  }
+
+  private getIconColor = (subPage: GroupSubPage) => {
+    if(this.props.selected === subPage || this.state.hovered === subPage) {
+      return 'purple';
+    }
+    return 'grey';
+  }
+
+  private getIconUnderlineStyle = (subPage: GroupSubPage) => {
+    if(subPage === this.props.selected) {
+      return MenuBar.STYLE.selectedBorder;
+    }
+    return MenuBar.STYLE.unselectedBorder;
+  }
+
+  private onIconMouseEnter = (subPage: GroupSubPage) => {
+    this.setState({hovered: subPage});
+  }
+
+  private onIconMouseLeave = () => {
+    this.setState({hovered: GroupSubPage.NONE});
+  }
+
+  private onIconClick = (subPage: GroupSubPage) => {
+    if(this.props.selected !== subPage) {
+      this.props.onClick(subPage);
+    }
+  }
+
+  private static readonly STYLE = {
+    base: {
+      height: '40px',
+      width: '590px'
+    },
+    small: {
+      width: '55%',
+      minWidth: '176px',
+      maxWidth: '308px'
+    },
+    smallPadding: {
+      width: '100%',
+      maxWidth: '30px',
+      height: '100%',
+      flex: '1 1 auto'
+    },
+    selectedBorder: {
+      width: '100%',
+      height: '2px',
+      backgroundColor: '#4B23A0'
+    },
+    unselectedBorder: {
+      width: '100%',
+      height: '2px',
+      backgroundColor: 'rgba(255, 255, 255, 0)'
+    }
+  }
+}
+
+interface ItemProperties {
+  iconSrc: string;
+  isSelected: boolean;
+  displaySize: DisplaySize;
+  name: string;
+  onMouseEnter?: () => void;
+  onMouseLeave?: () => void;
+  onClick?: () => void;
+}
+
+class Item extends React.Component<ItemProperties> {
+  public static defaultProps = {
+    isSelected: false,
+    onMouseEnter: () => {},
+    onMouseLeave: () => {},
+    onClick: () => {}
+  };
+
+  public render(): JSX.Element {
+    const itemClassName = (() => {
+      if(this.props.isSelected) {
+        return css([Item.STYLE.item, Item.STYLE.selectedItem]);
+      }
+      return css([Item.STYLE.item, Item.STYLE.unSelectedItem]);
+    })();
+    if(this.props.displaySize === DisplaySize.SMALL) {
+      return (
+        <VBoxLayout height='38px'>
+          <Padding size='8px'/>
+          <Center width='24px' height='24px' className={itemClassName}
+              onMouseEnter={this.props.onMouseEnter}
+              onMouseLeave={this.props.onMouseLeave}
+              onClick={this.props.onClick}>
+            <img src={this.props.iconSrc} width='20px' height='20px'/>
+          </Center>
+          <Padding size='6px'/>
+        </VBoxLayout>);
+    }
+    return (
+      <VBoxLayout className={itemClassName}
+          onMouseEnter={this.props.onMouseEnter}
+          onMouseLeave={this.props.onMouseLeave}
+          onClick={this.props.onClick}>
+        <Padding size='12px'/>
+        <div className={css(Item.STYLE.container)}>
+          <div className={css(Item.STYLE.wrapper)}>
+            <img src={this.props.iconSrc} width='16px' height='16px'/>
+            <div className={css(Item.STYLE.padding)}/>
+            <Center id='center-1' height='16px'>
+              <span className={css(Item.STYLE.label)}>{this.props.name}</span>
+            </Center>
+          </div>
+        </div>
+        <Padding size='10px'/>
+      </VBoxLayout>);
+  }
+
+  private static STYLE = StyleSheet.create({
+    item: {
+      height: '38px',
+      cursor: 'pointer',
+      '-webkit-tap-highlight-color': 'transparent',
+      ':hover': {
+        color: '#4B23A0'
+      }
+    },
+    selectedItem: {
+      color: '#4B23A0'
+    },
+    unSelectedItem: {
+      color: '#7D7E90'
+    },
+    container: {
+      height: '16px',
+      display: 'flex',
+      flexWrap: 'wrap'
+    },
+    wrapper: {
+      height: '16px',
+      width: 'auto',
+      display: 'flex'
+    },
+    padding: {
+      height: '16px',
+      width: '8px'
+    },
+    label: {
+      color: 'inherit',
+      font: '400 14px Roboto',
+      whiteSpace: 'nowrap'
+    }
+  });
+}
