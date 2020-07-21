c7df36da242c794fc0fe654aa5a6e9add432ac9b
Devang Patel <devang@spiretrading.com>
2020-07-21 11:56:07 -0400

    Update group page tester

--- Applications/WebPortal/WebApp/tests/group_page_tester/source/index.tsx
+++ Applications/WebPortal/WebApp/tests/group_page_tester/source/index.tsx
@@ -1,14 +1,39 @@
 import * as Beam from 'beam';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import * as WebPortal from 'web_portal';
+import { DisplaySize, displaySizeRenderer, GroupPage, GroupSubPage }
+  from 'web_portal';
 
-const account = Beam.DirectoryEntry.makeAccount(124,
-  'Group_name_goes_here');
+interface Properties {
+  displaySize: DisplaySize;
+}
 
-ReactDOM.render(
-  <WebPortal.GroupPage
-    displaySize={WebPortal.DisplaySize.getDisplaySize()}
-    account={account}
-    subPage={WebPortal.GroupSubPage.GROUP}/>,
-  document.getElementById('main'));
+interface State {
+  subPage: GroupSubPage;
+  account: Beam.DirectoryEntry;
+}
+
+class GroupPageTester extends React.Component<Properties, State> {
+  constructor(props: Properties) {
+    super(props);
+    this.state = {
+      subPage: GroupSubPage.NONE,
+      account: Beam.DirectoryEntry.makeAccount(124, 'Group_name_goes_here')
+    };
+  }
+
+  public render(): JSX.Element {
+    return (
+      <GroupPage displaySize={this.props.displaySize}
+        subPage={this.state.subPage} account={this.state.account}
+        onMenuClick={this.onMenuClick}/>);
+  }
+
+  private onMenuClick = (subPage: GroupSubPage) => {
+    this.setState({subPage: subPage});
+  }
+}
+
+const ResponsiveGroupPageTester = displaySizeRenderer(GroupPageTester);
+
+ReactDOM.render(<ResponsiveGroupPageTester/>, document.getElementById('main'));
