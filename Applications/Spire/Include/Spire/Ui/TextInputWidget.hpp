fc624a91cc58fcccf7bf05e40c3fcc552b29b594
Ken <ken@spiretrading.com>
2020-07-28 15:33:26 -0400

    Added TextInputWidget implementation

---
+++ Applications/Spire/Include/Spire/Ui/TextInputWidget.hpp
@@ -0,0 +1,27 @@
+#ifndef SPIRE_TEXT_INPUT_WIDGET_HPP
+#define SPIRE_TEXT_INPUT_WIDGET_HPP
+#include <QLineEdit>
+#include "Spire/Spire/Spire.hpp"
+
+namespace Spire {
+
+  //! Represents a Spire-style line edit.
+  class TextInputWidget : public QLineEdit {
+    public:
+
+      //! Constructs a TextInputWidget.
+      /*!
+        \param parent The parent widget.
+      */
+      explicit TextInputWidget(QWidget* parent = nullptr);
+
+      //! Constructs a TextInputWidget.
+      /*!
+        \param text The text to display.
+        \param parent The parent widget.
+      */
+      explicit TextInputWidget(QString text, QWidget* parent = nullptr);
+  };
+}
+
+#endif
