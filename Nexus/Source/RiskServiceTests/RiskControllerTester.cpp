b07109e8904c26f26b569de2d0f8fb4d80eeda2a
Kamal <kamal@eidolonsystems.com>
2020-08-05 11:51:03 -0400

    WIP.

--- Nexus/Source/RiskServiceTests/RiskControllerTester.cpp
+++ Nexus/Source/RiskServiceTests/RiskControllerTester.cpp
@@ -6,6 +6,8 @@
 
 using namespace Beam;
 using namespace Beam::ServiceLocator;
+using namespace boost;
+using namespace boost::posix_time;
 using namespace Nexus;
 using namespace Nexus::RiskService;
 
@@ -18,12 +20,32 @@ namespace {
 
   struct Fixture {
     TestEnvironment m_environment;
+    TestServiceClients m_serviceClients;
 
-    Fixture() {}
+    Fixture()
+        : m_serviceClients(Ref(m_environment)) {
+      m_environment.Open();
+      m_serviceClients.Open();
+    }
   };
 }
 
 TEST_SUITE("RiskController") {
   TEST_CASE_FIXTURE(Fixture, "single_account") {
+    auto accountUpdateQueue = std::make_shared<Queue<AccountUpdate>>();
+    m_serviceClients.GetServiceLocatorClient().MonitorAccounts(accountQueue);
+    auto accountQueue = MakeConverterQueueReader(MakeFilteredQueueReader(
+      std::move(accountUpdateQueue),
+      [] (const auto& update) {
+        return update.m_type == AccountUpdate::Type::ADDED;
+      }),
+      [] (const auto& update) {
+        return update.m_account;
+      });
+    auto controller = RiskController(accountQueue,
+      m_serviceClients.GetAdministrationClient(),
+      m_serviceClients.GetMarketDataClient(),
+      m_serviceClients.GetOrderExecutionClient(),
+      m_serviceClients.BuildTimer(seconds(1)));
   }
 }
