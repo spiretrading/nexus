8c93a566b9def5c8d80e304e12f2a8807c489dd5
Kamal <kamal@eidolonsystems.com>
2020-08-07 17:15:27 -0400

    Updated Beam.

--- Nexus/setup.bat
+++ Nexus/setup.bat
@@ -20,7 +20,7 @@ FOR /f "usebackq delims=" %%i IN (`!VSWHERE! -prerelease -latest -property insta
   )
 )
 SET BUILD_BEAM=
-SET BEAM_COMMIT="ddff8d249b79b4dd7947f9f2cf0f54431b236b92"
+SET BEAM_COMMIT="833d83374ede0ee4803acabd6a36c05a45d36d41"
 IF NOT EXIST Beam (
   git clone https://www.github.com/spiretrading/beam Beam
   IF !ERRORLEVEL! EQU 0 (
--- Nexus/setup.sh
+++ Nexus/setup.sh
@@ -21,7 +21,7 @@ if [ -f "cache_files/nexus.txt" ]; then
   fi
 fi
 cores="`grep -c "processor" < /proc/cpuinfo`"
-beam_commit="ddff8d249b79b4dd7947f9f2cf0f54431b236b92"
+beam_commit="833d83374ede0ee4803acabd6a36c05a45d36d41"
 build_beam=0
 if [ ! -d "Beam" ]; then
   git clone https://www.github.com/spiretrading/beam.git Beam
