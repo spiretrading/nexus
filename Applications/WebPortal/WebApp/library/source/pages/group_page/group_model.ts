91e481f58da637c8941734ea821f43a9860465d4
Devang Patel <devang@spiretrading.com>
2020-08-14 10:26:18 -0400

    Put load method at end of GroupModel

--- Applications/WebPortal/WebApp/library/source/pages/group_page/group_model.ts
+++ Applications/WebPortal/WebApp/library/source/pages/group_page/group_model.ts
@@ -7,9 +7,9 @@ export abstract class GroupModel {
   /** The group this model represents. */
   public abstract get group(): Beam.DirectoryEntry;
 
-  /** Loads this model. */
-  public abstract async load(): Promise<void>;
-  
   /** Returns the accounts belonging to the group. */
   public abstract get accounts(): AccountEntry[];
+
+  /** Loads this model. */
+  public abstract async load(): Promise<void>;
 }
