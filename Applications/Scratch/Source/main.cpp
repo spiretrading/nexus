99a4f051ffe88d78cf31f01b6505ee749d690240
Kamal Mansouri <kamal@spiretrading.com>
2020-09-24 01:14:32 +0000

    Modified the test used by Scratch.

--- Applications/Scratch/Source/main.cpp
+++ Applications/Scratch/Source/main.cpp
@@ -3,11 +3,5 @@
 
 int main(int argc, const char** argv) {
   auto g = Nexus::TestEnvironment();
-  auto r = Beam::Routines::RoutineHandlerGroup();
-  for(auto i = 0UL; i < boost::thread::hardware_concurrency(); ++i) {
-    r.Spawn([] {
-      auto t = Nexus::TestEnvironment();
-    });
-  }
   return 0;
 }
