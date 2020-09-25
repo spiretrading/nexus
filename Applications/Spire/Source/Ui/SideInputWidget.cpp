9928b644391d33177afa3240aec02378f19fe4ee
Ken <ken@spiretrading.com>
2020-09-25 19:45:52 -0400

    Made StaticDropDownMenu member of SideInputWidget

--- Applications/Spire/Source/Ui/SideInputWidget.cpp
+++ Applications/Spire/Source/Ui/SideInputWidget.cpp
@@ -1,13 +1,23 @@
 #include "Spire/Ui/SideInputWidget.hpp"
+#include <QHBoxLayout>
+#include "Spire/Ui/StaticDropDownMenu.hpp"
 
+using namespace boost::signals2;
 using namespace Nexus;
 using namespace Spire;
 
 SideInputWidget::SideInputWidget(QWidget* parent)
-  : FilteredDropDownMenu({QVariant::fromValue<Side>(Side::ASK),
-      QVariant::fromValue<Side>(Side::BID)},
-    parent) {}
+    : QWidget(parent) {
+  auto menu = new StaticDropDownMenu({QVariant::fromValue<Side>(Side::ASK),
+        QVariant::fromValue<Side>(Side::BID)}, this);
+  auto layout = new QHBoxLayout(this);
+  layout->setContentsMargins({});
+  layout->addWidget(menu);
+  m_value_connection = menu->connect_value_selected_signal(
+    [=] (const auto& value) { m_side_signal(value.value<Side>()); });
+}
 
-Side SideInputWidget::get_side() const {
-  return get_item().value<Side>();
+connection SideInputWidget::connect_side_signal(
+    const SideSignal::slot_type& slot) const {
+  return m_side_signal.connect(slot);
 }
