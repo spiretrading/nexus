40a73f58ada042e711509c5a653f4cd376f0341d
Ken <ken@spiretrading.com>
2020-09-25 21:51:39 -0400

    Code review changes

--- Applications/Spire/Source/Ui/SideComboBox.cpp
+++ Applications/Spire/Source/Ui/SideComboBox.cpp
@@ -1,4 +1,4 @@
-#include "Spire/Ui/SideInputWidget.hpp"
+#include "Spire/Ui/SideComboBox.hpp"
 #include <QHBoxLayout>
 #include "Spire/Ui/StaticDropDownMenu.hpp"
 
@@ -6,18 +6,18 @@ using namespace boost::signals2;
 using namespace Nexus;
 using namespace Spire;
 
-SideInputWidget::SideInputWidget(QWidget* parent)
+SideComboBox::SideComboBox(QWidget* parent)
     : QWidget(parent) {
   auto menu = new StaticDropDownMenu({QVariant::fromValue<Side>(Side::ASK),
-        QVariant::fromValue<Side>(Side::BID)}, this);
+    QVariant::fromValue<Side>(Side::BID)}, this);
   auto layout = new QHBoxLayout(this);
   layout->setContentsMargins({});
   layout->addWidget(menu);
   m_value_connection = menu->connect_value_selected_signal(
-    [=] (const auto& value) { m_side_signal(value.value<Side>()); });
+    [=] (const auto& value) { m_selected_signal(value.value<Side>()); });
 }
 
-connection SideInputWidget::connect_side_signal(
-    const SideSignal::slot_type& slot) const {
-  return m_side_signal.connect(slot);
+connection SideComboBox::connect_selected_signal(
+    const SelectedSignal::slot_type& slot) const {
+  return m_selected_signal.connect(slot);
 }
