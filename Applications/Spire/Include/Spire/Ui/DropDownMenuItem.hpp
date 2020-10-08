e2a010f23a2c29af9e0c743000e5d89bc7d33009
Ken <ken@spiretrading.com>
2020-10-08 12:37:58 -0400

    Replaced old DropDownMenuItem implementation

--- Applications/Spire/Include/Spire/Ui/DropDownMenuItem.hpp
+++ Applications/Spire/Include/Spire/Ui/DropDownMenuItem.hpp
@@ -1,48 +1,75 @@
 #ifndef SPIRE_DROP_DOWN_MENU_ITEM_HPP
 #define SPIRE_DROP_DOWN_MENU_ITEM_HPP
-#include <QLabel>
-#include "Spire/Ui/Ui.hpp"
+#include <QImage>
+#include <QWidget>
+#include "Spire/Spire/Spire.hpp"
+#include "Spire/Ui/CustomQtVariants.hpp"
 
 namespace Spire {
 
-  //! Represents an item in a DropDownMenu.
-  class DropDownMenuItem : public QLabel {
+  //! Represents an item used in a DropDownMenuList.
+  class DropDownMenuItem : public QWidget {
     public:
 
-      //! Signals that an user selected an item.
+      //! Signals that the item was highlighted with the mouse.
       /*!
-        \param text The item selected.
+        \param value The value of the DropDownMenuItem.
       */
-      using SelectedSignal = Signal<void (const QString& text)>;
+      using HighlightedSignal = Signal<void (QVariant value)>;
 
-      //! Constructs a DropDownMenuItem with displayed text.
+      //! Signals that the item was selected.
       /*!
-        \param text The text to display.
-        \param parent The parent to this widget.
+        \param value The value of the DropDownMenuItem.
       */
-      explicit DropDownMenuItem(const QString& text, QWidget* parent = nullptr);
+      using SelectedSignal = Signal<void (QVariant value)>;
 
-      //! Sets the item's highlighted style.
+      //! Constructs a DropDownMenuItem.
+      /*!
+        \param value The value represented by the DropDownMenuItem.
+        \param parent The parent widget.
+      */
+      explicit DropDownMenuItem(QVariant value, QWidget* parent = nullptr);
+
+      //! Consstructs a DropDownMenuItem with an icon.
+      /*!
+        \param value The value represented by the DropDownMenuItem.
+        \param icon The icon to display beside the value.
+        \param parent The parent widget.
+      */
+      DropDownMenuItem(QVariant value, QImage icon, QWidget* parent = nullptr);
+
+      //! Returns the item's value.
+      const QVariant& get_value() const;
+
+      //! Returns true iff the item is highlighted.
+      bool is_highlighted() const;
+
+      //! Displays the item's highlight style.
       void set_highlight();
 
-      //! Removes the item's highlighted style.
-      void remove_highlight();
+      //! Removes the item's highlight style.
+      void reset_highlight();
 
-      //! Calls the provided slot when the selected signal is triggered.
-      /*!
-        \param slot The slot to call.
-      */
+      //! Connects a slot to the highlighted signal.
+      boost::signals2::connection connect_highlighted_signal(
+        const HighlightedSignal::slot_type& slot) const;
+      
+      //! Connects a slot to the selected signal.
       boost::signals2::connection connect_selected_signal(
         const SelectedSignal::slot_type& slot) const;
 
     protected:
-      void keyPressEvent(QKeyEvent* event) override;
+      void enterEvent(QEvent* event) override;
       void mousePressEvent(QMouseEvent* event) override;
       void paintEvent(QPaintEvent* event) override;
 
     private:
+      QVariant m_value;
+      QImage m_icon;
+      mutable HighlightedSignal m_highlighted_signal;
       mutable SelectedSignal m_selected_signal;
       bool m_is_highlighted;
+      CustomVariantItemDelegate m_item_delegate;
   };
 }
 
--- Applications/Spire/Source/Ui/DropDownMenuItem.cpp
+++ Applications/Spire/Source/Ui/DropDownMenuItem.cpp
@@ -1,5 +1,4 @@
-#include "Spire/Ui/DropdownMenuItem.hpp"
-#include <QFontMetrics>
+#include "Spire/Ui/DropDownMenuItem.hpp"
 #include <QMouseEvent>
 #include <QPainter>
 #include "Spire/Spire/Dimensions.hpp"
@@ -12,30 +11,64 @@ namespace {
     static auto padding = scale_width(8);
     return padding;
   }
-
+  
   auto RIGHT_PADDING() {
     static auto padding = scale_width(12);
     return padding;
   }
 }
 
-DropDownMenuItem::DropDownMenuItem(const QString& text, QWidget* parent)
-    : QLabel(text, parent),
+DropDownMenuItem::DropDownMenuItem(QVariant value, QWidget* parent)
+  : DropDownMenuItem(std::move(value), {}, parent) {}
+
+DropDownMenuItem::DropDownMenuItem(QVariant value, QImage icon,
+    QWidget* parent)
+    : QWidget(parent),
+      m_value(std::move(value)),
+      m_icon(std::move(icon)),
       m_is_highlighted(false) {
-  setMouseTracking(true);
   setAttribute(Qt::WA_Hover);
-  setFixedHeight(scale_height(20));
   auto font = QFont("Roboto");
   font.setPixelSize(scale_height(12));
   setFont(font);
 }
 
-void DropDownMenuItem::set_highlight() {
-  m_is_highlighted = true;
+void DropDownMenuItem::enterEvent(QEvent* event) {
+  auto value = m_value;
+  m_highlighted_signal(value);
 }
 
-void DropDownMenuItem::remove_highlight() {
-  m_is_highlighted = false;
+void DropDownMenuItem::mousePressEvent(QMouseEvent* event) {
+  if(event->button() == Qt::LeftButton) {
+    auto value = m_value;
+    m_selected_signal(value);
+  }
+}
+
+void DropDownMenuItem::paintEvent(QPaintEvent* event) {
+  auto painter = QPainter(this);
+  if(underMouse() || m_is_highlighted) {
+    painter.fillRect(rect(), QColor("#F2F2FF"));
+  } else {
+    painter.fillRect(rect(), Qt::white);
+  }
+  auto metrics = QFontMetrics(font());
+  auto shortened_text = metrics.elidedText(
+    m_item_delegate.displayText(m_value), Qt::ElideRight,
+    width() - RIGHT_PADDING());
+  if(!m_icon.isNull()) {
+    painter.drawImage(LEFT_PADDING(), (height() / 2) - (m_icon.height() / 2),
+      m_icon);
+    painter.drawText(2 * LEFT_PADDING() + m_icon.width(), metrics.height(),
+      shortened_text);
+  } else {
+    painter.drawText(LEFT_PADDING(), metrics.height(), shortened_text);
+  }
+}
+
+connection DropDownMenuItem::connect_highlighted_signal(
+    const SelectedSignal::slot_type& slot) const {
+  return m_selected_signal.connect(slot);
 }
 
 connection DropDownMenuItem::connect_selected_signal(
@@ -43,27 +76,20 @@ connection DropDownMenuItem::connect_selected_signal(
   return m_selected_signal.connect(slot);
 }
 
-void DropDownMenuItem::keyPressEvent(QKeyEvent* event) {
-  if(event->key() == Qt::Key_Enter || event->key() == Qt::Key_Return) {
-    m_selected_signal(text());
-  }
+const QVariant& DropDownMenuItem::get_value() const {
+  return m_value;
 }
 
-void DropDownMenuItem::mousePressEvent(QMouseEvent* event) {
-  if(event->button() == Qt::LeftButton) {
-    m_selected_signal(text());
-  }
+bool DropDownMenuItem::is_highlighted() const {
+  return m_is_highlighted;
 }
 
-void DropDownMenuItem::paintEvent(QPaintEvent* event) {
-  auto painter = QPainter(this);
-  if(m_is_highlighted || underMouse()) {
-    painter.fillRect(event->rect(), QColor("#F2F2FF"));
-  } else {
-    painter.fillRect(event->rect(), Qt::white);
-  }
-  auto metrics = QFontMetrics(font());
-  auto shortened_text = metrics.elidedText(text(), Qt::ElideRight,
-    width() - RIGHT_PADDING());
-  painter.drawText(LEFT_PADDING(), metrics.height(), shortened_text);
+void DropDownMenuItem::set_highlight() {
+  m_is_highlighted = true;
+  update();
+}
+
+void DropDownMenuItem::reset_highlight() {
+  m_is_highlighted = false;
+  update();
 }
