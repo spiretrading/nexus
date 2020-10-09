5dfe85b9a8eb6da01bd7eb65891362908f9256d1
Kamal <kamal@eidolonsystems.com>
2020-10-09 10:39:01 -0400

    Added starter code for the UiViewer project.

--- Applications/Spire/Source/UiViewer/main.cpp
+++ Applications/Spire/Source/UiViewer/main.cpp
@@ -1,17 +1,12 @@
 #include <QApplication>
 #include "Spire/Spire/Resources.hpp"
-#include "Spire/Spire/SpireController.hpp"
-#include "Version.hpp"
 
 using namespace Spire;
 
 int main(int argc, char** argv) {
   auto application = new QApplication(argc, argv);
   application->setOrganizationName(QObject::tr("Spire Trading Inc"));
-  application->setApplicationName(QObject::tr("UI Viewer"));
-  application->setApplicationVersion(SPIRE_VERSION);
+  application->setApplicationName(QObject::tr("Scratch"));
   initialize_resources();
-  auto controller = SpireController();
-  controller.open();
   application->exec();
 }
