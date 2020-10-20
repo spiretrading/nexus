56d1add94207c8f30657bae3d5e2ee7d29dda2a8
Ken <ken@spiretrading.com>
2020-10-20 18:21:38 -0400

    Added SecurityWidgetTestWidget

---
+++ Applications/Spire/Include/Spire/UiViewer/SecurityWidgetTestWidget.hpp
@@ -0,0 +1,37 @@
+#ifndef SPIRE_SECURITY_WIDGET_TEST_WIDGET_HPP
+#define SPIRE_SECURITY_WIDGET_TEST_WIDGET_HPP
+#include <QGridLayout>
+#include <QLabel>
+#include <QWidget>
+#include "Nexus/Definitions/Security.hpp"
+#include "Spire/SecurityInput/LocalSecurityInputModel.hpp"
+#include "Spire/Ui/CheckBox.hpp"
+#include "Spire/Ui/CustomQtVariants.hpp"
+#include "Spire/Ui/SecurityWidget.hpp"
+
+namespace Spire {
+
+  //! Represents a widget for testing a SecurityWidget.
+  class SecurityWidgetTestWidget : public QWidget {
+    public:
+
+      //! Constructs a SecurityWidgetTestWidget.
+      /*!
+        \param parent The parent widget.
+      */
+      SecurityWidgetTestWidget(QWidget* parent = nullptr);
+
+    private:
+      QGridLayout* m_layout;
+      CheckBox* m_dark_theme_check_box;
+      QLabel* m_status_label;
+      SecurityWidget* m_security_widget;
+      LocalSecurityInputModel m_model;
+      CustomVariantItemDelegate m_item_delegate;
+
+      void on_reset_button();
+      void on_security_selected(const Nexus::Security& security);
+  };
+}
+
+#endif
