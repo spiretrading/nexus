e23fb9b1e5130fe8698a7ba39fc6f65eeac3c636
Ken <ken@spiretrading.com>
2020-10-23 16:04:59 -0400

    Added add_tab(...) to simplify TabTestWidget construction

--- Applications/Spire/Source/UiViewer/TabTestWidget.cpp
+++ Applications/Spire/Source/UiViewer/TabTestWidget.cpp
@@ -5,15 +5,12 @@
 
 using namespace Spire;
 
-TabTestWidget::TabTestWidget(QWidget* parent)
-    : QWidget(parent) {
-  auto layout = new QHBoxLayout(this);
-  auto tab_widget = new TabWidget(this);
-  layout->addWidget(tab_widget);
-  auto tab1_label = new QLabel(tr("Tab 1"), this);
-  tab1_label->setAlignment(Qt::AlignCenter);
-  tab1_label->setFocusPolicy(Qt::StrongFocus);
-  auto style = QString(R"(
+namespace {
+  void add_tab(const QString& label, TabWidget& tab_widget, QWidget& parent) {
+  auto tab_label = new QLabel(label, &parent);
+  tab_label->setAlignment(Qt::AlignCenter);
+  tab_label->setFocusPolicy(Qt::StrongFocus);
+  tab_label->setStyleSheet(R"(
     QLabel {
       background-color: white;
       font-size: 20pt;
@@ -21,16 +18,16 @@ TabTestWidget::TabTestWidget(QWidget* parent)
     QLabel:focus {
       border: 2px solid #4B23A0;
     })");
-  tab1_label->setStyleSheet(style);
-  tab_widget->addTab(tab1_label, tr("Tab 1"));
-  auto tab2_label = new QLabel(tr("Tab 2"), this);
-  tab2_label->setAlignment(Qt::AlignCenter);
-  tab2_label->setFocusPolicy(Qt::StrongFocus);
-  tab2_label->setStyleSheet(style);
-  tab_widget->addTab(tab2_label, tr("Tab 2"));
-  auto tab3_label = new QLabel(tr("Tab 3"), this);
-  tab3_label->setAlignment(Qt::AlignCenter);
-  tab3_label->setFocusPolicy(Qt::StrongFocus);
-  tab3_label->setStyleSheet(style);
-  tab_widget->addTab(tab3_label, tr("Tab 3"));
+  tab_widget.addTab(tab_label, label);
+  }
+}
+
+TabTestWidget::TabTestWidget(QWidget* parent)
+    : QWidget(parent) {
+  auto layout = new QHBoxLayout(this);
+  auto tab_widget = new TabWidget(this);
+  layout->addWidget(tab_widget);
+  add_tab(tr("Tab 1"), *tab_widget, *this);
+  add_tab(tr("Tab 2"), *tab_widget, *this);
+  add_tab(tr("Tab 3"), *tab_widget, *this);
 }
