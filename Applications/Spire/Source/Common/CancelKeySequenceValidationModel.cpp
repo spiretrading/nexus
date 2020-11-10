faa7b815a97be01be118713fa796d72fdfdc6146
Ken <ken@spiretrading.com>
2020-11-10 17:58:40 -0500

    Removed key sequence validation models' key sets

--- Applications/Spire/Source/Common/CancelKeySequenceValidationModel.cpp
+++ Applications/Spire/Source/Common/CancelKeySequenceValidationModel.cpp
@@ -4,10 +4,9 @@
 using namespace Spire;
 
 namespace {
-  auto MODIFIER_KEYS() {
-    static auto keys = QSet<Qt::Key>({Qt::Key_Alt, Qt::Key_Control,
-      Qt::Key_Shift});
-    return keys;
+  auto is_modifier_key(Qt::Key key) {
+    return key == Qt::Key_Alt || key == Qt::Key_Control ||
+      key == Qt::Key_Shift;
   }
 }
 
@@ -17,7 +16,7 @@ bool CancelKeySequenceValidationModel::is_valid(
     case 1:
       return sequence[0] == Qt::Key_Escape;
     case 2:
-      return MODIFIER_KEYS().contains(Qt::Key(sequence[0])) &&
+      return is_modifier_key(Qt::Key(sequence[0])) &&
         sequence[1] == Qt::Key_Escape;
   }
   return false;
--- Applications/Spire/Source/Common/TaskKeySequenceValidationModel.cpp
+++ Applications/Spire/Source/Common/TaskKeySequenceValidationModel.cpp
@@ -4,17 +4,13 @@
 using namespace Spire;
 
 namespace {
-  auto MODIFIER_KEYS() {
-    static auto keys = QSet<Qt::Key>({Qt::Key_Alt, Qt::Key_Control,
-      Qt::Key_Shift});
-    return keys;
+  auto is_modifier_key(Qt::Key key) {
+    return key == Qt::Key_Alt || key == Qt::Key_Control ||
+      key == Qt::Key_Shift;
   }
 
-  auto FUNCTION_KEYS() {
-    static auto keys = QSet<Qt::Key>({Qt::Key_F1, Qt::Key_F2, Qt::Key_F3,
-      Qt::Key_F4, Qt::Key_F5, Qt::Key_F6, Qt::Key_F7, Qt::Key_F8, Qt::Key_F9,
-      Qt::Key_F10, Qt::Key_F11, Qt::Key_F12});
-    return keys;
+  auto is_function_key(Qt::Key key) {
+    return key >= Qt::Key_F1 && key <= Qt::Key_F12;
   }
 }
 
@@ -22,10 +18,10 @@ bool TaskKeySequenceValidationModel::is_valid(
     const QKeySequence& sequence) const {
   switch(sequence.count()) {
     case 1:
-      return FUNCTION_KEYS().contains(Qt::Key(sequence[0]));
+      return is_function_key(Qt::Key(sequence[0]));
     case 2:
-      return MODIFIER_KEYS().contains(Qt::Key(sequence[0])) &&
-        FUNCTION_KEYS().contains(Qt::Key(sequence[1]));
+      return is_modifier_key(Qt::Key(sequence[0])) &&
+        is_function_key(Qt::Key(sequence[1]));
   }
   return false;
 }
