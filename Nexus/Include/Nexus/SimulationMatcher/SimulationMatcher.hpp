fcb7a3f09c2ef32ba5755cb90427d65d403c050a
Kamal <kamal@eidolonsystems.com>
2020-11-13 21:46:11 -0500

    The SimulationMatcher.hpp was previously empty, modified it to include forward declarations.

---
+++ Nexus/Include/Nexus/SimulationMatcher/SimulationMatcher.hpp
@@ -0,0 +1,9 @@
+#ifndef NEXUS_SIMULATION_MATCHER_HPP
+#define NEXUS_SIMULATION_MATCHER_HPP
+
+namespace Nexus {
+  template<typename TimeClientType> class SecurityOrderSimulator;
+  template<typename C, typename T> class SimulationOrderExecutionDriver;
+}
+
+#endif
