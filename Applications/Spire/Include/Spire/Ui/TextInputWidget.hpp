9cee43716b6ce1f592dec9dd11d986a261d5e9cf
Ken <ken@spiretrading.com>
2020-11-27 13:41:55 -0500

    Removed cell style from TextInputWidget

--- Applications/Spire/Include/Spire/Ui/TextInputWidget.hpp
+++ Applications/Spire/Include/Spire/Ui/TextInputWidget.hpp
@@ -9,16 +9,6 @@ namespace Spire {
   class TextInputWidget : public QLineEdit {
     public:
 
-      /** The styles available to render the TextInputWidget. */
-      enum class Style {
-
-        /** Render using the default style. */
-        DEFAULT,
-
-        /** Render using the table cell style. */
-        CELL
-      };
-
       //! Constructs a TextInputWidget with the default style.
       /*!
         \param parent The parent widget.
@@ -32,19 +22,10 @@ namespace Spire {
       */
       explicit TextInputWidget(QString text, QWidget* parent = nullptr);
 
-      //! Returns the left text padding.
-      int get_padding() const;
-
-      //! Sets the TextInputWidget's style.
-      void set_style(Style style);
-
     protected:
       void focusInEvent(QFocusEvent* event) override;
       void keyPressEvent(QKeyEvent* event) override;
       void paintEvent(QPaintEvent* event) override;
-
-    private:
-      int m_left_padding;
   };
 }
 
