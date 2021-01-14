7ab573d7c1a1c5cf58865df3765d55b1261fb6b5
Ken <ken@spiretrading.com>
2021-01-14 17:27:18 -0500

    Updated ToggleButton to refer to its state as 'checked' instead of 'toggled'.

--- Applications/Spire/Include/Spire/Ui/ToggleButton.hpp
+++ Applications/Spire/Include/Spire/Ui/ToggleButton.hpp
@@ -6,7 +6,7 @@
 
 namespace Spire {
 
-  //! Displays a button using an icon that can be toggled.
+  //! Displays a button using an icon that can be checked.
   class ToggleButton : public QWidget {
     public:
 
@@ -20,21 +20,21 @@ namespace Spire {
       */
       explicit ToggleButton(QImage icon, QWidget* parent = nullptr);
 
-      //! Sets the toggled state of the button.
+      //! Sets the checked state of the button.
       /*!
-        \param toggled The toggled state of the button.
+        \param is_checked True iff the ToggleButton should be checked.
       */
-      void set_toggled(bool toggled);
+      void set_checked(bool is_checked);
 
       //! Sets the status of the button, and applies the appropriate style.
       /*!
-        \param enabled Enables the button if true, disables it otherwise.
+        \param enabled Enables the button iff enabled is true.
       */
       void setEnabled(bool enabled);
 
       //! Sets the status of the button, and applies the appropriate style.
       /*!
-        \param disabled Disables the button if true, enables it otherwise.
+        \param disabled Disables the button iff disabled is true.
       */
       void setDisabled(bool disabled);
 
@@ -47,9 +47,9 @@ namespace Spire {
       QImage m_icon;
       IconButton* m_icon_button;
       boost::signals2::scoped_connection m_clicked_connection;
-      bool m_is_toggled;
+      bool m_is_checked;
 
-      void swap_toggle();
+      void swap_check_state();
       void update_button();
       void update_button(bool enabled);
   };
--- Applications/Spire/Source/Ui/ToggleButton.cpp
+++ Applications/Spire/Source/Ui/ToggleButton.cpp
@@ -15,7 +15,7 @@ ToggleButton::ToggleButton(QImage icon, QWidget* parent)
     : QWidget(parent),
       m_icon(std::move(icon)),
       m_icon_button(nullptr),
-      m_is_toggled(false) {
+      m_is_checked(false) {
   auto layout = new QHBoxLayout(this);
   layout->setContentsMargins({});
   update_button();
@@ -30,9 +30,9 @@ ToggleButton::ToggleButton(QImage icon, QWidget* parent)
     })").arg(scale_height(10)).arg(scale_height(2)).arg(scale_width(6)));
 }
 
-void ToggleButton::set_toggled(bool toggled) {
-  if(m_is_toggled != toggled) {
-    swap_toggle();
+void ToggleButton::set_checked(bool is_checked) {
+  if(m_is_checked != is_checked) {
+    swap_check_state();
   }
 }
 
@@ -51,15 +51,15 @@ connection ToggleButton::connect_clicked_signal(
   return m_clicked_signal.connect(slot);
 }
 
-void ToggleButton::swap_toggle() {
-  m_is_toggled = !m_is_toggled;
+void ToggleButton::swap_check_state() {
+  m_is_checked = !m_is_checked;
   update_button();
 }
 
 void ToggleButton::update_button() {
   delete_later(m_icon_button);
   auto style = IconButton::Style();
-  if(m_is_toggled) {
+  if(m_is_checked) {
     style.m_default_color = "#1FD37A";
     style.m_hover_color = "#1FD37A";
     style.m_blur_color = "#1FD37A";
@@ -70,7 +70,7 @@ void ToggleButton::update_button() {
   }
   m_icon_button = new IconButton(m_icon, style, this);
   m_clicked_connection = m_icon_button->connect_clicked_signal([=] {
-    swap_toggle();
+    swap_check_state();
     m_clicked_signal();
   });
   setFocusProxy(m_icon_button);
