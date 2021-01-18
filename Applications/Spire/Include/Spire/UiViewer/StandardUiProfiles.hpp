0fcf91e68fdcc2cd0459cffe2769ba1630f97cd1
Ken <ken@spiretrading.com>
2021-01-18 12:52:02 -0500

    Added IconButton profile to UiViewer.

--- Applications/Spire/Include/Spire/UiViewer/StandardUiProfiles.hpp
+++ Applications/Spire/Include/Spire/UiViewer/StandardUiProfiles.hpp
@@ -15,6 +15,9 @@ namespace Spire {
 
   //! Returns a UiProfile for the FlatButton.
   UiProfile make_flat_button_profile();
+
+  //! Returns a UiProfile for the IconButton.
+  UiProfile make_icon_button_profile();
 }
 
 #endif
--- Applications/Spire/Source/UiViewer/StandardUiProfiles.cpp
+++ Applications/Spire/Source/UiViewer/StandardUiProfiles.cpp
@@ -5,6 +5,7 @@
 #include "Spire/Ui/ColorSelectorButton.hpp"
 #include "Spire/Ui/CurrencyComboBox.hpp"
 #include "Spire/Ui/FlatButton.hpp"
+#include "Spire/Ui/IconButton.hpp"
 #include "Spire/UiViewer/StandardUiProperties.hpp"
 #include "Spire/UiViewer/UiProfile.hpp"
 
@@ -115,6 +116,21 @@ UiProfile Spire::make_flat_button_profile() {
       });
       button->connect_clicked_signal(
         profile.make_event_slot(QString::fromUtf8("ClickedSignal")));
+      return button;
+    });
+  return profile;
+}
+
+UiProfile Spire::make_icon_button_profile() {
+  auto properties = std::vector<std::shared_ptr<UiProperty>>();
+  populate_widget_properties(properties);
+  auto profile = UiProfile(QString::fromUtf8("IconButton"), properties,
+    [] (auto& profile) {
+      auto button = new IconButton(imageFromSvg(":/Icons/demo.svg",
+        scale(26, 26)));
+      apply_widget_properties(button, profile.get_properties());
+      QObject::connect(button, &IconButton::released,
+        profile.make_event_slot(QString::fromUtf8("released")));
       return button;
     });
   return profile;
