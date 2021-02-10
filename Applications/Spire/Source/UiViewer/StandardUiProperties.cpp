8e68f8e9ade2282aea94e4e9335e54fdeb73a136
Ken <ken@spiretrading.com>
2021-02-10 13:44:27 -0500

    Removed redundant variable declaration.

--- Applications/Spire/Source/UiViewer/StandardUiProperties.cpp
+++ Applications/Spire/Source/UiViewer/StandardUiProperties.cpp
@@ -156,7 +156,6 @@ std::shared_ptr<TypedUiProperty<QColor>> Spire::make_standard_qcolor_property(
       });
       QObject::connect(setter, &QLineEdit::textChanged,
         [&] (const auto& text) {
-          auto color = QColor(text.toUpper());
           if(text.length() == 7) {
             auto color = QColor(text.toUpper());
             if(color.isValid()) {
