c5c6a0127f3d223e4a4c835a131fb835cd244691
Ken <ken@spiretrading.com>
2021-04-26 14:03:55 -0400

    Updated BookViewPropertiesDialog to use new label Button implementation.

--- Applications/Spire/Source/Ui/PropertiesWindowButtonsWidget.cpp
+++ Applications/Spire/Source/Ui/PropertiesWindowButtonsWidget.cpp
@@ -2,7 +2,7 @@
 #include <QGridLayout>
 #include <QHBoxLayout>
 #include "Spire/Spire/Dimensions.hpp"
-#include "Spire/Ui/FlatButton.hpp"
+#include "Spire/Ui/Button.hpp"
 
 using namespace boost::signals2;
 using namespace Spire;
@@ -20,18 +20,18 @@ PropertiesWindowButtonsWidget::PropertiesWindowButtonsWidget(
   left_layout->setContentsMargins({});
   left_layout->setHorizontalSpacing(scale_width(8));
   left_layout->setVerticalSpacing(scale_height(8));
-  auto save_as_default_button = new FlatButton(tr("Save As Default"), this);
-  connect(save_as_default_button, &FlatButton::clicked, [=] {
+  auto save_as_default_button = make_label_button(tr("Save As Default"), this);
+  save_as_default_button->connect_clicked_signal([=] {
     m_save_as_default_signal();
   });
   left_layout->addWidget(save_as_default_button, 0, 0);
-  auto load_default_button = new FlatButton(tr("Load Default"), this);
-  connect(load_default_button, &FlatButton::clicked, [=] {
+  auto load_default_button = make_label_button(tr("Load Default"), this);
+  load_default_button->connect_clicked_signal([=] {
     m_load_default_signal();
   });
   left_layout->addWidget(load_default_button, 1, 0);
-  auto reset_default_button = new FlatButton(tr("Reset Default"), this);
-  connect(reset_default_button, &FlatButton::clicked, [=] {
+  auto reset_default_button = make_label_button(tr("Reset Default"), this);
+  reset_default_button->connect_clicked_signal([=] {
     m_reset_default_signal();
   });
   left_layout->addWidget(reset_default_button, 1, 1);
@@ -43,26 +43,18 @@ PropertiesWindowButtonsWidget::PropertiesWindowButtonsWidget(
   right_layout->setContentsMargins({});
   right_layout->setHorizontalSpacing(scale_width(8));
   right_layout->setVerticalSpacing(scale_height(8));
-  auto apply_button = new FlatButton(tr("Apply"), this);
-  connect(apply_button, &FlatButton::clicked, [=] {
-    m_apply_signal();
-  });
+  auto apply_button = make_label_button(tr("Apply"), this);
+  apply_button->connect_clicked_signal([=] { m_apply_signal(); });
   right_layout->addWidget(apply_button, 0, 0);
   setTabOrder(reset_default_button, apply_button);
-  auto apply_to_all_button = new FlatButton(tr("Apply To All"), this);
-  connect(apply_to_all_button, &FlatButton::clicked, [=] {
-    m_apply_to_all_signal();
-  });
+  auto apply_to_all_button = make_label_button(tr("Apply To All"), this);
+  apply_to_all_button->connect_clicked_signal([=] { m_apply_to_all_signal(); });
   right_layout->addWidget(apply_to_all_button, 0, 1);
-  auto ok_button = new FlatButton(tr("OK"), this);
-  connect(ok_button, &FlatButton::clicked, [=] {
-    m_ok_signal();
-  });
+  auto ok_button = make_label_button(tr("OK"), this);
+  ok_button->connect_clicked_signal([=] { m_ok_signal(); });
   right_layout->addWidget(ok_button, 1, 0);
-  auto cancel_button = new FlatButton(tr("Cancel"), this);
-  connect(cancel_button, &FlatButton::clicked, [=] {
-    m_cancel_signal();
-  });
+  auto cancel_button = make_label_button(tr("Cancel"), this);
+  cancel_button->connect_clicked_signal([=] { m_cancel_signal(); });
   right_layout->addWidget(cancel_button, 1, 1);
   main_layout->addWidget(right_widget);
 }
