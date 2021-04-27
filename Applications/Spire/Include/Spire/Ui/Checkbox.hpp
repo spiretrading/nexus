0bd6cf618af4a71b4b733f44f489921ae147e123
Ken <ken@spiretrading.com>
2021-04-27 12:56:18 -0400

    Added un-styled CheckBox implementation.

--- Applications/Spire/Include/Spire/Ui/Checkbox.hpp
+++ Applications/Spire/Include/Spire/Ui/Checkbox.hpp
@@ -1,12 +1,26 @@
 #ifndef SPIRE_CHECKBOX_HPP
 #define SPIRE_CHECKBOX_HPP
-#include <QCheckBox>
+#include <QHBoxLayout>
+#include <QWidget>
+#include "Spire/Styles/Stylist.hpp"
+#include "Spire/Ui/Ui.hpp"
 
 namespace Spire {
+namespace Styles {
+
+  /** Selects a checked Checkbox. */
+  using Checked = StateSelector<void, struct CheckedTag>;
+}
 
   //! Represents a Spire-styled checkbox.
-  class Checkbox : public QCheckBox {
+  class Checkbox : public QWidget {
     public:
+
+      /**
+      * Signals that the checked state has changed.
+      * @param is_checked True iff the Checkbox is checked.
+      */
+      using CheckedSignal = Signal<void (bool is_checked)>;
 
       //! Constructs a Checkbox without a label.
       /*!
@@ -21,6 +35,18 @@ namespace Spire {
       */
       explicit Checkbox(const QString& label, QWidget* parent = nullptr);
   
+      //! Sets the checked state of the Checkbox.
+      /*!
+        \param is_checked True iff the CheckBox is checked.
+      */
+      void set_checked(bool is_checked);
+
+      //! Sets the text of the label.
+      /*!
+        \param label The label's text.
+      */
+      void set_label(const QString& label);
+
       //! Sets the read-only state of the Checkbox.
       /*!
         \param is_read_only Sets the Checkbox to read-only iff is_read_only is
@@ -28,11 +54,21 @@ namespace Spire {
       */
       void set_read_only(bool is_read_only);
 
+      QSize sizeHint() const override;
+
+      //! Connects a slot to the checked signal.
+      boost::signals2::connection connect_checked_signal(
+        const CheckedSignal::slot_type& slot) const;
+
     protected:
-      void paintEvent(QPaintEvent* event) override;
-      bool hitButton(const QPoint& pos) const override;
+      void changeEvent(QEvent* event) override;
 
     private:
+      mutable CheckedSignal m_checked_signal;
+      Icon* m_check;
+      TextBox* m_label;
+      QHBoxLayout* m_body_layout;
+      bool m_is_checked;
       bool m_is_read_only;
   };
 }
--- Applications/Spire/Source/Ui/Checkbox.cpp
+++ Applications/Spire/Source/Ui/Checkbox.cpp
@@ -1,13 +1,16 @@
 #include "Spire/Ui/Checkbox.hpp"
 #include <QEvent>
-#include <QFontMetrics>
-#include <QMouseEvent>
-#include <QPainter>
-#include <QStylePainter>
+#include <QHBoxLayout>
 #include "Spire/Spire/Dimensions.hpp"
+#include "Spire/Spire/Utility.hpp"
+#include "Spire/Ui/Button.hpp"
+#include "Spire/Ui/Icon.hpp"
+#include "Spire/Ui/TextBox.hpp"
 #include "Spire/Ui/Ui.hpp"
 
+using namespace boost::signals2;
 using namespace Spire;
+using namespace Spire::Styles;
 
 namespace {
   const auto& CHECK_ICON() {
@@ -15,62 +18,79 @@ namespace {
     return icon;
   }
 
-  const auto& CHECK_DISABLED_COLOR() {
-    static auto color = QColor("#C8C8C8");
-    return color;
+  auto DEFAULT_STYLE() {
+    auto style = StyleSheet();
+    return style;
+  }
+
+  auto create_check(bool is_checked, QWidget* parent) {
+    auto check = [=] {
+      if(is_checked) {
+        return new Icon(CHECK_ICON(), parent);
+      }
+      return new Icon({}, parent);
+    }();
+    check->setFixedSize(scale(16, 16));
+    return check;
   }
 }
 
 Checkbox::Checkbox(QWidget* parent)
-  : QCheckBox("", parent) {}
+  : Checkbox("", parent) {}
 
 Checkbox::Checkbox(const QString& label, QWidget* parent)
-    : QCheckBox(label, parent),
+    : QWidget(parent),
+      m_is_checked(false),
       m_is_read_only(false) {
-  setObjectName("Checkbox");
-  setStyleSheet(QString(R"(
-    #Checkbox {
-      background-color: #00000000;
-      color: #000000;
-      font-family: "Roboto";
-      font-size: %1px;
-      outline: none;
-    }
-    #Checkbox::indicator {
-      background-color: #FFFFFF;
-      border: %2px solid #C8C8C8;
-      height: %3px;
-      width: %4px;
-    }
-    #Checkbox::indicator:hover {
-      border-color: #4B23A0;
-    }
-    #Checkbox::indicator:focus {
-      border-color: #4B23A0;
-    }
-    #Checkbox::indicator:read-only {
-      background-color: #00000000;
-      border-color: #00000000;
-    }
-    #Checkbox:disabled {
-      color: #C8C8C8;
-    }
-    #Checkbox::indicator:disabled {
-      background-color: #F5F5F5;
-    }
-    #Checkbox::indicator:read-only:disabled {
-      background-color: #00000000;
-      border-color: #00000000;
-    })").
-      arg(scale_width(12)).arg(scale_width(1)).arg(scale_height(16)).
-      arg(scale_width(16)));
+  set_style(*this, DEFAULT_STYLE());
+  auto layout = new QHBoxLayout(this);
+  layout->setContentsMargins({});
+  layout->setSpacing(0);
+  auto body = new QWidget(this);
+  auto button = new Button(body, this);
+  button->connect_clicked_signal([=] { set_checked(!m_is_checked); });
+  layout->addWidget(button);
+  m_body_layout = new QHBoxLayout(body);
+  m_body_layout->setContentsMargins({});
+  m_body_layout->setSpacing(0);
+  m_check = create_check(m_is_checked, this);
+  m_body_layout->addWidget(m_check);
+  m_label = new TextBox(label, this);
+  m_label->set_read_only(true);
+  m_label->setDisabled(true);
+  m_body_layout->addWidget(m_label);
+}
+
+void Checkbox::changeEvent(QEvent* event) {
+  if(event->type() == QEvent::LayoutDirectionChange) {
+    // TODO: update text alignment
+  }
+}
+
+void Checkbox::set_checked(bool is_checked) {
+  if(m_is_checked == is_checked) {
+    return;
+  }
+  m_is_checked = is_checked;
+  delete_later(m_check);
+  m_check = create_check(m_is_checked, this);
+  if(m_body_layout->direction() == Qt::LeftToRight) {
+    m_body_layout->insertWidget(0, m_check);
+  } else {
+    m_body_layout->addWidget(m_check);
+  }
+  m_checked_signal(m_is_checked);
+}
+
+void Checkbox::set_label(const QString& label) {
+  m_label->get_model()->set_current(label);
 }
 
 void Checkbox::set_read_only(bool is_read_only) {
   if(is_read_only != m_is_read_only) {
     m_is_read_only = is_read_only;
     if(m_is_read_only) {
-      setAttribute(Qt::WA_TransparentForMouseEvents, true);
+      setAttribute(Qt::WA_TransparentForMouseEvents);
       setFocusPolicy(Qt::NoFocus);
     } else {
       setAttribute(Qt::WA_TransparentForMouseEvents, false);
@@ -80,50 +100,11 @@ void Checkbox::set_read_only(bool is_read_only) {
   }
 }
 
-void Checkbox::paintEvent(QPaintEvent* event) {
-  if(m_is_read_only && !isChecked()) {
-    return;
-  }
-  auto painter = QStylePainter(this);
-  auto option = QStyleOptionButton();
-  initStyleOption(&option);
-  if(m_is_read_only) {
-    option.state |= QStyle::State_ReadOnly;
-  }
-  auto style = painter.style();
-  auto indicator_rect = style->subElementRect(QStyle::SE_CheckBoxIndicator,
-    &option, this);
-  option.rect = indicator_rect;
-  painter.drawPrimitive(QStyle::PE_IndicatorCheckBox, option);
-  if(isChecked()) {
-    auto icon = QPixmap::fromImage(CHECK_ICON());
-    if(!isEnabled()) {
-      auto image_painter = QPainter(&icon);
-      image_painter.setCompositionMode(QPainter::CompositionMode_SourceIn);
-      image_painter.fillRect(icon.rect(), CHECK_DISABLED_COLOR());
-    }
-    painter.drawItemPixmap(indicator_rect, Qt::AlignCenter, icon);
-  }
-  auto alignment = Qt::AlignLeft | Qt::AlignVCenter | Qt::TextShowMnemonic;
-  auto font_metrics = fontMetrics();
-  auto label_width = style->itemTextRect(font_metrics, QRect(), alignment, true,
-    text()).width();
-  auto contents_rect = contentsRect();
-  indicator_rect = style->visualRect(option.direction, contents_rect,
-    indicator_rect);
-  auto label_x = contents_rect.width() - label_width;
-  if(label_x <= indicator_rect.right()) {
-    label_x = indicator_rect.right() + 1;
-    label_width = contents_rect.right() - indicator_rect.right();
-  }
-  auto label_rect = QRect(label_x, contents_rect.y(), label_width,
-    contents_rect.height());
-  label_rect = style->visualRect(option.direction, contents_rect, label_rect);
-  painter.drawItemText(label_rect, alignment, palette(), true,
-    font_metrics.elidedText(text(), Qt::ElideRight, label_rect.width(),
-    Qt::TextShowMnemonic));
+QSize Checkbox::sizeHint() const {
+  return scale(80, 16);
 }
 
-bool Checkbox::hitButton(const QPoint& pos) const {
-  return rect().contains(pos);
+connection Checkbox::connect_checked_signal(
+    const CheckedSignal::slot_type& slot) const {
+  return m_checked_signal.connect(slot);
 }
