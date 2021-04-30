c8d24ead9288701aae1887f5d2813864fa4cffbb
Kamal <kamal@eidolonsystems.com>
2021-04-30 00:05:06 -0400

    CheckBox uses the descendant selector instead of the child selector to style its Icon and TextBox. Fixed some style API bugs in the CheckBox.

--- Applications/Spire/Source/Ui/CheckBox.cpp
+++ Applications/Spire/Source/Ui/CheckBox.cpp
@@ -27,30 +27,30 @@ namespace {
       }
       return Qt::AlignLeft;
     }();
-    style.get(Any() > is_a<Icon>()).
+    style.get(Any() >> is_a<Icon>()).
       set(BackgroundColor(QColor::fromRgb(0xFF, 0xFF, 0xFF))).
       set(Fill(QColor::fromRgb(0x33, 0x33, 0x33))).
       set(border(scale_width(1), QColor::fromRgb(0xC8C8C8)));
-    style.get((Focus() || (Hover() && !Disabled())) > is_a<Icon>())
+    style.get((Focus() || (Hover() && !Disabled())) >> is_a<Icon>())
       .set(border_color(QColor(0x4B, 0x23, 0xA0)));
-    style.get(Disabled() > is_a<Icon>()).
+    style.get(Disabled() >> is_a<Icon>()).
       set(BackgroundColor(QColor::fromRgb(0xF5, 0xF5, 0xF5)));
-    style.get(ReadOnly() > is_a<Icon>()).
+    style.get(ReadOnly() >> is_a<Icon>()).
       set(BackgroundColor(QColor(0, 0, 0, 0))).
       set(border_color(QColor(0, 0, 0, 0)));
-    style.get(!Checked() > is_a<Icon>()).
+    style.get(!Checked() >> is_a<Icon>()).
       set(Fill(QColor(0, 0, 0, 0)));
-    style.get((Disabled() && Checked()) > is_a<Icon>()).
+    style.get((Disabled() && Checked()) >> is_a<Icon>()).
       set(Fill(QColor::fromRgb(0xC8, 0xC8, 0xC8)));
-    style.get(Any() > is_a<TextBox>()).
+    style.get(Any() >> is_a<TextBox>()).
       set(BackgroundColor(QColor::fromRgb(0, 0, 0, 0))).
       set(TextColor(QColor(0, 0, 0))).
       set(TextAlign(alignment | Qt::AlignVCenter));
-    style.get(Disabled() > is_a<TextBox>()).
+    style.get(Disabled() >> is_a<TextBox>()).
       set(BackgroundColor(QColor(0, 0, 0, 0)));
-    style.get(ReadOnly() > is_a<TextBox>()).
+    style.get(ReadOnly() >> is_a<TextBox>()).
       set(BackgroundColor(QColor(0, 0, 0, 0)));
-    style.get((ReadOnly() && !Checked()) > is_a<TextBox>()).
+    style.get((ReadOnly() && !Checked()) >> is_a<TextBox>()).
       set(TextColor(QColor(0, 0, 0, 0)));
     return style;
   }
@@ -67,7 +67,6 @@ CheckBox::CheckBox(std::shared_ptr<BoolModel> model, QWidget* parent)
   layout->setContentsMargins({});
   layout->setSpacing(0);
   m_body = new QWidget(this);
-  set_style(*m_body, DEFAULT_STYLE(layoutDirection()));
   auto button = new Button(m_body, this);
   setFocusProxy(button);
   button->connect_clicked_signal([=] {
@@ -89,6 +88,7 @@ CheckBox::CheckBox(std::shared_ptr<BoolModel> model, QWidget* parent)
     on_checked(is_checked);
   });
   on_checked(m_model->get_current());
+  set_style(*this, DEFAULT_STYLE(layoutDirection()));
 }
 
 void CheckBox::changeEvent(QEvent* event) {
@@ -111,11 +111,11 @@ void CheckBox::set_read_only(bool is_read_only) {
     if(m_is_read_only) {
       setAttribute(Qt::WA_TransparentForMouseEvents);
       setFocusPolicy(Qt::NoFocus);
-      find_stylist(*m_body).match(ReadOnly());
+      find_stylist(*this).match(ReadOnly());
     } else {
       setAttribute(Qt::WA_TransparentForMouseEvents, false);
       setFocusPolicy(Qt::StrongFocus);
-      find_stylist(*m_body).unmatch(ReadOnly());
+      find_stylist(*this).unmatch(ReadOnly());
     }
     update();
   }
@@ -131,14 +131,11 @@ connection CheckBox::connect_checked_signal(
 }
 
 void CheckBox::on_checked(bool is_checked) {
-  auto& stylist = find_stylist(*m_body);
+  auto& stylist = find_stylist(*this);
   if(is_checked) {
     stylist.match(Checked());
-    stylist.unmatch(!Checked());
   } else {
     stylist.unmatch(Checked());
-    stylist.match(!Checked());
   }
-  update();
   m_checked_signal(is_checked);
 }
