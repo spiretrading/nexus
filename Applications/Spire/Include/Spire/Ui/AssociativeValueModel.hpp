a1880757525ba69127f92d4476d14d3a7e68f7cd
Ken <ken@spiretrading.com>
2021-05-04 22:04:03 -0400

    Added specialized std::optional<T> implementation for the AssociativeValueModel. Added tests for specialized AssociativeValueModel.

--- Applications/Spire/Include/Spire/Ui/AssociativeValueModel.hpp
+++ Applications/Spire/Include/Spire/Ui/AssociativeValueModel.hpp
@@ -1,10 +1,16 @@
 #ifndef SPIRE_ASSOCIATIVE_VALUE_MODEL_HPP
 #define SPIRE_ASSOCIATIVE_VALUE_MODEL_HPP
+#include <optional>
 #include <boost/signals2/shared_connection_block.hpp>
 #include "Spire/Ui/Ui.hpp"
 #include "Spire/Ui/LocalValueModel.hpp"
 
 namespace Spire {
+
+  struct AssociatedBooleanModel {
+    std::shared_ptr<BooleanModel> m_model;
+    boost::signals2::scoped_connection m_connection;
+  };
 
   template <typename T>
   class AssociativeValueModel : public ValueModel<T> {
@@ -29,14 +35,9 @@ namespace Spire {
         const typename CurrentSignal::slot_type& slot) const;
 
     private:
-      struct Model {
-        std::shared_ptr<BooleanModel> m_model;
-        boost::signals2::scoped_connection m_connection;
-      };
-
       mutable CurrentSignal m_current_signal;
       T m_current;
-      std::unordered_map<T, Model> m_models;
+      std::unordered_map<T, AssociatedBooleanModel> m_models;
       bool m_is_blocked;
 
       void set_associated_model_value(const T& value, bool model_value);
@@ -97,6 +98,7 @@ namespace Spire {
   template <typename T>
   QValidator::State AssociativeValueModel<T>::set_current(const T& value) {
     if(m_models.find(value) != m_models.end()) {
+      set_associated_model_value(m_current, false);
       m_current = value;
       set_associated_model_value(m_current, true);
       m_current_signal(m_current);
@@ -137,6 +139,152 @@ namespace Spire {
     m_is_blocked = true;
     set_current(value);
     m_is_blocked = false;
+  }
+
+  template <typename T>
+  class AssociativeValueModel<std::optional<T>> :
+      public ValueModel<std::optional<T>> {
+    public:
+
+      AssociativeValueModel();
+
+      void associate(const std::shared_ptr<BooleanModel>& model,
+        const std::optional<T>& value);
+
+      std::shared_ptr<BooleanModel> make_association(
+        const std::optional<T>& value);
+
+      std::shared_ptr<BooleanModel> find(
+        const std::optional<T>& value) const;
+
+      QValidator::State get_state() const;
+
+      const std::optional<T>& get_current() const;
+
+      QValidator::State set_current(const std::optional<T>& value);
+
+      boost::signals2::connection connect_current_signal(
+        const typename ValueModel<std::optional<T>>::CurrentSignal::slot_type& slot) const;
+
+    private:
+      mutable typename ValueModel<std::optional<T>>::CurrentSignal m_current_signal;
+      std::optional<T> m_current;
+      std::unordered_map<std::optional<T>, AssociatedBooleanModel> m_models;
+      bool m_is_blocked;
+
+      void set_associated_model_value(const std::optional<T>& value,
+        bool model_value);
+      void on_current(const std::optional<T>& value, bool is_selected);
+  };
+
+  template <typename T>
+  AssociativeValueModel<std::optional<T>>::AssociativeValueModel()
+    : m_is_blocked(false) {}
+
+  template <typename T>
+  void AssociativeValueModel<std::optional<T>>::associate(
+      const std::shared_ptr<BooleanModel>& model,
+      const std::optional<T>& value) {
+    if(!value) {
+      throw std::invalid_argument(
+        "Associated BooleanModel must have a corresponding value.");
+    }
+    if(m_models.find(value) != m_models.end()) {
+      return;
+    }
+
+    m_models[value] = {model, model->connect_current_signal(
+      [=] (auto is_selected) { on_current(value, is_selected); })};
+    if(model->get_current() == true) {
+      if(m_current) {
+        set_associated_model_value(value, false);
+      } else {
+        set_current(value);
+      }
+    }
+  }
+
+  template <typename T>
+  std::shared_ptr<BooleanModel>
+      AssociativeValueModel<std::optional<T>>::make_association(
+      const std::optional<T>& value) {
+    auto model = std::make_shared<LocalBooleanModel>(false);
+    associate(model, value);
+    return model;
+  }
+
+  template <typename T>
+  std::shared_ptr<BooleanModel>
+      AssociativeValueModel<std::optional<T>>::find(
+      const std::optional<T>& value) const {
+    auto iterator = m_models.find(value);
+    if(iterator == m_models.end()) {
+      return nullptr;
+    }
+    return iterator->second.m_model;
+  }
+
+  template <typename T>
+  QValidator::State
+      AssociativeValueModel<std::optional<T>>::get_state() const {
+    if(!m_models.empty()) {
+      return QValidator::Acceptable;
+    }
+    return QValidator::Invalid;
+  }
+
+  template <typename T>
+  const std::optional<T>&
+      AssociativeValueModel<std::optional<T>>::get_current() const {
+    return m_current;
+  }
+
+  template <typename T>
+  QValidator::State AssociativeValueModel<std::optional<T>>::set_current(
+      const std::optional<T>& value) {
+    if(!value && m_current) {
+      set_associated_model_value(m_current, false);
+      m_current_signal(std::nullopt);
+      return QValidator::Acceptable;
+    } else if(auto iterator = m_models.find(value); iterator != m_models.end()) {
+      set_associated_model_value(m_current, false);
+      m_current = value;
+      set_associated_model_value(m_current, true);
+      m_current_signal(m_current);
+      return QValidator::Acceptable;
+    }
+    return QValidator::Invalid;
+  }
+
+  template <typename T>
+  boost::signals2::connection
+      AssociativeValueModel<std::optional<T>>::connect_current_signal(
+      const typename ValueModel<std::optional<T>>::CurrentSignal::slot_type& slot) const {
+    return m_current_signal.connect(slot);
+  }
+
+  template <typename T>
+  void AssociativeValueModel<std::optional<T>>::set_associated_model_value(
+      const std::optional<T>& value, bool model_value) {
+    if(auto iterator = m_models.find(value); iterator != m_models.end()) {
+      auto blocker = boost::signals2::shared_connection_block(
+        iterator->second.m_connection);
+      iterator->second.m_model->set_current(model_value);
+    }
+  }
+
+  template <typename T>
+  void AssociativeValueModel<std::optional<T>>::on_current(
+      const std::optional<T>& value, bool is_selected) {
+    if(m_is_blocked && is_selected) {
+      set_associated_model_value(value, false);
+      return;
+    }
+    if(is_selected) {
+      m_is_blocked = true;
+      set_current(value);
+      m_is_blocked = false;
+    }
   }
 }
 
--- Applications/Spire/Source/SpireTester/AssociativeValueModelTester.cpp
+++ Applications/Spire/Source/SpireTester/AssociativeValueModelTester.cpp
@@ -1,4 +1,5 @@
 #include <doctest/doctest.h>
+#include <optional>
 #include "Spire/Ui/AssociativeValueModel.hpp"
 #include "Spire/Ui/LocalValueModel.hpp"
 
@@ -80,12 +81,16 @@ TEST_SUITE("AssociativeValueModel") {
     model.associate(bool_model1, "model1");
     model.associate(bool_model2, "model2");
     REQUIRE(model.get_current() == "model1");
-    model.set_current("model2");
+    REQUIRE(model.set_current("model2") == QValidator::Acceptable);
+    REQUIRE(bool_model1->get_current() == false);
     REQUIRE(model.get_current() == "model2");
-    model.set_current("model1");
+    REQUIRE(model.set_current("model1") == QValidator::Acceptable);
     REQUIRE(model.get_current() == "model1");
-    model.set_current("null");
+    REQUIRE(bool_model2->get_current() == false);
+    REQUIRE(model.set_current("null") == QValidator::Invalid);
     REQUIRE(model.get_current() == "model1");
+    REQUIRE(bool_model1->get_current() == true);
+    REQUIRE(bool_model2->get_current() == false);
   }
 
   TEST_CASE("current_signal") {
@@ -130,6 +135,133 @@ TEST_SUITE("AssociativeValueModel") {
 
   TEST_CASE("reentrant") {
     auto model = AssociativeValueModel<std::string>();
+    auto bool_model1 = std::make_shared<LocalBooleanModel>(false);
+    auto bool_model2 = std::make_shared<LocalBooleanModel>(false);
+    model.associate(bool_model1, "model1");
+    model.associate(bool_model2, "model2");
+    model.connect_current_signal([=] (const auto& current) {
+      if(current == "model1") {
+        bool_model2->set_current(true);
+      } else {
+        bool_model1->set_current(true);
+      }
+    });
+    bool_model2->set_current(true);
+    REQUIRE(model.get_current() == "model2");
+    bool_model2->set_current(false);
+    REQUIRE(model.get_current() == "model2");
+    bool_model1->set_current(true);
+    REQUIRE(model.get_current() == "model1");
+    REQUIRE(bool_model1->get_current() == true);
+    REQUIRE(bool_model2->get_current() == false);
+  }
+
+  TEST_CASE("optional_associate") {
+    auto model = AssociativeValueModel<std::optional<std::string>>();
+    auto bool_model1 = std::make_shared<LocalBooleanModel>(false);
+    auto bool_model2 = std::make_shared<LocalBooleanModel>(false);
+    auto bool_model3 = std::make_shared<LocalBooleanModel>(false);
+    model.associate(bool_model1, "model1");
+    model.associate(bool_model2, "model2");
+    REQUIRE_THROWS(model.associate(bool_model3, {}));
+    REQUIRE(bool_model1->get_current() == false);
+    REQUIRE(bool_model2->get_current() == false);
+    REQUIRE(model.get_current() == std::nullopt);
+    bool_model1->set_current(true);
+    REQUIRE(bool_model1->get_current() == true);
+    REQUIRE(bool_model2->get_current() == false);
+    REQUIRE(*model.get_current() == "model1");
+    bool_model2->set_current(true);
+    REQUIRE(bool_model1->get_current() == false);
+    REQUIRE(bool_model2->get_current() == true);
+    REQUIRE(*model.get_current() == "model2");
+  }
+
+  TEST_CASE("optional_make_association") {
+    auto model = AssociativeValueModel<std::optional<std::string>>();
+    auto bool_model1 = model.make_association("model1");
+    auto bool_model2 = model.make_association("model2");
+    REQUIRE_THROWS(model.make_association({}));
+    REQUIRE(bool_model1->get_current() == false);
+    REQUIRE(bool_model2->get_current() == false);
+    REQUIRE(model.get_current() == std::nullopt);
+    bool_model1->set_current(true);
+    REQUIRE(bool_model1->get_current() == true);
+    REQUIRE(bool_model2->get_current() == false);
+    REQUIRE(*model.get_current() == "model1");
+    bool_model2->set_current(true);
+    REQUIRE(bool_model1->get_current() == false);
+    REQUIRE(bool_model2->get_current() == true);
+    REQUIRE(*model.get_current() == "model2");
+  }
+
+  TEST_CASE("optional_find") {
+    auto model = AssociativeValueModel<std::optional<std::string>>();
+    auto bool_model1 = model.make_association("model1");
+    auto bool_model2 = model.make_association("model2");
+    REQUIRE(model.find("model1") == bool_model1);
+    REQUIRE(model.find("model2") == bool_model2);
+    REQUIRE(model.find({}) == nullptr);
+  }
+
+  TEST_CASE("optional_get_state") {
+    auto model = AssociativeValueModel<std::optional<std::string>>();
+    auto bool_model = std::make_shared<LocalBooleanModel>(false);
+    REQUIRE(model.get_state() == QValidator::Invalid);
+    model.associate(bool_model, "model");
+    REQUIRE(model.get_state() == QValidator::Acceptable);
+    REQUIRE(bool_model->get_current() == false);
+  }
+
+  TEST_CASE("optional_get_current") {
+    auto model = AssociativeValueModel<std::optional<std::string>>();
+    auto bool_model1 = std::make_shared<LocalBooleanModel>(false);
+    model.associate(bool_model1, "model1");
+    REQUIRE(model.get_current() == std::nullopt);
+    bool_model1->set_current(true);
+    REQUIRE(model.get_current() == "model1");
+  }
+
+  TEST_CASE("optional_set_current") {
+    auto model = AssociativeValueModel<std::optional<std::string>>();
+    auto bool_model1 = std::make_shared<LocalBooleanModel>(false);
+    auto bool_model2 = std::make_shared<LocalBooleanModel>(false);
+    model.associate(bool_model1, "model1");
+    model.associate(bool_model2, "model2");
+    REQUIRE(model.set_current("model1") == QValidator::Acceptable);
+    REQUIRE(model.set_current("model2") == QValidator::Acceptable);
+    REQUIRE(model.set_current("null") == QValidator::Invalid);
+    REQUIRE(model.set_current(std::nullopt) == QValidator::Acceptable);
+  }
+
+  TEST_CASE("optional_current_signal") {
+    auto model = AssociativeValueModel<std::optional<std::string>>();
+    auto current_value = std::optional<std::string>();
+    auto signal_count = 0;
+    model.connect_current_signal([&] (const auto& current) {
+      ++signal_count;
+      current_value = current;
+    });
+    auto bool_model1 = std::make_shared<LocalBooleanModel>(false);
+    model.associate(bool_model1, "model1");
+    REQUIRE(signal_count == 0);
+    auto bool_model2 = std::make_shared<LocalBooleanModel>(true);
+    model.associate(bool_model2, "model2");
+    REQUIRE(current_value == "model2");
+    REQUIRE(signal_count == 1);
+    bool_model1->set_current(true);
+    REQUIRE(current_value == "model1");
+    REQUIRE(signal_count == 2);
+    model.set_current("model2");
+    REQUIRE(current_value == "model2");
+    REQUIRE(signal_count == 3);
+    model.set_current(std::nullopt);
+    REQUIRE(current_value == std::nullopt);
+    REQUIRE(signal_count == 4);
+  }
+
+  TEST_CASE("optional_reentrant") {
+    auto model = AssociativeValueModel<std::optional<std::string>>();
     auto bool_model1 = std::make_shared<LocalBooleanModel>(false);
     auto bool_model2 = std::make_shared<LocalBooleanModel>(false);
     model.associate(bool_model1, "model1");
