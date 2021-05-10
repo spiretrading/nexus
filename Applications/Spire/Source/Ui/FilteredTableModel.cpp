8a6c7e25188c7ade2f6081f3735859400e72efaf
Shaobin <shaobin@spiretrading.com>
2021-05-10 14:52:18 -0400

    Added std::move to filter.

--- Applications/Spire/Source/Ui/FilteredTableModel.cpp
+++ Applications/Spire/Source/Ui/FilteredTableModel.cpp
@@ -7,7 +7,7 @@ using namespace Spire;
 FilteredTableModel::FilteredTableModel(std::shared_ptr<TableModel> source,
   std::function<bool(const TableModel&, int)> filter)
     : m_source(std::move(source)),
-      m_filter(filter) {
+      m_filter(std::move(filter)) {
   for(auto i = 0; i != m_source->get_row_size(); ++i) {
     if(!m_filter(*m_source, i)) {
       m_filtered_data.push_back(i);
