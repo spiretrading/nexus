2a577c1e5aace3777a04dbe4a6c42d036cf53af7
Shaobin <shaobin@spiretrading.com>
2021-05-21 19:48:42 -0400

    Unit test using std::deque<Operation>

--- Applications/Spire/Source/SpireTester/ArrayListModelTester.cpp
+++ Applications/Spire/Source/SpireTester/ArrayListModelTester.cpp
@@ -1,3 +1,4 @@
+#include <deque>
 #include <doctest/doctest.h>
 #include "Spire/Ui/ArrayListModel.hpp"
 
@@ -5,26 +6,42 @@ using namespace boost;
 using namespace boost::signals2;
 using namespace Spire;
 
+namespace {
+  template<typename... F>
+  decltype(auto) test_operation(
+      const ListModel::Operation& operation, F&&... f) {
+    return visit(
+      operation, std::forward<F>(f)..., [] (const auto&) { REQUIRE(false); });
+  }
+}
+
 TEST_SUITE("ArrayListModel") {
   TEST_CASE("push") {
     auto model = ArrayListModel();
     REQUIRE(model.get_size() == 0);
-    auto signal_count = 0;
+    auto operations = std::deque<ListModel::Operation>();
     auto connection = scoped_connection(model.connect_operation_signal(
       [&] (const ListModel::Operation& operation) {
-        ++signal_count;
-        auto add_operation = get<ListModel::AddOperation>(&operation);
-        REQUIRE(add_operation != nullptr);
-        REQUIRE(add_operation->m_index == model.get_size() - 1);
+        operations.push_back(operation);
       }));
     REQUIRE_NOTHROW(model.push(3));
-    REQUIRE(signal_count == 1);
+    REQUIRE(operations.size() == 1);
     REQUIRE(model.get_size() == 1);
     REQUIRE(model.get<int>(0) == 3);
+    auto operation = operations.front();
+    operations.pop_front();
+    test_operation(operation, [&] (const ListModel::AddOperation& operation) {
+      REQUIRE(operation.m_index == model.get_size() - 1);
+    });
     REQUIRE_NOTHROW(model.push(2));
-    REQUIRE(signal_count == 2);
     REQUIRE(model.get_size() == 2);
     REQUIRE(model.get<int>(1) == 2);
+    REQUIRE(operations.size() == 1);
+    operation = operations.front();
+    operations.pop_front();
+    test_operation(operation, [&] (const ListModel::AddOperation& operation) {
+      REQUIRE(operation.m_index == model.get_size() - 1);
+    });
   }
 
   TEST_CASE("remove") {
@@ -34,33 +51,29 @@ TEST_SUITE("ArrayListModel") {
         REQUIRE(false);
       }));
     REQUIRE_THROWS(model.remove(0));
-    auto signal_count = 0;
-    auto remove_count = 0;
+    auto operations = std::deque<ListModel::Operation>();
     connection = model.connect_operation_signal(
       [&] (const ListModel::Operation& operation) {
-        ++signal_count;
-        visit(operation,
-          [&] (const ListModel::AddOperation& add_operation) {
-            REQUIRE(add_operation.m_index == model.get_size() - 1);
-          },
-          [&] (const ListModel::RemoveOperation& remove_operation) {
-            ++remove_count;
-            REQUIRE(remove_operation.m_index == 0);
-          });
+        operations.push_back(operation);
       });
     REQUIRE_NOTHROW(model.push(1));
-    REQUIRE(signal_count == 1);
+    REQUIRE(operations.size() == 1);
     REQUIRE_NOTHROW(model.push(3));
-    REQUIRE(signal_count == 2);
+    REQUIRE(operations.size() == 2);
     REQUIRE_NOTHROW(model.push(5));
-    REQUIRE(signal_count == 3);
+    REQUIRE(operations.size() == 3);
     REQUIRE(model.get_size() == 3);
+    operations.clear();
     REQUIRE_NOTHROW(model.remove(0));
-    REQUIRE(signal_count == 4);
-    REQUIRE(remove_count == 1);
+    REQUIRE(operations.size() == 1);
     REQUIRE(model.get_size() == 2);
     REQUIRE(model.get<int>(0) == 3);
     REQUIRE(model.get<int>(1) == 5);
+    auto operation = operations.front();
+    operations.pop_front();
+    test_operation(operation, [&] (const ListModel::RemoveOperation& operation) {
+      REQUIRE(operation.m_index == 0);
+    });
     connection = model.connect_operation_signal(
       [&] (const ListModel::Operation& operation) {
         REQUIRE(false);
@@ -75,148 +88,144 @@ TEST_SUITE("ArrayListModel") {
         REQUIRE(false);
       }));
     REQUIRE_THROWS(model.move(1, 3));
-    auto signal_count = 0;
-    auto move_count = 0;
-    auto source = 0;
-    auto destination = 0;
+    auto operations = std::deque<ListModel::Operation>();
     connection = model.connect_operation_signal(
       [&] (const ListModel::Operation& operation) {
-        ++signal_count;
-        visit(operation,
-          [&] (const ListModel::AddOperation& add_operation) {
-            REQUIRE(add_operation.m_index == model.get_size() - 1);
-          },
-          [&] (const ListModel::MoveOperation& move_operation) {
-            ++move_count;
-            REQUIRE(move_operation.m_source == source);
-            REQUIRE(move_operation.m_destination == destination);
-          });
+        operations.push_back(operation);
       });
     REQUIRE_NOTHROW(model.push(1));
-    REQUIRE(signal_count == 1);
+    REQUIRE(operations.size() == 1);
     REQUIRE_NOTHROW(model.push(4));
-    REQUIRE(signal_count == 2);
+    REQUIRE(operations.size() == 2);
     REQUIRE_NOTHROW(model.push(7));
-    REQUIRE(signal_count == 3);
+    REQUIRE(operations.size() == 3);
     REQUIRE_NOTHROW(model.push(10));
-    REQUIRE(signal_count == 4);
+    REQUIRE(operations.size() == 4);
     REQUIRE(model.get_size() == 4);
+    operations.clear();
     REQUIRE_THROWS(model.move(0, 4));
-    REQUIRE(signal_count == 4);
+    REQUIRE(operations.empty());
     REQUIRE_THROWS(model.move(5, 1));
-    REQUIRE(signal_count == 4);
-    source = 2;
-    destination = 2;
-    REQUIRE_NOTHROW(model.move(source, destination));
+    REQUIRE(operations.empty());
+    REQUIRE_NOTHROW(model.move(2, 2));
+    REQUIRE(operations.empty());
     REQUIRE(model.get<int>(0) == 1);
     REQUIRE(model.get<int>(1) == 4);
     REQUIRE(model.get<int>(2) == 7);
     REQUIRE(model.get<int>(3) == 10);
-    source = 0;
-    destination = 3;
-    REQUIRE_NOTHROW(model.move(source, destination));
-    REQUIRE(signal_count == 5);
+    REQUIRE_NOTHROW(model.move(0, 3));
     REQUIRE(model.get<int>(0) == 4);
     REQUIRE(model.get<int>(1) == 7);
     REQUIRE(model.get<int>(2) == 10);
     REQUIRE(model.get<int>(3) == 1);
-    source = 3;
-    destination = 1;
-    REQUIRE_NOTHROW(model.move(source, destination));
-    REQUIRE(signal_count == 6);
+    REQUIRE(operations.size() == 1);
+    auto operation = operations.front();
+    operations.pop_front();
+    test_operation(operation, [&] (const ListModel::MoveOperation& operation) {
+      REQUIRE(operation.m_source == 0);
+      REQUIRE(operation.m_destination == 3);
+    });
+    REQUIRE_NOTHROW(model.move(3, 1));
     REQUIRE(model.get<int>(0) == 4);
     REQUIRE(model.get<int>(1) == 1);
     REQUIRE(model.get<int>(2) == 7);
     REQUIRE(model.get<int>(3) == 10);
+    REQUIRE(operations.size() == 1);
+    operation = operations.front();
+    operations.pop_front();
+    test_operation(operation, [&] (const ListModel::MoveOperation& operation) {
+      REQUIRE(operation.m_source == 3);
+      REQUIRE(operation.m_destination == 1);
+    });
   }
 
   TEST_CASE("insert") {
     auto model = ArrayListModel();
-    auto signal_count = 0;
-    auto index = 0;
+    auto operations = std::deque<ListModel::Operation>();
     auto connection = scoped_connection(model.connect_operation_signal(
       [&] (const ListModel::Operation& operation) {
-        ++signal_count;
-        auto add_operation = get<ListModel::AddOperation>(&operation);
-        REQUIRE(add_operation != nullptr);
-        REQUIRE(add_operation->m_index == index);
+        operations.push_back(operation);
       }));
-    index = 1;
-    REQUIRE_THROWS(model.insert(1, index));
+    REQUIRE_THROWS(model.insert(1, 1));
     REQUIRE(model.get_size() == 0);
-    REQUIRE(signal_count == 0);
-    index = 0;
-    REQUIRE_NOTHROW(model.insert(1, index));
+    REQUIRE(operations.empty());
+    REQUIRE_NOTHROW(model.insert(1, 0));
     REQUIRE(model.get_size() == 1);
-    REQUIRE(signal_count == 1);
     REQUIRE(model.get<int>(0) == 1);
-    index = 0;
-    REQUIRE_NOTHROW(model.insert(2, index));
+    REQUIRE(operations.size() == 1);
+    auto operation = operations.front();
+    operations.pop_front();
+    test_operation(operation, [&] (const ListModel::AddOperation& operation) {
+      REQUIRE(operation.m_index == 0);
+    });
+    REQUIRE_NOTHROW(model.insert(2, 0));
     REQUIRE(model.get_size() == 2);
-    REQUIRE(signal_count == 2);
     REQUIRE(model.get<int>(0) == 2);
     REQUIRE(model.get<int>(1) == 1);
-    index = -1;
-    REQUIRE_THROWS(model.insert(3, index));
+    REQUIRE(operations.size() == 1);
+    operation = operations.front();
+    operations.pop_front();
+    test_operation(operation, [&] (const ListModel::AddOperation& operation) {
+      REQUIRE(operation.m_index == 0);
+    });
+    REQUIRE_THROWS(model.insert(3, -1));
     REQUIRE(model.get_size() == 2);
-    REQUIRE(signal_count == 2);
-    index = 1;
-    REQUIRE_NOTHROW(model.insert(3, index));
+    REQUIRE(operations.empty());
+    REQUIRE_NOTHROW(model.insert(3, 1));
     REQUIRE(model.get_size() == 3);
-    REQUIRE(signal_count == 3);
     REQUIRE(model.get<int>(0) == 2);
     REQUIRE(model.get<int>(1) == 3);
     REQUIRE(model.get<int>(2) == 1);
+    operation = operations.front();
+    operations.pop_front();
+    test_operation(operation, [&] (const ListModel::AddOperation& operation) {
+      REQUIRE(operation.m_index == 1);
+    });
   }
 
   TEST_CASE("update") {
     auto model = ArrayListModel();
-    auto signal_count = 0;
-    auto update_count = 0;
-    auto updated_index = 0;
+    auto operations = std::deque<ListModel::Operation>();
     auto connection = scoped_connection(model.connect_operation_signal(
       [&] (const auto& operation) {
-        ++signal_count;
-        visit(operation,
-          [&] (const ListModel::AddOperation& add_operation) {
-            REQUIRE(add_operation.m_index == model.get_size() - 1);
-          },
-          [&] (const ListModel::UpdateOperation& update_operation) {
-            ++update_count;
-            REQUIRE(update_operation.m_index == updated_index);
-          },
-          [] (const auto&) {
-            REQUIRE(false);
-          });
+        operations.push_back(operation);
       }));
-    updated_index = 2;
-    REQUIRE(model.set(updated_index, 0) == QValidator::State::Invalid);
+    REQUIRE(model.set(2, 0) == QValidator::State::Invalid);
     REQUIRE(model.get_size() == 0);
-    REQUIRE(signal_count == 0);
+    REQUIRE(operations.empty());
     REQUIRE_NOTHROW(model.push(1));
-    REQUIRE(signal_count == 1);
+    REQUIRE(operations.size() == 1);
     REQUIRE_NOTHROW(model.push(4));
-    REQUIRE(signal_count == 2);
+    REQUIRE(operations.size() == 2);
     REQUIRE_NOTHROW(model.push(7));
-    REQUIRE(signal_count == 3);
-    updated_index = 0;
-    REQUIRE(model.set(updated_index, 0) == QValidator::State::Acceptable);
+    REQUIRE(operations.size() == 3);
+    operations.clear();
+    REQUIRE(model.set(0, 0) == QValidator::State::Acceptable);
     REQUIRE(model.get_size() == 3);
-    REQUIRE(signal_count == 4);
-    REQUIRE(model.get<int>(updated_index) == 0);
-    updated_index = 2;
-    REQUIRE(model.set(updated_index, 10) == QValidator::State::Acceptable);
+    REQUIRE(model.get<int>(0) == 0);
+    REQUIRE(operations.size() == 1);
+    auto operation = operations.front();
+    operations.pop_front();
+    test_operation(operation, [&] (const ListModel::UpdateOperation& operation) {
+      REQUIRE(operation.m_index == 0);
+    });
+    REQUIRE(model.set(2, 10) == QValidator::State::Acceptable);
     REQUIRE(model.get_size() == 3);
-    REQUIRE(signal_count == 5);
-    REQUIRE(model.get<int>(updated_index) == 10);
+    REQUIRE(model.get<int>(2) == 10);
+    REQUIRE(operations.size() == 1);
+    operation = operations.front();
+    operations.pop_front();
+    test_operation(operation, [&] (const ListModel::UpdateOperation& operation) {
+      REQUIRE(operation.m_index == 2);
+    });
   }
   
   TEST_CASE("transactions") {
     auto model = ArrayListModel();
-    auto signal_count = 0;
+    auto operations = std::deque<ListModel::Operation>();
     auto connection = scoped_connection(model.connect_operation_signal(
       [&] (const ListModel::Operation& operation) {
-        ++signal_count;
+        operations.push_back(operation);
         auto transaction = get<ListModel::Transaction>(&operation);
         REQUIRE(transaction != nullptr);
         REQUIRE(transaction->m_operations.size() == 5);
@@ -238,10 +247,11 @@ TEST_SUITE("ArrayListModel") {
         model.push(8);
       });
     });
-    REQUIRE(signal_count == 1);
+    REQUIRE(operations.size() == 1);
+    operations.clear();
     connection = model.connect_operation_signal(
       [&] (const ListModel::Operation& operation) {
-        ++signal_count;
+        operations.push_back(operation);
         connection.disconnect();
         model.transact([&] {
           model.push(7);
@@ -257,17 +267,20 @@ TEST_SUITE("ArrayListModel") {
       model.push(1);
       model.push(4);
     });
-    REQUIRE(signal_count == 2);
+    REQUIRE(operations.size() == 1);
+    operations.clear();
     connection = model.connect_operation_signal(
       [&] (const ListModel::Operation& operation) {
-        ++signal_count;
-        auto add_operation = get<ListModel::AddOperation>(&operation);
-        REQUIRE(add_operation != nullptr);
-        REQUIRE(add_operation->m_index == model.get_size() - 1);
+        operations.push_back(operation);
       });
     model.transact([&] {
       model.push(1);
     });
-    REQUIRE(signal_count == 3);
+    REQUIRE(operations.size() == 1);
+    auto operation = operations.front();
+    operations.pop_front();
+    test_operation(operation, [&] (const ListModel::AddOperation& operation) {
+      REQUIRE(operation.m_index == model.get_size() - 1);
+    });
   }
 }
