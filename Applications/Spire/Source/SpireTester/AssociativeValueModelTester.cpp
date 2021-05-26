a09413ac3ed38525136ce9c2ba4b715f215a2681
Kamal <kamal@eidolonsystems.com>
2021-05-26 18:16:59 -0400

    The AssociativeValueModel manages its own internal boolean models to guarantee consistency at all times.

--- Applications/Spire/Source/SpireTester/AssociativeValueModelTester.cpp
+++ Applications/Spire/Source/SpireTester/AssociativeValueModelTester.cpp
@@ -1,17 +1,16 @@
-#include <doctest/doctest.h>
+#include <queue>
 #include <boost/optional/optional_io.hpp>
+#include <doctest/doctest.h>
 #include "Spire/Ui/AssociativeValueModel.hpp"
-#include "Spire/Ui/LocalValueModel.hpp"
 
+using namespace boost;
 using namespace Spire;
 
 TEST_SUITE("AssociativeValueModel") {
   TEST_CASE("associate") {
     auto model = AssociativeValueModel<std::string>();
-    auto bool_model1 = std::make_shared<LocalBooleanModel>(false);
-    auto bool_model2 = std::make_shared<LocalBooleanModel>(false);
-    model.associate(bool_model1, "model1");
-    model.associate(bool_model2, "model2");
+    auto bool_model1 = model.get_association("model1");
+    auto bool_model2 = model.get_association("model2");
     bool_model1->set_current(true);
     REQUIRE(bool_model1->get_current());
     REQUIRE_FALSE(bool_model2->get_current());
@@ -28,41 +27,23 @@ TEST_SUITE("AssociativeValueModel") {
 
   TEST_CASE("reassociate_current") {
     auto model = AssociativeValueModel<std::string>();
-    auto bool_model1 = std::make_shared<LocalBooleanModel>(false);
-    model.associate(bool_model1, "model1");
+    auto bool_model1 = model.get_association("model1");
     model.set_current("model1");
     REQUIRE(model.get_current() == "model1");
-    auto bool_model2 = std::make_shared<LocalBooleanModel>(false);
-    model.associate(bool_model2, "model1");
+    auto bool_model2 = model.get_association("model1");
+    REQUIRE(bool_model1 == bool_model2);
     REQUIRE(model.get_current() == "model1");
     REQUIRE(bool_model2->get_current());
-    auto bool_model3 = model.make_association("model1");
+    auto bool_model3 = model.get_association("model1");
+    REQUIRE(bool_model2 == bool_model3);
     REQUIRE(model.get_current() == "model1");
     REQUIRE(bool_model3->get_current());
   }
 
-  TEST_CASE("reassociate_signal") {
-    auto model = AssociativeValueModel<std::string>();
-    auto signal_count = 0;
-    model.connect_current_signal([&] (auto value) {
-      ++signal_count;
-    });
-    auto bool_model1 = model.make_association("model1");
-    model.set_current("model1");
-    REQUIRE(signal_count == 1);
-    auto bool_model2 = model.make_association("model1");
-    model.set_current("model1");
-    REQUIRE(signal_count == 2);
-    bool_model1->set_current(true);
-    REQUIRE(signal_count == 2);
-    REQUIRE(bool_model1->get_current());
-    REQUIRE(bool_model2->get_current());
-  }
-
   TEST_CASE("make_association") {
     auto model = AssociativeValueModel<std::string>();
-    auto bool_model1 = model.make_association("model1");
-    auto bool_model2 = model.make_association("model2");
+    auto bool_model1 = model.get_association("model1");
+    auto bool_model2 = model.get_association("model2");
     bool_model1->set_current(true);
     REQUIRE(bool_model1->get_current());
     REQUIRE_FALSE(bool_model2->get_current());
@@ -79,8 +60,8 @@ TEST_SUITE("AssociativeValueModel") {
 
   TEST_CASE("find") {
     auto model = AssociativeValueModel<std::string>();
-    auto bool_model1 = model.make_association("model1");
-    auto bool_model2 = model.make_association("model2");
+    auto bool_model1 = model.get_association("model1");
+    auto bool_model2 = model.get_association("model2");
     REQUIRE(model.find("model1") == bool_model1);
     REQUIRE(model.find("model2") == bool_model2);
     REQUIRE(model.find("model3") == nullptr);
@@ -93,8 +74,7 @@ TEST_SUITE("AssociativeValueModel") {
 
   TEST_CASE("get_current") {
     auto model = AssociativeValueModel<std::string>();
-    auto bool_model1 = std::make_shared<LocalBooleanModel>(false);
-    model.associate(bool_model1, "model1");
+    auto bool_model1 = model.get_association("model1");
     REQUIRE(model.get_current() == "");
     bool_model1->set_current(true);
     REQUIRE(model.get_current() == "model1");
@@ -104,10 +84,8 @@ TEST_SUITE("AssociativeValueModel") {
 
   TEST_CASE("set_current") {
     auto model = AssociativeValueModel<std::string>();
-    auto bool_model1 = std::make_shared<LocalBooleanModel>(false);
-    auto bool_model2 = std::make_shared<LocalBooleanModel>(false);
-    model.associate(bool_model1, "model1");
-    model.associate(bool_model2, "model2");
+    auto bool_model1 = model.get_association("model1");
+    auto bool_model2 = model.get_association("model2");
     REQUIRE(model.get_current() == "");
     REQUIRE(model.set_current("model2") == QValidator::Acceptable);
     REQUIRE_FALSE(bool_model1->get_current());
@@ -130,17 +108,10 @@ TEST_SUITE("AssociativeValueModel") {
         model.set_current("model2");
       }
     });
-    model.connect_current_signal([&] (auto value) {
-      updates.push(value);
-    });
-    model.make_association("model1");
-    model.make_association("model2");
+    model.get_association("model1");
+    model.get_association("model2");
     model.set_current("model1");
     REQUIRE(updates.front() == "model1");
-    updates.pop();
-    REQUIRE(updates.front() == "model1");
-    updates.pop();
-    REQUIRE(updates.front() == "model2");
     updates.pop();
     REQUIRE(updates.front() == "model2");
     updates.pop();
@@ -155,13 +126,11 @@ TEST_SUITE("AssociativeValueModel") {
       ++signal_count;
       current_value = current;
     });
-    auto bool_model1 = std::make_shared<LocalBooleanModel>(false);
-    auto bool_model2 = std::make_shared<LocalBooleanModel>(false);
-    model.associate(bool_model1, "model1");
+    auto bool_model1 = model.get_association("model1");
+    auto bool_model2 = model.get_association("model2");
     model.set_current("model1");
     REQUIRE(current_value == "model1");
     REQUIRE(signal_count == 1);
-    model.associate(bool_model2, "model2");
     REQUIRE(current_value == "model1");
     REQUIRE(signal_count == 1);
     bool_model1->set_current(false);
@@ -177,9 +146,8 @@ TEST_SUITE("AssociativeValueModel") {
 
   TEST_CASE("default_value") {
     auto model = AssociativeValueModel<std::string>("default");
-    auto default_model = model.make_association("default");
-    auto bool_model = std::make_shared<LocalBooleanModel>(false);
-    model.associate(bool_model, "model1");
+    auto default_model = model.get_association("default");
+    auto bool_model = model.get_association("model1");
     bool_model->set_current(true);
     REQUIRE(model.get_current() == "model1");
     bool_model->set_current(false);
@@ -191,24 +159,21 @@ TEST_SUITE("AssociativeValueModel") {
 
   TEST_CASE("default_instance_values") {
     auto model = AssociativeValueModel<std::string>();
-    auto bool_model1 = std::make_shared<LocalBooleanModel>(false);
-    model.associate(bool_model1, "model1");
+    auto bool_model1 = model.get_association("model1");
     REQUIRE(model.get_current() == "");
     REQUIRE_FALSE(bool_model1->get_current());
   }
 
-  TEST_CASE("nullable_model") {
-    auto model = make_nullable_associative_model<std::string>();
-    auto bool_model1 = std::make_shared<LocalBooleanModel>(false);
-    auto bool_model2 = std::make_shared<LocalBooleanModel>(false);
-    model->associate(bool_model1, std::string("model1"));
-    model->associate(bool_model2, std::string("model2"));
-    REQUIRE_FALSE(model->get_current());
+  TEST_CASE("optional_model") {
+    auto model = AssociativeValueModel<optional<std::string>>();
+    auto bool_model1 = model.get_association(std::string("model1"));
+    auto bool_model2 = model.get_association(std::string("model2"));
+    REQUIRE_FALSE(model.get_current());
     bool_model1->set_current(true);
-    REQUIRE(model->get_current() == std::string("model1"));
+    REQUIRE(model.get_current() == std::string("model1"));
     REQUIRE_FALSE(bool_model2->get_current());
     bool_model1->set_current(false);
-    REQUIRE_FALSE(model->get_current());
+    REQUIRE_FALSE(model.get_current());
     REQUIRE_FALSE(bool_model1->get_current());
     REQUIRE_FALSE(bool_model2->get_current());
   }
