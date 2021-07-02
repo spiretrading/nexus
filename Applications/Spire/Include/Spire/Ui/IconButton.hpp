2d6b27ac2a199cc4acf78307e6a834e04f87f229
Ken <ken@spiretrading.com>
2021-07-02 12:25:03 -0400

    Revert "Progress on IconButton update."
    This reverts commit 393032cb24fc35252674ab0c004fc8739056f07c.

--- Applications/Spire/Include/Spire/Ui/IconButton.hpp
+++ Applications/Spire/Include/Spire/Ui/IconButton.hpp
@@ -8,29 +8,84 @@
 namespace Spire {
 
   //! Displays a button using an icon.
-  class IconButton : public QWidget {
+  class IconButton : public QAbstractButton {
     public:
 
-      /** Signals that the IconButton is clicked. */
-      using ClickedSignal = Signal<void ()>;
+      //! Represents the style options for an IconButton.
+      struct Style {
 
-      //! Constructs an IconButton.
+        //! The icon's color when the parent window lacks focus.
+        QColor m_blur_color;
+
+        //! The icon's color when the button is checked.
+        QColor m_checked_color;
+
+        //! The icon's color when the button is checked and the window lacks
+        //! focus.
+        QColor m_checked_blur_color;
+
+        //! The icon's color when the button is checked and hovered.
+        QColor m_checked_hovered_color;
+
+        //! The icon's default color.
+        QColor m_default_color;
+
+        //! The icon's disabled color.
+        QColor m_disabled_color;
+
+        //! The icon's disabled background color.
+        QColor m_disabled_background_color;
+
+        //! The icon's hover color.
+        QColor m_hover_color;
+
+        //! The button's default background color.
+        QColor m_default_background_color;
+
+        //! The button's hover background color.
+        QColor m_hover_background_color;
+
+        //! Constructs a Style with the default style.
+        Style();
+      };
+
+      //! Constructs an IconButton with the default Style.
       /*!
         \param icon The icon to show.
         \param parent The parent widget.
       */
-      explicit IconButton(Icon* icon, QWidget* parent = nullptr);
+      explicit IconButton(QImage icon, QWidget* parent = nullptr);
 
-      /** Connects a slot to the click signal. */
-      boost::signals2::connection connect_clicked_signal(
-        const ClickedSignal::slot_type& slot) const;
+      //! Constructs an IconButton with a custom style.
+      /*!
+        \param icon The icon to show.
+        \param style The button's style.
+        \param parent The parent widget.
+      */
+      IconButton(QImage icon, Style style, QWidget* parent = nullptr);
+
+      //! Returns the IconButton's Style.
+      const Style& get_style() const;
+
+      //! Sets the IconButton's Style.
+      /*!
+        \param style The current Style.
+      */
+      void set_style(const Style& style);
 
     protected:
       bool event(QEvent* event) override;
+      void keyPressEvent(QKeyEvent* event) override;
+      void paintEvent(QPaintEvent* event) override;
+      QSize sizeHint() const override;
 
     private:
-      Button* m_button;
+      QImage m_icon;
+      Style m_style;
       Tooltip* m_tooltip;
+
+      const QColor& get_background_color() const;
+      const QColor& get_current_icon_color() const;
   };
 }
 
--- Applications/Spire/Source/Ui/IconButton.cpp
+++ Applications/Spire/Source/Ui/IconButton.cpp
@@ -1,41 +1,45 @@
 #include "Spire/Ui/IconButton.hpp"
 #include <QHBoxLayout>
-#include "Spire/Ui/Box.hpp"
+#include <QKeyEvent>
+#include <QMouseEvent>
+#include <QPainter>
+#include <QStyleOption>
+#include "Spire/Spire/Dimensions.hpp"
+#include "Spire/Spire/Utility.hpp"
 #include "Spire/Ui/Tooltip.hpp"
 
+using namespace boost;
 using namespace boost::signals2;
 using namespace Spire;
-using namespace Spire::Styles;
-
-//IconButton::Style::Style()
-//  : m_blur_color("#7F5EEC"),
-//    m_checked_blur_color("#1FD37A"),
-//    m_checked_color(m_checked_blur_color),
-//    m_checked_hovered_color("#2CAC79"),
-//    m_default_color(m_blur_color),
-//    m_disabled_color("#D0D0D0"),
-//    m_disabled_background_color("#F5F5F5"),
-//    m_hover_color("#4B23A0"),
-//    m_default_background_color(m_disabled_background_color),
-//    m_hover_background_color("#E3E3E3") {}
 
 namespace {
-  auto DEFAULT_STYLE() {
-    auto style = StyleSheet();
-
-    return style;
+  auto DEFAULT_SIZE() {
+    static auto size = scale(26, 26);
+    return size;
   }
 }
 
-IconButton::IconButton(Icon* icon, QWidget* parent)
-    : QWidget(parent),
+IconButton::Style::Style()
+  : m_blur_color("#7F5EEC"),
+    m_checked_blur_color("#1FD37A"),
+    m_checked_color(m_checked_blur_color),
+    m_checked_hovered_color("#2CAC79"),
+    m_default_color(m_blur_color),
+    m_disabled_color("#D0D0D0"),
+    m_disabled_background_color("#F5F5F5"),
+    m_hover_color("#4B23A0"),
+    m_default_background_color(m_disabled_background_color),
+    m_hover_background_color("#E3E3E3") {}
+
+IconButton::IconButton(QImage icon, QWidget* parent)
+  : IconButton(icon, {}, parent) {}
+
+IconButton::IconButton(QImage icon, Style style, QWidget* parent)
+    : QAbstractButton(parent),
+      m_icon(std::move(icon)),
+      m_style(std::move(style)),
       m_tooltip(nullptr) {
-  m_button = new Button(icon, this);
-  auto box = new Box(m_button, this);
-  auto layout = new QHBoxLayout(this);
-  layout->setContentsMargins({});
-  layout->addWidget(box);
-  set_style(*this, DEFAULT_STYLE);
+  setAttribute(Qt::WA_Hover);
 }
 
 bool IconButton::event(QEvent* event) {
@@ -45,10 +49,74 @@ bool IconButton::event(QEvent* event) {
       m_tooltip = make_text_tooltip(toolTip(), this);
     }
   }
-  return QWidget::event(event);
+  return QAbstractButton::event(event);
 }
 
-connection IconButton::connect_clicked_signal(
-    const ClickedSignal::slot_type& slot) const {
-  return m_button->connect_clicked_signal(slot);
+void IconButton::keyPressEvent(QKeyEvent* event) {
+  if(!event->isAutoRepeat()) {
+    switch(event->key()) {
+      case Qt::Key_Enter:
+      case Qt::Key_Return:
+        nextCheckState();
+        Q_EMIT clicked(isChecked());
+        return;
+    }
+  }
+  QAbstractButton::keyPressEvent(event);
+}
+
+void IconButton::paintEvent(QPaintEvent* event) {
+  auto painter = QPainter(this);
+  painter.fillRect(rect(), get_background_color());
+  auto icon = QPixmap::fromImage(m_icon);
+  auto image_painter = QPainter(&icon);
+  image_painter.setCompositionMode(QPainter::CompositionMode_SourceIn);
+  image_painter.fillRect(icon.rect(), get_current_icon_color());
+  painter.drawPixmap((width() - icon.width()) / 2,
+    (height() - icon.height()) / 2, icon);
+  if(hasFocus()) {
+    draw_border(rect(), "#4B23A0", &painter);
+  }
+}
+
+QSize IconButton::sizeHint() const {
+  return DEFAULT_SIZE();
+}
+
+const QColor& IconButton::get_background_color() const {
+  if(!isEnabled()) {
+    return m_style.m_disabled_background_color;
+  } else if(!underMouse()) {
+    return m_style.m_default_background_color;
+  }
+  return m_style.m_hover_background_color;
+}
+
+const QColor& IconButton::get_current_icon_color() const {
+  if(isEnabled()) {
+    if(isChecked()) {
+      if(underMouse()) {
+        return m_style.m_checked_hovered_color;
+      } else if(!window()->isActiveWindow()) {
+        return m_style.m_checked_blur_color;
+      }
+      return m_style.m_checked_color;
+    }
+    if(underMouse()) {
+      return m_style.m_hover_color;
+    } else if(!window()->isActiveWindow()) {
+      return m_style.m_blur_color;
+    }
+    return m_style.m_default_color;
+  }
+  return m_style.m_disabled_color;
+}
+
+const IconButton::Style& IconButton::get_style() const {
+  return m_style;
+}
+
+void IconButton::set_style(const Style& style) {
+  m_style = style;
+  update();
 }
