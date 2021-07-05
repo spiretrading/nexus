9b78cd32e522a431978c051f2cea4dd540905fcc
Ken <ken@spiretrading.com>
2021-07-05 19:35:17 -0400

    Completed Tooltip implementation.

--- Applications/Spire/Source/Ui/Tooltip.cpp
+++ Applications/Spire/Source/Ui/Tooltip.cpp
@@ -11,10 +11,14 @@ namespace {
     style.get(Any()).
       set(BackgroundColor(QColor::fromRgb(0, 0, 0))).
       set(border_size(0));
-    //style.get(Any() >> is_a<TextBox>()).
-    //  set(TextColor(QColor::fromRgb(0xFF, 0xFF, 0xFF))).
-    //  set(TextAlign(Qt::Alignment(Qt::AlignCenter))).
-    //  set(horizontal_padding(scale_width(8)));
+    auto font = QFont("Roboto");
+    font.setWeight(60);
+    font.setPixelSize(scale_width(10));
+    style.get(Any() >> is_a<TextBox>()).
+      set(Font(font)).
+      set(TextColor(QColor::fromRgb(0xFF, 0xFF, 0xFF))).
+      set(TextAlign(Qt::Alignment(Qt::AlignCenter))).
+      set(horizontal_padding(scale_width(8)));
     return style;
   }
 }
@@ -23,10 +27,14 @@ Tooltip::Tooltip(QString text, QWidget* parent)
     : QObject(parent) {
   m_label = make_label(std::move(text), parent);
   m_tooltip = new InfoTip(m_label, parent);
-  //set_style(*m_tooltip, DEFAULT_STYLE());
+  set_style(*m_tooltip, DEFAULT_STYLE());
 }
 
 void Tooltip::set_label(const QString& text) {
   m_label->get_model()->set_current(text);
+  m_tooltip->adjustSize();
   m_tooltip->setDisabled(text.isEmpty());
+  if(!m_tooltip->isEnabled()) {
+    m_tooltip->hide();
+  }
 }
