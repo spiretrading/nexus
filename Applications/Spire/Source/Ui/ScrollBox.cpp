05b31ef406d993d2d23beb6bff2b6701d9c08031
Shaobin <shaobin@spiretrading.com>
2021-07-12 13:22:46 -0400

    Implemented the ScrollableListBox.

--- Applications/Spire/Source/Ui/ScrollBox.cpp
+++ Applications/Spire/Source/Ui/ScrollBox.cpp
@@ -1,8 +1,11 @@
 #include "Spire/Ui/ScrollBox.hpp"
 #include <QEvent>
 #include <QHBoxLayout>
+#include "Spire/Spire/Dimensions.hpp"
+#include "Spire/Spire/ValueModel.hpp"
 #include "Spire/Ui/Box.hpp"
 #include "Spire/Ui/LayeredWidget.hpp"
+#include "Spire/Ui/ListView.hpp"
 #include "Spire/Ui/ScrollBar.hpp"
 #include "Spire/Ui/ScrollableLayer.hpp"
 
@@ -155,4 +158,45 @@ void ScrollBox::update_ranges() {
   m_scrollable_layer->get_horizontal_scroll_bar().set_range(
     0, horizontal_range);
   m_scrollable_layer->get_horizontal_scroll_bar().set_page_size(width());
+}
+
+ScrollBox* Spire::make_scrollable_list_box(ListView* list_view,
+    QWidget* parent) {
+  list_view->set_edge_navigation(ListView::EdgeNavigation::CONTAIN);
+  auto scroll_box = new ScrollBox(list_view, parent);
+  scroll_box->set(ScrollBox::DisplayPolicy::ON_OVERFLOW);
+  auto style = get_style(*scroll_box);
+  style.get(Any()).
+    set(BackgroundColor(QColor::fromRgb(0xFF, 0xFF, 0xFF))).
+    set(border(scale_width(1), QColor::fromRgb(0xC8, 0xC8, 0xC8)));
+  set_style(*scroll_box, std::move(style));
+  list_view->get_current_model()->connect_current_signal(
+    [=] (const auto& current) {
+      if(!current) {
+        return;
+      }
+      auto item = list_view->get_item(*current);
+      auto item_pos = item->pos();
+      auto item_height = item->height();
+      auto item_width = item->width();
+      auto viewport_x = scroll_box->get_horizontal_scroll_bar().get_position();
+      auto viewport_y = scroll_box->get_vertical_scroll_bar().get_position();
+      auto viewport_width =
+        scroll_box->get_horizontal_scroll_bar().get_page_size();
+      auto viewport_height =
+        scroll_box->get_vertical_scroll_bar().get_page_size();
+      if(item_height > viewport_height || viewport_y > item_pos.y()) {
+        scroll_box->get_vertical_scroll_bar().set_position(item_pos.y());
+      } else if(viewport_y + viewport_height < item_pos.y() + item_height) {
+        scroll_box->get_vertical_scroll_bar().set_position(
+           item_pos.y() + item_height - viewport_height);
+      }
+      if(item_width > viewport_width || viewport_x > item_pos.x()) {
+        scroll_box->get_horizontal_scroll_bar().set_position(item_pos.x());
+      } else if(viewport_x + viewport_width < item_pos.x() + item_width) {
+        scroll_box->get_horizontal_scroll_bar().set_position(
+           item_pos.x() + item_width - viewport_width);
+      }
+    });
+  return scroll_box;
 }
