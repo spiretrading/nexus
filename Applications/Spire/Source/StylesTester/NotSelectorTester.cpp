cbfcfa58b2aaf7399bec1573730fa3b26eb9db40
Kamal <kamal@eidolonsystems.com>
2021-08-30 15:32:45 -0400

    Added unit tests for the NotSelector.

---
+++ Applications/Spire/Source/StylesTester/NotSelectorTester.cpp
@@ -0,0 +1,57 @@
+#include <doctest/doctest.h>
+#include "Spire/SpireTester/SpireTester.hpp"
+#include "Spire/Styles/Selectors.hpp"
+#include "Spire/Styles/Stylist.hpp"
+
+using namespace Spire;
+using namespace Spire::Styles;
+
+TEST_SUITE("NotSelector") {
+  TEST_CASE("equality") {
+    REQUIRE(NotSelector(Any()) == NotSelector(Any()));
+    REQUIRE(NotSelector(Any()) != NotSelector(Hover()));
+    REQUIRE(NotSelector(Hover()) != NotSelector(Any()));
+  }
+
+  TEST_CASE("selection") {
+    run_test([] {
+      auto selector = NotSelector(Hover());
+      {
+        auto w1 = QWidget();
+        auto w2 = QWidget();
+        auto selection =
+          select(selector, {&find_stylist(w1), &find_stylist(w2)});
+        REQUIRE(selection.size() == 2);
+        REQUIRE(selection.contains(&find_stylist(w1)));
+        REQUIRE(selection.contains(&find_stylist(w2)));
+      }
+      {
+        auto w1 = QWidget();
+        auto w2 = QWidget();
+        match(w1, Hover());
+        auto selection =
+          select(selector, {&find_stylist(w1), &find_stylist(w2)});
+        REQUIRE(selection.size() == 1);
+        REQUIRE(selection.contains(&find_stylist(w2)));
+      }
+      {
+        auto w1 = QWidget();
+        auto w2 = QWidget();
+        match(w2, Hover());
+        auto selection =
+          select(selector, {&find_stylist(w1), &find_stylist(w2)});
+        REQUIRE(selection.size() == 1);
+        REQUIRE(selection.contains(&find_stylist(w1)));
+      }
+      {
+        auto w1 = QWidget();
+        auto w2 = QWidget();
+        match(w1, Hover());
+        match(w2, Hover());
+        auto selection =
+          select(selector, {&find_stylist(w1), &find_stylist(w2)});
+        REQUIRE(selection.empty());
+      }
+    });
+  }
+}
