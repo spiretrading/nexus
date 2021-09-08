03316cf0e6bd6505a2b67e846d15f2da7f517327
Kamal <kamal@eidolonsystems.com>
2021-09-08 11:50:41 -0400

    Implemented the CastingListModel class as a precursor to the DropDownBoxAdaptor. This class will increase type safety in the DropDownBoxAdaptor by automatically ensuring that the data it displays is of the correct type.

--- Applications/Spire/Include/Spire/Ui/Ui.hpp
+++ Applications/Spire/Include/Spire/Ui/Ui.hpp
@@ -14,6 +14,7 @@ namespace Spire {
   class ArrayTableModel;
   class Box;
   class Button;
+  template<typename T> class CastingListModel;
   class CheckBox;
   class DecimalBox;
   class DestinationListItem;
