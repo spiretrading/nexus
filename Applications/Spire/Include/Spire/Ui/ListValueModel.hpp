5a15529ed49dca53a60e1be57e85cc20848cf414
Kamal <kamal@eidolonsystems.com>
2021-09-15 19:32:48 -0400

    Added starter code for the ListValueModel.

---
+++ Applications/Spire/Include/Spire/Ui/ListValueModel.hpp
@@ -0,0 +1,34 @@
+#ifndef SPIRE_LIST_VALUE_MODEL_HPP
+#define SPIRE_LIST_VALUE_MODEL_HPP
+#include <any>
+#include "Spire/Spire/ValueModel.hpp"
+#include "Spire/Ui/Ui.hpp"
+
+namespace Spire {
+
+  /**
+   * Implements a ValueModel providing a view over the value in a ListModel at
+   * a specified index.
+   */
+  class ListValueModel : public ValueModel<std::any> {
+    public:
+
+      /** 
+       * Constructs a ListValueModel from a specified index into a ListModel.
+       * @param source The ListModel to view.
+       * @param index The index of the value in the <i>source</i> to view. 
+       */
+      ListValueModel(std::shared_ptr<ListModel> source, int index);
+
+      QValidator::State get_state() const override;
+
+      const Type& get_current() const override;
+
+      QValidator::State set_current(const Type& value) override;
+
+      boost::signals2::connection connect_current_signal(
+        const CurrentSignal::slot_type& slot) const override;
+  };
+}
+
+#endif
