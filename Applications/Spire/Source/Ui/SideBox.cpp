f9329e7764fb935a53e0d07d79886f9b0a8f2b2a
Shaobin <shaobin@spiretrading.com>
2021-09-16 16:10:03 -0400

    Avoid doing the same work.

--- Applications/Spire/Source/Ui/SideBox.cpp
+++ Applications/Spire/Source/Ui/SideBox.cpp
@@ -5,6 +5,19 @@
 using namespace Nexus;
 using namespace Spire;
 
+namespace {
+  SideBox::Settings setup() {
+    static auto settings = SideBox::Settings();
+    if(!settings.m_cases) {
+      auto cases = std::make_shared<ArrayListModel>();
+      cases->push(Side(Side::BID));
+      cases->push(Side(Side::ASK));
+      settings.m_cases = std::move(cases);
+    }
+    return settings;
+  }
+}
+
 SideBox* Spire::make_side_box(QWidget* parent) {
   return make_side_box(Side::BID, parent);
 }
@@ -15,11 +28,7 @@ SideBox* Spire::make_side_box(Side current, QWidget* parent) {
 
 SideBox* Spire::make_side_box(std::shared_ptr<SideModel> model,
     QWidget* parent) {
-  auto settings = SideBox::Settings();
-  auto cases = std::make_shared<ArrayListModel>();
-  cases->push(Side(Side::BID));
-  cases->push(Side(Side::ASK));
-  settings.m_cases = std::move(cases);
+  auto settings = setup();
   settings.m_current = std::move(model);
-  return new SideBox(settings, parent);
+  return new SideBox(std::move(settings), parent);
 }
