1681e85163c18346fbcb3ab2487e08bbda0afbbd
Ken <ken@spiretrading.com>
2021-10-20 13:15:58 -0400

    Added initial DateBox API.

---
+++ Applications/Spire/Include/Spire/Ui/DateBox.hpp
@@ -0,0 +1,41 @@
+#ifndef SPIRE_DATE_BOX_HPP
+#define SPIRE_DATE_BOX_HPP
+#include <QWidget>
+#include "Spire/Ui/CalendarDatePicker.hpp"
+
+namespace Spire {
+
+  class DateBox : public QWidget {
+    public:
+
+      /**
+       * Signals that the current value is being submitted.
+       * @param submission The submitted date.
+       */
+      using SubmitSignal =
+        Signal<void (const boost::gregorian::date& submission)>;
+
+      /** Signals that the current value was rejected as a submission. */
+      using RejectSignal = Signal<void ()>;
+
+      explicit DateBox(
+        boost::gregorian::date current, QWidget* parent = nullptr);
+
+      explicit DateBox(
+        std::shared_ptr<DateModel> model, QWidget* parent = nullptr);
+
+      /** Connects a slot to the SubmitSignal. */
+      boost::signals2::connection connect_submit_signal(
+        const SubmitSignal::slot_type& slot) const;
+
+      /** Connects a slot to the RejectedSignal. */
+      boost::signals2::connection connect_reject_signal(
+        const RejectSignal::slot_type& slot) const;
+
+    private:
+      mutable SubmitSignal m_submit_signal;
+      mutable RejectSignal m_reject_signal;
+  };
+}
+
+#endif
