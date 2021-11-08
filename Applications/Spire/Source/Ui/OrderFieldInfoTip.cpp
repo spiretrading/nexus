e557a3b788510354c15a9e724069c0abd1e9212f
Kamal <kamal@eidolonsystems.com>
2021-11-08 15:36:56 -0500

    Broke down the OrderFieldInfoTip's constructor into its sub-layouts so that each sub-layout is in its own function. Each sub-layout of the OrderFieldInfoTip has had its size policy explicitly specified. Removed the passing of parent QWidget's into OrderFieldInfoTip component constructors. The parent will be assigned by the layout, eliminates potential inconsistencies/accidental setting of incorrect parents.

--- Applications/Spire/Source/Ui/OrderFieldInfoTip.cpp
+++ Applications/Spire/Source/Ui/OrderFieldInfoTip.cpp
@@ -39,16 +39,38 @@ namespace {
     return style;
   }
 
-  auto make_value_widget(const OrderFieldInfoTip::Model::AllowedValue& value,
-      QWidget* parent) {
-    auto container = new QWidget(parent);
+  auto make_description_container(const OrderFieldInfoTip::Model& model) {
+    auto description_container = new QWidget();
+    description_container->setSizePolicy(
+      QSizePolicy::Expanding, QSizePolicy::Fixed);
+    auto description_layout = new QVBoxLayout(description_container);
+    description_layout->setContentsMargins(
+      scale_width(18), scale_height(18), scale_width(18), scale_height(18));
+    description_layout->setSpacing(0);
+    auto name_label = make_label(QString::fromStdString(model.m_tag.m_name));
+    name_label->set_read_only(true);
+    set_style(*name_label, NAME_STYLE());
+    description_layout->addWidget(name_label);
+    auto description_label =
+      new TextAreaBox(QString::fromStdString(model.m_tag.m_description));
+    description_label->set_read_only(true);
+    auto description_style = get_style(*description_label);
+    description_style.get(ReadOnly()).
+      set(PaddingTop(scale_height(6)));
+    set_style(*description_label, std::move(description_style));
+    description_layout->addWidget(description_label);
+    return description_container;
+  }
+
+  auto make_value_widget(const OrderFieldInfoTip::Model::AllowedValue& value) {
+    auto container = new QWidget();
     auto layout = new QHBoxLayout(container);
     layout->setContentsMargins({});
     layout->setSpacing(0);
-    auto value_label =
-      new TextBox(QString::fromStdString(value.m_value), container);
+    auto value_label = new TextBox(QString::fromStdString(value.m_value));
     value_label->set_read_only(true);
-    value_label->setSizePolicy(QSizePolicy::Minimum, QSizePolicy::Fixed);
+    value_label->setSizePolicy(
+      QSizePolicy::MinimumExpanding, QSizePolicy::Fixed);
     auto value_style = get_style(*value_label);
     value_style.get(ReadOnly()).
       set(border_size(0)).
@@ -57,10 +79,10 @@ namespace {
     set_style(*value_label, std::move(value_style));
     layout->addWidget(value_label, 0, Qt::AlignTop);
     auto description_label =
-      new TextAreaBox(QString::fromStdString(value.m_description), container);
+      new TextAreaBox(QString::fromStdString(value.m_description));
     description_label->set_read_only(true);
     description_label->setSizePolicy(
-      QSizePolicy::MinimumExpanding, QSizePolicy::Maximum);
+      QSizePolicy::Expanding, QSizePolicy::Maximum);
     auto description_style = get_style(*description_label);
     description_style.get(ReadOnly()).
       set(FontSize(scale_height(10))).
@@ -71,22 +93,43 @@ namespace {
   }
 
   auto make_values_container(
-      const std::vector<OrderFieldInfoTip::Model::AllowedValue>& values,
-      QWidget* parent) {
-    auto container = new QWidget(parent);
+      const std::vector<OrderFieldInfoTip::Model::AllowedValue>& values) {
+    auto container = new QWidget();
+    container->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Fixed);
     auto layout = new QVBoxLayout(container);
     layout->setContentsMargins(
       scale_width(18), scale_height(18), scale_width(18), scale_height(18));
     layout->setSpacing(scale_height(8));
     for(const auto& value : values) {
-      layout->addWidget(make_value_widget(value, container));
+      layout->addWidget(make_value_widget(value));
     }
     return container;
   }
 
-  auto make_prerequisite_container(
-      const QString& prerequisites, QWidget* parent) {
-    auto container = new QWidget(parent);
+  auto make_scroll_box_body(const OrderFieldInfoTip::Model& model) {
+    auto body = new QWidget();
+    body->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Fixed);
+    auto layout = new QVBoxLayout(body);
+    layout->setContentsMargins({});
+    layout->setSpacing(0);
+    layout->addWidget(make_description_container(model));
+    if(!model.m_tag.m_values.empty()) {
+      layout->addWidget(make_values_container(model.m_tag.m_values));
+    }
+    return body;
+  }
+
+  auto make_scroll_box(const OrderFieldInfoTip::Model& model) {
+    auto scroll_box_body = make_scroll_box_body(model);
+    auto scroll_box = new ScrollBox(scroll_box_body);
+    scroll_box->setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);
+    scroll_box->setFixedWidth(scale_width(280));
+    scroll_box->setMaximumHeight(scale_height(240));
+    return scroll_box;
+  }
+
+  auto make_prerequisite_container(const QString& prerequisites) {
+    auto container = new QWidget();
     auto layout = new QVBoxLayout(container);
     layout->setContentsMargins({});
     layout->setSpacing(0);
@@ -101,7 +144,7 @@ namespace {
     prerequisites_label->set_read_only(true);
     set_style(*prerequisites_label, PREREQUISITES_STYLE());
     layout->addWidget(prerequisites_label);
-    auto box = new Box(container, parent);
+    auto box = new Box(container);
     auto box_style = get_style(*box);
     box_style.get(Any()).
       set(BorderTopColor(QColor::fromRgb(0xE0E0E0))).
@@ -114,8 +157,8 @@ namespace {
 
 QString OrderFieldInfoTip::Model::get_prerequisites() const {
   auto text = QString();
-  for(auto tag_index = std::size_t(0);
-      tag_index < m_prerequisites.size(); ++tag_index) {
+  for(auto tag_index = std::size_t(0); tag_index < m_prerequisites.size();
+      ++tag_index) {
     auto tag = m_prerequisites[tag_index];
     text.append(QString::fromStdString(tag.m_name));
     if(!tag.m_values.empty()) {
@@ -137,45 +180,13 @@ QString OrderFieldInfoTip::Model::get_prerequisites() const {
 
 OrderFieldInfoTip::OrderFieldInfoTip(Model model, QWidget* parent)
     : QWidget(parent) {
-  setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);
-  setFixedWidth(280);
-  auto container = new QWidget(this);
-  container->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Preferred);
-  auto container_layout = new QVBoxLayout(container);
-  container_layout->setContentsMargins({});
-  container_layout->setSpacing(0);
-  auto description_container = new QWidget(this);
-  auto description_layout = new QVBoxLayout(description_container);
-  description_layout->setContentsMargins(scale_width(18), scale_height(18),
-    scale_width(18), 0);
-  description_layout->setSpacing(0);
-  auto name_label =
-    make_label(QString::fromStdString(model.m_tag.m_name), this);
-  name_label->set_read_only(true);
-  set_style(*name_label, NAME_STYLE());
-  description_layout->addWidget(name_label);
-  auto description_label =
-    new TextAreaBox(QString::fromStdString(model.m_tag.m_description), this);
-  description_label->set_read_only(true);
-  auto description_style = get_style(*description_label);
-  description_style.get(ReadOnly()).
-    set(PaddingTop(scale_height(6)));
-  set_style(*description_label, std::move(description_style));
-  description_layout->addWidget(description_label);
-  container_layout->addWidget(description_container);
-  if(!model.m_tag.m_values.empty()) {
-    container_layout->addWidget(
-      make_values_container(model.m_tag.m_values, this));
-  }
-  auto scroll_box = new ScrollBox(container, this);
-  scroll_box->setMaximumHeight(scale_height(240));
-  auto body_layout = new QVBoxLayout(this);
-  body_layout->setSpacing(0);
-  body_layout->setContentsMargins({});
-  body_layout->addWidget(scroll_box);
+  setFixedWidth(scale_width(280));
+  auto layout = new QVBoxLayout(this);
+  layout->setContentsMargins({});
+  layout->setSpacing(0);
+  layout->addWidget(make_scroll_box(model));
   if(!model.m_prerequisites.empty()) {
-    body_layout->addWidget(
-      make_prerequisite_container(model.get_prerequisites(), this));
+    layout->addWidget(make_prerequisite_container(model.get_prerequisites()));
   }
   auto tip = new InfoTip(this, parent);
   tip->set_interactive(true);
