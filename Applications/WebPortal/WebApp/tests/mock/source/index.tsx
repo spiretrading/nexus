a4cc7c0537bd70fb0083abada13ce7379c47ba1a
Kamal <kamal@eidolonsystems.com>
2021-12-07 15:51:00 -0500

    Added stub project from the mock application.

---
+++ Applications/WebPortal/WebApp/tests/mock/source/index.tsx
@@ -0,0 +1,12 @@
+import * as Beam from 'beam';
+import * as Nexus from 'nexus';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import * as Router from 'react-router-dom';
+import * as WebPortal from 'web_portal';
+
+ReactDOM.render(
+  <Router.HashRouter>
+    <div/>
+  </Router.HashRouter>,
+  document.getElementById('main'));
