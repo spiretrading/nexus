bd2d937b34c82411297d32644322cf511a1b345b
Kamal Mansouri <kamal@eidolonsystems.com>
2021-12-29 22:44:08 -0500

    Updated TableHeader to use the ListValueModel.

--- Applications/Spire/Source/Ui/TableHeader.cpp
+++ Applications/Spire/Source/Ui/TableHeader.cpp
@@ -1,6 +1,6 @@
 #include "Spire/Ui/TableHeader.hpp"
 #include <QHBoxLayout>
-#include "Spire/Spire/LocalCompositeValueModel.hpp"
+#include "Spire/Spire/ListValueModel.hpp"
 
 using namespace Spire;
 
@@ -13,10 +13,8 @@ TableHeader::TableHeader(
   layout->setContentsMargins({});
   layout->setSpacing(0);
   for(auto i = 0; i != m_cells->get_size(); ++i) {
-    auto cell = new TableHeaderCell(
-      std::make_shared<LocalCompositeValueModel<TableHeaderCell::Model>>(
-        m_cells->get(i)));
-    layout->addWidget(cell);
+    layout->addWidget(
+      new TableHeaderCell(make_list_value_model(m_cells, i)));
   }
 }
 
