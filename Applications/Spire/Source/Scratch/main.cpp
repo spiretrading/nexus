cb779543069fc6eda3e595605e8b34db86105dd7
Ken <ken@spiretrading.com>
2022-01-10 20:26:45 -0500

    Improved painted border demo; where two same-sized borders meet in a curve, the inside and outside edges align correctly.

--- Applications/Spire/Source/Scratch/main.cpp
+++ Applications/Spire/Source/Scratch/main.cpp
@@ -20,10 +20,10 @@ namespace {
 
   QPointF get_curve_pos(
       QPointF start, QPointF ctrl, QPointF end, double percent) {
-    float x1 = (ctrl.x() - start.x()) * percent + start.x();
-    float y1 = (ctrl.y() - start.y()) * percent + start.y();
-    float x2 = (end.x() - ctrl.x()) * percent + ctrl.x();
-    float y2 = (end.y() - ctrl.y()) * percent + ctrl.y();
+    double x1 = (ctrl.x() - start.x()) * percent + start.x();
+    double y1 = (ctrl.y() - start.y()) * percent + start.y();
+    double x2 = (end.x() - ctrl.x()) * percent + ctrl.x();
+    double y2 = (end.y() - ctrl.y()) * percent + ctrl.y();
     return {(x2 - x1) * percent + x1, (y2 - y1) * percent + y1};
   }
 
@@ -34,7 +34,7 @@ namespace {
   };
 
   Curve make_left_outer(int radius, int border, int previous_border) {
-    auto start = [&] () -> QPointF {
+    auto start = [&] {
       if(previous_border <= 0) {
         return QPointF(0, radius);
       }
@@ -44,18 +44,21 @@ namespace {
       auto point = get_curve_pos(start, QPointF(0, 0), end, 1 - tp);
       return point;
     }();
-    auto end = [&] () -> QPointF {
+    auto end = [&] {
       return QPointF(radius, 0);
     }();
-    return {start, QPointF(start.x(), end.y()), end};
+    auto control = [&] {
+      return QPointF(static_cast<double>(radius / 2), 0);
+    }();
+    return {start, control, end};
   }
 
   Curve make_right_outer(int radius, int widget_width,
       int border_width, int next_border_width) {
-    auto start = [&] () -> QPointF {
+    auto start = [&] {
       return QPointF(widget_width - radius, 0);
     }();
-    auto end = [&] () -> QPointF {
+    auto end = [&] {
       if(next_border_width <= 0) {
         return QPointF(widget_width, radius);
       }
@@ -65,41 +68,46 @@ namespace {
       auto point = get_curve_pos(start, QPointF(widget_width, 0), end, tp);
       return point;
     }();
-    return {start, QPointF(end.x(), start.y()), end};
+    auto control = [&] {
+      return QPointF(widget_width - (radius / 2), 0);
+    }();
+    return {start, control, end};
   }
 
   Curve make_right_inner(int radius, int widget_width,
       int border_width, int next_border_width) {
-    auto start = [&] () -> QPointF {
+    auto ideal_start = QPointF(widget_width - next_border_width, radius);
+    auto start = [&] {
       if(next_border_width <= 0) {
         return QPointF(widget_width, radius);
+      } else if(radius - next_border_width <= 0) {
+        return QPointF(widget_width - next_border_width, border_width);
       }
-      auto start = QPointF(widget_width - next_border_width, radius);
       auto end = QPointF(widget_width - radius, border_width);
       auto tp = get_transition_point(border_width, next_border_width);
       // TODO: address this 1 - tp calculation by having a way from inferring
       //        direction in the get_transition_point function.
-      auto point = get_curve_pos(start,
+      auto point = get_curve_pos(ideal_start,
         QPointF(widget_width - next_border_width, border_width), end, 1 - tp);
       return point;
     }();
-    auto ctrl = [&] () -> QPointF {
-      return QPointF(widget_width - next_border_width, border_width);
-    }();
-    auto end = [&] () -> QPointF {
+    auto end = [&] {
       if(next_border_width <= 0) {
         return QPointF(widget_width - radius, border_width);
       }
       return QPointF(
-        widget_width - (next_border_width + radius - next_border_width),
+        widget_width - (next_border_width + std::max(0, radius - next_border_width)),
         border_width);
+    }();
+    auto ctrl = [&] {
+      return QPointF(start.x() - (ideal_start.x() - start.x()), border_width);
     }();
     return {start, ctrl, end};
   }
 
   Curve make_left_inner(
       int radius, int border_width, int previous_border_width) {
-    auto start = [&] () -> QPointF {
+    auto start = [&] {
       if(previous_border_width <= 0) {
         return QPointF(previous_border_width + radius, border_width);
       }
@@ -107,47 +115,37 @@ namespace {
       //        so don't duplicate
       // TODO: potentially constrain these radius - border calculations
       return QPointF(
-        previous_border_width + radius - previous_border_width,
+        previous_border_width + std::max(0, radius - previous_border_width),
         border_width);
     }();
-    auto ctrl = [&] () -> QPointF {
-      return QPointF(previous_border_width, border_width);
-    }();
-    auto end = [&] () -> QPointF {
+    auto ideal_end = QPointF(previous_border_width,
+      border_width + std::max(0, radius - border_width));
+    auto end = [&] {
       if(previous_border_width <= 0) {
         return QPointF(
-          previous_border_width, border_width + (radius - border_width));
+          previous_border_width, border_width + std::max(0, radius - border_width));
+      } else if(radius - previous_border_width <= 0) {
+        return QPointF(previous_border_width, border_width);
       }
       auto start = QPointF(
-        previous_border_width + (radius - border_width),
+        previous_border_width + std::max(0, radius - border_width),
         border_width);
-      auto end = QPointF(previous_border_width,
-        border_width + (radius - border_width));
       auto tp = get_transition_point(border_width, previous_border_width);
       auto point = get_curve_pos(
-        start, QPointF(previous_border_width, border_width), end, tp);
+        start, QPointF(previous_border_width, border_width), ideal_end, tp);
       return point;
+    }();
+    auto ctrl = [&] {
+      return QPointF(end.x() + (end.x() - ideal_end.x()), border_width);
     }();
     return Curve{start, ctrl, end};
   }
 
-  // assumes top side, which can be rotated into place
-  // builds the path geometry without color
   QPainterPath create_border_side(int left_radius, int right_radius,
       int border_width, int width,
       int height, int previous_border_width, int next_border_width) {
     // TODO: if border_width is 0, still generate some border so the
     //        clip path works properly for the background.
-
-    // TODO: investigate if the presence of the left/right borders matter,
-    //        it may be the case that 'sibling-less' borders will function
-    //        properly with the same calculation that would be applied
-    //        to a border that shares a corner.
-
-    // TODO: curves needs an additional ctrl point adjustment to match the
-    //        connecting curve
-    // TODO: ^^ this basically defines one bezier curve out of two,
-    //        maybe write a function instead.
 
     auto loc = make_left_outer(
       left_radius, border_width, previous_border_width);
@@ -174,40 +172,50 @@ class PaintTest : public QWidget {
   protected:
     void paintEvent(QPaintEvent* event) override {
       auto top_width = 20;
-      auto right_width = 20;
-      auto bottom_width = 20;
-      auto left_width = 20;
+      auto right_width = 5;
+      auto bottom_width = 10;
+      auto left_width = 50;
       // Note: these need to be manually reduced!
-      auto tl_radius = 80;
-      auto tr_radius = 40;
-      auto br_radius = 80;
-      auto bl_radius = 40;
+      auto tl_radius = 0;
+      auto tr_radius = 0;
+      auto br_radius = 0;
+      auto bl_radius = 0;
       auto painter = QPainter(this);
       painter.fillRect(0, 0, width(), height(), QColor(0, 255, 255));
       painter.setPen(Qt::NoPen);
-      painter.setRenderHint(QPainter::HighQualityAntialiasing);
+      // Note: also produces corner blending effect, with QPainter's
+      //        default SourceOver
+      painter.setRenderHint(QPainter::Antialiasing);
 
       painter.setBrush(QColor(Qt::red));
-      painter.drawPath(create_border_side(tl_radius, tr_radius, top_width,
-        width(), height(), left_width, right_width));
+      if(top_width > 0) {
+        painter.drawPath(create_border_side(tl_radius, tr_radius, top_width,
+          width(), height(), left_width, right_width));
+      }
 
       painter.rotate(90);
       painter.translate(0, -width());
       painter.setBrush(QColor(Qt::green));
-      painter.drawPath(create_border_side(tr_radius, br_radius, right_width,
-        height(), width(), top_width, bottom_width));
+      if(right_width > 0) {
+        painter.drawPath(create_border_side(tr_radius, br_radius, right_width,
+          height(), width(), top_width, bottom_width));
+      }
 
       painter.rotate(90);
       painter.translate(0, -height());
       painter.setBrush(QColor(Qt::blue));
-      painter.drawPath(create_border_side(br_radius, bl_radius, bottom_width,
-        width(), height(), right_width, left_width));
+      if(bottom_width > 0) {
+        painter.drawPath(create_border_side(br_radius, bl_radius, bottom_width,
+          width(), height(), right_width, left_width));
+      }
 
       painter.rotate(90);
       painter.translate(0, -width());
       painter.setBrush(QColor(Qt::yellow));
-      painter.drawPath(create_border_side(bl_radius, tl_radius, left_width,
-        height(), width(), bottom_width, top_width));
+      if(left_width > 0) {
+        painter.drawPath(create_border_side(bl_radius, tl_radius, left_width,
+          height(), width(), bottom_width, top_width));
+      }
     }
 };
 
@@ -218,26 +226,25 @@ int main(int argc, char** argv) {
   initialize_resources();
   auto w = new QWidget();
   w->resize(350, 350);
-  //w->setAttribute(Qt::WA_TranslucentBackground);
   auto l = new QHBoxLayout(w);
-  auto pt = new PaintTest();
-  pt->setFixedSize(200, 200);
-  l->addWidget(pt);
-  auto nw = new QWidget();
-  nw->setStyleSheet(QString(R"(
-    border: 20px solid red;
-    border-radius: 25px;
-    border-top-color: red;
-    border-right-color: green;
-    /*border-right: none;*/
-    border-bottom-color: blue;
-    border-left-color: yellow;
-  )"));
-  nw->setFixedSize(200, 200);
-  l->addWidget(nw);
-  //auto b = make_input_box(nullptr, w);
-  //b->setFixedSize(100, 100);
-  //l->addWidget(b);
+  //auto pt = new PaintTest();
+  //pt->setFixedSize(200, 200);
+  //l->addWidget(pt);
+  //auto nw = new QWidget();
+  //nw->setStyleSheet(QString(R"(
+  //  border: 20px solid red;
+  //  border-radius: 25px;
+  //  border-top-color: red;
+  //  border-right-color: green;
+  //  /*border-right: none;*/
+  //  border-bottom-color: blue;
+  //  border-left-color: yellow;
+  //)"));
+  //nw->setFixedSize(200, 200);
+  //l->addWidget(nw);
+  auto b = make_input_box(nullptr, w);
+  b->setFixedSize(200, 200);
+  l->addWidget(b);
   w->show();
   application->exec();
 }
