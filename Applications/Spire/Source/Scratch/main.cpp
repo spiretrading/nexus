77cd9c66f4a389c3ecc7c1bb208ee1b2cc62b04b
Kamal <kamal@eidolonsystems.com>
2022-01-12 16:44:12 -0500

    Implemented support for drawing intersecting borders of different colors.

--- Applications/Spire/Source/Scratch/main.cpp
+++ Applications/Spire/Source/Scratch/main.cpp
@@ -1,3 +1,4 @@
+#include <numbers>
 #include <QApplication>
 #include <QWidget>
 #include <QPainter>
@@ -23,29 +24,48 @@ struct Borders {
   int m_top_left_radius;
 };
 
+void draw_corner(QPainter& painter, QPainterPath& path,
+    Border horizontal_border, Border vertical_border, int radius) {
+  path.moveTo(QPoint(0, radius - 1));
+  auto corner_angle = -90 * (static_cast<double>(horizontal_border.m_size) /
+    (vertical_border.m_size + horizontal_border.m_size));
+  path.arcTo(QRect(QPoint(0, 0), 2 * QSize(radius, radius)), 180, corner_angle);
+  auto outer_joint = path.currentPosition();
+  path.lineTo(QPoint(outer_joint.x(), radius - 1));
+  painter.fillPath(path, horizontal_border.m_color);
+  path.clear();
+  path.moveTo(QPoint(outer_joint.x(), radius - 1));
+  path.lineTo(QPoint(horizontal_border.m_size, radius - 1));
+  path.arcTo(QRect(QPoint(horizontal_border.m_size, vertical_border.m_size),
+    2 * QSize(radius - horizontal_border.m_size, radius - vertical_border.m_size)),
+    180, corner_angle);
+  auto inner_joint = path.currentPosition();
+  path.lineTo(outer_joint);
+  painter.fillPath(path, horizontal_border.m_color);
+  path.clear();
+  path.moveTo(outer_joint);
+  path.arcTo(QRect(QPoint(0, 0), 2 * QSize(radius, radius)), 180 + corner_angle,
+    -90 - corner_angle);
+  path.lineTo(QPoint(radius, vertical_border.m_size));
+  path.arcTo(QRect(QPoint(horizontal_border.m_size, vertical_border.m_size),
+    2 * QSize(radius - horizontal_border.m_size, radius - vertical_border.m_size)),
+    90, 90 + corner_angle);
+  painter.fillPath(path, vertical_border.m_color);
+}
+
 void draw_border(QSize size, Borders borders, QWidget& widget) {
   auto painter = QPainter(&widget);
   painter.setRenderHint(QPainter::Antialiasing);
-  painter.fillRect(QRect(QPoint(0, 0), size), Qt::white);
+  painter.fillRect(QRect(
+    QPoint(borders.m_left.m_size, borders.m_top.m_size),
+    size - QSize(borders.m_left.m_size + borders.m_right.m_size,
+      borders.m_top.m_size - borders.m_bottom.m_size)), Qt::white);
   painter.fillRect(QRect(QPoint(0, borders.m_top_left_radius - 1), QSize(
-    borders.m_left.m_size, size.height() - borders.m_top_left_radius)),
+    borders.m_left.m_size, size.height() - borders.m_top_left_radius + 1)),
     borders.m_left.m_color);
-  auto top_left_corner_path =
-    QPainterPath(QPoint(0, borders.m_top_left_radius - 1));
-  top_left_corner_path.arcTo(QRect(QPoint(0, 0),
-    QSize(2 * borders.m_top_left_radius, 2 * borders.m_top_left_radius)),
-    -180, -90);
-  top_left_corner_path.lineTo(
-    QPoint(borders.m_top_left_radius, borders.m_top.m_size));
-  if(borders.m_top_left_radius > borders.m_top.m_size ||
-      borders.m_top_left_radius > borders.m_left.m_size) {
-    top_left_corner_path.arcTo(
-      QRect(QPoint(borders.m_left.m_size, borders.m_top.m_size),
-      QSize(2 * (borders.m_top_left_radius - borders.m_left.m_size),
-      2 * (borders.m_top_left_radius - borders.m_top.m_size))), 90, 90);
-  }
-  top_left_corner_path.lineTo(QPoint(0, borders.m_top_left_radius));
-  painter.fillPath(top_left_corner_path, borders.m_top.m_color);
+  auto path = QPainterPath();
+  draw_corner(
+    painter, path, borders.m_left, borders.m_top, borders.m_top_left_radius);
   painter.fillRect(
     QRect(QPoint(borders.m_top_left_radius, 0),
     QSize(size.width() - borders.m_top_left_radius, borders.m_top.m_size)),
@@ -55,8 +75,8 @@ void draw_border(QSize size, Borders borders, QWidget& widget) {
 struct Canvas : QWidget {
   void paintEvent(QPaintEvent* event) override {
     auto borders = Borders();
-    borders.m_left = {scale_width(10), Qt::blue};
-    borders.m_top = {scale_height(20), Qt::blue};
+    borders.m_left = {scale_width(20), Qt::blue};
+    borders.m_top = {scale_height(20), Qt::red};
     borders.m_top_left_radius = scale_width(30);
     draw_border(size(), borders, *this);
   };
