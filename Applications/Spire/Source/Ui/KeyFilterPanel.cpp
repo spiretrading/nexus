e4b981a490c769cb4bd26c4526b0c7c14b379fae
Shaobin <shaobin@spiretrading.com>
2022-02-18 17:52:32 -0500

    Implemented a customized value model passed to the KeyInputBox inside the KeyFilterPanel.

--- Applications/Spire/Source/Ui/KeyFilterPanel.cpp
+++ Applications/Spire/Source/Ui/KeyFilterPanel.cpp
@@ -1,35 +1,83 @@
 #include "Spire/Ui/KeyFilterPanel.hpp"
-#include "Spire/Spire/ArrayListModel.hpp"
-#include "Spire/Spire/LocalValueModel.hpp"
 #include "Spire/Ui/AnyInputBox.hpp"
+#include "Spire/Ui/CustomQtVariants.hpp"
 #include "Spire/Ui/KeyInputBox.hpp"
 
+using namespace boost::signals2;
 using namespace Spire;
 
-namespace {
-  AnyInputBox* input_box_builder(std::shared_ptr<KeySequenceValueModel> current,
-      std::shared_ptr<AnyListModel> matches) {
-    auto input_box = new AnyInputBox(*(new KeyInputBox(std::move(current))));
-    input_box->get_current()->connect_update_signal([=] (const auto& value) {
-      auto& sequence = any_cast<QKeySequence>(value);
-      if(sequence.isEmpty()) {
-        return;
-      }
-      for(auto i = 0; i < matches->get_size(); ++i) {
-        if(sequence == std::any_cast<QKeySequence>(matches->get(i))) {
-          input_box->get_current()->set(QKeySequence());
-          break;
-        }
-      }
-    });
-    input_box->connect_submit_signal([=] (const auto& submission) {
-      if(!any_cast<QKeySequence>(submission).isEmpty()) {
-        input_box->get_current()->set(QKeySequence());
-        matches->push(any_cast<QKeySequence>(submission));
-      }
-    });
-    return input_box;
+struct KeyInputBoxValueModel : ValueModel<QKeySequence> {
+  std::shared_ptr<KeySequenceValueModel> m_source;
+  std::shared_ptr<AnyListModel> m_matches;
+  std::unordered_set<QString> m_matches_set;
+  std::vector<QString> m_matches_list;
+  scoped_connection m_matches_connection;
+
+  KeyInputBoxValueModel(std::shared_ptr<KeySequenceValueModel> source,
+      std::shared_ptr<AnyListModel> matches)
+      : m_source(std::move(source)),
+        m_matches(std::move(matches)),
+        m_matches_connection(m_matches->connect_operation_signal(
+          std::bind_front(&KeyInputBoxValueModel::on_operation, this))) {
+    for(auto i = 0; i < m_matches->get_size(); ++i) {
+      add_match(i);
+    }
   }
+
+  QValidator::State get_state() const override {
+    return m_source->get_state();
+  }
+
+  const Type& get() const override {
+    return m_source->get();
+  }
+
+  QValidator::State test(const Type& value) const {
+    return m_source->test(value);
+  }
+
+  QValidator::State set(const Type& value) {
+    if(m_matches_set.contains(value.toString())) {
+      return QValidator::State::Invalid;;
+    }
+    return m_source->set(value);;
+  }
+
+  connection connect_update_signal(
+      const UpdateSignal::slot_type& slot) const override {
+    return m_source->connect_update_signal(slot);
+  }
+
+  void on_operation(const AnyListModel::Operation& operation) {
+    visit(operation,
+      [&] (const AnyListModel::AddOperation& operation) {
+        add_match(operation.m_index);
+      },
+      [&] (const AnyListModel::RemoveOperation& operation) {
+        m_matches_set.erase(m_matches_list[operation.m_index]);
+        m_matches_list.erase(m_matches_list.begin() + operation.m_index);
+      });
+  }
+
+  void add_match(int index) {
+    auto value = displayTextAny(m_matches->get(index));
+    m_matches_set.insert(value);
+    m_matches_list.insert(m_matches_list.begin() + index, value);
+  }
+};
+
+AnyInputBox* key_input_box_builder(
+    std::shared_ptr<KeySequenceValueModel> current,
+    std::shared_ptr<AnyListModel> matches) {
+  auto input_box = new AnyInputBox(*(new KeyInputBox(
+    std::make_shared<KeyInputBoxValueModel>(std::move(current), matches))));
+  input_box->connect_submit_signal([=] (const auto& submission) {
+    if(!any_cast<QKeySequence>(submission).isEmpty()) {
+      input_box->get_current()->set(QKeySequence());
+      matches->push(any_cast<QKeySequence>(submission));
+    }
+  });
+  return input_box;
 }
 
 KeyFilterPanel* Spire::make_key_filter_panel(QWidget& parent) {
@@ -50,6 +98,6 @@ KeyFilterPanel* Spire::make_key_filter_panel(
   std::shared_ptr<ListModel<QKeySequence>> matches,
   std::shared_ptr<ValueModel<KeyFilterPanel::Mode>> mode, QWidget& parent) {
   return new KeyFilterPanel(
-    std::bind_front(input_box_builder, std::move(current)),
+    std::bind_front(key_input_box_builder, std::move(current)),
     std::move(matches), std::move(mode), QObject::tr("Filter by Key"), parent);
 }
