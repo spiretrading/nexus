6195e338a71ec26bd5bad2c7c5e212f81c216ec3
Ken <ken@spiretrading.com>
2022-03-03 14:30:15 -0500

    Added shorter disconnect example.

--- Applications/Spire/Source/Scratch/main.cpp
+++ Applications/Spire/Source/Scratch/main.cpp
@@ -1,10 +1,7 @@
 #include <QApplication>
-#include "Spire/OrderImbalanceIndicator/LocalOrderImbalanceIndicatorModel.hpp"
-#include "Spire/OrderImbalanceIndicator/OrderImbalanceIndicatorTableModel.hpp"
-#include "Spire/Spire/Intervals.hpp"
+#include "Spire/Spire/QtPromise.hpp"
 #include "Spire/Spire/Resources.hpp"
 
-using namespace boost::posix_time;
 using namespace Spire;
 
 int main(int argc, char** argv) {
@@ -12,8 +9,10 @@ int main(int argc, char** argv) {
   application->setOrganizationName(QObject::tr("Spire Trading Inc"));
   application->setApplicationName(QObject::tr("Scratch"));
   initialize_resources();
-  auto source = std::make_shared<LocalOrderImbalanceIndicatorModel>();
-  auto model = new OrderImbalanceIndicatorTableModel(
-    TimeInterval::closed(from_time_t(0), from_time_t(200)), source);
+  auto promise = QtPromise([] { return 5; });
+  auto p = promise.then([] (auto&& result) {
+    throw std::exception("Shouldn't be called");
+  });
+  p.disconnect();
   application->exec();
 }
