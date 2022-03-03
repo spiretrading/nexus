eead6d618bde99b6b6ff7606b4f02a40e7375cd5
Ken <ken@spiretrading.com>
2022-03-03 13:14:04 -0500

    Disconnected promise example.

--- Applications/Spire/Source/Scratch/main.cpp
+++ Applications/Spire/Source/Scratch/main.cpp
@@ -1,6 +1,10 @@
 #include <QApplication>
+#include "Spire/OrderImbalanceIndicator/LocalOrderImbalanceIndicatorModel.hpp"
+#include "Spire/OrderImbalanceIndicator/OrderImbalanceIndicatorTableModel.hpp"
+#include "Spire/Spire/Intervals.hpp"
 #include "Spire/Spire/Resources.hpp"
 
+using namespace boost::posix_time;
 using namespace Spire;
 
 int main(int argc, char** argv) {
@@ -8,5 +12,8 @@ int main(int argc, char** argv) {
   application->setOrganizationName(QObject::tr("Spire Trading Inc"));
   application->setApplicationName(QObject::tr("Scratch"));
   initialize_resources();
+  auto source = std::make_shared<LocalOrderImbalanceIndicatorModel>();
+  auto model = new OrderImbalanceIndicatorTableModel(
+    TimeInterval::closed(from_time_t(0), from_time_t(200)), source);
   application->exec();
 }
