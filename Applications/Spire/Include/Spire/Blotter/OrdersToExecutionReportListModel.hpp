ab8866145829d8628497753234211585e598b452
Kamal <kamal@eidolonsystems.com>
2022-06-07 13:49:50 -0400

    Implemented the OrdersToExecutionReportListModel.

--- Applications/Spire/Include/Spire/Blotter/OrdersToExecutionReportListModel.hpp
+++ Applications/Spire/Include/Spire/Blotter/OrdersToExecutionReportListModel.hpp
@@ -1,7 +1,10 @@
 #ifndef SPIRE_ORDERS_TO_EXECUTION_REPORT_LIST_MODEL_HPP
 #define SPIRE_ORDERS_TO_EXECUTION_REPORT_LIST_MODEL_HPP
+#include <unordered_map>
 #include "Spire/Blotter/Blotter.hpp"
 #include "Spire/Blotter/BlotterModel.hpp"
+#include "Spire/Spire/ArrayListModel.hpp"
+#include "Spire/Spire/QtTaskQueue.hpp"
 
 namespace Spire {
 
@@ -13,7 +16,7 @@ namespace Spire {
     public:
 
       /**
-       * Constructs a DerivedOrderListModel.
+       * Constructs an OrdersToExecutionReportListModel.
        * @param orders The list of orders producing the execution reports that
        *        this model is to publish.
        */
@@ -29,6 +32,21 @@ namespace Spire {
 
     protected:
       void transact(const std::function<void ()>& transaction) override;
+
+    private:
+      QtTaskQueue m_queue;
+      std::shared_ptr<OrderListModel> m_orders;
+      std::shared_ptr<ArrayListModel<
+        Nexus::OrderExecutionService::ExecutionReportEntry>> m_reports;
+      std::unordered_map<const Nexus::OrderExecutionService::Order*, int>
+        m_order_to_count;
+      boost::signals2::scoped_connection m_connection;
+
+      void add(const Nexus::OrderExecutionService::Order& order);
+      void remove(const Nexus::OrderExecutionService::Order& order);
+      void on_report(const Nexus::OrderExecutionService::Order& order,
+        const Nexus::OrderExecutionService::ExecutionReport& report);
+      void on_operation(const OrderListModel::Operation& operation);
   };
 }
 
--- Applications/Spire/Source/Blotter/OrdersToExecutionReportListModel.cpp
+++ Applications/Spire/Source/Blotter/OrdersToExecutionReportListModel.cpp
@@ -2,26 +2,77 @@
 
 using namespace boost;
 using namespace boost::signals2;
+using namespace Nexus;
+using namespace Nexus::OrderExecutionService;
 using namespace Spire;
 
 OrdersToExecutionReportListModel::OrdersToExecutionReportListModel(
-  std::shared_ptr<OrderListModel> orders) {}
+    std::shared_ptr<OrderListModel> orders)
+    : m_orders(std::move(orders)),
+      m_reports(std::make_shared<ArrayListModel<ExecutionReportEntry>>()),
+      m_connection(m_orders->connect_operation_signal(std::bind_front(
+        &OrdersToExecutionReportListModel::on_operation, this))) {
+  for(auto i = 0; i != m_orders->get_size(); ++i) {
+    add(*m_orders->get(i));
+  }
+}
 
 int OrdersToExecutionReportListModel::get_size() const {
-  throw std::runtime_error("Not implemented.");
+  return m_reports->get_size();
 }
 
 const OrdersToExecutionReportListModel::Type&
     OrdersToExecutionReportListModel::get(int index) const {
-  throw std::runtime_error("Not implemented.");
+  return m_reports->get(index);
 }
 
 connection OrdersToExecutionReportListModel::connect_operation_signal(
     const OperationSignal::slot_type& slot) const {
-  throw std::runtime_error("Not implemented.");
+  return m_reports->connect_operation_signal(slot);
 }
 
 void OrdersToExecutionReportListModel::transact(
     const std::function<void ()>& transaction) {
   transaction();
+}
+
+void OrdersToExecutionReportListModel::add(const Order& order) {
+  order.GetPublisher().Monitor(m_queue.get_slot<ExecutionReport>(
+    std::bind_front(&OrdersToExecutionReportListModel::on_report, this,
+      std::cref(order))));
+}
+
+void OrdersToExecutionReportListModel::remove(const Order& order) {
+  auto& count = m_order_to_count[&order];
+  --count;
+  if(count != 0) {
+    return;
+  }
+  m_reports->transact([&] {
+    for(auto i = m_reports->get_size() - 1; i >= 0; --i) {
+      if(m_reports->get(i).m_order == &order) {
+        m_orders->remove(i);
+      }
+    }
+  });
+}
+
+void OrdersToExecutionReportListModel::on_report(
+    const Order& order, const ExecutionReport& report) {
+  auto& count = m_order_to_count[&order];
+  ++count;
+  if(count == 1) {
+    m_reports->push(ExecutionReportEntry(&order, report));
+  }
+}
+
+void OrdersToExecutionReportListModel::on_operation(
+    const OrderListModel::Operation& operation) {
+  visit(operation,
+    [&] (const OrderListModel::AddOperation& operation) {
+      add(*operation.get_value());
+    },
+    [&] (const OrderListModel::RemoveOperation& operation) {
+      remove(*operation.get_value());
+    });
 }
