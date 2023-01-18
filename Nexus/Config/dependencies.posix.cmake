b9040cc9c16a51652d87d8b601982fd0f96d5af3
Kamal Mansouri <kamal@spiretrading.com>
2023-01-18 13:33:27 -0500

    Nexus builds and runs on Linux.

--- Nexus/Config/dependencies.posix.cmake
+++ Nexus/Config/dependencies.posix.cmake
@@ -8,11 +8,11 @@ set(BEAM_PYTHON_LIBRARY_DEBUG_PATH
 set(BEAM_PYTHON_LIBRARY_OPTIMIZED_PATH
   "${PROJECT_BINARY_DIR}/Dependencies/Beam/Beam/Libraries/Release/_beam.so")
 set(LUA_INCLUDE_PATH
-  "${PROJECT_BINARY_DIR}/Dependencies/lua-5.4.2/install/include")
+  "${PROJECT_BINARY_DIR}/Dependencies/lua-5.4.4/install/include")
 set(LUA_LIBRARY_DEBUG_PATH
-  "${PROJECT_BINARY_DIR}/Dependencies/lua-5.4.2/install/lib/liblua.a")
+  "${PROJECT_BINARY_DIR}/Dependencies/lua-5.4.4/install/lib/liblua.a")
 set(LUA_LIBRARY_OPTIMIZED_PATH
-  "${PROJECT_BINARY_DIR}/Dependencies/lua-5.4.2/install/lib/liblua.a")
+  "${PROJECT_BINARY_DIR}/Dependencies/lua-5.4.4/install/lib/liblua.a")
 set(QUICK_FIX_INCLUDE_PATH
   "${PROJECT_BINARY_DIR}/Dependencies/quickfix-v.1.15.1/include")
 set(QUICK_FIX_LIBRARY_DEBUG_PATH
