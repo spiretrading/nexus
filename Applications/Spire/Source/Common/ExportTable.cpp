f842e88c34eb94e03ba7440c46af538be9a36149
Shaobin <shaobin@spiretrading.com>
2023-05-10 17:14:21 -0400

    Updated the format of the export data.

--- Applications/Spire/Source/Common/ExportTable.cpp
+++ Applications/Spire/Source/Common/ExportTable.cpp
@@ -1,9 +1,23 @@
 #include "Spire/Spire/ExportTable.hpp"
+#include <Beam/TimeService/ToLocalTime.hpp>
 #include "Spire/Ui/CustomQtVariants.hpp"
 
+using namespace Beam::TimeService;
 using namespace boost::gregorian;
 using namespace boost::posix_time;
 using namespace Nexus;
+
+namespace {
+  auto to_string(ptime time) {
+    auto local_time = ToLocalTime(time);
+    auto ss = std::ostringstream();
+    auto facet = new time_facet();
+    facet->format("%Y-%m-%d %H:%M:%S%F");
+    ss.imbue(std::locale(std::locale::classic(), facet));
+    ss << local_time;
+    return ss.str();
+  }
+}
 
 void Spire::export_table_as_csv(const TableModel& table, std::ostream& out) {
   for(auto i = 0; i < table.get_row_size(); ++i) {
@@ -16,11 +30,19 @@ void Spire::export_table_as_csv(const TableModel& table, std::ostream& out) {
       }
       auto value = table.at(i, j);
       auto& value_type = value.get_type();
-      if(value_type == typeid(bool) || value_type == typeid(int) ||
+      if(value_type == typeid(ptime)) {
+        out << to_string(std::any_cast<ptime>(to_any(value)));
+      } else if(value_type == typeid(bool) || value_type == typeid(int) ||
           value_type == typeid(double) || value_type == typeid(date) ||
-          value_type == typeid(ptime) || value_type == typeid(time_duration) ||
-          value_type == typeid(Quantity) || value_type == typeid(Money)) {
-        out << displayText(value).toStdString();
+          value_type == typeid(time_duration) || value_type == typeid(Money) ||
+          value_type == typeid(Quantity)) {
+        if(value_type == typeid(Quantity)) {
+          value = static_cast<double>(std::any_cast<Quantity>(to_any(value)));
+        }
+        auto locale = QLocale();
+        locale.setNumberOptions(QLocale::OmitGroupSeparator);
+        out << CustomVariantItemDelegate().displayText(
+          to_qvariant(to_any(value)), locale).toStdString();
       } else {
         out << "\"" << displayText(value).toStdString() << "\"";
       }
--- Applications/Spire/Source/SpireTester/ExportTableTester.cpp
+++ Applications/Spire/Source/SpireTester/ExportTableTester.cpp
@@ -1,25 +1,53 @@
 #include <doctest/doctest.h>
+#include "boost/date_time/c_local_time_adjustor.hpp"
+#include "boost/date_time/posix_time/posix_time.hpp"
+#include <locale>
 #include "Nexus/Definitions/SecuritySet.hpp"
 #include "Spire/Spire/ArrayTableModel.hpp"
 #include "Spire/Spire/ExportTable.hpp"
 #include "Spire/Ui/CustomQtVariants.hpp"
 
+using namespace boost;
+using namespace boost::gregorian;
 using namespace boost::posix_time;
 using namespace Nexus;
 using namespace Spire;
 
+auto to_string(ptime t) {
+  auto ss = std::ostringstream();
+  auto facet = new time_facet();
+  facet->format("%Y-%m-%d %H:%M:%S%F");
+  ss.imbue(std::locale(std::locale::classic(), facet));
+  ss << t;
+  return ss.str();
+}
+
 TEST_SUITE("ExportTable") {
   TEST_CASE("export_as_csv") {
-    auto ts1 = std::string("01:02:03");
-    auto ts2 = std::string("03:04:05");
-    auto ts3 = std::string("05:06:07");
+    auto date1 = date(2023, Jan, 20);
+    auto time_duration1 = time_duration(1, 2, 3, 741874);
+    auto date2 = date(2023, Mar, 2);
+    auto time_duration2 = time_duration(3, 4, 5, 645650);
+    auto date3 = date(2023, May, 11);
+    auto time_duration3 = time_duration(5, 6, 7);
+    auto time1 = ptime(date1, time_duration1);
+    auto time2 = ptime(date2, time_duration2);
+    auto time3 = ptime(date3, time_duration3);
+    auto local_time1 = date_time::c_local_adjustor<ptime>::utc_to_local(time1);
+    auto local_time2 = date_time::c_local_adjustor<ptime>::utc_to_local(time2);
+    auto local_time3 = date_time::c_local_adjustor<ptime>::utc_to_local(time3);
     auto result = std::format(
-      R"("Time","Price","Quantity","Market","Security","Country","Condition")"
-      "\n"
-      R"({},10.00,1,"NYSE","MRU.TSX","CAN","@")""\n"
-      R"({},20.00,2,"NYSE","MRU.TSX","CAN","@")""\n"
-      R"({},30.00,4,"NYSE","MRU.TSX","CAN","@")",
-      ts1, ts2, ts3);
+      "\"Time\",\"Date\",\"Duration\",\"Int\",\"Double\",\"Price\","
+      "\"Quantity\",\"Market\",\"Security\",\"Country\",\"Condition\"\n"
+      R"({},{},{},100,1234.56,1000.55,1000.32,"NYSE","MRU.TSX","CAN","@")""\n"
+      R"({},{},{},1000,0.21,20.00,10.5,"NYSE","MRU.TSX","CAN","@")""\n"
+      R"({},{},{},10000,1000,30.12,4000,"NYSE","MRU.TSX","CAN","@")",
+      to_string(local_time1), to_iso_extended_string(date1),
+      to_simple_string(time_duration1),
+      to_string(local_time2), to_iso_extended_string(date2),
+      to_simple_string(time_duration2),
+      to_string(local_time3), to_iso_extended_string(date3),
+      to_simple_string(time_duration3));
     auto table = ArrayTableModel();
     auto market = MarketToken(MarketCode("XNYS"));
     auto security = ParseWildCardSecurity(
@@ -27,14 +55,14 @@ TEST_SUITE("ExportTable") {
     auto country = DefaultCountries::CA();
     auto condition =
       TimeAndSale::Condition(TimeAndSale::Condition::Type::REGULAR, "@");
-    table.push({"Time", "Price", "Quantity", "Market", "Security", "Country",
-      "Condition"});
-    table.push({duration_from_string(ts1), Money(10), Quantity(1), market,
-      *security, country, condition});
-    table.push({duration_from_string(ts2), Money(20), Quantity(2), market,
-      *security, country, condition});
-    table.push({duration_from_string(ts3), Money(30), Quantity(4), market,
-      *security, country, condition});
+    table.push({"Time", "Date", "Duration", "Int", "Double", "Price",
+      "Quantity", "Market", "Security", "Country", "Condition"});
+    table.push({time1, date1, time_duration1, 100, 1234.56, Money(1000.55),
+      Quantity(1000.32), market, *security, country, condition});
+    table.push({time2, date2, time_duration2, 1000, 0.21, Money(20),
+      Quantity(10.5), market, *security, country, condition});
+    table.push({time3, date3, time_duration3, 10000, 1000, Money(30.12),
+      Quantity(4000), market, *security, country, condition});
     auto out = std::stringstream();
     export_table_as_csv(table, out);
     REQUIRE(out.str() == result);
