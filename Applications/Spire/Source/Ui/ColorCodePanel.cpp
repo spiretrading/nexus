b40145baa0d6fdb392928a2cbc001daff0deb962
Shaobin <shaobin@spiretrading.com>
2023-09-28 14:00:51 -0400

    Handle NULL input boxes.

--- Applications/Spire/Source/Ui/ColorCodePanel.cpp
+++ Applications/Spire/Source/Ui/ColorCodePanel.cpp
@@ -16,6 +16,66 @@ using namespace Spire;
 using namespace Spire::Styles;
 
 namespace {
+  auto make_modifiers() {
+    return QHash<Qt::KeyboardModifier, Decimal>(
+      {{Qt::NoModifier, 1}, {Qt::AltModifier, 5}, {Qt::ControlModifier, 10},
+        {Qt::ShiftModifier, 20}});
+  }
+
+  struct CustomIntegerBox : IntegerBox {
+    using EditingFinishSignal = Signal<void(const optional<int>& value)>;
+    mutable EditingFinishSignal m_editing_finish_signal;
+
+    CustomIntegerBox(std::shared_ptr<OptionalIntegerModel> current,
+      QWidget* parent = nullptr)
+      : IntegerBox(std::move(current), parent) {}
+
+    bool eventFilter(QObject* watched, QEvent* event) override {
+      if(event->type() == QEvent::KeyPress) {
+        auto& key_event = *static_cast<QKeyEvent*>(event);
+        if(key_event.key() == Qt::Key_Enter ||
+            key_event.key() == Qt::Key_Return) {
+          m_editing_finish_signal(get_current()->get());
+        }
+      } else if(event->type() == QEvent::FocusOut) {
+        m_editing_finish_signal(get_current()->get());
+      }
+      return IntegerBox::eventFilter(watched, event);
+    }
+
+    void showEvent(QShowEvent* event) override {
+      find_focus_proxy(*this)->installEventFilter(this);
+      IntegerBox::showEvent(event);
+    }
+  };
+
+  struct CustomPercentBox : PercentBox {
+    using EditingFinishSignal = Signal<void(const optional<Decimal>& value)>;
+    mutable EditingFinishSignal m_editing_finish_signal;
+
+    CustomPercentBox(std::shared_ptr<OptionalDecimalModel> current,
+      QWidget* parent = nullptr)
+      : PercentBox(std::move(current), make_modifiers(), parent) {}
+
+    bool eventFilter(QObject* watched, QEvent* event) override {
+      if(event->type() == QEvent::KeyPress) {
+        auto& key_event = *static_cast<QKeyEvent*>(event);
+        if(key_event.key() == Qt::Key_Enter ||
+            key_event.key() == Qt::Key_Return) {
+          m_editing_finish_signal(get_current()->get());
+        }
+      } else if(event->type() == QEvent::FocusOut) {
+        m_editing_finish_signal(get_current()->get());
+      }
+      return PercentBox::eventFilter(watched, event);
+    }
+
+    void showEvent(QShowEvent* event) override {
+      find_focus_proxy(*this)->installEventFilter(this);
+      PercentBox::showEvent(event);
+    }
+  };
+
   auto get_character_width() {
     static auto width = optional<int>();
     if(width) {
@@ -74,12 +134,6 @@ namespace {
     return std::round(hue * 360);
   }
 
-  auto make_modifiers() {
-    return QHash<Qt::KeyboardModifier, Decimal>(
-      {{Qt::NoModifier, 1}, {Qt::AltModifier, 5}, {Qt::ControlModifier, 10},
-        {Qt::ShiftModifier, 20}});
-  }
-
   void update_component_style(QWidget& component) {
     update_style(component, [] (auto& style) {
       style.get(Any()).set(TextAlign(Qt::AlignCenter));
@@ -87,58 +141,116 @@ namespace {
     });
   }
 
-  auto make_rgb_color_box(std::shared_ptr<OptionalIntegerModel> red_model,
+  auto make_color_format_box(QWidget* parent) {
+    auto list_model = std::make_shared<ArrayListModel<QString>>();
+    list_model->push("HEX");
+    list_model->push("RGB");
+    list_model->push("HSB");
+    auto color_format_box = new DropDownBox(std::move(list_model), parent);
+    color_format_box->setMinimumWidth(
+      get_character_width() * 6 + scale_width(10));
+    color_format_box->get_current()->set(0);
+    return color_format_box;
+  }
+
+  auto make_rgb_color_box(std::shared_ptr<ValueModel<QColor>> color_model,
+      std::shared_ptr<OptionalIntegerModel> red_model,
       std::shared_ptr<OptionalIntegerModel> green_model,
       std::shared_ptr<OptionalIntegerModel> blue_model) {
     auto rgb_color_box = new QWidget();
     rgb_color_box->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Fixed);
     auto layout = make_hbox_layout(rgb_color_box);
-    auto red_box = new IntegerBox(std::move(red_model));
+    auto red_box = new CustomIntegerBox(std::move(red_model));
     red_box->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Fixed);
+    red_box->m_editing_finish_signal.connect([=] (const auto& value) {
+      if(!value) {
+        red_box->get_current()->set(color_model->get().red());
+      }
+    });
     set_width_range(*red_box);
     update_component_style(*red_box);
     layout->addWidget(red_box);
     layout->addSpacing(scale_width(4));
-    auto green_box = new IntegerBox(std::move(green_model));
+    auto green_box = new CustomIntegerBox(std::move(green_model));
     green_box->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Fixed);
+    green_box->m_editing_finish_signal.connect([=] (const auto& value) {
+      if(!value) {
+        green_box->get_current()->set(color_model->get().green());
+      }
+    });
     set_width_range(*green_box);
     update_component_style(*green_box);
     layout->addWidget(green_box);
     layout->addSpacing(scale_width(4));
-    auto blue_box = new IntegerBox(std::move(blue_model));
+    auto blue_box = new CustomIntegerBox(std::move(blue_model));
     blue_box->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Fixed);
+    blue_box->m_editing_finish_signal.connect([=] (const auto& value) {
+      if(!value) {
+        blue_box->get_current()->set(color_model->get().blue());
+      }
+    });
     set_width_range(*blue_box);
     update_component_style(*blue_box);
     layout->addWidget(blue_box);
     return rgb_color_box;
   }
 
-  auto make_hsb_color_box(std::shared_ptr<OptionalIntegerModel> hue_model,
+  auto make_hsb_color_box(std::shared_ptr<ValueModel<QColor>> color_model,
+      std::shared_ptr<OptionalIntegerModel> hue_model,
       std::shared_ptr<OptionalDecimalModel> saturation_model,
       std::shared_ptr<OptionalDecimalModel> brightness_model) {
     auto hsb_color_box = new QWidget();
     hsb_color_box->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Fixed);
     auto layout = make_hbox_layout(hsb_color_box);
-    auto hue_box = new IntegerBox(std::move(hue_model));
+    auto hue_box = new CustomIntegerBox(std::move(hue_model));
     hue_box->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Fixed);
+    hue_box->m_editing_finish_signal.connect([=] (const auto& value) {
+      if(!value) {
+        hue_box->get_current()->set(to_hue(color_model->get().hsvHueF()));
+      }
+    });
     set_width_range(*hue_box);
     update_component_style(*hue_box);
     layout->addWidget(hue_box);
     layout->addSpacing(scale_width(4));
-    auto saturation_box = new PercentBox(std::move(saturation_model),
-      make_modifiers());
+    auto saturation_box = new CustomPercentBox(std::move(saturation_model));
     saturation_box->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Fixed);
+    saturation_box->m_editing_finish_signal.connect([=] (const auto& value) {
+      if(!value) {
+        saturation_box->get_current()->set(
+          round_value(Decimal(color_model->get().hsvSaturationF())));
+      }
+    });
     set_width_range(*saturation_box);
     update_component_style(*saturation_box);
     layout->addWidget(saturation_box);
     layout->addSpacing(scale_width(4));
-    auto brightness_box = new PercentBox(std::move(brightness_model),
-      make_modifiers());
+    auto brightness_box = new CustomPercentBox(std::move(brightness_model));
     brightness_box->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Fixed);
+    brightness_box->m_editing_finish_signal.connect([=] (const auto& value) {
+      if(!value) {
+        brightness_box->get_current()->set(
+          round_value(Decimal(color_model->get().valueF())));
+      }
+    });
     set_width_range(*brightness_box);
     update_component_style(*brightness_box);
     layout->addWidget(brightness_box);
     return hsb_color_box;
+  }
+
+  auto make_alpha_box(std::shared_ptr<ValueModel<QColor>> color_model,
+      std::shared_ptr<OptionalDecimalModel> alpha_model, QWidget* parent) {
+    auto alpha_box = new CustomPercentBox(alpha_model, parent);
+    alpha_box->m_editing_finish_signal.connect([=] (const auto& value) {
+      if(!value) {
+        alpha_box->get_current()->set(
+          round_value(Decimal(color_model->get().alphaF())));
+      }
+      });
+    update_component_style(*alpha_box);
+    match(*alpha_box, Alpha());
+    return alpha_box;
   }
 }
 
@@ -331,14 +443,7 @@ ColorCodePanel::ColorCodePanel(std::shared_ptr<ValueModel<QColor>> current,
     QWidget* parent)
     : QWidget(parent),
       m_current(std::make_shared<ColorCodeValueModel>(std::move(current))) {
-  auto list_model = std::make_shared<ArrayListModel<QString>>();
-  list_model->push("HEX");
-  list_model->push("RGB");
-  list_model->push("HSB");
-  m_color_format_box = new DropDownBox(std::move(list_model), this);
-  m_color_format_box->setMinimumWidth(
-    get_character_width() * 6 + scale_width(10));
-  m_color_format_box->get_current()->set(0);
+  m_color_format_box = make_color_format_box(this);
   m_color_format_box->get_current()->connect_update_signal(
     std::bind_front(&ColorCodePanel::on_mode_current, this));
   m_color_input = new QStackedWidget(this);
@@ -349,14 +454,13 @@ ColorCodePanel::ColorCodePanel(std::shared_ptr<ValueModel<QColor>> current,
   auto hex_color_box = new HexColorBox(m_current->m_hex_model);
   hex_color_box->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Fixed);
   m_color_input->addWidget(hex_color_box);
-  m_color_input->addWidget(make_rgb_color_box(m_current->m_red_model,
-    m_current->m_green_model, m_current->m_blue_model));
-  m_color_input->addWidget(make_hsb_color_box(m_current->m_hue_model,
-    m_current->m_saturation_model, m_current->m_brightness_model));
-  m_alpha_box = new PercentBox(
-    m_current->m_alpha_model, make_modifiers(), this);
-  update_component_style(*m_alpha_box);
-  match(*m_alpha_box, Alpha());
+  m_color_input->addWidget(make_rgb_color_box(m_current->m_source,
+    m_current->m_red_model, m_current->m_green_model, m_current->m_blue_model));
+  m_color_input->addWidget(make_hsb_color_box(m_current->m_source,
+    m_current->m_hue_model, m_current->m_saturation_model,
+    m_current->m_brightness_model));
+  m_alpha_box =
+    make_alpha_box(m_current->m_source, m_current->m_alpha_model, this);
   m_alpha_box->installEventFilter(this);
 }
 
@@ -414,8 +518,7 @@ void ColorCodePanel::update_layout() {
 }
 
 void ColorCodePanel::on_mode_current(const optional<int>& current) {
-  if(!current) {
-    return;
+  if(current) {
+    m_color_input->setCurrentIndex(*current);
   }
-  m_color_input->setCurrentIndex(*current);
 }
