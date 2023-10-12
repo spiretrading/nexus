ea1f8b918fa83e57b2a591222e7054834eb124ff
Shaobin <shaobin@spiretrading.com>
2023-10-12 16:21:53 -0400

    Installs the event filter to the QLineEdit inside the HexColorBox when it is addecd to the TextBox.

--- Applications/Spire/Include/Spire/Ui/HexColorBox.hpp
+++ Applications/Spire/Include/Spire/Ui/HexColorBox.hpp
@@ -58,7 +58,6 @@ namespace Spire {
 
     protected:
       bool eventFilter(QObject* watched, QEvent* event) override;
-      void showEvent(QShowEvent* event) override;
 
     private:
       struct ColorToTextModel;
