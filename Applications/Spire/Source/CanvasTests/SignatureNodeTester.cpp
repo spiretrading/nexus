e26cc21aab54d726c908fd288381da4760d06c83
Kamal Mansouri <kamal@eidolonsystems.com>
2023-11-06 22:46:38 -0500

    Added additional checks to each of SignatureNodeTester's unit tests.

--- Applications/Spire/Source/CanvasTests/SignatureNodeTester.cpp
+++ Applications/Spire/Source/CanvasTests/SignatureNodeTester.cpp
@@ -8,6 +8,7 @@
 #include "Spire/Canvas/Operations/CanvasNodeStructuralEquality.hpp"
 #include "Spire/Canvas/Operations/CanvasTypeCompatibilityException.hpp"
 #include "Spire/Canvas/Operations/DefaultCanvasNodeFromCanvasTypeVisitor.hpp"
+#include "Spire/Canvas/Types/DecimalType.hpp"
 #include "Spire/Canvas/Types/IntegerType.hpp"
 #include "Spire/Canvas/Types/MoneyType.hpp"
 #include "Spire/Canvas/Types/UnionType.hpp"
@@ -64,6 +65,8 @@ TEST_SUITE("SignatureNode") {
     auto node =
       std::unique_ptr<CanvasNode>(std::make_unique<TestSignatureNode>());
     node = node->Convert(IntegerType::GetInstance());
+    REQUIRE(node->GetType().GetCompatibility(IntegerType::GetInstance()) ==
+      CanvasType::Compatibility::EQUAL);
     auto childP1 = node->FindChild("p1");
     REQUIRE(childP1.has_value());
     REQUIRE(IsStructurallyEqual(*childP1, IntegerNode(0)));
@@ -72,23 +75,34 @@ TEST_SUITE("SignatureNode") {
     REQUIRE(IsStructurallyEqual(*childP2, IntegerNode(0)));
   }
 
-  TEST_CASE("replace") {
+  TEST_CASE("replace_then_convert") {
     auto node =
       std::unique_ptr<CanvasNode>(std::make_unique<TestSignatureNode>());
     node = node->Replace("p1", std::make_unique<IntegerNode>(0));
+    auto expectedType = UnionType::Create(
+      MakeDereferenceView(std::vector<std::shared_ptr<NativeType>>{
+        IntegerType::GetInstance(), MoneyType::GetInstance()}));
+    REQUIRE(node->GetType().GetCompatibility(*expectedType) ==
+      CanvasType::Compatibility::EQUAL);
     node = node->Replace("p2", std::make_unique<TimerNode>());
+    REQUIRE(node->GetType().GetCompatibility(IntegerType::GetInstance()) ==
+      CanvasType::Compatibility::EQUAL);
     REQUIRE_THROWS_AS(node->Convert(
       MoneyType::GetInstance()), CanvasTypeCompatibilityException);
   }
 
-  TEST_CASE("replace_polymorphic") {
+  TEST_CASE("polymorphic_replace") {
     auto node =
       std::unique_ptr<CanvasNode>(std::make_unique<TestSignatureNode>());
     node = node->Replace("p1", std::make_unique<MoneyNode>(3 * Money::CENT));
+    REQUIRE(node->GetType().GetCompatibility(MoneyType::GetInstance()) ==
+      CanvasType::Compatibility::EQUAL);
     auto p2 = NoneNode().Convert(*UnionType::Create(
       MakeDereferenceView(std::vector<std::shared_ptr<NativeType>>{
-        MoneyType::GetInstance(), IntegerType::GetInstance()})));
+        IntegerType::GetInstance(), MoneyType::GetInstance()})));
     node = node->Replace("p2", std::move(p2));
+    REQUIRE(node->GetType().GetCompatibility(MoneyType::GetInstance()) ==
+      CanvasType::Compatibility::EQUAL);
     auto childP2 = node->FindChild("p2");
     REQUIRE(childP2.has_value());
     REQUIRE(childP2->GetType().GetCompatibility(IntegerType::GetInstance()) ==
@@ -104,5 +118,14 @@ TEST_SUITE("SignatureNode") {
       std::unique_ptr<CanvasNode>(std::make_unique<AdditionNode>());
     node = node->Replace("left", std::make_unique<IntegerNode>(711));
     node = node->Replace("right", std::make_unique<MultiplicationNode>());
+    auto expectedType = UnionType::Create(
+      MakeDereferenceView(std::vector<std::shared_ptr<NativeType>>{
+        DecimalType::GetInstance(), IntegerType::GetInstance()}));
+    REQUIRE(node->GetType().GetCompatibility(*expectedType) ==
+        CanvasType::Compatibility::EQUAL);
+    REQUIRE(node->GetChildren().size() == 2);
+    REQUIRE(
+      IsStructurallyEqual(node->GetChildren()[0], IntegerNode(711)));
+    REQUIRE(node->GetChildren()[1].GetChildren().size() == 2);
   }
 }
