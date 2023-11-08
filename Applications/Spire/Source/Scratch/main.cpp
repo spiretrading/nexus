2e980689aa4fea833074adef2bf2a094258428b7
Kamal <kamal@eidolonsystems.com>
2023-11-08 16:46:10 -0500

    Fixed a crash in TableBody when the model provided to it changes its number of columns.

--- Applications/Spire/Source/Scratch/main.cpp
+++ Applications/Spire/Source/Scratch/main.cpp
@@ -1,5 +1,8 @@
 #include <QApplication>
+#include <QTimer>
 #include "Spire/Spire/Resources.hpp"
+#include "Spire/Spire/ArrayTableModel.hpp"
+#include "Spire/Ui/TableView.hpp"
 
 using namespace Spire;
 
@@ -8,5 +11,18 @@ int main(int argc, char** argv) {
   application->setOrganizationName(QObject::tr("Spire Trading Inc"));
   application->setApplicationName(QObject::tr("Scratch"));
   initialize_resources();
+  auto model = std::make_shared<ArrayTableModel>();
+  auto view = TableViewBuilder(model).
+    add_header_item("A").
+    make();
+  view->show();
+  auto timer = QTimer();
+  timer.setInterval(100);
+  auto counter = 0;
+  QObject::connect(&timer, &QTimer::timeout, [&] {
+    model->push({counter});
+    ++counter;
+  });
+  timer.start();
   application->exec();
 }
