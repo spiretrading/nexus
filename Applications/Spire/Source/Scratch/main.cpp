27b1ad4c09b83b78c39fd98d3ce636d9977cc3dd
Kamal <kamal@eidolonsystems.com>
2023-12-07 14:58:50 -0500

    Reverted Spire/Scratch.

--- Applications/Spire/Source/Scratch/main.cpp
+++ Applications/Spire/Source/Scratch/main.cpp
@@ -1,6 +1,5 @@
 #include <QApplication>
 #include "Spire/Spire/Resources.hpp"
-#include "Spire/Ui/IntegerBox.hpp"
 
 using namespace Spire;
 
@@ -9,13 +8,5 @@ int main(int argc, char** argv) {
   application.setOrganizationName(QObject::tr("Spire Trading Inc"));
   application.setApplicationName(QObject::tr("Scratch"));
   initialize_resources();
-  auto parent = QWidget();
-  for(auto i = 0; i != 1000; ++i) {
-    auto label =
-      new IntegerBox(std::make_shared<LocalOptionalIntegerModel>(123), &parent);
-    label->set_read_only(true);
-    apply_label_style(*label);
-  }
-  parent.show();
   application.exec();
 }
