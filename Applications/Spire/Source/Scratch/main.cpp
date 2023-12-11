30de800d99fe369f7223e0d149b6e2222e02b88d
Kamal <kamal@eidolonsystems.com>
2023-12-11 17:38:13 -0500

    The IsASelector has to potentially wait until the QEvent::Polish event has been sent to determine if an object is an instance of a given type. This is because objects can change type during construction and a lot of styling is done in a constructor, before an object has been fully constructed down to its most derived type.

--- Applications/Spire/Source/Scratch/main.cpp
+++ Applications/Spire/Source/Scratch/main.cpp
@@ -1,14 +1,22 @@
 #include <QApplication>
 #include "Spire/Spire/Resources.hpp"
+#include "Spire/Ui/Button.hpp"
 #include "Spire/Ui/IntegerBox.hpp"
 
 using namespace Spire;
+using namespace Spire::Styles;
 
 int main(int argc, char** argv) {
   auto application = QApplication(argc, argv);
   application.setOrganizationName(QObject::tr("Spire Trading Inc"));
   application.setApplicationName(QObject::tr("Scratch"));
   initialize_resources();
+  auto box = IntegerBox();
+  update_style(box, [&] (auto& style) {
+    style.get(Any() > is_a<Button>()).set(Visibility::NONE);
+  });
+  box.connect_submit_signal([] (auto) {});
+/*
   auto parent = QWidget();
   for(auto i = 0; i != 1000; ++i) {
     auto label =
@@ -17,5 +25,7 @@ int main(int argc, char** argv) {
     apply_label_style(*label);
   }
   parent.show();
+*/
+  box.show();
   application.exec();
 }
--- Applications/Spire/Source/Styles/IsASelector.cpp
+++ Applications/Spire/Source/Styles/IsASelector.cpp
@@ -1,4 +1,5 @@
 #include "Spire/Styles/IsASelector.hpp"
+#include <QEvent>
 #include "Spire/Styles/Stylist.hpp"
 
 using namespace Spire;
@@ -18,10 +19,39 @@ bool IsASelector::operator ==(const IsASelector& selector) const {
 
 SelectConnection Spire::Styles::select(const IsASelector& selector,
     const Stylist& base, const SelectionUpdateSignal& on_update) {
-  if(!base.get_pseudo_element() && selector.is_instance(base.get_widget())) {
+  if(base.get_pseudo_element()) {
+    return {};
+  } else if(selector.is_instance(base.get_widget())) {
     on_update({&base}, {});
+  } else if(base.get_widget().testAttribute(Qt::WA_WState_Polished)) {
+    return {};
   }
-  return {};
+  struct Executor : QObject {
+    IsASelector m_selector;
+    Stylist* m_stylist;
+    SelectionUpdateSignal m_on_update;
+
+    Executor(const IsASelector& selector, const Stylist& base,
+        const SelectionUpdateSignal& on_update)
+        : m_selector(selector),
+          m_stylist(&const_cast<Stylist&>(base)),
+          m_on_update(on_update) {
+      auto& widget = m_stylist->get_widget();
+      widget.installEventFilter(this);
+    }
+
+    bool eventFilter(QObject* watched, QEvent* event) override {
+      if(event->type() == QEvent::Polish) {
+        if(m_selector.is_instance(m_stylist->get_widget())) {
+          m_on_update({m_stylist}, {});
+        }
+        m_stylist->get_widget().removeEventFilter(this);
+      }
+      return QObject::eventFilter(watched, event);
+    }
+  };
+  return SelectConnection(
+    std::make_unique<Executor>(selector, base, on_update));
 }
 
 std::size_t std::hash<IsASelector>::operator ()(
