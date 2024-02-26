5c72d8056d920786234f8af94e779174cbeb6a10
Shaobin <shaobin@spiretrading.com>
2024-02-26 14:45:44 -0500

    Implemented the DeletableListItem.

---
+++ Applications/Spire/Include/Spire/Ui/DeletableListItem.hpp
@@ -0,0 +1,34 @@
+#ifndef SPIRE_DELETABLE_LIST_ITEM_HPP
+#define SPIRE_DELETABLE_LIST_ITEM_HPP
+#include "Spire/Ui/Ui.hpp"
+
+namespace Spire {
+  
+  /** Represents a deletable item in a list. */
+  class DeletableListItem : public QWidget {
+    public:
+
+      /** Signals that the item was being requested to delete. */
+      using DeleteSignal = Signal<void ()>;
+
+      /**
+       * Constructs a DeletableListItem.
+       * @param body The body of the item.
+       * @param parent The parent widget.
+       */
+      explicit DeletableListItem(QWidget& body, QWidget* parent = nullptr);
+
+      /** Returns the body of this item. */
+      QWidget& get_body();
+
+     /** Connects a slot to the delete signal. */
+      boost::signals2::connection connect_delete_signal(
+        const DeleteSignal::slot_type& slot) const;
+
+    private:
+      mutable DeleteSignal m_delete_signal;
+      QWidget* m_body;
+  };
+}
+
+#endif
