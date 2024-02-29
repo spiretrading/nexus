f0d22b624c1d4d1d53b0eedae7344439449e9cf6
Shaobin <shaobin@spiretrading.com>
2024-02-29 11:50:42 -0500

    Fixed indentation.

--- Applications/Spire/Include/Spire/KeyBindings/AddRegionForm.hpp
+++ Applications/Spire/Include/Spire/KeyBindings/AddRegionForm.hpp
@@ -11,23 +11,23 @@ namespace Spire {
   /** Displays a panel to add a region. */
   class AddRegionForm : public QWidget {
     public:
-    
+
       /**
        * Signals the submission of the region.
        * @param submission The region to be submitted.
        */
       using SubmitSignal = Signal<void (const Nexus::Region& submission)>;
 
-       /**
+      /**
        * Constructs an AddRegionForm.
        * @param regions A list of regions for the user to select.
        * @param parent The parent widget.
        */
       AddRegionForm(std::shared_ptr<RegionListModel> regions, QWidget& parent);
 
-      /** Returns A list of regions for the user to select. */
+      /** Returns a list of regions for the user to select. */
       const std::shared_ptr<RegionListModel>& get_regions() const;
-    
+
       /** Connects a slot to the submit signal. */
       boost::signals2::connection connect_submit_signal(
         const SubmitSignal::slot_type& slot) const;
