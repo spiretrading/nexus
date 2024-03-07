8644a2c5c609c47d2d12f57534cebde053120b5a
Kamal <kamal@eidolonsystems.com>
2024-03-07 16:38:11 -0500

    Updated the API for the QuantitySpinBox to remove dependence on LegacyKeyBindings.

--- Applications/Spire/Include/Spire/LegacyUI/QuantitySpinBox.hpp
+++ Applications/Spire/Include/Spire/LegacyUI/QuantitySpinBox.hpp
@@ -1,42 +1,37 @@
-#ifndef SPIRE_QUANTITYSPINBOX_HPP
-#define SPIRE_QUANTITYSPINBOX_HPP
+#ifndef SPIRE_QUANTITY_SPIN_BOX_HPP
+#define SPIRE_QUANTITY_SPIN_BOX_HPP
 #include <Beam/Pointers/Ref.hpp>
 #include <boost/optional/optional.hpp>
 #include <QSpinBox>
 #include "Nexus/Definitions/Security.hpp"
 #include "Spire/Canvas/Canvas.hpp"
-#include "Spire/LegacyKeyBindings/KeyBindings.hpp"
 #include "Spire/Spire/Spire.hpp"
 
-namespace Spire {
-namespace LegacyUI {
+namespace Spire::LegacyUI {
 
-  /*! \class QuantitySpinBox
-      \brief Displays a QSpinBox used to represent a Quantity.
-   */
+  /** Displays a QSpinBox used to represent a Quantity. */
   class QuantitySpinBox : public QSpinBox {
     public:
 
-      //! Constructs a QuantitySpinBox.
-      /*!
-        \param userProfile The user's profile.
-        \param node The IntegerNode this QuantitySpinBox represents.
-        \param parent The parent widget.
-      */
+      /**
+       * Constructs a QuantitySpinBox.
+       * @param userProfile The user's profile.
+       * @param node The IntegerNode this QuantitySpinBox represents.
+       * @param parent The parent widget.
+       */
       QuantitySpinBox(Beam::Ref<UserProfile> userProfile,
         const IntegerNode& node, QWidget* parent = nullptr);
 
     protected:
-      virtual void keyPressEvent(QKeyEvent* event);
-      virtual void keyReleaseEvent(QKeyEvent* event);
+      void keyPressEvent(QKeyEvent* event) override;
+      void keyReleaseEvent(QKeyEvent* event) override;
 
     private:
       UserProfile* m_userProfile;
       boost::optional<Nexus::Security> m_security;
 
-      void AdjustIncrement(KeyModifiers modifier);
+      void AdjustIncrement(Qt::KeyboardModifiers modifier);
   };
-}
 }
 
 #endif
