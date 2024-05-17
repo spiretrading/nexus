f6e2f5c50041bcb1c414b72f161d9de6144dc2c2
Shaobin <shaobin@spiretrading.com>
2024-05-17 12:19:13 -0400

    Added the TimeAndSaleUiTester to the Spire solution.

---
+++ Applications/Spire/Source/TimeAndSalesUiTester/main.cpp
@@ -0,0 +1,12 @@
+#include <QApplication>
+#include "Spire/Spire/Resources.hpp"
+
+using namespace Spire;
+
+int main(int argc, char** argv) {
+  auto application = QApplication(argc, argv);
+  application.setOrganizationName(QObject::tr("Spire Trading Inc"));
+  application.setApplicationName(QObject::tr("TimeAndSales Ui Tester"));
+  initialize_resources();
+  application.exec();
+}
