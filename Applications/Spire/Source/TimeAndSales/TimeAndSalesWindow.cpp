61a9dfac62a24e173bf5a9839812efb81e67a499
Shaobin <shaobin@spiretrading.com>
2024-05-24 15:26:00 -0400

    Removed the ResponsiveLabel from the TimeAndSalesWindow.

--- Applications/Spire/Source/TimeAndSales/TimeAndSalesWindow.cpp
+++ Applications/Spire/Source/TimeAndSales/TimeAndSalesWindow.cpp
@@ -14,7 +14,6 @@ using namespace Spire::Styles;
 
 namespace {
   const auto TITLE_NAME = QObject::tr("Time and Sales");
-  const auto TITLE_SHORT_NAME = QObject::tr("T&S");
 }
 
 TimeAndSalesWindow::TimeAndSalesWindow(
@@ -24,12 +23,7 @@ TimeAndSalesWindow::TimeAndSalesWindow(
     : Window(parent) {
   set_svg_icon(":/Icons/time-sales.svg");
   setWindowIcon(QIcon(":/Icons/taskbar_icons/time-sales.png"));
-  auto labels = std::make_shared<ArrayListModel<QString>>();
-  labels->push(TITLE_NAME);
-  labels->push(TITLE_SHORT_NAME);
-  m_responsive_title_label = new ResponsiveLabel(std::move(labels), this);
-  setWindowTitle(m_responsive_title_label->get_current()->get());
-  m_responsive_title_label->stackUnder(layout()->itemAt(0)->widget());
+  setWindowTitle(TITLE_NAME);
   m_transition_view = new TransitionView(nullptr);
   auto security_view =
     new SecurityView(std::move(securities), *m_transition_view);
@@ -41,20 +35,10 @@ TimeAndSalesWindow::TimeAndSalesWindow(
     style.get(Any()).set(BackgroundColor(QColor(0xFFFFFF)));
   });
   set_body(box);
-  resize(get_title_bar().sizeHint().width(), scale_height(361));
-}
-
-void TimeAndSalesWindow::resizeEvent(QResizeEvent* event) {
-  m_responsive_title_label->resize(get_title_bar().get_title_label().size());
-  setWindowTitle(m_responsive_title_label->get_current()->get());
-  Window::resizeEvent(event);
+  resize(sizeHint().width(), scale_height(361));
 }
 
 void TimeAndSalesWindow::on_current(const Security& security) {
-  auto prefix_name = to_text(security) + " " + QString(0x2013) + " ";
-  m_responsive_title_label->get_labels()->set(0, prefix_name + TITLE_NAME);
-  m_responsive_title_label->get_labels()->set(
-    1, prefix_name + TITLE_SHORT_NAME);
-  setWindowTitle(m_responsive_title_label->get_current()->get());
+  setWindowTitle(to_text(security) + " " + QString(0x2013) + " " + TITLE_NAME);
   m_transition_view->set_status(TransitionView::Status::LOADING);
 }
