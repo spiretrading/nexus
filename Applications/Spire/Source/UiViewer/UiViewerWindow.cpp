7ef67c2d2fd12e8e3a7b6226b83da44cb4b26063
Shaobin <shaobin@spiretrading.com>
2024-06-07 10:18:43 -0400

    Updated the TitleBar implementation.

--- Applications/Spire/Source/UiViewer/UiViewerWindow.cpp
+++ Applications/Spire/Source/UiViewer/UiViewerWindow.cpp
@@ -222,6 +222,7 @@ UiViewerWindow::UiViewerWindow(QWidget* parent)
   add(make_time_box_profile());
   add(make_time_in_force_box_profile());
   add(make_time_in_force_filter_panel_profile());
+  add(make_title_bar_profile());
   add(make_tooltip_profile());
   add(make_transition_view_profile());
   m_widget_list->setCurrentRow(0);
