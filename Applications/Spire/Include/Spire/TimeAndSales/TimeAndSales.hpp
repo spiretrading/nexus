b48122ba59e98f603d8d2858b067bbf84fc53160
Shaobin <shaobin@spiretrading.com>
2024-09-05 18:14:19 -0400

    Removed legacy time and sales related files.

--- Applications/Spire/Include/Spire/TimeAndSales/TimeAndSales.hpp
+++ Applications/Spire/Include/Spire/TimeAndSales/TimeAndSales.hpp
@@ -2,11 +2,6 @@
 #define SPIRE_TIMEANDSALES_HPP
 
 namespace Spire {
-  class TimeAndSalesModel;
-  class TimeAndSalesProperties;
-  class TimeAndSalesPropertiesDialog;
-  class TimeAndSalesWindow;
-  class TimeAndSalesWindowSettings;
 }
 
 #endif
