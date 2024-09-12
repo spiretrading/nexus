5be21c74f7565f388fff363b24f47bd1f0d698f7
Shaobin <shaobin@spiretrading.com>
2024-09-12 16:35:28 -0400

    Fixed the issue where the last table header item can't expand and fill the remaining space.

--- Applications/Spire/Source/Ui/TableView.cpp
+++ Applications/Spire/Source/Ui/TableView.cpp
@@ -72,14 +72,14 @@ TableView::TableView(
     }
   }
   m_header_view = new TableHeader(m_header);
-  m_header_view->setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);
+  m_header_view->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Fixed);
   m_header_view->setContentsMargins({scale_width(1), 0, 0, 0});
   link(*this, *m_header_view);
   for(auto i = 0; i < m_header->get_size(); ++i) {
     m_header_view->get_item(i)->installEventFilter(this);
   }
   auto box = new Box(m_header_view);
-  box->setSizePolicy(QSizePolicy::Fixed, QSizePolicy::Fixed);
+  box->setSizePolicy(QSizePolicy::MinimumExpanding, QSizePolicy::Fixed);
   update_style(*box, [] (auto& style) {
     style.get(Any()).set(BackgroundColor(QColor(0xFFFFFF)));
   });
