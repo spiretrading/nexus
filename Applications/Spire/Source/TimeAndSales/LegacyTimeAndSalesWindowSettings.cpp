349b53d03ae1bebff6f38e5e7c467539b02baed4
Kamal <kamal@eidolonsystems.com>
2024-10-17 14:37:08 -0400

    LegacyTimeAndSalesWindowSettings needs to offset the height and position of the TimeAndSalesWindow by the native Windows caption height since the TimeAndSalesWindow uses its own title bar.

--- Applications/Spire/Source/TimeAndSales/LegacyTimeAndSalesWindowSettings.cpp
+++ Applications/Spire/Source/TimeAndSales/LegacyTimeAndSalesWindowSettings.cpp
@@ -7,6 +7,13 @@ using namespace Beam;
 using namespace Nexus;
 using namespace Spire;
 
+namespace {
+  auto get_frame_height() {
+    return GetSystemMetrics(SM_CYCAPTION) + GetSystemMetrics(SM_CYFRAME) +
+      GetSystemMetrics(SM_CXPADDEDBORDER);
+  }
+}
+
 std::string LegacyTimeAndSalesWindowSettings::GetName() const {
   return m_name;
 }
@@ -25,6 +32,9 @@ void LegacyTimeAndSalesWindowSettings::Apply(Ref<UserProfile> userProfile,
     Out<QWidget> widget) const {
   auto& window = dynamic_cast<TimeAndSalesWindow&>(*widget);
   restore_geometry(window, m_geometry);
+  auto frame_height = get_frame_height();
+  window.move(window.x(), window.y() - frame_height);
+  window.resize(window.width(), window.height() + frame_height);
   if(m_security != Security()) {
     window.m_security_view->get_current()->set(m_security);
   }
