55dfceb3ad986c21bd20969873a921b93d338f20
Kamal <kamal@eidolonsystems.com>
2024-10-25 14:41:39 -0400

    Merge export_table_as_csv from time and sales branch.

--- Applications/Spire/Include/Spire/Spire/ExportTable.hpp
+++ Applications/Spire/Include/Spire/Spire/ExportTable.hpp
@@ -4,13 +4,15 @@
 #include "Spire/Spire/TableModel.hpp"
 
 namespace Spire {
-  
+
   /**
    * Exports a table model in CSV format.
    * @param table The table model to export.
+   * @param headers The table headers to export.
    * @param out The stream to output in CSV format.
    */
-  void export_table_as_csv(const TableModel& table, std::ostream& out);
+  void export_table_as_csv(const TableModel& table,
+    const std::vector<QString>& headers, std::ostream& out);
 
 }
 
