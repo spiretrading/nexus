3983582308bd2e6d5d0eb0b9dd34fc7090c3dd7c
Shaobin <shaobin@spiretrading.com>
2025-01-29 15:49:15 -0500

    Implemented the BookViewModel.

--- Applications/Spire/Include/Spire/BookView/BookView.hpp
+++ Applications/Spire/Include/Spire/BookView/BookView.hpp
@@ -7,6 +7,7 @@ namespace Spire {
   class BookViewHighlightPropertiesPage;
   class BookViewInteractionPropertiesPage;
   class BookViewLevelPropertiesPage;
+  class BookViewModel;
   struct BookViewProperties;
   class BookViewPropertiesWindow;
   class TechnicalsPanel;
