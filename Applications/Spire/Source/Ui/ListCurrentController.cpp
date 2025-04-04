0c203e1afa49a15b790a59b40dd40aec3a1bab77
Shaobin <shaobin@spiretrading.com>
2025-04-04 13:03:39 -0400

    Code cleanup.

--- Applications/Spire/Source/Ui/ListCurrentController.cpp
+++ Applications/Spire/Source/Ui/ListCurrentController.cpp
@@ -184,9 +184,7 @@ bool ListCurrentController::is_initialized() const {
 }
 
 void ListCurrentController::on_current(optional<int> current) {
-  if(!current) {
-    m_update_signal(current);
-  } else if(current && *current < m_size) {
+  if(!current || *current < m_size) {
     m_update_signal(current);
   }
 }
