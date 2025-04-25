61af7d5828eea6b11fb780cb6aa9315b653562bd
Kamal Mansouri <kamal@eidolonsystems.com>
2025-04-25 18:27:33 -0400

    Fixed an off-by-one error in ReversedListModel when a row is removed.

--- Applications/Spire/Source/SpireTester/ReversedListModelTester.cpp
+++ Applications/Spire/Source/SpireTester/ReversedListModelTester.cpp
@@ -1,6 +1,7 @@
 #include <doctest/doctest.h>
 #include "Spire/Spire/ArrayListModel.hpp"
 #include "Spire/Spire/ReversedListModel.hpp"
+#include "Spire/SpireTester/ListModelTester.hpp"
 
 using namespace boost;
 using namespace boost::signals2;
@@ -9,9 +10,9 @@ using namespace Spire;
 TEST_SUITE("ReversedListModel") {
   TEST_CASE("constructor") {
     auto source = std::make_shared<ArrayListModel<int>>();
-    source->insert(1, 0);
-    source->insert(2, 1);
-    source->insert(3, 2);
+    source->push(1);
+    source->push(2);
+    source->push(3);
     auto reverse = ReversedListModel(source);
     REQUIRE(reverse.get_size() == 3);
     REQUIRE(reverse.get(0) == 3);
@@ -37,5 +38,21 @@ TEST_SUITE("ReversedListModel") {
       REQUIRE(source->get(1) == 9);
       REQUIRE(reverse.get(1) == 9);
     }
+  }
+  TEST_CASE("remove_to_empty") {
+    auto source = std::make_shared<ArrayListModel<int>>();
+    source->push(123);
+    auto reverse = ReversedListModel(source);
+    auto operations = std::deque<ReversedListModel<int>::Operation>();
+    auto connection = scoped_connection(reverse.connect_operation_signal(
+      [&] (const auto& operation) {
+        operations.push_back(operation);
+      }));
+    reverse.remove(reverse.begin());
+    require_list_transaction<int>(operations,
+      {
+        ListModel<int>::PreRemoveOperation(0),
+        ListModel<int>::RemoveOperation(0)
+      });
   }
 }
