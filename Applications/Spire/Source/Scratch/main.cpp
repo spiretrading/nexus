a36db585d0915ffcad2b85b3a8b20754add229ca
Kamal <kamal@eidolonsystems.com>
2025-05-01 11:27:56 -0400

    Added prototype for the progress bar and time left message.

--- Applications/Spire/Source/Scratch/main.cpp
+++ Applications/Spire/Source/Scratch/main.cpp
@@ -40,6 +40,10 @@ struct ActivityMessageBox : QWidget {
         set(TextColor(QColor(0xFFFFFF)));
       style.get(StateStyle(State::DOWNLOADING) > Label()).
         set(PaddingLeft(ease(scale_width(76), 0, milliseconds(800))));
+      style.get(StateStyle(State::DOWNLOAD_COMPLETE) > Label()).
+        set(PaddingLeft(0));
+      style.get(StateStyle(State::INSTALLING) > Label()).
+        set(PaddingLeft(0));
     });
   }
 
@@ -47,15 +51,59 @@ struct ActivityMessageBox : QWidget {
     if(state == m_state) {
       return;
     }
+    unmatch(*this, StateStyle(m_state));
+    m_state = state;
     if(state == State::DOWNLOADING) {
       m_label->get_current()->set(tr("Downloading update..."));
-      match(*this, StateStyle(State::DOWNLOADING));
+    } else if(state == State::DOWNLOAD_COMPLETE) {
+      m_label->get_current()->set(tr("Download complete"));
+    } else if(state == State::INSTALLING) {
+      m_label->get_current()->set(tr("Installing update..."));
     }
+    match(*this, StateStyle(m_state));
+  }
+};
+
+struct ProgressBox : QWidget {
+  ProgressBox() {
+    auto box = new Box();
+    setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Fixed);
+    enclose(*this, *box);
+    proxy_style(*this, *box);
+    update_style(*this, [] (auto& style) {
+      style.get(Any()).
+        set(BackgroundColor(QColor(0x321471)));
+    });
+  }
+
+  QSize sizeHint() const {
+    return scale(1, 4);
+  }
+};
+
+struct TimeLeftBox : QWidget {
+  TextBox* m_label;
+
+  TimeLeftBox() {
+    setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Preferred);
+    auto layout = make_vbox_layout(this);
+    m_label = make_label(tr("About 5s left"));
+    layout->addWidget(m_label);
+    update_style(*m_label, [] (auto& style) {
+      auto font = QFont("Roboto");
+      font.setWeight(QFont::Medium);
+      font.setPixelSize(scale_width(10));
+      style.get(Any()).
+        set(text_style(font, QColor(0xE2E0FF))).
+        set(PaddingTop(scale_height(4)));
+    });
   }
 };
 
 struct UpdaterBox : QWidget {
   ActivityMessageBox* m_activity_message_box;
+  ProgressBox* m_progress_box;
+  TimeLeftBox* m_time_left_box;
 
   UpdaterBox() {
     auto body = new QWidget();
@@ -63,6 +111,12 @@ struct UpdaterBox : QWidget {
     layout->addSpacing(scale_height(38));
     m_activity_message_box = new ActivityMessageBox();
     layout->addWidget(m_activity_message_box);
+    m_progress_box = new ProgressBox();
+    m_progress_box->setSizePolicy(
+      QSizePolicy::Expanding, m_progress_box->sizePolicy().verticalPolicy());
+    layout->addWidget(m_progress_box);
+    m_time_left_box = new TimeLeftBox();
+    layout->addWidget(m_time_left_box);
     auto box = new Box(body);
     enclose(*this, *box);
     proxy_style(*this, *box);
@@ -83,6 +137,14 @@ int main(int argc, char** argv) {
   QTimer::singleShot(1000, [&] {
     window.m_activity_message_box->set_state(
       ActivityMessageBox::State::DOWNLOADING);
+    QTimer::singleShot(2000, [&] {
+      window.m_activity_message_box->set_state(
+        ActivityMessageBox::State::DOWNLOAD_COMPLETE);
+      QTimer::singleShot(2000, [&] {
+        window.m_activity_message_box->set_state(
+          ActivityMessageBox::State::INSTALLING);
+      });
+    });
   });
   application.exec();
 }
