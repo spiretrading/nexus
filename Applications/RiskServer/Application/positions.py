fc54b7e3bf0bac39036fdf5e1da00cacd4f29869
Kamal <kamal@eidolonsystems.com>
2025-06-18 15:45:21 -0400

    Added scripts to get all open positions and to automatically MOE positions. Reimplemented subset testing for Region, with additional unit tests.

---
+++ Applications/RiskServer/Application/positions.py
@@ -0,0 +1,83 @@
+import argparse
+import csv
+import sys
+
+import beam
+import nexus
+import yaml
+
+def report_positions(service_clients, account, markets, currencies, writer):
+  snapshot = service_clients.get_risk_client().load_inventory_snapshot(account)
+  portfolio, sequence, excluded_orders = nexus.risk_service.make_portfolio(
+    snapshot, account, markets, service_clients.get_order_execution_client())
+  for order in excluded_orders:
+    execution_reports = order.get_publisher().get_snapshot()
+    if execution_reports is not None:
+      for execution_report in execution_reports:
+        portfolio.update(order.info.fields, execution_report)
+  for security in portfolio.security_entries:
+    currency = markets.from_code(security.market).currency
+    inventory = portfolio.bookkeeper.get_inventory(security, currency)
+    position = inventory.position
+    if nexus.accounting.side(position) == nexus.Side.BID:
+      side_code = 'Long'
+    else:
+      side_code = 'Short'
+    writer.writerow([account.name, str(security),
+      currencies.from_id(currency).code, side_code, position.quantity,
+      position.cost_basis])
+
+def report_yaml_error(error):
+  if hasattr(error, 'problem_mark'):
+    sys.stderr.write('Invalid YAML at line %s, column %s: %s\n' %
+      (error.problem_mark.line, error.problem_mark.column, error.problem))
+  else:
+    sys.stderr.write('Invalid YAML provided\n')
+
+def parse_ip_address(source):
+  separator = source.find(':')
+  if separator == -1:
+    return beam.network.IpAddress(source, 0)
+  return beam.network.IpAddress(source[:separator], int(source[separator + 1:]))
+
+def main():
+  parser = argparse.ArgumentParser(
+    description='v1.0 Copyright (C) 2025 Spire Trading Inc.')
+  parser.add_argument('-c', '--config', type=str, help='Configuration file',
+    default='config.yml')
+  parser.add_argument('-a', '--account', type=str, help='Account name.')
+  args = parser.parse_args()
+  try:
+    stream = open(args.config, 'r').read()
+    config = yaml.load(stream, yaml.SafeLoader)
+  except IOError:
+    sys.stderr.write(f'{args.config} not found\n')
+    exit(1)
+  except yaml.YAMLError as e:
+    report_yaml_error(e)
+    exit(1)
+  section = config['service_locator']
+  address = parse_ip_address(section['address'])
+  username = section['username']
+  password = section['password']
+  service_clients = nexus.ApplicationServiceClients(username, password, address)
+  csv_writer = csv.writer(sys.stdout, quoting=csv.QUOTE_NONNUMERIC)
+  csv_writer.writerow(["Account", "Security", "Currency", "Side",
+    "Open Quantity", "Cost Basis"])
+  markets = service_clients.get_definitions_client().load_market_database()
+  currencies = service_clients.get_definitions_client().load_currency_database()
+  if args.account:
+    account = service_clients.get_service_locator_client().find_account(
+      args.account)
+    if not account:
+      print(f'Account {args.account} not found.')
+      return
+    report_positions(service_clients, account, markets, currencies, csv_writer)
+  else:
+    for account in \
+        service_clients.get_service_locator_client().load_all_accounts():
+      report_positions(service_clients, account, markets, currencies,
+        csv_writer)
+
+if __name__ == '__main__':
+  main()
--- Nexus/Source/DefinitionsTests/RegionTester.cpp
+++ Nexus/Source/DefinitionsTests/RegionTester.cpp
@@ -68,4 +68,91 @@ TEST_SUITE("Region") {
     TestProperSubset(us, northAmerica);
     TestProperSubset(ca, northAmerica);
   }
+
+  TEST_CASE("empty_regions_are_equal_and_subsets") {
+    auto empty1 = Region();
+    auto empty2 = Region("");
+    REQUIRE(empty1 == empty2);
+    REQUIRE(empty1 <= empty2);
+    REQUIRE(empty1 >= empty2);
+    REQUIRE(!(empty1 < empty2));
+    REQUIRE(!(empty1 > empty2));
+  }
+
+  TEST_CASE("named_and_unnamed_region_equality") {
+    auto r1 = Region(DefaultCountries::US());
+    auto r2 = Region(DefaultCountries::US());
+    r2.SetName("US Region");
+    REQUIRE(r1 == r2);
+    REQUIRE(r1 <= r2);
+    REQUIRE(r1 >= r2);
+    REQUIRE(!(r1 < r2));
+    REQUIRE(!(r1 > r2));
+  }
+
+  TEST_CASE("global_region_superset_of_all") {
+    auto country = Region(DefaultCountries::US());
+    auto global = Region::Global();
+    auto namedGlobal = Region::Global("All Markets");
+    TestProperSubset(country, global);
+    TestProperSubset(country, namedGlobal);
+    REQUIRE(global == namedGlobal);
+    REQUIRE(global <= namedGlobal);
+    REQUIRE(global >= namedGlobal);
+    REQUIRE(!(global < namedGlobal));
+    REQUIRE(!(global > namedGlobal));
+  }
+
+  TEST_CASE("distinct_markets") {
+    auto nasdaq = Region(
+      GetDefaultMarketDatabase().FromCode(DefaultMarkets::NASDAQ()));
+    auto nyse = Region(
+      GetDefaultMarketDatabase().FromCode(DefaultMarkets::NYSE()));
+    TestDistinctSets(nasdaq, nyse);
+  }
+
+  TEST_CASE("market_entry_constructor_equivalence") {
+    auto fromCodes = Region(DefaultMarkets::NASDAQ(), DefaultCountries::US());
+    auto fromEntry = Region(
+      GetDefaultMarketDatabase().FromCode(DefaultMarkets::NASDAQ()));
+    REQUIRE(fromCodes == fromEntry);
+    REQUIRE(fromCodes <= fromEntry);
+  }
+
+  TEST_CASE("empty_subset_of_non_empty") {
+    auto empty = Region();
+    auto country = Region(DefaultCountries::US());
+    TestProperSubset(empty, country);
+  }
+
+  TEST_CASE("security_in_union_region") {
+    auto us = Region(DefaultCountries::US());
+    auto ca = Region(DefaultCountries::CA());
+    auto unionRegion = us + ca;
+    auto security =
+      Security("TST", DefaultMarkets::NASDAQ(), DefaultCountries::US());
+    REQUIRE(security <= unionRegion);
+    REQUIRE(security < unionRegion);
+    REQUIRE(unionRegion >= security);
+    REQUIRE(unionRegion > security);
+  }
+
+  TEST_CASE("combine_regions_operator_plus_and_plus_eq") {
+    auto us = Region(DefaultCountries::US());
+    auto ca = Region(DefaultCountries::CA());
+    auto combined = us;
+    combined += ca;
+    auto plusCombined = us + ca;
+    REQUIRE(combined == plusCombined);
+    TestProperSubset(us, combined);
+    TestProperSubset(ca, combined);
+  }
+
+  TEST_CASE("foo") {
+    auto ca = Region(DefaultCountries::CA());
+    auto ry = ParseSecurity("RY.TSX");
+    auto foo = Region(ry);
+    REQUIRE(ry <= ca);
+    REQUIRE(foo <= ca);
+  }
 }
