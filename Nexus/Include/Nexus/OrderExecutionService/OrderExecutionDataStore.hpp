389ded0ccd5e4c55963337cd12e414e7747d2217
Kamal <kamal@eidolonsystems.com>
2025-07-17 16:35:00 -0400

    Reimplemented the OrderExecutionDataStoreBox.

--- Nexus/Include/Nexus/OrderExecutionService/OrderExecutionDataStore.hpp
+++ Nexus/Include/Nexus/OrderExecutionService/OrderExecutionDataStore.hpp
@@ -1,55 +1,253 @@
 #ifndef NEXUS_ORDER_EXECUTION_DATA_STORE_HPP
 #define NEXUS_ORDER_EXECUTION_DATA_STORE_HPP
-#include <vector>
-#include <Beam/IO/Connection.hpp>
-#include <Beam/Utilities/Concept.hpp>
-#include "Nexus/OrderExecutionService/AccountOrderSubmissionEntry.hpp"
+#include <memory>
+#include <utility>
+#include <Beam/Pointers/LocalPtr.hpp>
+#include <boost/optional/optional.hpp>
 #include "Nexus/OrderExecutionService/AccountQuery.hpp"
-#include "Nexus/OrderExecutionService/OrderExecutionService.hpp"
 
 namespace Nexus::OrderExecutionService {
 
-  /** Base class used to store Order execution data. */
-  struct OrderExecutionDataStore : Beam::Concept<OrderExecutionDataStore> {
+  /** Provides a generic interface over an arbitrary OrderExecutionDataStore. */
+  class OrderExecutionDataStore {
+    public:
 
-    /**
-     * Loads an OrderRecord from its id.
-     * @param id The Order id to load.
-     * @return The OrderRecord with the given <i>id</i> if it exists.
-     */
-    boost::optional<SequencedOrderRecord> LoadOrder(OrderId id);
+      /**
+       * Constructs an OrderExecutionDataStore of a specified type using
+       * emplacement.
+       * @param <T> The type of data store to emplace.
+       * @param args The arguments to pass to the emplaced data store.
+       */
+      template<typename T, typename... Args>
+      explicit OrderExecutionDataStore(std::in_place_type_t<T>, Args&&... args);
 
-    /**
-     * Executes an Order submission query.
-     * @param query The search query to execute.
-     * @return The list of SequencedOrderRecords satisfying the <i>query</i>.
-     */
-    std::vector<SequencedOrderRecord> LoadOrderSubmissions(
-      const AccountQuery& query);
+      /**
+       * Constructs an OrderExecutionDataStore by copying an existing data
+       * store.
+       * @param client The client to copy.
+       */
+      template<typename DataStore>
+      explicit OrderExecutionDataStore(DataStore dataStore);
 
-    /**
-     * Executes an ExecutionReport query.
-     * @param query The search query to execute.
-     * @return The list of SequencedExecutionReports satisfying the
-     *         <i>query</i>.
-     */
-    std::vector<SequencedExecutionReport> LoadExecutionReports(
-      const AccountQuery& query);
+      explicit OrderExecutionDataStore(
+        OrderExecutionDataStore* dataStore);
 
-    /**
-     * Stores a SequencedAccountOrderInfo.
-     * @param orderInfo The SequencedAccountOrderInfo to store.
-     */
-    void Store(const SequencedAccountOrderInfo& orderInfo);
+      explicit OrderExecutionDataStore(
+        const std::shared_ptr<OrderExecutionDataStore>& dataStore);
 
-    /**
-     * Stores a SequencedAccountExecutionReport.
-     * @param executionReport The SequencedAccountExecutionReport to store.
-     */
-    void Store(const SequencedAccountExecutionReport& executionReport);
+      explicit OrderExecutionDataStore(
+        const std::unique_ptr<OrderExecutionDataStore>& dataStore);
 
-    void Close();
+      /**
+       * Loads an OrderRecord from its id.
+       * @param id The Order id to load.
+       * @return The OrderRecord with the given <i>id</i> if it exists.
+       */
+      boost::optional<SequencedAccountOrderRecord>
+        load_order_record(OrderId id);
+
+      /**
+       * Executes a query to load OrderRecords.
+       * @param query The query to execute.
+       * @return The list of SequencedOrderRecords satisfying the <i>query</i>.
+       */
+      std::vector<SequencedOrderRecord>
+        load_order_records(const AccountQuery& query);
+
+      /**
+       * Stores a SequencedAccountOrderInfo.
+       * @param info The SequencedAccountOrderInfo to store.
+       */
+      void store(const SequencedAccountOrderInfo& info);
+
+      /**
+       * Stores a list of SequencedAccountOrderInfo entries.
+       * @param info The list of SequencedAccountOrderInfo entries to store.
+       */
+      void store(const std::vector<SequencedAccountOrderInfo>& info);
+
+      /**
+       * Executes a query to load ExecutionReports.
+       * @param query The search query to execute.
+       * @return The list of SequencedExecutionReports satisfying the
+       *         <i>query</i>.
+       */
+      std::vector<SequencedExecutionReport>
+        load_execution_reports(const AccountQuery& query);
+
+      /**
+       * Stores a SequencedAccountExecutionReport.
+       * @param report The SequencedAccountExecutionReport to store.
+       */
+      void store(const SequencedAccountExecutionReport& report);
+
+      /**
+       * Stores a list of SequencedAccountExecutionReports.
+       * @param reports The list of SequencedAccountExecutionReports to store.
+       */
+      void store(const std::vector<SequencedAccountExecutionReport>& reports);
+
+      void close();
+
+    private:
+      struct VirtualOrderExecutionDataStore {
+        virtual ~VirtualOrderExecutionDataStore() = default;
+        virtual boost::optional<SequencedAccountOrderRecord>
+          load_order_record(OrderId) = 0;
+        virtual std::vector<SequencedOrderRecord>
+          load_order_records(const AccountQuery&) = 0;
+        virtual void store(const SequencedAccountOrderInfo&) = 0;
+        virtual void store(const std::vector<SequencedAccountOrderInfo>&) = 0;
+        virtual std::vector<SequencedExecutionReport>
+          load_execution_reports(const AccountQuery&) = 0;
+        virtual void store(const SequencedAccountExecutionReport&) = 0;
+        virtual void store(
+          const std::vector<SequencedAccountExecutionReport>&) = 0;
+        virtual void close() = 0;
+      };
+      template<typename D>
+      struct WrappedOrderExecutionDataStore final :
+          VirtualOrderExecutionDataStore {
+        using OrderExecutionDataStore = D;
+        Beam::GetOptionalLocalPtr<OrderExecutionDataStore> m_data_store;
+
+        template<typename... Args>
+        WrappedOrderExecutionDataStore(Args&&... args);
+        boost::optional<SequencedAccountOrderRecord>
+          load_order_record(OrderId) override;
+        std::vector<SequencedOrderRecord>
+          load_order_records(const AccountQuery&) override;
+        void store(const SequencedAccountOrderInfo&) override;
+        void store(const std::vector<SequencedAccountOrderInfo>&) override;
+        std::vector<SequencedExecutionReport>
+          load_execution_reports(const AccountQuery&) override;
+        void store(const SequencedAccountExecutionReport&) override;
+        void store(
+          const std::vector<SequencedAccountExecutionReport>&) override;
+        void close() override;
+      };
+      std::shared_ptr<VirtualOrderExecutionDataStore> m_data_store;
   };
+
+  template<typename T, typename... Args>
+  OrderExecutionDataStore::OrderExecutionDataStore(
+    std::in_place_type_t<T>, Args&&... args)
+    : m_data_store(std::make_shared<WrappedOrderExecutionDataStore<T>>(
+        std::forward<Args>(args)...)) {}
+
+  template<typename DataStore>
+  OrderExecutionDataStore::OrderExecutionDataStore(DataStore dataStore)
+    : OrderExecutionDataStore(
+        std::in_place_type<DataStore>, std::move(dataStore)) {}
+
+  inline OrderExecutionDataStore::OrderExecutionDataStore(
+    OrderExecutionDataStore* dataStore)
+    : OrderExecutionDataStore(*dataStore) {}
+
+  inline OrderExecutionDataStore::OrderExecutionDataStore(
+    const std::shared_ptr<OrderExecutionDataStore>& dataStore)
+    : OrderExecutionDataStore(*dataStore) {}
+
+  inline OrderExecutionDataStore::OrderExecutionDataStore(
+    const std::unique_ptr<OrderExecutionDataStore>& dataStore)
+    : OrderExecutionDataStore(*dataStore) {}
+
+  inline boost::optional<SequencedAccountOrderRecord>
+      OrderExecutionDataStore::load_order_record(OrderId id) {
+    return m_data_store->load_order_record(id);
+  }
+
+  inline std::vector<SequencedOrderRecord>
+      OrderExecutionDataStore::load_order_records(
+        const AccountQuery& query) {
+    return m_data_store->load_order_records(query);
+  }
+
+  inline void OrderExecutionDataStore::store(
+      const SequencedAccountOrderInfo& info) {
+    m_data_store->store(info);
+  }
+
+  inline void OrderExecutionDataStore::store(
+      const std::vector<SequencedAccountOrderInfo>& info) {
+    m_data_store->store(info);
+  }
+
+  inline std::vector<SequencedExecutionReport>
+      OrderExecutionDataStore::load_execution_reports(
+        const AccountQuery& query) {
+    return m_data_store->load_execution_reports(query);
+  }
+
+  inline void OrderExecutionDataStore::store(
+      const SequencedAccountExecutionReport& report) {
+    m_data_store->store(report);
+  }
+
+  inline void OrderExecutionDataStore::store(
+      const std::vector<SequencedAccountExecutionReport>& reports) {
+    m_data_store->store(reports);
+  }
+
+  inline void OrderExecutionDataStore::close() {
+    m_data_store->close();
+  }
+
+  template<typename D>
+  template<typename... Args>
+  OrderExecutionDataStore::WrappedOrderExecutionDataStore<D>::
+    WrappedOrderExecutionDataStore(Args&&... args)
+    : m_data_store(std::forward<Args>(args)...) {}
+
+  template<typename D>
+  boost::optional<SequencedAccountOrderRecord>
+      OrderExecutionDataStore::WrappedOrderExecutionDataStore<D>::
+        load_order_record(OrderId id) {
+    return m_data_store->load_order_record(id);
+  }
+
+  template<typename D>
+  std::vector<SequencedOrderRecord>
+      OrderExecutionDataStore::WrappedOrderExecutionDataStore<D>::
+        load_order_records(const AccountQuery& query) {
+    return m_data_store->load_order_records(query);
+  }
+
+  template<typename D>
+  void OrderExecutionDataStore::WrappedOrderExecutionDataStore<D>::store(
+      const SequencedAccountOrderInfo& info) {
+    m_data_store->store(info);
+  }
+
+  template<typename D>
+  void OrderExecutionDataStore::WrappedOrderExecutionDataStore<D>::store(
+      const std::vector<SequencedAccountOrderInfo>& info) {
+    m_data_store->store(info);
+  }
+
+  template<typename D>
+  std::vector<SequencedExecutionReport>
+      OrderExecutionDataStore::WrappedOrderExecutionDataStore<D>::
+        load_execution_reports(const AccountQuery& query) {
+    return m_data_store->load_execution_reports(query);
+  }
+
+  template<typename D>
+  void OrderExecutionDataStore::WrappedOrderExecutionDataStore<D>::store(
+      const SequencedAccountExecutionReport& report) {
+    m_data_store->store(report);
+  }
+
+  template<typename D>
+  void OrderExecutionDataStore::WrappedOrderExecutionDataStore<D>::store(
+      const std::vector<SequencedAccountExecutionReport>& reports) {
+    m_data_store->store(reports);
+  }
+
+  template<typename D>
+  void OrderExecutionDataStore::WrappedOrderExecutionDataStore<D>::close() {
+    m_data_store->close();
+  }
 }
 
 #endif
