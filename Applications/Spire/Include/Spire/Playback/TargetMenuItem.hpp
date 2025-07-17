a520ebe5ee01532bc5adbb6dbc03de4ccc1b216b
Shaobin <shaobin@spiretrading.com>
2025-07-17 17:15:00 -0400

    Implemented the TargetMenuItem.

---
+++ Applications/Spire/Include/Spire/Playback/TargetMenuItem.hpp
@@ -0,0 +1,63 @@
+#ifndef SPIRE_TARGET_MENU_ITEM_HPP
+#define SPIRE_TARGET_MENU_ITEM_HPP
+#include "Nexus/Definitions/Security.hpp"
+#include "Spire/Playback/Playback.hpp"
+#include "Spire/Toolbar/ToolbarWindow.hpp"
+#include "Spire/Ui/ClickObserver.hpp"
+
+namespace Spire {
+
+  /** Represents a menu item in the replay attach menu. */
+  class TargetMenuItem : public QWidget {
+    public:
+
+      /** Represents a window group target available for replay attachment. */
+      struct Target {
+
+        /** The group’s associated color, or null if uncolored. */
+        boost::optional<QColor> m_color;
+
+        /** The assigned security, or null if unassigned. */
+        boost::optional<Nexus::Security> m_security;
+
+        /** The window types in the group. */
+        std::vector<ToolbarWindow::WindowType> m_window_types;
+
+        auto operator <=>(const Target&) const = default;
+      };
+
+      /**
+       * Constructs a TargetMenuItem with the default current.
+       * @param target The target of the replay attachment.
+       * @param parent The parent widget.
+       */
+      explicit TargetMenuItem(Target target, QWidget* parent = nullptr);
+
+      /**
+       * Constructs a TargetMenuItem.
+       * @param target The replay attachment target that the item represents.
+       * @param current Whether the item is checked.
+       * @param parent The parent widget.
+       */
+      TargetMenuItem(Target target, std::shared_ptr<BooleanModel> current,
+        QWidget* parent = nullptr);
+
+      /** Returns the target. */
+      const Target& get_target() const;
+
+      /** Returns the current value, <code>true</code> iff item is checked. */
+      const std::shared_ptr<BooleanModel>& get_current() const;
+
+    private:
+      std::shared_ptr<BooleanModel> m_current;
+      Target m_target;
+      ClickObserver m_click_observer;
+
+      void on_click();
+  };
+
+  /** Returns the text representation of a Target. */ 
+  QString to_text(const TargetMenuItem::Target& target);
+}
+
+#endif
