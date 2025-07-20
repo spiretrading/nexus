2ada55e8055e7c69e4c95153b2b01b56a1dcabb0
Kamal Mansouri <kamal@eidolonsystems.com>
2025-07-20 13:59:21 -0400

    Reimplemented the TrueAverageBookkeeper.

--- Nexus/Include/Nexus/Accounting/Bookkeeper.hpp
+++ Nexus/Include/Nexus/Accounting/Bookkeeper.hpp
@@ -1,7 +1,13 @@
 #ifndef NEXUS_BOOKKEEPER_HPP
 #define NEXUS_BOOKKEEPER_HPP
-#include "Nexus/Accounting/Accounting.hpp"
+#include <memory>
+#include <type_traits>
+#include <utility>
+#include <Beam/Collections/View.hpp>
 #include "Nexus/Accounting/Inventory.hpp"
+#include "Nexus/Definitions/Currency.hpp"
+#include "Nexus/Definitions/Money.hpp"
+#include "Nexus/Definitions/Quantity.hpp"
 
 namespace Nexus::Accounting {
 
@@ -21,7 +27,188 @@ namespace Nexus::Accounting {
 
       /** The key used to lookup Inventories. */
       using Key = typename Inventory::Position::Key;
+
+      /**
+       * Constructs a Bookkeeper of a specified type using emplacement.
+       * @param <T> The type of bookkeeper to emplace.
+       * @param args The arguments to pass to the emplaced bookkeeper.
+       */
+      template<typename T, typename... Args>
+      explicit Bookkeeper(std::in_place_type_t<T>, Args&&... args);
+
+      /**
+       * Constructs a Bookkeeper by copying an existing bookkeeper.
+       * @param bookkeeper The bookkeeper to copy.
+       */
+      template<typename B,
+        typename = std::enable_if_t<
+          !std::is_same_v<std::decay_t<B>, Bookkeeper>>>
+      explicit Bookkeeper(B&& bookkeeper);
+
+      Bookkeeper(const Bookkeeper& bookkeeper);
+
+      /**
+       * Records a transaction.
+       * @param index The index of the position to record the transaction for.
+       * @param currency The currency of the transaction.
+       * @param quantity The quantity to transact.
+       * @param cost_basis The cost basis of the transaction.
+       * @param fees Any fees incurred from the transaction.
+       */
+      void record(const Index& index, CurrencyId currency,
+        Quantity quantity, Money cost_basis, Money fees);
+
+      /**
+       * Returns the Inventory for a given index and currency.
+       * @param index The index of the Inventory to retrieve.
+       * @param currency The currency of the Inventory to retrieve.
+       * @return The Inventory for the specified index and currency.
+       * @throws std::out_of_range If no inventory is found.
+       */
+      const Inventory& get_inventory(
+        const Index& index, CurrencyId currency) const;
+
+      /**
+       * Returns the total Inventory for a given currency.
+       * @param currency The currency of the total Inventory to retrieve.
+       * @return The total Inventory for the specified currency.
+       * @throws std::out_of_range If no inventory is found for the currency.
+       */
+      const Inventory& get_total(CurrencyId currency) const;
+
+      /** Returns a view of all inventories. */
+      Beam::View<const Inventory> get_inventory_range() const;
+
+      /** Returns a view of all total inventories. */
+      Beam::View<const Inventory> get_totals_range() const;
+
+    private:
+      struct VirtualBookkeeper {
+        virtual ~VirtualBookkeeper() = default;
+        virtual std::unique_ptr<VirtualBookkeeper> clone() const = 0;
+        virtual void record(const Index& index, CurrencyId currency,
+          Quantity quantity, Money cost_basis, Money fees) = 0;
+        virtual const Inventory& get_inventory(
+          const Index& index, CurrencyId currency) const = 0;
+        virtual const Inventory& get_total(CurrencyId currency) const = 0;
+        virtual Beam::View<const Inventory> get_inventory_range() const = 0;
+        virtual Beam::View<const Inventory> get_totals_range() const = 0;
+      };
+
+      template<typename B>
+      struct WrappedBookkeeper final : VirtualBookkeeper {
+        using Bookkeeper = B;
+        Beam::GetOptionalLocalPtr<Bookkeeper> m_bookkeeper;
+
+        template<typename... Args>
+        WrappedBookkeeper(Args&&... args);
+        std::unique_ptr<VirtualBookkeeper> clone() const override;
+        void record(const Index& index, CurrencyId currency,
+          Quantity quantity, Money cost_basis, Money fees) override;
+        const Inventory& get_inventory(
+          const Index& index, CurrencyId currency) const override;
+        const Inventory& get_total(CurrencyId currency) const override;
+        Beam::View<const Inventory> get_inventory_range() const override;
+        Beam::View<const Inventory> get_totals_range() const override;
+      };
+      std::unique_ptr<VirtualBookkeeper> m_bookkeeper;
   };
+
+  template<typename I>
+  template<typename T, typename... Args>
+  Bookkeeper<I>::Bookkeeper(std::in_place_type_t<T>, Args&&... args)
+    : m_bookkeeper(
+        std::make_unique<WrappedBookkeeper<T>>(std::forward<Args>(args)...)) {}
+
+  template<typename I>
+  template<typename B, typename>
+  Bookkeeper<I>::Bookkeeper(B&& bookkeeper)
+    : Bookkeeper(
+        std::in_place_type<std::decay_t<B>>, std::forward<B>(bookkeeper)) {}
+
+  template<typename I>
+  Bookkeeper<I>::Bookkeeper(const Bookkeeper& bookkeeper)
+    : m_bookkeeper(bookkeeper.m_bookkeeper->clone()) {}
+
+  template<typename I>
+  void Bookkeeper<I>::record(const Index& index, CurrencyId currency,
+      Quantity quantity, Money cost_basis, Money fees) {
+    m_bookkeeper->record(index, currency, quantity, cost_basis, fees);
+  }
+
+  template<typename I>
+  const typename Bookkeeper<I>::Inventory& Bookkeeper<I>::get_inventory(
+      const Index& index, CurrencyId currency) const {
+    return m_bookkeeper->get_inventory(index, currency);
+  }
+
+  template<typename I>
+  const typename Bookkeeper<I>::Inventory& Bookkeeper<I>::get_total(
+      CurrencyId currency) const {
+    return m_bookkeeper->get_total(currency);
+  }
+
+  template<typename I>
+  Beam::View<const typename Bookkeeper<I>::Inventory>
+      Bookkeeper<I>::get_inventory_range() const {
+    return m_bookkeeper->get_inventory_range();
+  }
+
+  template<typename I>
+  Beam::View<const typename Bookkeeper<I>::Inventory>
+      Bookkeeper<I>::get_totals_range() const {
+    return m_bookkeeper->get_totals_range();
+  }
+
+  template<typename I>
+  template<typename B>
+  template<typename... Args>
+  Bookkeeper<I>::WrappedBookkeeper<B>::WrappedBookkeeper(Args&&... args)
+    : m_bookkeeper(std::forward<Args>(args)...) {}
+
+  template<typename I>
+  template<typename B>
+  std::unique_ptr<typename Bookkeeper<I>::VirtualBookkeeper>
+      Bookkeeper<I>::WrappedBookkeeper<B>::clone() const {
+    return std::make_unique<WrappedBookkeeper<B>>(*m_bookkeeper);
+  }
+
+  template<typename I>
+  template<typename B>
+  void Bookkeeper<I>::WrappedBookkeeper<B>::record(const Index& index,
+      CurrencyId currency, Quantity quantity, Money cost_basis, Money fees) {
+    m_bookkeeper->record(index, currency, quantity, cost_basis, fees);
+  }
+
+  template<typename I>
+  template<typename B>
+  const typename Bookkeeper<I>::Inventory&
+      Bookkeeper<I>::WrappedBookkeeper<B>::get_inventory(const Index& index,
+        CurrencyId currency) const {
+    return m_bookkeeper->get_inventory(index, currency);
+  }
+
+  template<typename I>
+  template<typename B>
+  const typename Bookkeeper<I>::Inventory&
+      Bookkeeper<I>::WrappedBookkeeper<B>::get_total(
+        CurrencyId currency) const {
+    return m_bookkeeper->get_total(currency);
+  }
+
+  template<typename I>
+  template<typename B>
+  Beam::View<const typename Bookkeeper<I>::Inventory>
+      Bookkeeper<I>::WrappedBookkeeper<B>::get_inventory_range() const {
+    return m_bookkeeper->get_inventory_range();
+  }
+
+  template<typename I>
+  template<typename B>
+  Beam::View<const typename Bookkeeper<I>::Inventory>
+      Bookkeeper<I>::WrappedBookkeeper<B>::get_totals_range() const {
+    return m_bookkeeper->get_totals_range();
+  }
 }
 
 #endif
