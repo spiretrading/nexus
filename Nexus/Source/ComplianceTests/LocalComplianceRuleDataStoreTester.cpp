126d5a29794f29cf6801b04f234a3dce37cabc38
Kamal <kamal@eidolonsystems.com>
2025-07-25 16:24:08 -0400

    Reimplemented the LocalComplianceRuleDataStore with unit tests.

---
+++ Nexus/Source/ComplianceTests/LocalComplianceRuleDataStoreTester.cpp
@@ -0,0 +1,19 @@
+#include <doctest/doctest.h>
+#include "Nexus/Compliance/LocalComplianceRuleDataStore.hpp"
+#include "Nexus/ComplianceTests/ComplianceRuleDataStoreTestSuite.hpp"
+
+using namespace Nexus;
+using namespace Nexus::Compliance;
+using namespace Nexus::Compliance::Tests;
+
+namespace {
+  struct Builder {
+    auto operator ()() const {
+      return LocalComplianceRuleDataStore();
+    }
+  };
+}
+
+TEST_SUITE("LocalComplianceRuleDataStore") {
+  TEST_CASE_TEMPLATE_INVOKE(ComplianceRuleDataStoreTestSuite, Builder);
+}
