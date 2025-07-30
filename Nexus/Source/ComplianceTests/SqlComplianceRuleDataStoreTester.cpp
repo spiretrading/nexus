582c3ff5abd0d75089ebd77ca46d459b06dfb30a
Kamal Mansouri <kamal@eidolonsystems.com>
2025-07-30 12:30:41 -0400

    Reimplemented the SqlComplianceRuleDataStore.

---
+++ Nexus/Source/ComplianceTests/SqlComplianceRuleDataStoreTester.cpp
@@ -0,0 +1,22 @@
+#include <doctest/doctest.h>
+#include <Viper/Sqlite3/Connection.hpp>
+#include "Nexus/Compliance/SqlComplianceRuleDataStore.hpp"
+#include "Nexus/ComplianceTests/ComplianceRuleDataStoreTestSuite.hpp"
+
+using namespace Nexus;
+using namespace Nexus::Compliance;
+using namespace Nexus::Compliance::Tests;
+using namespace Viper;
+using namespace Viper::Sqlite3;
+
+namespace {
+  struct Builder {
+    auto operator ()() const {
+      return SqlComplianceRuleDataStore();
+    }
+  };
+}
+
+TEST_SUITE("LocalComplianceRuleDataStore") {
+  TEST_CASE_TEMPLATE_INVOKE(ComplianceRuleDataStoreTestSuite, Builder);
+}
