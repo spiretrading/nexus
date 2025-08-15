b8032fd4bdf7ee160489b32257f330bdecb6646c
Kamal <kamal@eidolonsystems.com>
2025-08-15 16:52:35 -0400

    Reimplemented AsxTradeMatchFeeTable with unit tests.

--- Nexus/Source/FeeHandlingTests/AsxTradeMatchFeeHandlingTester.cpp
+++ Nexus/Source/FeeHandlingTests/AsxTradeMatchFeeHandlingTester.cpp
@@ -1,5 +1,4 @@
 #include <doctest/doctest.h>
-#include "Nexus/Definitions/DefaultCountryDatabase.hpp"
 #include "Nexus/Definitions/DefaultCurrencyDatabase.hpp"
 #include "Nexus/Definitions/DefaultDestinationDatabase.hpp"
 #include "Nexus/Definitions/DefaultVenueDatabase.hpp"
@@ -30,111 +29,119 @@ namespace {
   }
 
   auto make_fee_table() {
-    auto feeTable = AsxTradeMatchFeeTable();
-    feeTable.m_gstRate = 1;
-    feeTable.m_tradeRate = 1;
-    feeTable.m_clearingRateTable[0][0] = rational<int>{1, 1000};
-    feeTable.m_clearingRateTable[0][1] = rational<int>{1, 100};
-    feeTable.m_clearingRateTable[0][2] = rational<int>{1, 10};
-    feeTable.m_clearingRateTable[1][0] = rational<int>{5, 1000};
-    feeTable.m_clearingRateTable[1][1] = rational<int>{5, 100};
-    feeTable.m_clearingRateTable[1][2] = rational<int>{5, 10};
-    return feeTable;
+    auto table = AsxTradeMatchFeeTable();
+    table.m_gst_rate = 1;
+    table.m_trade_rate = 1;
+    table.m_clearing_rate_table[0][0] = rational<int>{1, 1000};
+    table.m_clearing_rate_table[0][1] = rational<int>{1, 100};
+    table.m_clearing_rate_table[0][2] = rational<int>{1, 10};
+    table.m_clearing_rate_table[1][0] = rational<int>{5, 1000};
+    table.m_clearing_rate_table[1][1] = rational<int>{5, 100};
+    table.m_clearing_rate_table[1][2] = rational<int>{5, 10};
+    return table;
   }
 }
 
 TEST_SUITE("AsxtFeeHandling") {
   TEST_CASE("zero_quantity") {
-    auto feeTable = make_fee_table();
+    auto table = make_fee_table();
     auto fields = make_order_fields(Money::ONE);
     fields.m_quantity = 0;
-    TestNotionalValueFeeCalculation(feeTable, fields,
-      std::bind(CalculateClearingFee, std::placeholders::_1, fields,
-      std::placeholders::_2), 0);
-    TestNotionalValueFeeCalculation(feeTable, fields, CalculateExecutionFee, 0);
+    test_notional_value_fee_calculation(table, fields, 0,
+      [&] (const auto& table, const auto& report) {
+        return calculate_clearing_fee(table, fields, report);
+      });
+    test_notional_value_fee_calculation(
+      table, fields, 0, calculate_execution_fee);
   }
 
   TEST_CASE("regular_tier_one") {
-    auto feeTable = make_fee_table();
+    auto table = make_fee_table();
     auto fields = make_order_fields(5 * Money::CENT);
     fields.m_quantity = 100;
-    TestNotionalValueFeeCalculation(feeTable, fields,
-      std::bind(CalculateClearingFee, std::placeholders::_1, fields,
-      std::placeholders::_2), LookupClearingFee(feeTable,
-      AsxTradeMatchFeeTable::PriceClass::TIER_ONE,
-      AsxTradeMatchFeeTable::OrderTypeClass::REGULAR));
-    TestFeeCalculation(feeTable, fields, "A", CalculateExecutionFee,
-      (1 + feeTable.m_gstRate) * feeTable.m_tradeRate * fields.m_quantity *
-      fields.m_price);
+    test_notional_value_fee_calculation(table, fields,
+      lookup_clearing_fee(table, AsxTradeMatchFeeTable::PriceClass::TIER_ONE,
+        AsxTradeMatchFeeTable::OrderTypeClass::REGULAR),
+      [&] (const auto& table, const auto& report) {
+        return calculate_clearing_fee(table, fields, report);
+      });
+    test_fee_calculation(table, fields, "A",
+      (1 + table.m_gst_rate) * table.m_trade_rate * fields.m_quantity *
+        fields.m_price, calculate_execution_fee);
   }
 
   TEST_CASE("regular_tier_two") {
-    auto feeTable = make_fee_table();
+    auto table = make_fee_table();
     auto fields = make_order_fields(50 * Money::CENT);
     fields.m_quantity = 500;
-    TestNotionalValueFeeCalculation(feeTable, fields,
-      std::bind(CalculateClearingFee, std::placeholders::_1, fields,
-      std::placeholders::_2), LookupClearingFee(feeTable,
-      AsxTradeMatchFeeTable::PriceClass::TIER_TWO,
-      AsxTradeMatchFeeTable::OrderTypeClass::REGULAR));
-    TestFeeCalculation(feeTable, fields, "R", CalculateExecutionFee,
-      (1 + feeTable.m_gstRate) * feeTable.m_tradeRate * fields.m_quantity *
-      fields.m_price);
+    test_notional_value_fee_calculation(table, fields,
+      lookup_clearing_fee(table, AsxTradeMatchFeeTable::PriceClass::TIER_TWO,
+        AsxTradeMatchFeeTable::OrderTypeClass::REGULAR),
+      [&] (const auto& table, const auto& report) {
+        return calculate_clearing_fee(table, fields, report);
+      });
+    test_fee_calculation(table, fields, "R",
+      (1 + table.m_gst_rate) * table.m_trade_rate * fields.m_quantity *
+        fields.m_price, calculate_execution_fee);
   }
 
   TEST_CASE("regular_tier_three") {
-    auto feeTable = make_fee_table();
+    auto table = make_fee_table();
     auto fields = make_order_fields(Money::ONE);
     fields.m_quantity = 12000;
-    TestNotionalValueFeeCalculation(feeTable, fields,
-      std::bind(CalculateClearingFee, std::placeholders::_1, fields,
-      std::placeholders::_2), LookupClearingFee(feeTable,
-      AsxTradeMatchFeeTable::PriceClass::TIER_THREE,
-      AsxTradeMatchFeeTable::OrderTypeClass::REGULAR));
-    TestFeeCalculation(feeTable, fields, "R", CalculateExecutionFee,
-      (1 + feeTable.m_gstRate) * feeTable.m_tradeRate * fields.m_quantity *
-      fields.m_price);
+    test_notional_value_fee_calculation(table, fields,
+      lookup_clearing_fee(table, AsxTradeMatchFeeTable::PriceClass::TIER_THREE,
+        AsxTradeMatchFeeTable::OrderTypeClass::REGULAR),
+      [&] (const auto& table, const auto& report) {
+        return calculate_clearing_fee(table, fields, report);
+      });
+    test_fee_calculation(table, fields, "R",
+      (1 + table.m_gst_rate) * table.m_trade_rate * fields.m_quantity *
+        fields.m_price, calculate_execution_fee);
   }
 
   TEST_CASE("pegged_tier_one") {
-    auto feeTable = make_fee_table();
+    auto table = make_fee_table();
     auto fields = make_pegged_order_fields(5 * Money::CENT);
     fields.m_quantity = 6000;
-    TestNotionalValueFeeCalculation(feeTable, fields,
-      std::bind(CalculateClearingFee, std::placeholders::_1, fields,
-      std::placeholders::_2), LookupClearingFee(feeTable,
-      AsxTradeMatchFeeTable::PriceClass::TIER_ONE,
-      AsxTradeMatchFeeTable::OrderTypeClass::PEGGED));
-    TestFeeCalculation(feeTable, fields, "R", CalculateExecutionFee,
-      (1 + feeTable.m_gstRate) * feeTable.m_tradeRate * fields.m_quantity *
-      fields.m_price);
+    test_notional_value_fee_calculation(table, fields,
+      lookup_clearing_fee(table, AsxTradeMatchFeeTable::PriceClass::TIER_ONE,
+        AsxTradeMatchFeeTable::OrderTypeClass::PEGGED),
+      [&] (const auto& table, const auto& report) {
+        return calculate_clearing_fee(table, fields, report);
+      });
+    test_fee_calculation(table, fields, "R",
+      (1 + table.m_gst_rate) * table.m_trade_rate * fields.m_quantity *
+        fields.m_price, calculate_execution_fee);
   }
 
   TEST_CASE("pegged_tier_two") {
-    auto feeTable = make_fee_table();
+    auto table = make_fee_table();
     auto fields = make_pegged_order_fields(50 * Money::CENT);
     fields.m_quantity = 300;
-    TestNotionalValueFeeCalculation(feeTable, fields,
-      std::bind(CalculateClearingFee, std::placeholders::_1, fields,
-      std::placeholders::_2), LookupClearingFee(feeTable,
-      AsxTradeMatchFeeTable::PriceClass::TIER_TWO,
-      AsxTradeMatchFeeTable::OrderTypeClass::PEGGED));
-    TestFeeCalculation(feeTable, fields, "A", CalculateExecutionFee,
-      (1 + feeTable.m_gstRate) * feeTable.m_tradeRate * fields.m_quantity *
-      fields.m_price);
+    test_notional_value_fee_calculation(table, fields,
+      lookup_clearing_fee(table, AsxTradeMatchFeeTable::PriceClass::TIER_TWO,
+        AsxTradeMatchFeeTable::OrderTypeClass::PEGGED),
+      [&] (const auto& table, const auto& report) {
+        return calculate_clearing_fee(table, fields, report);
+      });
+    test_fee_calculation(table, fields, "A",
+      (1 + table.m_gst_rate) * table.m_trade_rate * fields.m_quantity *
+        fields.m_price, calculate_execution_fee);
   }
 
   TEST_CASE("pegged_tier_three") {
-    auto feeTable = make_fee_table();
+    auto table = make_fee_table();
     auto fields = make_pegged_order_fields(Money::ONE);
     fields.m_quantity = 4400;
-    TestNotionalValueFeeCalculation(feeTable, fields,
-      std::bind(CalculateClearingFee, std::placeholders::_1, fields,
-      std::placeholders::_2), LookupClearingFee(feeTable,
-      AsxTradeMatchFeeTable::PriceClass::TIER_THREE,
-      AsxTradeMatchFeeTable::OrderTypeClass::PEGGED));
-    TestFeeCalculation(feeTable, fields, "R", CalculateExecutionFee,
-      (1 + feeTable.m_gstRate) * feeTable.m_tradeRate * fields.m_quantity *
-      fields.m_price);
+    test_notional_value_fee_calculation(table, fields,
+      lookup_clearing_fee(table, AsxTradeMatchFeeTable::PriceClass::TIER_THREE,
+        AsxTradeMatchFeeTable::OrderTypeClass::PEGGED),
+      [&] (const auto& table, const auto& report) {
+        return calculate_clearing_fee(table, fields, report);
+      });
+    test_fee_calculation(table, fields, "R",
+      (1 + table.m_gst_rate) * table.m_trade_rate * fields.m_quantity *
+        fields.m_price, calculate_execution_fee);
   }
 }
