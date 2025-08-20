e1bff2386242211c356718cad63265324f634e48
Kamal <kamal@eidolonsystems.com>
2025-08-20 17:50:10 -0400

    Prepared MarketDataService for export.

--- Nexus/Include/Nexus/Python/MarketDataService.hpp
+++ Nexus/Include/Nexus/Python/MarketDataService.hpp
@@ -1,37 +1,20 @@
 #ifndef NEXUS_PYTHON_MARKET_DATA_SERVICE_HPP
 #define NEXUS_PYTHON_MARKET_DATA_SERVICE_HPP
-#include <type_traits>
 #include <pybind11/pybind11.h>
-#include "Nexus/MarketDataService/HistoricalDataStoreBox.hpp"
-#include "Nexus/MarketDataService/MarketDataClientBox.hpp"
-#include "Nexus/MarketDataService/MarketDataFeedClientBox.hpp"
-#include "Nexus/Python/DllExport.hpp"
 
 namespace Nexus::Python {
 
-  /** Returns the exported HistoricalDataStoreBox. */
-  NEXUS_EXPORT_DLL pybind11::class_<MarketDataService::HistoricalDataStoreBox>&
-    GetExportedHistoricalDataStoreBox();
-
-  /** Returns the exported MarketDataClientBox. */
-  NEXUS_EXPORT_DLL pybind11::class_<MarketDataService::MarketDataClientBox>&
-    GetExportedMarketDataClientBox();
-
-  /** Returns the exported MarketDataFeedClientBox. */
-  NEXUS_EXPORT_DLL pybind11::class_<MarketDataService::MarketDataFeedClientBox>&
-    GetExportedMarketDataFeedClientBox();
-
   /**
-   * Exports the ApplicationMarketDataClient class.
+   * Exports the AsyncHistoricalDataStore class connecting to MySQL.
    * @param module The module to export to.
    */
-  void ExportApplicationMarketDataClient(pybind11::module& module);
+  void ExportAsyncHistoricalDataStore(pybind11::module& module);
 
   /**
-   * Exports the ApplicationMarketDataFeedClient class.
+   * Exports the CachedHistoricalDataStore class connecting to MySQL.
    * @param module The module to export to.
    */
-  void ExportApplicationMarketDataFeedClient(pybind11::module& module);
+  void ExportCachedHistoricalDataStore(pybind11::module& module);
 
   /**
    * Exports the HistoricalDataStore class.
@@ -77,155 +60,37 @@ namespace Nexus::Python {
 
   /**
    * Exports an HistoricalDataStore class.
-   * @param <DataStore> The type of HistoricalDataStore to export.
+   * @param <D> The type of HistoricalDataStore to export.
    * @param module The module to export to.
    * @param name The name of the class.
    * @return The exported HistoricalDataStore.
    */
-  template<typename DataStore>
-  auto ExportHistoricalDataStore(pybind11::module& module,
-      const std::string& name) {
-    auto dataStore = pybind11::class_<DataStore, std::shared_ptr<DataStore>>(
-        module, name.c_str()).
-      def("load_security_info", &DataStore::LoadSecurityInfo).
-      def("load_order_imbalances", &DataStore::LoadOrderImbalances).
-      def("load_bbo_quotes", &DataStore::LoadBboQuotes).
-      def("load_book_quotes", &DataStore::LoadBookQuotes).
-      def("load_market_quotes", &DataStore::LoadMarketQuotes).
-      def("load_time_and_sales", &DataStore::LoadTimeAndSales).
-      def("store", static_cast<void (DataStore::*)(const SecurityInfo&)>(
-        &DataStore::Store)).
-      def("store", static_cast<void (DataStore::*)(
-        const SequencedMarketOrderImbalance&)>(&DataStore::Store)).
-      def("store", static_cast<void (DataStore::*)(
-        const std::vector<SequencedMarketOrderImbalance>&)>(&DataStore::Store)).
-      def("store", static_cast<void (DataStore::*)(
-        const SequencedSecurityBboQuote&)>(&DataStore::Store)).
-      def("store", static_cast<void (DataStore::*)(
-        const std::vector<SequencedSecurityBboQuote>&)>(&DataStore::Store)).
-      def("store", static_cast<void (DataStore::*)(
-        const SequencedSecurityMarketQuote&)>(&DataStore::Store)).
-      def("store", static_cast<void (DataStore::*)(
-        const std::vector<SequencedSecurityMarketQuote>&)>(&DataStore::Store)).
-      def("store", static_cast<void (DataStore::*)(
-        const SequencedSecurityBookQuote&)>(&DataStore::Store)).
-      def("store", static_cast<void (DataStore::*)(
-        const std::vector<SequencedSecurityBookQuote>&)>(&DataStore::Store)).
-      def("store", static_cast<void (DataStore::*)(
-        const SequencedSecurityTimeAndSale&)>(&DataStore::Store)).
-      def("store", static_cast<void (DataStore::*)(
-        const std::vector<SequencedSecurityTimeAndSale>&)>(&DataStore::Store)).
-      def("close", &DataStore::Close);
-    if constexpr(!std::is_same_v<DataStore,
-        MarketDataService::HistoricalDataStoreBox>) {
-      pybind11::implicitly_convertible<DataStore,
-        MarketDataService::HistoricalDataStoreBox>();
-      GetExportedHistoricalDataStoreBox().def(
-        pybind11::init<std::shared_ptr<DataStore>>());
-    }
-    return dataStore;
-  }
+  template<typename D>
+  auto ExportHistoricalDataStore(
+    pybind11::module& module, std::string_view name) {}
 
   /**
    * Exports a MarketDataClient class.
-   * @param <Client> The type of MarketDataClient to export.
+   * @param <C> The type of MarketDataClient to export.
    * @param module The module to export to.
    * @param name The name of the class.
    * @return The exported MarketDataClient.
    */
-  template<typename Client>
-  auto ExportMarketDataClient(pybind11::module& module,
-      const std::string& name) {
-    auto client = pybind11::class_<Client, std::shared_ptr<Client>>(module,
-      name.c_str()).
-      def("query_sequenced_order_imbalances", static_cast<void (Client::*)(
-        const MarketDataService::MarketWideDataQuery&,
-        Beam::ScopedQueueWriter<SequencedOrderImbalance>)>(
-          &Client::QueryOrderImbalances)).
-      def("query_order_imbalances", static_cast<void (Client::*)(
-        const MarketDataService::MarketWideDataQuery&,
-        Beam::ScopedQueueWriter<OrderImbalance>)>(
-          &Client::QueryOrderImbalances)).
-      def("query_sequenced_bbo_quotes", static_cast<void (Client::*)(
-        const MarketDataService::SecurityMarketDataQuery&,
-        Beam::ScopedQueueWriter<SequencedBboQuote>)>(&Client::QueryBboQuotes)).
-      def("query_bbo_quotes", static_cast<void (Client::*)(
-        const MarketDataService::SecurityMarketDataQuery&,
-        Beam::ScopedQueueWriter<BboQuote>)>(&Client::QueryBboQuotes)).
-      def("query_sequenced_book_quotes", static_cast<void (Client::*)(
-        const MarketDataService::SecurityMarketDataQuery&,
-        Beam::ScopedQueueWriter<SequencedBookQuote>)>(
-          &Client::QueryBookQuotes)).
-      def("query_book_quotes", static_cast<void (Client::*)(
-        const MarketDataService::SecurityMarketDataQuery&,
-        Beam::ScopedQueueWriter<BookQuote>)>(&Client::QueryBookQuotes)).
-      def("query_sequenced_market_quotes", static_cast<void (Client::*)(
-        const MarketDataService::SecurityMarketDataQuery&,
-        Beam::ScopedQueueWriter<SequencedMarketQuote>)>(
-          &Client::QueryMarketQuotes)).
-      def("query_market_quotes", static_cast<void (Client::*)(
-        const MarketDataService::SecurityMarketDataQuery&,
-        Beam::ScopedQueueWriter<MarketQuote>)>(&Client::QueryMarketQuotes)).
-      def("query_sequenced_time_and_sales", static_cast<void (Client::*)(
-        const MarketDataService::SecurityMarketDataQuery&,
-        Beam::ScopedQueueWriter<SequencedTimeAndSale>)>(
-          &Client::QueryTimeAndSales)).
-      def("query_time_and_sales", static_cast<void (Client::*)(
-        const MarketDataService::SecurityMarketDataQuery&,
-        Beam::ScopedQueueWriter<TimeAndSale>)>(&Client::QueryTimeAndSales)).
-      def("load_security_snapshot", &Client::LoadSecuritySnapshot).
-      def("load_security_technicals", &Client::LoadSecurityTechnicals).
-      def("query_security_info", &Client::QuerySecurityInfo).
-      def("load_security_info_from_prefix",
-        &Client::LoadSecurityInfoFromPrefix).
-      def("close", &Client::Close);
-    if constexpr(!std::is_same_v<Client,
-        MarketDataService::MarketDataClientBox>) {
-      pybind11::implicitly_convertible<Client,
-        MarketDataService::MarketDataClientBox>();
-      GetExportedMarketDataClientBox().def(
-        pybind11::init<std::shared_ptr<Client>>());
-    }
-    return client;
+  template<typename C>
+  auto ExportMarketDataClient(
+      pybind11::module& module, std::string_view name) {
   }
 
   /**
    * Exports a MarketDataFeedClient class.
-   * @param <Client> The type of MarketDataFeedClient to export.
+   * @param <C> The type of MarketDataFeedClient to export.
    * @param module The module to export to.
    * @param name The name of the class.
    * @return The exported MarketDataFeedClient.
    */
-  template<typename Client>
-  auto ExportMarketDataFeedClient(pybind11::module& module,
-      const std::string& name) {
-    auto client = pybind11::class_<Client, std::shared_ptr<Client>>(module,
-      name.c_str()).
-      def("add", &Client::Add).
-      def("publish", static_cast<void (Client::*)(const MarketOrderImbalance&)>(
-        &Client::Publish)).
-      def("publish", static_cast<void (Client::*)(const SecurityBboQuote&)>(
-        &Client::Publish)).
-      def("publish", static_cast<void (Client::*)(const SecurityMarketQuote&)>(
-        &Client::Publish)).
-      def("publish", static_cast<void (Client::*)(const SecurityBookQuote&)>(
-        &Client::Publish)).
-      def("publish", static_cast<void (Client::*)(const SecurityTimeAndSale&)>(
-        &Client::Publish)).
-      def("add_order", &Client::AddOrder).
-      def("modify_order_size", &Client::ModifyOrderSize).
-      def("offset_order_size", &Client::OffsetOrderSize).
-      def("modify_order_price", &Client::ModifyOrderPrice).
-      def("delete_order", &Client::DeleteOrder).
-      def("close", &Client::Close);
-    if constexpr(!std::is_same_v<Client,
-        MarketDataService::MarketDataFeedClientBox>) {
-      pybind11::implicitly_convertible<Client,
-        MarketDataService::MarketDataFeedClientBox>();
-      GetExportedMarketDataFeedClientBox().def(
-        pybind11::init<std::shared_ptr<Client>>());
-    }
-    return client;
+  template<typename C>
+  auto ExportMarketDataFeedClient(
+      pybind11::module& module, std::string_view name) {
   }
 }
 
