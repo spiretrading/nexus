e1afc9a349d0516cd667959ec8af6872a4a0c779
Kamal <kamal@eidolonsystems.com>
2025-08-29 15:36:43 -0400

    Updated Python bindings for CseFeeTable.

--- Nexus/Source/Python/FeeHandling.cpp
+++ Nexus/Source/Python/FeeHandling.cpp
@@ -242,19 +242,49 @@ void Nexus::Python::ExportCseFeeTable(module& module) {
   auto outer = class_<CseFeeTable>(module, "CseFeeTable")
     .def(init())
     .def(init<const CseFeeTable&>())
-    .def_readwrite("fee_table", &CseFeeTable::m_feeTable);
-  enum_<CseFeeTable::Section>(outer, "Section")
-    .value("NONE", CseFeeTable::Section::NONE)
-    .value("DEFAULT", CseFeeTable::Section::DEFAULT)
-    .value("SUBDOLLAR", CseFeeTable::Section::SUBDOLLAR)
-    .value("DARK", CseFeeTable::Section::DARK);
+    .def_readwrite("fee_table", &CseFeeTable::m_feeTable)
+    .def_readwrite(
+      "interlisted_fee_table", &CseFeeTable::m_interlistedFeeTable)
+    .def_readwrite("etf_fee_table", &CseFeeTable::m_etfFeeTable)
+    .def_readwrite("cse_listed_fee_table", &CseFeeTable::m_cseListedFeeTable)
+    .def_readwrite("open_fee_table", &CseFeeTable::m_cseOpenFeeTable)
+    .def_readwrite("close_fee_table", &CseFeeTable::m_cseCloseFeeTable);
+  enum_<CseFeeTable::CseListing>(outer, "CseListing")
+    .value("DEFAULT", CseFeeTable::CseListing::DEFAULT)
+    .value("INTERLISTED", CseFeeTable::CseListing::INTERLISTED)
+    .value("ETF", CseFeeTable::CseListing::ETF)
+    .value("CSE_LISTED", CseFeeTable::CseListing::CSE_LISTED);
+  enum_<CseFeeTable::PriceClass>(outer, "PriceClass")
+    .value("DEFAULT", CseFeeTable::PriceClass::DEFAULT)
+    .value("SUBDOLLAR", CseFeeTable::PriceClass::SUBDOLLAR);
+  enum_<CseFeeTable::Session>(outer, "Session")
+    .value("DEFAULT", CseFeeTable::Session::DEFAULT)
+    .value("OPEN", CseFeeTable::Session::OPEN)
+    .value("CLOSE", CseFeeTable::Session::CLOSE);
+  enum_<CseFeeTable::TradeType>(outer, "TradeType")
+    .value("DEFAULT", CseFeeTable::TradeType::DEFAULT)
+    .value("DARK", CseFeeTable::TradeType::DARK);
   module.def("parse_cse_fee_table", &ParseCseFeeTable);
-  module.def("lookup_cse_fee_table_section", &LookupCseFeeTableSection);
-  module.def("lookup_cse_liquidity_flag", &LookupCseLiquidityFlag);
-  module.def("lookup_fee", static_cast<Money (*)(const CseFeeTable&,
-    LiquidityFlag, CseFeeTable::Section)>(&LookupFee));
+  module.def("get_cse_liquidity_flag", &GetCseLiquidityFlag);
+  module.def("get_cse_price_class", &GetCsePriceClass);
+  module.def("get_cse_session", &GetCseSession);
+  module.def("get_cse_trade_type", &GetCseTradeType);
+  module.def("get_cse_listed_fee", &GetCseListedFee);
+  module.def("get_default_fee", static_cast<Money (*)(const CseFeeTable&,
+    CseFeeTable::TradeType, CseFeeTable::PriceClass, LiquidityFlag)>(
+      &GetDefaultFee));
+  module.def("get_interlisted_fee", static_cast<Money (*)(const CseFeeTable&,
+    CseFeeTable::TradeType, CseFeeTable::PriceClass, LiquidityFlag)>(
+      &GetInterlistedFee));
+  module.def("get_etf_fee", static_cast<Money (*)(const CseFeeTable&,
+    CseFeeTable::TradeType, CseFeeTable::PriceClass, LiquidityFlag)>(
+      &GetEtfFee));
+  module.def("get_open_fee", static_cast<Money (*)(const CseFeeTable&,
+    CseFeeTable::PriceClass, LiquidityFlag)>(&GetOpenFee));
+  module.def("get_close_fee", static_cast<Money (*)(const CseFeeTable&,
+    CseFeeTable::PriceClass, LiquidityFlag)>(&GetCloseFee));
   module.def("calculate_fee", static_cast<Money (*)(const CseFeeTable&,
-    const ExecutionReport&)>(&CalculateFee));
+    CseFeeTable::CseListing, const ExecutionReport&)>(&CalculateFee));
 }
 
 void Nexus::Python::ExportCse2FeeTable(module& module) {
