fe5f39168f0b281c186eabec619c551b340c1577
Kamal Mansouri <kamal@eidolonsystems.com>
2025-08-30 22:27:45 -0400

    Updated unit tests.

--- Applications/Lollipop/Source/CanvasTests/TranslationTester.cpp
+++ Applications/Lollipop/Source/CanvasTests/TranslationTester.cpp
@@ -1,9 +1,7 @@
 #include <doctest/doctest.h>
 #include "Nexus/Definitions/DefaultDestinationDatabase.hpp"
 #include "Nexus/Definitions/DefaultTimeZoneDatabase.hpp"
-#include "Nexus/ServiceClients/TestEnvironment.hpp"
-#include "Nexus/ServiceClients/TestServiceClients.hpp"
-#include "Nexus/TelemetryServiceTests/TelemetryServiceTestEnvironment.hpp"
+#include "Nexus/TestEnvironment/TestClients.hpp"
 #include "Spire/Canvas/ControlNodes/ChainNode.hpp"
 #include "Spire/Canvas/ControlNodes/SpawnNode.hpp"
 #include "Spire/Canvas/ControlNodes/UntilNode.hpp"
@@ -22,35 +20,23 @@
 using namespace Beam;
 using namespace Beam::ServiceLocator;
 using namespace Nexus;
-using namespace Nexus::MarketDataService;
-using namespace Nexus::OrderExecutionService;
-using namespace Nexus::TelemetryService;
-using namespace Nexus::TelemetryService::Tests;
 using namespace Spire;
 
 namespace {
   struct Environment {
     TestEnvironment m_environment;
-    ServiceClientsBox m_serviceClients;
-    TelemetryServiceTestEnvironment m_telemetryEnvironment;
-    TelemetryClientBox m_telemetryClient;
+    Clients m_clients;
     UserProfile m_userProfile;
 
     Environment()
-      : m_serviceClients(std::in_place_type<TestServiceClients>,
-          Ref(m_environment)),
-        m_telemetryEnvironment(m_serviceClients.GetServiceLocatorClient(),
-          m_serviceClients.GetTimeClient(),
-          m_serviceClients.GetAdministrationClient()),
-        m_telemetryClient(m_telemetryEnvironment.MakeClient(
-          m_serviceClients.GetServiceLocatorClient())),
-        m_userProfile("", false, false, GetDefaultCountryDatabase(),
-          GetDefaultTimeZoneDatabase(), GetDefaultCurrencyDatabase(), {},
-          GetDefaultMarketDatabase(), GetDefaultDestinationDatabase(),
-          EntitlementDatabase(), m_serviceClients, m_telemetryClient) {}
+      : m_clients(std::in_place_type<TestClients>, Ref(m_environment)),
+        m_userProfile("", false, false, DEFAULT_COUNTRIES,
+          get_default_time_zone_database(), DEFAULT_CURRENCIES, {},
+          DEFAULT_VENUES, DEFAULT_DESTINATIONS, EntitlementDatabase(),
+          m_clients) {}
   };
 
-  const auto TEST_SECURITY = ParseSecurity("TST.TSX");
+  const auto TEST_SECURITY = parse_security("TST.TSX");
 }
 
 TEST_SUITE("Translation") {
@@ -58,8 +44,8 @@ TEST_SUITE("Translation") {
     auto environment = Environment();
     auto value = IntegerNode(100);
     auto executor = Executor();
-    auto context = CanvasNodeTranslationContext(Ref(environment.m_userProfile),
-      Ref(executor), DirectoryEntry());
+    auto context = CanvasNodeTranslationContext(
+      Ref(environment.m_userProfile), Ref(executor), DirectoryEntry());
     auto translation = Translate(context, value);
     auto result = translation.Extract<Aspen::Box<Quantity>>();
     REQUIRE(result.commit(0) == Aspen::State::COMPLETE_EVALUATED);
@@ -72,8 +58,8 @@ TEST_SUITE("Translation") {
     chain = chain->Replace("i0", std::make_unique<IntegerNode>(123));
     chain = chain->Replace("i1", std::make_unique<IntegerNode>(456));
     auto executor = Executor();
-    auto context = CanvasNodeTranslationContext(Ref(environment.m_userProfile),
-      Ref(executor), DirectoryEntry());
+    auto context = CanvasNodeTranslationContext(
+      Ref(environment.m_userProfile), Ref(executor), DirectoryEntry());
     auto translation = Translate(context, *chain);
     auto result = translation.Extract<Aspen::Box<Quantity>>();
     REQUIRE(result.commit(0) == Aspen::State::CONTINUE_EVALUATED);
@@ -88,8 +74,8 @@ TEST_SUITE("Translation") {
     chain = chain->Replace("i0", std::make_unique<IntegerNode>(123));
     chain = chain->Replace("i1", std::make_unique<ReferenceNode>("i0"));
     auto executor = Executor();
-    auto context = CanvasNodeTranslationContext(Ref(environment.m_userProfile),
-      Ref(executor), DirectoryEntry());
+    auto context = CanvasNodeTranslationContext(
+      Ref(environment.m_userProfile), Ref(executor), DirectoryEntry());
     auto translation = Translate(context, *chain);
     auto result = translation.Extract<Aspen::Box<Quantity>>();
     REQUIRE(result.commit(0) == Aspen::State::CONTINUE_EVALUATED);
@@ -105,8 +91,8 @@ TEST_SUITE("Translation") {
     chain = chain->Replace("i1", std::make_unique<IntegerNode>(123));
     chain = chain->Convert(IntegerType::GetInstance());
     auto executor = Executor();
-    auto context = CanvasNodeTranslationContext(Ref(environment.m_userProfile),
-      Ref(executor), DirectoryEntry());
+    auto context = CanvasNodeTranslationContext(
+      Ref(environment.m_userProfile), Ref(executor), DirectoryEntry());
     auto translation = Translate(context, *chain);
     auto result = translation.Extract<Aspen::Box<Quantity>>();
     REQUIRE(result.commit(0) == Aspen::State::CONTINUE_EVALUATED);
@@ -117,11 +103,11 @@ TEST_SUITE("Translation") {
 
   TEST_CASE("translating_order") {
     auto environment = Environment();
-    auto orderNode = std::unique_ptr<CanvasNode>(
-      std::make_unique<SingleOrderTaskNode>());
+    auto orderNode =
+      std::unique_ptr<CanvasNode>(std::make_unique<SingleOrderTaskNode>());
     orderNode = orderNode->Replace(SingleOrderTaskNode::SECURITY_PROPERTY,
       std::make_unique<SecurityNode>(TEST_SECURITY,
-      environment.m_userProfile.GetMarketDatabase()));
+      environment.m_userProfile.GetVenueDatabase()));
     orderNode = orderNode->Replace(SingleOrderTaskNode::QUANTITY_PROPERTY,
       std::make_unique<IntegerNode>(100));
     orderNode = orderNode->Replace(SingleOrderTaskNode::SIDE_PROPERTY,
@@ -134,23 +120,23 @@ TEST_SUITE("Translation") {
     auto context = CanvasNodeTranslationContext(Ref(environment.m_userProfile),
       Ref(executor), DirectoryEntry());
     auto translation = Translate(context, *orderNode);
-    auto result = translation.Extract<Aspen::Box<const Order*>>();
+    auto result = translation.Extract<Aspen::Box<std::shared_ptr<Order>>>();
     REQUIRE(result.commit(0) == Aspen::State::EVALUATED);
     auto order1 = result.eval();
-    REQUIRE(order1->GetInfo().m_fields.m_security == TEST_SECURITY);
-    REQUIRE(order1->GetInfo().m_fields.m_quantity == 100);
-    REQUIRE(order1->GetInfo().m_fields.m_side == Side::BID);
-    REQUIRE(order1->GetInfo().m_fields.m_price == Money::ONE);
-    REQUIRE(order1->GetInfo().m_fields.m_type == OrderType::LIMIT);
+    REQUIRE(order1->get_info().m_fields.m_security == TEST_SECURITY);
+    REQUIRE(order1->get_info().m_fields.m_quantity == 100);
+    REQUIRE(order1->get_info().m_fields.m_side == Side::BID);
+    REQUIRE(order1->get_info().m_fields.m_price == Money::ONE);
+    REQUIRE(order1->get_info().m_fields.m_type == OrderType::LIMIT);
   }
 
   TEST_CASE("translating_order_task") {
     auto environment = Environment();
-    auto orderNode = std::unique_ptr<CanvasNode>(
-      std::make_unique<SingleOrderTaskNode>());
+    auto orderNode =
+      std::unique_ptr<CanvasNode>(std::make_unique<SingleOrderTaskNode>());
     orderNode = orderNode->Replace(SingleOrderTaskNode::SECURITY_PROPERTY,
-      std::make_unique<SecurityNode>(TEST_SECURITY,
-      environment.m_userProfile.GetMarketDatabase()));
+      std::make_unique<SecurityNode>(
+        TEST_SECURITY, environment.m_userProfile.GetVenueDatabase()));
     orderNode = orderNode->Replace(SingleOrderTaskNode::QUANTITY_PROPERTY,
       std::make_unique<IntegerNode>(100));
     orderNode = orderNode->Replace(SingleOrderTaskNode::SIDE_PROPERTY,
@@ -161,7 +147,7 @@ TEST_SUITE("Translation") {
       std::make_unique<OrderTypeNode>(OrderType::LIMIT));
     auto task = std::make_shared<Task>(*orderNode, DirectoryEntry(),
       Ref(environment.m_userProfile));
-    auto submittedOrders = std::make_shared<Queue<const Order*>>();
+    auto submittedOrders = std::make_shared<Queue<std::shared_ptr<Order>>>();
     task->GetContext().GetOrderPublisher().Monitor(submittedOrders);
     auto taskState = std::make_shared<Queue<Task::StateEntry>>();
     task->GetPublisher().Monitor(taskState);
@@ -169,25 +155,24 @@ TEST_SUITE("Translation") {
     REQUIRE(taskState->Pop().m_state == Task::State::INITIALIZING);
     auto submittedOrder1 = submittedOrders->Pop();
     REQUIRE(taskState->Pop().m_state == Task::State::ACTIVE);
-    REQUIRE(submittedOrder1->GetInfo().m_fields.m_security ==
+    REQUIRE(submittedOrder1->get_info().m_fields.m_security ==
       TEST_SECURITY);
-    REQUIRE(submittedOrder1->GetInfo().m_fields.m_quantity == 100);
-    REQUIRE(submittedOrder1->GetInfo().m_fields.m_side == Side::BID);
-    REQUIRE(submittedOrder1->GetInfo().m_fields.m_price == Money::ONE);
-    REQUIRE(submittedOrder1->GetInfo().m_fields.m_type ==
-      OrderType::LIMIT);
-    auto receivedOrders = std::make_shared<Queue<const Order*>>();
-    environment.m_environment.MonitorOrderSubmissions(receivedOrders);
+    REQUIRE(submittedOrder1->get_info().m_fields.m_quantity == 100);
+    REQUIRE(submittedOrder1->get_info().m_fields.m_side == Side::BID);
+    REQUIRE(submittedOrder1->get_info().m_fields.m_price == Money::ONE);
+    REQUIRE(submittedOrder1->get_info().m_fields.m_type == OrderType::LIMIT);
+    auto receivedOrders = std::make_shared<Queue<std::shared_ptr<Order>>>();
+    environment.m_environment.monitor_order_submissions(receivedOrders);
     auto receivedOrder1 = receivedOrders->Pop();
-    environment.m_environment.Accept(*receivedOrder1);
+    environment.m_environment.accept(*receivedOrder1);
     auto executionReports = std::make_shared<Queue<ExecutionReport>>();
-    submittedOrder1->GetPublisher().Monitor(executionReports);
+    submittedOrder1->get_publisher().Monitor(executionReports);
     REQUIRE(executionReports->Pop().m_status == OrderStatus::PENDING_NEW);
     REQUIRE(executionReports->Pop().m_status == OrderStatus::NEW);
     task->Cancel();
     REQUIRE(taskState->Pop().m_state == Task::State::PENDING_CANCEL);
     REQUIRE(executionReports->Pop().m_status == OrderStatus::PENDING_CANCEL);
-    environment.m_environment.Cancel(*receivedOrder1);
+    environment.m_environment.cancel(*receivedOrder1);
     REQUIRE(taskState->Pop().m_state == Task::State::CANCELED);
   }
 
