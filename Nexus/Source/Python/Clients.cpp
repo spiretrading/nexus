79680b59fd0183c20b32a170c51db0999460b385
Kamal Mansouri <kamal@spiretrading.com>
2025-12-05 11:08:04 -0500

    Updated ServiceClients constructor to take IpAddress directly.

--- Nexus/Source/Python/Clients.cpp
+++ Nexus/Source/Python/Clients.cpp
@@ -19,10 +19,9 @@ void Nexus::Python::export_service_clients(module& module) {
   clients = std::make_unique<class_<Clients>>(
     export_clients<Clients>(module, "Clients"));
   export_clients<ToPythonClients<ServiceClients>>(module, "ServiceClients").
-    def(init([] (std::string username, std::string password, std::string host,
-        unsigned short port) {
-      return std::make_unique<ToPythonClients<ServiceClients>>(
-        std::move(username), std::move(password),
-        IpAddress(std::move(host), port));
-    }));
+    def(init(
+      [] (std::string username, std::string password, IpAddress address) {
+        return std::make_unique<ToPythonClients<ServiceClients>>(
+          std::move(username), std::move(password), std::move(address));
+      }));
 }
