5d933adf5afcf1d1ea1a0b28cf5b4a9c52910862
Kamal <kamal@spiretrading.com>
2026-03-04 13:31:40 -0500

    Implemented the IconLabelButton. Changed implementation of NavigationTab to use one single SVG icon with a masking color instead of two icon files.

--- Applications/WebPortal/WebApp/library/source/components/index.ts
+++ Applications/WebPortal/WebApp/library/source/components/index.ts
@@ -8,6 +8,7 @@ export * from './date_time_field';
 export * from './drop_down_button';
 export * from './duration_field';
 export * from './hline';
+export * from './icon_label_button';
 export * from './integer_field';
 export * from './modal';
 export * from './money_field';
--- Applications/WebPortal/WebApp/tests/scratch/source/index.tsx
+++ Applications/WebPortal/WebApp/tests/scratch/source/index.tsx
@@ -1,4 +1,10 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import * as WebPortal from 'web_portal';
 
-ReactDOM.render(<div/>, document.getElementById('main'));
+ReactDOM.render(
+  <WebPortal.IconLabelButton
+    icon='resources/arrow-expand.svg'
+    label='Expand'
+    variant={WebPortal.IconLabelButton.Variant.ICON_LABEL}/>,
+  document.getElementById('main'));
