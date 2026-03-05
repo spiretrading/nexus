4afd90cecbda309e6d6c62ccf594077c7705cb67
Kamal <kamal@spiretrading.com>
2026-03-05 16:12:49 -0500

    Add RequestActivityItem component with UI Catalog entry.

--- Applications/WebPortal/WebApp/library/source/pages/requests_page/index.ts
+++ Applications/WebPortal/WebApp/library/source/pages/requests_page/index.ts
@@ -3,6 +3,7 @@ export * from './compliance_rule_status_tag';
 export * from './diff_badge';
 export * from './entitlements_status_tag';
 export * from './request_category_tag';
+export * from './request_activity_item';
 export * from './request_effective_date';
 export * from './request_state_indicator';
 export * from './requests_page';
