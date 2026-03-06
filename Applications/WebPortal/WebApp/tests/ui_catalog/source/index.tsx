16c013f84dd9f5a8251325bace62b9d7b37c892e
Kamal <kamal@spiretrading.com>
2026-03-05 23:48:09 -0500

    Add collapsible sections to UI Catalog component list.

--- Applications/WebPortal/WebApp/tests/ui_catalog/source/index.tsx
+++ Applications/WebPortal/WebApp/tests/ui_catalog/source/index.tsx
@@ -2,26 +2,26 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import * as Router from 'react-router-dom';
 import * as WebPortal from 'web_portal';
-import { componentsList } from './data/component_schemas';
-import { ComponentSchema } from './data/schemas';
+import { componentSections } from './data/component_schemas';
+import { ComponentSection } from './data/schemas';
 import { UICatalog } from './ui_catalog';
 import { Viewport } from './viewport';
 import { viewportRenderer } from './viewport_renderer';
 
 interface Properties {
   viewport: Viewport;
-  components: ComponentSchema[];
+  sections: ComponentSection[];
 }
 
 class App extends React.Component<Properties> {
   public render(): JSX.Element {
     return (
       <Router.BrowserRouter>
-        <UICatalog componentList={this.props.components}/>
+        <UICatalog sections={this.props.sections}/>
       </Router.BrowserRouter>);
   }
 }
 
 const ResponsivePage = viewportRenderer(App);
-ReactDOM.render(<ResponsivePage components={componentsList}/>,
+ReactDOM.render(<ResponsivePage sections={componentSections}/>,
   document.getElementById('main'));
