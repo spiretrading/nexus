234674ea3aca10d416cda970fc8be7ff0b72b556
Kamal <kamal@spiretrading.com>
2026-03-09 13:57:53 -0400

    Add URL-based routing to RequestsController with HashRouter in tester.

--- Applications/WebPortal/WebApp/library/source/pages/requests_page/requests_controller.tsx
+++ Applications/WebPortal/WebApp/library/source/pages/requests_page/requests_controller.tsx
@@ -1,12 +1,13 @@
 import * as Nexus from 'nexus';
 import * as React from 'react';
+import * as Router from 'react-router-dom';
 import { RequestDirectoryPage } from './request_directory_page';
 import { RequestsModel } from './requests_model';
 import { RequestsPage } from './requests_page';
 
 type Type = Nexus.AccountModificationRequest.Type;
 
-interface Properties {
+interface Properties extends Router.RouteComponentProps {
 
   /** The account's roles. */
   roles: Nexus.AccountRoles;
@@ -16,7 +17,7 @@ interface Properties {
 }
 
 interface State {
-  page: RequestsPage.Page;
+  redirect: string;
   displayStatus: RequestDirectoryPage.DisplayStatus;
   requestState: RequestsModel.RequestState;
   filters: RequestsModel.Filters;
@@ -30,7 +31,7 @@ export class RequestsController extends React.Component<Properties, State> {
   constructor(props: Properties) {
     super(props);
     this.state = {
-      page: RequestsPage.Page.YOUR_REQUESTS,
+      redirect: null,
       displayStatus: RequestDirectoryPage.DisplayStatus.IN_PROGRESS,
       requestState: RequestsModel.RequestState.PENDING,
       filters: {
@@ -49,10 +50,14 @@ export class RequestsController extends React.Component<Properties, State> {
   }
 
   public render(): JSX.Element {
+    if(this.state.redirect) {
+      return <Router.Redirect push to={this.state.redirect}/>;
+    }
+    const page = this.currentPage();
     return (
       <RequestsPage
         roles={this.props.roles}
-        current={this.state.page}
+        current={page}
         displayStatus={this.state.displayStatus}
         requestState={this.state.requestState}
         filters={this.state.filters}
@@ -67,27 +72,56 @@ export class RequestsController extends React.Component<Properties, State> {
     await this.loadDirectory();
   }
 
+  public componentDidUpdate(prevProps: Properties): void {
+    if(this.state.redirect) {
+      this.setState({redirect: null});
+    }
+    if(prevProps.location.pathname !== this.props.location.pathname) {
+      const defaultFilters: RequestsModel.Filters = {
+        query: '',
+        categories: new Set<Type>(),
+        sortKey: RequestsModel.SortField.LAST_UPDATED
+      };
+      this.setState({
+        displayStatus: RequestDirectoryPage.DisplayStatus.IN_PROGRESS,
+        requestState: RequestsModel.RequestState.PENDING,
+        filters: defaultFilters,
+        filterCount: 0,
+        pageIndex: 0
+      });
+      this.loadDirectory({
+        scope: this.currentPage() === RequestsPage.Page.YOUR_REQUESTS ?
+          RequestsModel.Scope.YOU : RequestsModel.Scope.GROUP,
+        requestState: RequestsModel.RequestState.PENDING,
+        filters: defaultFilters,
+        pageIndex: 0
+      });
+    }
+  }
+
+  private currentPage(): RequestsPage.Page {
+    if(this.props.location.pathname.endsWith('/group')) {
+      return RequestsPage.Page.GROUP_REQUESTS;
+    }
+    return RequestsPage.Page.YOUR_REQUESTS;
+  }
+
   private onNavigate = (page: RequestsPage.Page) => {
-    const defaultFilters: RequestsModel.Filters = {
-      query: '',
-      categories: new Set<Type>(),
-      sortKey: RequestsModel.SortField.LAST_UPDATED
-    };
-    this.setState({
-      page,
-      displayStatus: RequestDirectoryPage.DisplayStatus.IN_PROGRESS,
-      requestState: RequestsModel.RequestState.PENDING,
-      filters: defaultFilters,
-      filterCount: 0,
-      pageIndex: 0
-    });
-    this.loadDirectory({
-      scope: page === RequestsPage.Page.YOUR_REQUESTS ?
-        RequestsModel.Scope.YOU : RequestsModel.Scope.GROUP,
-      requestState: RequestsModel.RequestState.PENDING,
-      filters: defaultFilters,
-      pageIndex: 0
-    });
+    const prefix = this.parseUrlPrefix();
+    if(page === RequestsPage.Page.YOUR_REQUESTS) {
+      this.setState({redirect: `${prefix}/you`});
+    } else {
+      this.setState({redirect: `${prefix}/group`});
+    }
+  }
+
+  private parseUrlPrefix(): string {
+    const url = this.props.location.pathname;
+    const prefix = url.substring(0, url.lastIndexOf('/'));
+    if(prefix === '') {
+      return url;
+    }
+    return prefix;
   }
 
   private onSubmit = (submission: RequestsModel.Submission) => {
@@ -104,7 +138,7 @@ export class RequestsController extends React.Component<Properties, State> {
   private async loadDirectory(
       submission?: RequestsModel.Submission): Promise<void> {
     const sub = submission ?? {
-      scope: this.state.page === RequestsPage.Page.YOUR_REQUESTS ?
+      scope: this.currentPage() === RequestsPage.Page.YOUR_REQUESTS ?
         RequestsModel.Scope.YOU : RequestsModel.Scope.GROUP,
       requestState: this.state.requestState,
       filters: this.state.filters,
