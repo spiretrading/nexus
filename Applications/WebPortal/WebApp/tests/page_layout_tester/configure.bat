cd108cdf413f6f11353a1430df381536923332e3
Kamal <kamal@spiretrading.com>
2026-03-10 16:00:34 -0400

    Removed the PageLayout tester in favor of having it in the UI Catalog.

--- Applications/WebPortal/WebApp/tests/page_layout_tester/configure.bat
+++
@@ -1,3 +0,0 @@
-@ECHO OFF
-CALL "%~dp0..\..\application\configure.bat" -D="%~dp0" %*
-EXIT /B %ERRORLEVEL%
--- Applications/WebPortal/WebApp/tests/page_layout_tester/configure.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -o errexit
-set -o pipefail
-SCRIPT_DIR="$(cd -P "$(dirname "${BASH_SOURCE[0]}")" > /dev/null 2>&1 && \
-  pwd -P)"
-exec "$SCRIPT_DIR/../../application/configure.sh" -D="$SCRIPT_DIR" "$@"
