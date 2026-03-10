a5bb206c90e14ae83724b15129b9cd1e9c47216e
Kamal <kamal@spiretrading.com>
2026-03-10 15:19:00 -0400

    Replace ResizeObserver with CSS container queries in RequestItemPlaceholder and RequestDetailPage.

--- Applications/WebPortal/WebApp/library/source/pages/requests_page/request_item_placeholder.tsx
+++ Applications/WebPortal/WebApp/library/source/pages/requests_page/request_item_placeholder.tsx
@@ -1,63 +1,21 @@
 import { css, StyleSheet } from 'aphrodite';
 import * as React from 'react';
 
-interface State {
-  isWide: boolean;
-}
-
 /** Displays a skeleton loading placeholder for a RequestItem. */
-export class RequestItemPlaceholder extends React.Component<{}, State> {
-  constructor(props: {}) {
-    super(props);
-    this.containerRef = React.createRef<HTMLDivElement>();
-    this.state = {
-      isWide: false
-    };
-  }
-
-  public componentDidMount(): void {
-    this.resizeObserver = new ResizeObserver(this.onResize);
-    if(this.containerRef.current) {
-      this.resizeObserver.observe(this.containerRef.current);
-    }
-  }
-
-  public componentWillUnmount(): void {
-    this.resizeObserver?.disconnect();
-  }
-
-  public render(): JSX.Element {
-    return (
-      <div ref={this.containerRef} className={css(STYLES.container)}>
-        <div className={css(STYLES.header)}>
-          <div className={css(STYLES.skeleton, STYLES.headerLeft)}/>
-          <div className={css(STYLES.skeleton,
-            this.state.isWide ?
-              STYLES.headerRightWide : STYLES.headerRight)}/>
-        </div>
-        <div className={css(STYLES.metadata)}>
-          <div className={css(STYLES.skeleton,
-            this.state.isWide ?
-              STYLES.metadataWide : STYLES.metadataNarrow)}/>
-        </div>
-        <div className={css(STYLES.changes)}>
-          <div className={css(STYLES.skeleton, STYLES.changesSkeleton,
-            !this.state.isWide && STYLES.changesSkeletonNarrow)}/>
-        </div>
-      </div>);
-  }
-
-  private onResize = (entries: ResizeObserverEntry[]) => {
-    for(const entry of entries) {
-      const isWide = entry.contentRect.width >= 768;
-      if(isWide !== this.state.isWide) {
-        this.setState({isWide});
-      }
-    }
-  };
-
-  private containerRef: React.RefObject<HTMLDivElement>;
-  private resizeObserver?: ResizeObserver;
+export function RequestItemPlaceholder() {
+  return (
+    <div className={css(STYLES.container)}>
+      <div className={css(STYLES.header)}>
+        <div className={css(STYLES.skeleton, STYLES.headerLeft)}/>
+        <div className={css(STYLES.skeleton, STYLES.headerRight)}/>
+      </div>
+      <div className={css(STYLES.metadata)}>
+        <div className={css(STYLES.skeleton, STYLES.metadataSkeleton)}/>
+      </div>
+      <div className={css(STYLES.changes)}>
+        <div className={css(STYLES.skeleton, STYLES.changesSkeleton)}/>
+      </div>
+    </div>);
 }
 
 const STYLES = StyleSheet.create({
@@ -66,14 +24,15 @@ const STYLES = StyleSheet.create({
     border: '1px solid transparent',
     borderBottomColor: '#E6E6E6',
     backgroundColor: '#FFFFFF',
-    padding: '11px 17px'
+    padding: '11px 17px',
+    containerType: 'inline-size'
   },
   skeleton: {
     background: 'linear-gradient(to right, #EAEAEA, #FAFAFA)'
   },
   header: {
     display: 'flex',
-    alignItems: 'center' as 'center',
+    alignItems: 'center',
     gap: '18px'
   },
   headerLeft: {
@@ -85,33 +44,29 @@ const STYLES = StyleSheet.create({
     width: '60px',
     height: '18px',
     flexShrink: 0,
-    marginLeft: 'auto'
-  },
-  headerRightWide: {
-    width: '116px',
-    height: '18px',
-    flexShrink: 0,
-    marginLeft: 'auto'
+    marginLeft: 'auto',
+    '@container (min-width: 768px)': {
+      width: '116px'
+    }
   },
   metadata: {
     marginTop: '4px'
   },
-  metadataNarrow: {
+  metadataSkeleton: {
     width: '60px',
-    height: '16px'
-  },
-  metadataWide: {
-    width: '284px',
-    height: '16px'
+    height: '16px',
+    '@container (min-width: 768px)': {
+      width: '284px'
+    }
   },
   changes: {
     marginTop: '10px'
   },
   changesSkeleton: {
-    width: '284px',
-    height: '24px'
-  },
-  changesSkeletonNarrow: {
-    width: '100%'
+    width: '100%',
+    height: '24px',
+    '@container (min-width: 768px)': {
+      width: '284px'
+    }
   }
 });
