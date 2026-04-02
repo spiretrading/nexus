bb613e1acd75fdc2b0c28247515d48185709f556
Shaobin <shaobin@spiretrading.com>
2026-04-02 15:29:03 -0400

    Updated the TimeInForceFilterPanel with make_closed_filter_panel.

--- Applications/Spire/Source/Ui/TimeInForceFilterPanel.cpp
+++ Applications/Spire/Source/Ui/TimeInForceFilterPanel.cpp
@@ -1,19 +1,8 @@
 #include "Spire/Ui/TimeInForceFilterPanel.hpp"
 #include "Spire/Spire/ArrayListModel.hpp"
-#include "Spire/Spire/ArrayTableModel.hpp"
 
 using namespace Nexus;
 using namespace Spire;
-
-namespace {
-  const auto& get_time_in_forces() {
-    static const auto values = std::vector<TimeInForce>{
-      TimeInForce::Type::DAY, TimeInForce::Type::GTC, TimeInForce::Type::OPG,
-      TimeInForce::Type::IOC, TimeInForce::Type::FOK, TimeInForce::Type::GTX,
-      TimeInForce::Type::GTD, TimeInForce::Type::MOC};
-    return values;
-  }
-}
 
 TimeInForceFilterPanel* Spire::make_time_in_force_filter_panel(
     QWidget* parent) {
@@ -23,16 +12,9 @@ TimeInForceFilterPanel* Spire::make_time_in_force_filter_panel(
 
 TimeInForceFilterPanel* Spire::make_time_in_force_filter_panel(
     std::shared_ptr<TimeInForceListModel> selection, QWidget* parent) {
-  auto model = std::make_shared<ArrayTableModel>();
-  auto& values = get_time_in_forces();
-  for(auto& value : values) {
-    model->push({value, false});
-  }
-  for(auto i = 0; i < selection->get_size(); ++i) {
-    if(auto iterator = std::find(values.begin(), values.end(),
-        selection->get(i)); iterator != values.end()) {
-      model->set(std::distance(values.begin(), iterator), 1, true);
-    }
-  }
-  return new ClosedFilterPanel(std::move(model), parent);
+  return make_closed_filter_panel(std::vector<TimeInForce>{
+    TimeInForce::Type::DAY, TimeInForce::Type::GTC, TimeInForce::Type::OPG,
+    TimeInForce::Type::IOC, TimeInForce::Type::FOK, TimeInForce::Type::GTX,
+    TimeInForce::Type::GTD, TimeInForce::Type::MOC},
+    std::move(selection), parent);
 }
