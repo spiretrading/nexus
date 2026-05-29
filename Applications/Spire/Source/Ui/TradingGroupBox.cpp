4da4ed444e4c4145c44a4cad6c099888f5e7d720
Shaobin <shaobin@spiretrading.com>
2026-05-29 14:48:57 -0400

    Implemented the TradingGroupBox.

---
+++ Applications/Spire/Source/Ui/TradingGroupBox.cpp
@@ -0,0 +1,18 @@
+#include "Spire/Ui/TradingGroupBox.hpp"
+#include "Spire/Ui/ListView.hpp"
+
+using namespace Beam;
+using namespace Spire;
+
+TradingGroupBox* Spire::make_trading_group_box(
+    std::shared_ptr<TradingGroupQueryModel> groups, QWidget* parent) {
+  return make_trading_group_box(
+    std::move(groups), std::make_shared<LocalTradingGroupModel>(), parent);
+}
+
+TradingGroupBox* Spire::make_trading_group_box(
+    std::shared_ptr<TradingGroupQueryModel> groups,
+    std::shared_ptr<TradingGroupModel> current, QWidget* parent) {
+  return new TradingGroupBox(std::move(groups), std::move(current),
+    &ListView::default_item_builder, parent);
+}
