2d701f386b3d3d8e31872469ce93471c5ff69d69
Shaobin <shaobin@spiretrading.com>
2026-06-03 16:52:03 -0400

    Remove duplicate documentation from OpenFilterPanelAdaptor specializations.

--- Applications/Spire/Include/Spire/Ui/KeyFilterPanel.hpp
+++ Applications/Spire/Include/Spire/Ui/KeyFilterPanel.hpp
@@ -5,23 +5,13 @@
 
 namespace Spire {
 
-  /** An OpenFilterPanelAdaptor specialized for an AnyTagBox. */
   template<>
   struct OpenFilterPanelAdaptor<AnyTagBox> {
-
-    /** The type of the input's current value. */
     using Type = std::shared_ptr<AnyListModel>;
 
-    /** Returns whether the input contains no values. */
     static bool is_empty(AnyTagBox& tag_box);
-
-    /** Removes all values from the input. */
     static void clear(AnyTagBox& tag_box);
-
-    /** Returns the current value of the input. */
     static Type get_current(AnyTagBox& tag_box);
-
-    /** Connects a slot called when the input's current value changes. */
     static boost::signals2::connection connect_current(
       AnyTagBox& tag_box, const std::function<void()>& slot);
   };
--- Applications/Spire/Include/Spire/Ui/ScopeFilterPanel.hpp
+++ Applications/Spire/Include/Spire/Ui/ScopeFilterPanel.hpp
@@ -5,23 +5,13 @@
 
 namespace Spire {
 
-  /** An OpenFilterPanelAdaptor specialized for a ScopeBox. */
   template<>
   struct OpenFilterPanelAdaptor<ScopeBox> {
-
-    /** The type of the input's current value. */
     using Type = Nexus::Scope;
 
-    /** Returns whether the input contains no values. */
     static bool is_empty(ScopeBox& scope_box);
-
-    /** Removes all values from the input. */
     static void clear(ScopeBox& scope_box);
-
-    /** Returns the current value of the input. */
     static Type get_current(ScopeBox& scope_box);
-
-    /** Connects a slot called when the input's current value changes. */
     static boost::signals2::connection connect_current(
       ScopeBox& scope_box, const std::function<void()>& slot);
   };
