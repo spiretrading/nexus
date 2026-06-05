5cc7c658871b3fc74d608ba15405200df0fca6d0
Kamal <kamal@spiretrading.com>
2026-06-05 14:52:27 -0400

    @ Wait for MarketDataServer to accept connections before start.sh exits. @

--- Applications/MarketDataServer/Application/start.sh
+++ Applications/MarketDataServer/Application/start.sh
@@ -2,6 +2,30 @@
 APPLICATION="MarketDataServer"
 CONFIG_FILE="config.yml"
 LOG_DIR="./logs"
+
+wait_for_server() {
+  local server_name="$1"
+  local addresses=($(yq -r \
+    ".$server_name.interface? | select(. != null),
+     .$server_name.addresses[]? | select(. != null) | @sh" "$CONFIG_FILE"))
+  if [[ ${#addresses[@]} -eq 0 ]]; then
+    return 0
+  fi
+  while true; do
+    if ! ./check.sh > /dev/null; then
+      exit 1
+    fi
+    for addr in "${addresses[@]}"; do
+      host="${addr%%:*}"
+      port="${addr##*:}"
+      if ss -ltn | grep -qE "$host:$port"; then
+        return 0
+      fi
+    done
+    sleep 0.5
+  done
+}
+
 if ./check.sh > /dev/null; then
   exit 0
 fi
@@ -16,29 +40,5 @@ done
 ./$APPLICATION > "$log_name" 2>&1 &
 new_pid=$!
 echo "$new_pid" > "pid.lock"
-all_addresses=($(yq -r \
-  '.server.interface? | select(. != null), .server.addresses[]? | select(. != null) | @sh' "$CONFIG_FILE"))
-if [[ ${#all_addresses[@]} -eq 0 ]]; then
-  exit 0
-fi
-
-is_any_address_listening() {
-  for addr in "${all_addresses[@]}"; do
-    host="${addr%%:*}"
-    port="${addr##*:}"
-    if ss -ltn | grep -qE "$host:$port"; then
-      return 0
-    fi
-  done
-  return 1
-}
-
-while true; do
-  if ! ./check.sh > /dev/null; then
-    exit 1
-  fi
-  if is_any_address_listening; then
-    break
-  fi
-  sleep 0.5
-done
+wait_for_server registry_server
+wait_for_server feed_server
