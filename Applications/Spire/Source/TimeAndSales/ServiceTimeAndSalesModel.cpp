4f7853ce320968eba9d51c0eac4254007a904895
Kamal <kamal@spiretrading.com>
2026-06-14 23:31:37 -0400

    Assign BBO indicators to historical time and sales by replaying BBO quotes.

--- Applications/Spire/Source/TimeAndSales/ServiceTimeAndSalesModel.cpp
+++ Applications/Spire/Source/TimeAndSales/ServiceTimeAndSalesModel.cpp
@@ -2,9 +2,79 @@
 
 using namespace Beam;
 using namespace boost;
+using namespace boost::posix_time;
 using namespace boost::signals2;
 using namespace Nexus;
 using namespace Spire;
+
+namespace {
+  BboIndicator get_indicator(
+      const BboQuote& bbo, const TimeAndSale& time_and_sale) {
+    if(bbo.m_ask.m_price == Money::ZERO) {
+      return BboIndicator::UNKNOWN;
+    } else if(time_and_sale.m_price == bbo.m_bid.m_price) {
+      return BboIndicator::AT_BID;
+    } else if(time_and_sale.m_price < bbo.m_bid.m_price) {
+      return BboIndicator::BELOW_BID;
+    } else if(time_and_sale.m_price == bbo.m_ask.m_price) {
+      return BboIndicator::AT_ASK;
+    } else if(time_and_sale.m_price > bbo.m_ask.m_price) {
+      return BboIndicator::ABOVE_ASK;
+    }
+    return BboIndicator::INSIDE;
+  }
+
+  std::vector<SequencedBboQuote> load_bbo_quotes(MarketDataClient& client,
+      const Ticker& ticker, ptime first_timestamp, ptime last_timestamp) {
+    auto result = std::vector<SequencedBboQuote>();
+    auto head_query = TickerQuery();
+    head_query.set_index(ticker);
+    head_query.set_range(Beam::Sequence::FIRST, first_timestamp);
+    head_query.set_snapshot_limit(SnapshotLimit::from_tail(1));
+    auto head_queue = std::make_shared<Queue<SequencedBboQuote>>();
+    client.query(head_query, head_queue);
+    try {
+      result.push_back(head_queue->pop());
+    } catch(const PipeBrokenException&) {}
+    auto tail_query = TickerQuery();
+    tail_query.set_index(ticker);
+    if(result.empty()) {
+      tail_query.set_range(first_timestamp, last_timestamp);
+    } else {
+      tail_query.set_range(
+        increment(result.back().get_sequence()), last_timestamp);
+    }
+    tail_query.set_snapshot_limit(SnapshotLimit::UNLIMITED);
+    auto tail_queue = std::make_shared<Queue<SequencedBboQuote>>();
+    client.query(tail_query, tail_queue);
+    try {
+      while(true) {
+        result.push_back(tail_queue->pop());
+      }
+    } catch(const PipeBrokenException&) {}
+    return result;
+  }
+
+  void replay_bbo_indicators(MarketDataClient& client, const Ticker& ticker,
+      std::vector<TimeAndSalesModel::Entry>& entries) {
+    if(entries.empty()) {
+      return;
+    }
+    auto bbos = load_bbo_quotes(client, ticker,
+      entries.front().m_time_and_sale->m_timestamp,
+      entries.back().m_time_and_sale->m_timestamp);
+    auto bbo = BboQuote();
+    auto index = std::size_t(0);
+    for(auto& entry : entries) {
+      auto& timestamp = entry.m_time_and_sale->m_timestamp;
+      while(index < bbos.size() && bbos[index]->m_timestamp <= timestamp) {
+        bbo = *bbos[index];
+        ++index;
+      }
+      entry.m_indicator = get_indicator(bbo, *entry.m_time_and_sale);
+    }
+  }
+}
 
 ServiceTimeAndSalesModel::ServiceTimeAndSalesModel(
     Ticker ticker, MarketDataClient client)
@@ -36,6 +106,7 @@ QtPromise<std::vector<TimeAndSalesModel::Entry>>
           Entry(std::move(time_and_sale), BboIndicator::UNKNOWN));
       }
     } catch(const PipeBrokenException&) {}
+    replay_bbo_indicators(client, ticker, result);
     return result;
   }, LaunchPolicy::ASYNC);
 }
@@ -51,19 +122,5 @@ void ServiceTimeAndSalesModel::on_bbo(const SequencedBboQuote& bbo) {
 
 void ServiceTimeAndSalesModel::on_time_and_sale(
     const SequencedTimeAndSale& time_and_sale) {
-  auto range = [&] {
-    if(m_bbo->m_ask.m_price == Money::ZERO) {
-      return BboIndicator::UNKNOWN;
-    } else if(time_and_sale->m_price == m_bbo->m_bid.m_price) {
-      return BboIndicator::AT_BID;
-    } else if(time_and_sale->m_price < m_bbo->m_bid.m_price) {
-      return BboIndicator::BELOW_BID;
-    } else if(time_and_sale->m_price == m_bbo->m_ask.m_price) {
-      return BboIndicator::AT_ASK;
-    } else if(time_and_sale->m_price > m_bbo->m_ask.m_price) {
-      return BboIndicator::ABOVE_ASK;
-    }
-    return BboIndicator::INSIDE;
-  }();
-  m_update_signal({time_and_sale, range});
+  m_update_signal({time_and_sale, get_indicator(*m_bbo, *time_and_sale)});
 }
