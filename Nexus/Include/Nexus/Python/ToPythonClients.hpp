f6b5f49d75d61ea483a949d5c4b76d5a09f7ce70
Kamal Mansouri <kamal@spiretrading.com>
2026-07-03 23:18:19 -0400

    Release the GIL in Python-exposed Timer.wait via ToPythonTimer
    ToPythonClients::make_timer returned the timer wrapped only in
    Beam::Timer, so wait()/start()/cancel() blocked while holding the GIL and
    could deadlock Python. Wrap it in a GIL-releasing ToPythonTimer instead.

--- Nexus/Include/Nexus/Python/ToPythonClients.hpp
+++ Nexus/Include/Nexus/Python/ToPythonClients.hpp
@@ -198,12 +198,9 @@ namespace Nexus {
   std::unique_ptr<typename ToPythonClients<C>::Timer>
       ToPythonClients<C>::make_timer(boost::posix_time::time_duration expiry) {
     auto release = Beam::Python::GilRelease();
-    if constexpr(std::is_same_v<
-        decltype(m_clients->make_timer(expiry)), std::unique_ptr<Timer>>) {
-      return m_clients->make_timer(expiry);
-    } else {
-      return std::make_unique<Timer>(m_clients->make_timer(expiry));
-    }
+    return std::make_unique<Timer>(
+      std::in_place_type<Beam::Python::ToPythonTimer<Timer>>,
+      m_clients->make_timer(expiry));
   }
 
   template<IsClients C>
