f53f6cb3ae821ab21678eae589015bb2d309f704
Shaobin <shaobin@spiretrading.com>
2026-07-08 14:09:18 -0400

    Implemented the LinkSubMenuItem.

--- Applications/Spire/Source/Utilities/LinkMenu.cpp
+++ Applications/Spire/Source/Utilities/LinkMenu.cpp
@@ -1,13 +1,18 @@
 #include "Spire/Utilities/LinkMenu.hpp"
+#include <tuple>
+#include <unordered_map>
 #include <QApplication>
+#include "Spire/BookView/BookViewWindow.hpp"
+#include "Spire/Charting/ChartWindow.hpp"
+#include "Spire/Spire/AssociativeValueModel.hpp"
 #include "Spire/Spire/Dimensions.hpp"
+#include "Spire/TimeAndSales/TimeAndSalesWindow.hpp"
 #include "Spire/Ui/CheckButtonMenuItem.hpp"
 #include "Spire/Ui/ContextMenu.hpp"
 #include "Spire/Ui/CustomQtVariants.hpp"
 #include "Spire/Ui/Ui.hpp"
 
 using namespace boost;
-using namespace boost::signals2;
 using namespace Nexus;
 using namespace Spire;
 using namespace Spire::LegacyUI;
@@ -46,75 +51,55 @@ namespace {
     return icon;
   }
 
-  struct LinkModel : BooleanModel {
-    TickerContext* m_source;
-    TickerContext* m_target;
-    LocalBooleanModel m_value;
-
-    LinkModel(TickerContext& source, TickerContext& target)
-      : m_source(&source),
-        m_target(&target),
-        m_value(m_source->GetLinkedIdentifier() == m_target->GetIdentifier()) {}
-
-    const Type& get() const override {
-      return m_value.get();
+  optional<LinkableWindowType> get_window_type(QWidget& widget) {
+    if(dynamic_cast<TimeAndSalesWindow*>(&widget)) {
+      return LinkableWindowType::TIME_AND_SALES;
+    } else if(dynamic_cast<BookViewWindow*>(&widget)) {
+      return LinkableWindowType::BOOK_VIEW;
+    } else if(dynamic_cast<ChartWindow*>(&widget)) {
+      return LinkableWindowType::CHART;
     }
-
-    QValidator::State set(const bool& value) {
-      if(value == get()) {
-        return QValidator::State::Acceptable;
-      }
-      if(value) {
-        m_source->Link(*m_target);
-        if(m_target->GetLinkedIdentifier().empty()) {
-          m_target->Link(*m_source);
-        }
-      } else {
-        m_source->Unlink();
-      }
-      return m_value.set(value);
-    }
-
-    connection connect_update_signal(
-        const UpdateSignal::slot_type& slot) const {
-      return m_value.connect_update_signal(slot);
-    }
-  };
+    return none;
+  }
 }
 
 void Spire::add_link_menu(ContextMenu& parent, TickerContext& context) {
-  struct Item {
-    Ticker m_ticker;
-    QString m_title;
-    std::shared_ptr<BooleanModel> m_is_linked;
-  };
-  auto windows = QApplication::topLevelWidgets();
-  auto items = std::vector<Item>();
-  for(auto& window : windows) {
-    auto window_context = dynamic_cast<TickerContext*>(window);
-    if(window->isVisible() && window_context != nullptr &&
-        window_context != &context) {
-      auto item = Item(window_context->GetDisplayedTicker(),
-        window->windowTitle(),
-        std::make_shared<LinkModel>(context, *window_context));
-      items.push_back(std::move(item));
+  auto windows = std::vector<LinkableWindowInfo>();
+  auto initial = optional<LinkableWindowInfo>();
+  auto linked_id = QString::fromStdString(context.GetLinkedIdentifier());
+  for(auto& widget : QApplication::topLevelWidgets()) {
+    auto target = dynamic_cast<TickerContext*>(widget);
+    if(!widget->isVisible() || !target || target == &context) {
+      continue;
+    }
+    if(auto type = get_window_type(*widget)) {
+      auto info = LinkableWindowInfo{
+        QString::fromStdString(target->GetIdentifier()),
+        *type, target->GetDisplayedTicker()};
+      if(!linked_id.isEmpty() && info.m_id == linked_id) {
+        initial = info;
+      }
+      windows.push_back(std::move(info));
     }
   }
-  if(items.empty()) {
-    return;
+  auto current = std::make_shared<LocalOptionalLinkableWindowInfoModel>();
+  if(initial) {
+    current->set(*initial);
   }
-  auto link_menu = new ContextMenu(static_cast<QWidget&>(parent));
-  std::sort(items.begin(), items.end(),
-    [&] (const auto& left, const auto& right) {
-      return to_text(left.m_ticker) < to_text(right.m_ticker);
-    });
-  for(auto& item : items) {
-    link_menu->add_check_box(item.m_title, item.m_is_linked);
-    item.m_is_linked->connect_update_signal([&] (auto value) {
-      parent.hide();
-    });
-  }
-  parent.add_menu(QObject::tr("Link to"), *link_menu);
+  current->connect_update_signal([&context] (const auto& selection) {
+    if(!selection) {
+      context.Unlink();
+      return;
+    }
+    if(auto target =
+        TickerContext::FindTickerContext(selection->m_id.toStdString())) {
+      context.Link(*target);
+      if(target->GetLinkedIdentifier().empty()) {
+        target->Link(context);
+      }
+    }
+  });
+  add_link_sub_menu_item(parent, std::move(windows), current);
 }
 
 CheckButtonMenuItem* Spire::make_link_menu_item(LinkableWindowType type,
@@ -134,4 +119,50 @@ CheckButtonMenuItem* Spire::make_link_menu_item(LinkableWindowType type,
   }();
   return new CheckButtonMenuItem(load_icon(type), std::move(label),
     std::move(current), parent);
+}
+
+std::shared_ptr<OptionalLinkableWindowInfoModel>
+    Spire::add_link_sub_menu_item(ContextMenu& parent,
+      std::vector<LinkableWindowInfo> windows) {
+  auto current = std::make_shared<LocalOptionalLinkableWindowInfoModel>();
+  add_link_sub_menu_item(parent, std::move(windows), current);
+  return current;
+}
+
+void Spire::add_link_sub_menu_item(ContextMenu& parent,
+    std::vector<LinkableWindowInfo> windows,
+    std::shared_ptr<OptionalLinkableWindowInfoModel> current) {
+  auto submenu = new ContextMenu(static_cast<QWidget&>(parent));
+  std::sort(windows.begin(), windows.end(),
+    [] (const auto& left, const auto& right) {
+      return std::tie(left.m_ticker, left.m_type) <
+        std::tie(right.m_ticker, right.m_type);
+    });
+  auto current_id = std::make_shared<AssociativeValueModel<QString>>();
+  auto lookup =
+    std::make_shared<std::unordered_map<QString, LinkableWindowInfo>>();
+  for(auto& window : windows) {
+    auto item = make_link_menu_item(window.m_type, window.m_ticker,
+      current_id->get_association(window.m_id));
+    item->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Fixed);
+    submenu->add_action("",
+      [item] {
+        item->get_current()->set(!item->get_current()->get());
+      }, item);
+    lookup->emplace(window.m_id, window);
+  }
+  if(auto initial = current->get()) {
+    current_id->set(initial->m_id);
+  }
+  current_id->connect_update_signal(
+    [current, lookup, &parent] (const auto& id) {
+      if(id.isEmpty()) {
+        current->set(none);
+      } else if(auto i = lookup->find(id); i != lookup->end()) {
+        current->set(i->second);
+      }
+      parent.hide();
+    });
+  QObject::connect(submenu, &QObject::destroyed, [current_id] {});
+  parent.add_menu(QObject::tr("Link To"), *submenu);
 }
