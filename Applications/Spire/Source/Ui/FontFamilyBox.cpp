cbcf706d2f97dea367bed4bc0f109cdc63cf10f4
Shaobin <shaobin@spiretrading.com>
2026-08-05 15:54:43 -0400

    Cache each font family's resolved font instead of recomputing it on every list item mount, and pin a minimum item width so rows don't shift as they're recycled.

--- Applications/Spire/Source/Ui/FontFamilyBox.cpp
+++ Applications/Spire/Source/Ui/FontFamilyBox.cpp
@@ -1,4 +1,5 @@
 #include "Spire/Ui/FontFamilyBox.hpp"
+#include <map>
 #include <QFontDatabase>
 #include "Spire/Spire/ArrayListModel.hpp"
 #include "Spire/Spire/Dimensions.hpp"
@@ -9,6 +10,41 @@
 
 using namespace Spire;
 using namespace Spire::Styles;
+
+namespace {
+  struct FamilyMetrics {
+    QFont m_font;
+    int m_width;
+  };
+
+  const FamilyMetrics& get_family_metrics(const QString& family) {
+    static auto metrics = std::map<QString, FamilyMetrics>();
+    if(auto i = metrics.find(family); i != metrics.end()) {
+      return i->second;
+    }
+    auto database = QFontDatabase();
+    auto family_metrics = FamilyMetrics();
+    family_metrics.m_font = QFont([&] {
+      if(database.writingSystems(family).contains(QFontDatabase::Latin)) {
+        return family;
+      }
+      return QString("Roboto");
+    }());
+    family_metrics.m_font.setWeight(QFont::Normal);
+    family_metrics.m_font.setPixelSize(scale_width(12));
+    family_metrics.m_width =
+      QFontMetrics(family_metrics.m_font).horizontalAdvance(family);
+    return metrics.emplace(family, std::move(family_metrics)).first->second;
+  }
+
+  int get_max_family_width(const ListModel<QString>& families) {
+    auto width = 0;
+    for(auto i = 0; i != families.get_size(); ++i) {
+      width = std::max(width, get_family_metrics(families.get(i)).m_width);
+    }
+    return width;
+  }
+}
 
 FontFamilyBox* Spire::make_font_family_box(const QString& current,
     QWidget* parent) {
@@ -24,24 +60,17 @@ FontFamilyBox* Spire::make_font_family_box(const QString& current,
 FontFamilyBox* Spire::make_font_family_box(
     std::shared_ptr<ListModel<QString>> font_families,
     std::shared_ptr<ValueModel<QString>> current, QWidget* parent) {
+  auto width = get_max_family_width(*font_families);
   auto settings = FontFamilyBox::Settings();
   settings.m_cases = std::move(font_families);
   settings.m_current = std::move(current);
-  settings.m_view_builder = [] (auto& value) {
-    auto family = [&] {
-      if(QFontDatabase().writingSystems(value).contains(QFontDatabase::Latin)) {
-        return value;
-      }
-      return QString("Roboto");
-    }();
-    auto font = QFont(family);
-    font.setWeight(QFont::Normal);
-    font.setPixelSize(scale_width(12));
+  settings.m_view_builder = [width] (auto& value) {
     auto label = make_label(value);
     update_style(*label, [&] (auto& style) {
-      style.get(Any()).set(Font(font));
+      style.get(Any()).set(Font(get_family_metrics(value).m_font));
     });
     label->setFixedHeight(scale_height(26));
+    label->setMinimumWidth(width);
     return label;
   };
   auto box = new FontFamilyBox(std::move(settings), parent);
