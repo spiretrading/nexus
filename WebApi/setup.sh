8713bd8a33f57b7388470afe67900a051238185f
Kamal <kamal@spiretrading.com>
2026-09-18 00:03:50 -0400

    Unify WebApi setup and builds with tracked cleanup and reliable dependency caching. Support out-of-source tests and restrict package contents.

--- WebApi/setup.sh
+++ WebApi/setup.sh
@@ -1,22 +1,11 @@
 #!/bin/bash
-beam_commit="c149608fae1b4b3298db9b192747f632e506bc16"
-if [ ! -d "Beam" ]; then
-  git clone https://www.github.com/spiretrading/beam.git Beam
-  if [ "$?" == "0" ]; then
-    pushd Beam
-    git checkout "$beam_commit"
-    popd
-  else
-    rm -rf Beam
-    exit_status=1
-  fi
-fi
-if [ -d "Beam" ]; then
-  pushd Beam
-  if ! git merge-base --is-ancestor "$beam_commit" HEAD; then
-    git checkout master
-    git pull
-    git checkout "$beam_commit"
-  fi
-  popd
-fi
+set -o errexit
+set -o pipefail
+SOURCE="${BASH_SOURCE[0]}"
+while [[ -h "$SOURCE" ]]; do
+  DIRECTORY="$(cd -P "$(dirname "$SOURCE")" >/dev/null && pwd -P)"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIRECTORY/$SOURCE"
+done
+DIRECTORY="$(cd -P "$(dirname "$SOURCE")" >/dev/null && pwd -P)"
+exec node "$DIRECTORY/build.js" setup "$@"
